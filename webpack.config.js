2ef92030b9b99689a1f6872e58554472e758d5df
Bastien Caudan <bastien.caudan@gmail.com>
2019-01-30 12:05:03 +0100

    init typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts']
+    },
     output: {
         filename: 'browser-agent.js',
         path: path.resolve(__dirname, 'dist')
-   }
+    }
 };
