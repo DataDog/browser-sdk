e72915e098ee635a5395d48bf60218f1fcfe4896
Bastien Caudan <bastien.caudan@gmail.com>
2019-01-30 14:18:11 +0100

    init prettier + tslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts']
-    },
-    output: {
-        filename: 'browser-agent.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: "./src/index.ts",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts"]
+  },
+  output: {
+    filename: "browser-agent.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
