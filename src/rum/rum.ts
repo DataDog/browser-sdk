c6a852d8d6e366d73378b7c2cc0491de04e6834a
Bastien Caudan <bastien.caudan@gmail.com>
2019-02-26 14:19:15 +0100

    Track input delay

--- src/rum/rum.ts
+++ src/rum/rum.ts
@@ -27,6 +27,7 @@ const RUM_EVENT_PREFIX = `[RUM Event]`;
 export function rumModule(logger: Logger) {
   trackPerformanceTiming(logger);
   trackFirstIdle(logger);
+  trackInputDelay(logger);
 }
 
 function trackPerformanceTiming(logger: Logger) {
@@ -55,5 +56,69 @@ function trackFirstIdle(logger: Logger) {
         });
       })
     );
+  }
+}
+
+interface Delay {
+  entryType: string;
+  threshold: number;
+}
+
+/**
+ * cf https://developers.google.com/web/fundamentals/performance/rail
+ */
+const DELAYS = {
+  ANIMATION: {
+    entryType: "animation delay",
+    threshold: 10
+  },
+  RESPONSE: {
+    entryType: "response delay",
+    threshold: 100
+  }
+};
+
+/**
+ * Avoid to spam with scroll events
+ */
+const DELAY_BETWEEN_DISTINCT_SCROLL = 2000;
+
+function trackInputDelay(logger: Logger) {
+  const options = { capture: true, passive: true };
+  document.addEventListener("click", logIfAboveThreshold(DELAYS.RESPONSE), options);
+  document.addEventListener("keydown", logIfAboveThreshold(DELAYS.RESPONSE), options);
+  document.addEventListener(
+    "scroll",
+    throttle(logIfAboveThreshold(DELAYS.ANIMATION), DELAY_BETWEEN_DISTINCT_SCROLL),
+    options
+  );
+
+  function logIfAboveThreshold({ entryType, threshold }: Delay) {
+    return (event: Event) => {
+      const startTime = performance.now();
+      const duration = startTime - event.timeStamp;
+      if (duration > threshold) {
+        logger.log(`${RUM_EVENT_PREFIX} ${entryType}`, {
+          data: {
+            duration,
+            entryType,
+            startTime
+          }
+        });
+      }
+    };
+  }
+
+  // tslint:disable-next-line ban-types
+  function throttle<T extends Function>(fn: T, wait: number): T {
+    let lastCall = 0;
+    return (function(this: any) {
+      const now = new Date().getTime();
+      if (lastCall + wait < now) {
+        lastCall = now;
+        return fn.apply(this, arguments);
+      }
+      return;
+    } as unknown) as T; // consider output type has input type
   }
 }
