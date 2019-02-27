b5fe5d1d2543c0b856d702170e5e21a2f055838b
sdeprez <sdeprez@users.noreply.github.com>
2019-02-27 11:13:27 +0100

    Format rum

--- src/rum/rum.ts
+++ src/rum/rum.ts
@@ -1,18 +1,18 @@
-import { Logger } from "../core/logger";
-import { monitor } from "../core/monitoring";
+import { Logger } from '../core/logger'
+import { monitor } from '../core/monitoring'
 
 export function rumModule(logger: Logger) {
-  trackPerformanceResourceTiming(logger);
+  trackPerformanceResourceTiming(logger)
 }
 
 function trackPerformanceResourceTiming(logger: Logger) {
   if (PerformanceObserver) {
     const observer = new PerformanceObserver(
       monitor((list: PerformanceObserverEntryList) => {
-        const data = list.getEntries().map(e => e.toJSON());
-        logger.log("[RUM Event] PerformanceResourceTiming", { data });
+        const data = list.getEntries().map((e) => e.toJSON())
+        logger.log('[RUM Event] PerformanceResourceTiming', { data })
       })
-    );
-    observer.observe({ entryTypes: ["resource"] });
+    )
+    observer.observe({ entryTypes: ['resource'] })
   }
 }
