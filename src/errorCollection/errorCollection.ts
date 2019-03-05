633bbaeb37c3346425a9981bc380cabbfe4a34a2
Bastien Caudan <bastien.caudan@gmail.com>
2019-03-05 09:39:49 +0100

    track XHR errors (#17)
    * track XHR errors
    
    * Reduce XHR monkey-patching to only the open method

--- src/errorCollection/errorCollection.ts
+++ src/errorCollection/errorCollection.ts
@@ -1,11 +1,13 @@
 import { Configuration } from '../core/configuration'
 import { Logger } from '../core/logger'
+import { monitor } from '../core/monitoring'
 import { report, StackTrace } from '../tracekit/tracekit'
 
 export function errorCollectionModule(configuration: Configuration, logger: Logger) {
   if (configuration.isCollectingError) {
     startConsoleTracking(logger)
     startRuntimeErrorTracking(logger)
+    trackXhrError(logger)
   }
 }
 
@@ -32,4 +34,41 @@ export function startRuntimeErrorTracking(logger: Logger) {
 
 export function stopRuntimeErrorTracking() {
   report.unsubscribe(traceKitReporthandler)
+}
+
+interface XhrInfo {
+  method?: string
+  url?: string
+  status?: number
+  statusText?: string
+}
+
+export function trackXhrError(logger: Logger) {
+  const originalOpen = XMLHttpRequest.prototype.open
+  XMLHttpRequest.prototype.open = function(method: string, url: string) {
+    const xhrInfo: XhrInfo = {}
+    // tslint:disable-next-line no-this-assignment
+    const xhr = this
+
+    xhrInfo.method = method
+    xhrInfo.url = url
+
+    xhr.addEventListener(
+      'load',
+      monitor(() => {
+        if (xhr.status < 200 || xhr.status >= 400) {
+          reportXhrError()
+        }
+      })
+    )
+    xhr.addEventListener('error', monitor(reportXhrError))
+
+    function reportXhrError() {
+      xhrInfo.status = xhr.status
+      xhrInfo.statusText = xhr.status === 0 ? 'Network Error' : xhr.statusText
+      logger.error('XHR error', { xhr: xhrInfo })
+    }
+
+    return originalOpen.apply(this, arguments as any)
+  }
 }
