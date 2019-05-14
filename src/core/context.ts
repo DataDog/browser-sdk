94b267fe2c8d8eda50b09cdf97c85bc34d17a522
Bastien Caudan <bastien.caudan@gmail.com>
2019-05-14 11:07:44 +0200

    Remove project id from logger global context (#55)
    + rework context priorities

--- src/core/context.ts
+++ src/core/context.ts
@@ -2,20 +2,6 @@ import { getSessionId } from './session'
 
 export type Context = any
 
-let globalContext: Context = {}
-
-export function setLoggerGlobalContext(context: Context) {
-  globalContext = context
-}
-
-export function addLoggerGlobalContext(key: string, value: any) {
-  globalContext[key] = value
-}
-
-export function getLoggerGlobalContext() {
-  return globalContext
-}
-
 export function getCommonContext() {
   return {
     date: new Date().getTime(),
