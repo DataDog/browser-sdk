a0e82d8fb550a01435aa5df63884823f240d397f
Bastien Caudan <bastien.caudan@gmail.com>
2019-05-27 15:10:24 +0200

    Use git sha1 for version tag

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
-const packageJson = require('./package.json')
+const execSync = require('child_process').execSync
 
 const targetDC = process.env.TARGET_DC || 'us'
 
@@ -27,7 +27,11 @@ module.exports = (env, argv) => ({
       buildEnv: {
         TARGET_DC: JSON.stringify(targetDC),
         TARGET_ENV: JSON.stringify(process.env.TARGET_ENV || 'staging'),
-        VERSION: JSON.stringify(argv.mode === 'development' ? 'dev' : packageJson.version),
+        VERSION: JSON.stringify(
+          execSync('git rev-parse HEAD')
+            .toString()
+            .trim()
+        ),
       },
     }),
   ],
