e08d934ab5ade6c26ba7fb0a0f592d6eba742193
Bastien Caudan <bastien.caudan@gmail.com>
2019-08-23 14:33:30 +0200

    Setup CrossBrowserTesting integration (#111)

--- test/unit/karma.unit.conf.js
+++ test/unit/karma.unit.conf.js
@@ -1,14 +1,39 @@
+const path = require('path')
 const karmaBaseConf = require('./karma.base.conf')
 
 module.exports = function(config) {
   config.set({
     ...karmaBaseConf,
+    reporters: ['coverage-istanbul', ...karmaBaseConf.reporters],
     browsers: ['ChromeHeadlessNoSandbox'],
+    coverageIstanbulReporter: {
+      reports: ['html', 'text-summary'],
+      dir: path.join(__dirname, '../../coverage'),
+    },
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox'],
       },
     },
+    webpack: {
+      ...karmaBaseConf.webpack,
+      module: withIstanbulRule(karmaBaseConf.webpack.module),
+    },
   })
+}
+
+function withIstanbulRule(module) {
+  module.rules.push({
+    test: /^.*\.ts$/,
+    exclude: [/.*\.spec\.ts$/, /.*\.d\.ts$/, /.*capturedExceptions\.ts$/, /.*specHelper\.ts$/],
+    enforce: 'post',
+    use: {
+      loader: 'istanbul-instrumenter-loader',
+      options: {
+        esModules: true,
+      },
+    },
+  })
+  return module
 }
