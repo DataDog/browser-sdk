d4eaadd40b88c0e9cdc4ae8a0765b3313d1de413
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2019-10-28 16:50:16 +0100

    Split e2e and unit CBT tests into two CI jobs

--- ci-cbt.sh
+++ ci-cbt.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 export CBT_USERNAME=$(aws ssm get-parameter --region us-east-1 --name ci.browser-agent.cbt_username --with-decryption --query "Parameter.Value" --out text)
 export CBT_AUTHKEY=$(aws ssm get-parameter --region us-east-1 --name ci.browser-agent.cbt_authkey --with-decryption --query "Parameter.Value" --out text)
-yarn test:unit:cbt
-yarn test:e2e:cbt
+yarn "$1":cbt
