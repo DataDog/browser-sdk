236366d014c4905202ce56cbcad74bf2e9d9e40d
Bastien Caudan <bastien.caudan@gmail.com>
2019-10-30 15:28:26 +0100

    update page view after each change
    debounce 3s to avoid spam

--- packages/rum/src/pageViewTracker.ts
+++ packages/rum/src/pageViewTracker.ts
@@ -1,4 +1,4 @@
-import { Batch, ErrorObservable, generateUUID, monitor, msToNs, Observable } from '@browser-agent/core'
+import { Batch, debounce, ErrorObservable, generateUUID, monitor, msToNs, Observable } from '@browser-agent/core'
 
 import { PerformancePaintTiming, RawCustomEvent, RumEvent, RumEventCategory } from './rum'
 
@@ -18,6 +18,7 @@ export interface PageViewSummary {
 
 export let pageViewId: string
 
+const DEBOUNCE_PAGE_VIEW_UPDATE_PERIOD = 3000
 let startTimestamp: number
 let startOrigin: number
 let documentVersion: number
@@ -33,13 +34,16 @@ export function trackPageView(
   performanceObservable: Observable<PerformanceEntry>,
   customEventObservable: Observable<RawCustomEvent>
 ) {
+  const schedulePageViewUpdate = debounce(monitor(() => updatePageView(addRumEvent)), DEBOUNCE_PAGE_VIEW_UPDATE_PERIOD)
+
   newPageView(location, addRumEvent)
   trackHistory(location, addRumEvent)
-  trackPerformance(performanceObservable)
-  trackSummary(errorObservable, performanceObservable, customEventObservable)
+  trackPerformance(performanceObservable, schedulePageViewUpdate)
+  trackSummary(errorObservable, performanceObservable, customEventObservable, schedulePageViewUpdate)
 
   batch.beforeFlushOnUnload(() => updatePageView(addRumEvent))
 }
+
 function newPageView(location: Location, addRumEvent: (event: RumEvent) => void) {
   pageViewId = generateUUID()
   startTimestamp = new Date().getTime()
@@ -104,7 +108,7 @@ function areDifferentPages(previous: Location, current: Location) {
   return previous.pathname !== current.pathname
 }
 
-function trackPerformance(performanceObservable: Observable<PerformanceEntry>) {
+function trackPerformance(performanceObservable: Observable<PerformanceEntry>, schedulePageViewUpdate: () => void) {
   performanceObservable.subscribe((entry) => {
     if (entry.entryType === 'navigation') {
       const navigationEntry = entry as PerformanceNavigationTiming
@@ -115,12 +119,14 @@ function trackPerformance(performanceObservable: Observable<PerformanceEntry>) {
         domInteractive: msToNs(navigationEntry.domInteractive),
         loadEventEnd: msToNs(navigationEntry.loadEventEnd),
       }
+      schedulePageViewUpdate()
     } else if (entry.entryType === 'paint' && entry.name === 'first-contentful-paint') {
       const paintEntry = entry as PerformancePaintTiming
       screenPerformance = {
         ...screenPerformance,
         firstContentfulPaint: msToNs(paintEntry.startTime),
       }
+      schedulePageViewUpdate()
     }
   })
 }
@@ -128,13 +134,21 @@ function trackPerformance(performanceObservable: Observable<PerformanceEntry>) {
 function trackSummary(
   errorObservable: ErrorObservable,
   performanceObservable: Observable<PerformanceEntry>,
-  customEventObservable: Observable<RawCustomEvent>
+  customEventObservable: Observable<RawCustomEvent>,
+  schedulePageViewUpdate: () => void
 ) {
-  errorObservable.subscribe(() => (summary.errorCount += 1))
-  customEventObservable.subscribe(() => (summary.customEventCount += 1))
+  errorObservable.subscribe(() => {
+    summary.errorCount += 1
+    schedulePageViewUpdate()
+  })
+  customEventObservable.subscribe(() => {
+    summary.customEventCount += 1
+    schedulePageViewUpdate()
+  })
   performanceObservable.subscribe((entry) => {
     if (entry.entryType === 'longtask') {
       summary.longTaskCount += 1
+      schedulePageViewUpdate()
     }
   })
 }
