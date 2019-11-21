7310934c002a1759efa03d307f3fca5dbbfe4ef6
Bastien Caudan <bastien.caudan@gmail.com>
2019-11-21 15:55:45 +0100

    :arrow_up:[e2e] bump wdio dependencies

--- test/e2e/wdio.local.conf.js
+++ test/e2e/wdio.local.conf.js
@@ -1,5 +1,7 @@
 const baseConf = require('./wdio.base.conf')
 
+const CHROME_DRIVER_VERSION = '77.0.3865.40'
+
 exports.config = {
   ...baseConf,
 
@@ -13,4 +15,14 @@ exports.config = {
   ],
   baseUrl: 'http://localhost:3000',
   services: ['selenium-standalone'],
+  seleniumInstallArgs: {
+    drivers: {
+      chrome: { version: CHROME_DRIVER_VERSION },
+    },
+  },
+  seleniumArgs: {
+    drivers: {
+      chrome: { version: CHROME_DRIVER_VERSION },
+    },
+  },
 }
