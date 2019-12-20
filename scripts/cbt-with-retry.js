1d8bfcf4c941915060512d95a8e05cda6d57d48d
Bastien Caudan <bastien.caudan@gmail.com>
2019-12-20 11:29:16 +0100

    :wrench:[e2e cbt] improve retry
    - add extra messages for e2e cases
    - add a delay between retries to handle case where other tests are already running

--- scripts/cbt-with-retry.js
+++ scripts/cbt-with-retry.js
@@ -4,6 +4,7 @@ const exec = require('child_process').exec
 
 const [_, __, ...args] = process.argv
 const COMMAND = `yarn ${args.join(' ')}`
+const RETRY_DELAY_IN_MIN = 5
 const MAX_RETRY_COUNT = 3
 let retryCount = 0
 
@@ -16,8 +17,11 @@ function executeWithRetry() {
     if (code !== 0 && isRetryAllowed(logs)) {
       if (retryCount < MAX_RETRY_COUNT) {
         retryCount += 1
-        console.log(`\n${COMMAND} (Retry ${retryCount}/${MAX_RETRY_COUNT})\n\n`)
-        executeWithRetry()
+        console.log(`\nRetry in ${RETRY_DELAY_IN_MIN} min\n\n`)
+        setTimeout(() => {
+          console.log(`\n${COMMAND} (Retry ${retryCount}/${MAX_RETRY_COUNT})\n\n`)
+          executeWithRetry()
+        }, RETRY_DELAY_IN_MIN * 60 * 1000)
       }
     } else {
       process.exit(code)
@@ -26,7 +30,11 @@ function executeWithRetry() {
 }
 
 function isRetryAllowed(logs) {
-  return logs.indexOf('UnhandledRejection') !== -1
+  return (
+    logs.indexOf('UnhandledRejection') !== -1 ||
+    logs.indexOf('Request failed due to Error') !== -1 ||
+    logs.indexOf('Failed to load resource') !== -1
+  )
 }
 
 executeWithRetry()
