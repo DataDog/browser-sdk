ba579216e853c0a0a624f085d556a7e1154c1101
Michael Chen <mchen@chanzuckerberg.com>
2020-06-16 11:35:30 -0700

    add optional collectErrorMessage && collectErrorStack flags

--- packages/rum/src/rum.entry.ts
+++ packages/rum/src/rum.entry.ts
@@ -5,6 +5,7 @@ import {
   commonInit,
   Context,
   ContextValue,
+  ErrorMessage,
   getGlobalObject,
   isPercentage,
   makeGlobal,
@@ -24,7 +25,9 @@ import { startUserActionCollection, UserActionReference } from './userActionColl
 import { startViewCollection } from './viewCollection'
 
 export interface RumUserConfiguration extends UserConfiguration {
-  applicationId: string
+  applicationId: string,
+  collectErrorMessage?: boolean, //default behavior as if true
+  collectErrorStack?: boolean, //default behavior as if true
 }
 
 export interface InternalContext {
@@ -81,7 +84,7 @@ datadogRum.init = monitor((userConfiguration: RumUserConfiguration) => {
     startUserActionCollection(lifeCycle)
   }
 
-  errorObservable.subscribe((errorMessage) => lifeCycle.notify(LifeCycleEventType.ERROR_COLLECTED, errorMessage))
+  errorObservable.subscribe((errorMessage) => lifeCycle.notify(LifeCycleEventType.ERROR_COLLECTED, customizeErrorCollection(rumUserConfiguration, errorMessage)))
   requestStartObservable.subscribe((startEvent) => lifeCycle.notify(LifeCycleEventType.REQUEST_STARTED, startEvent))
   requestCompleteObservable.subscribe((request) => lifeCycle.notify(LifeCycleEventType.REQUEST_COMPLETED, request))
 
@@ -113,6 +116,18 @@ function canInitRum(userConfiguration: RumUserConfiguration) {
     return false
   }
   return true
+}
+
+function customizeErrorCollection(userConfiguration: RumUserConfiguration, errorMessage: ErrorMessage): ErrorMessage {
+  if (userConfiguration.collectErrorMessage !== undefined && !userConfiguration.collectErrorMessage) {
+    errorMessage.message = "";
+  }
+
+  if (userConfiguration.collectErrorStack !== undefined && !userConfiguration.collectErrorStack) {
+    errorMessage.context.error.stack = undefined;
+  }
+
+  return errorMessage
 }
 
 interface BrowserWindow extends Window {
