1e68570af88ec5e6906c3032839073a11a173351
Bastien Caudan <bastien.caudan@gmail.com>
2020-07-01 10:07:42 +0200

    ✨[RUMF-609] export Datacenter enum from logs and rum (#436)

--- packages/logs/src/index.ts
+++ packages/logs/src/index.ts
@@ -1,2 +1,3 @@
+export { Datacenter } from '@datadog/browser-core'
 export { StatusType, HandlerType, LoggerConfiguration, Logger, LogsMessage } from './logger'
 export { LogsUserConfiguration, Status, LogsGlobal, datadogLogs } from './logs.entry'
--- packages/rum/src/index.ts
+++ packages/rum/src/index.ts
@@ -1,2 +1,3 @@
+export { Datacenter } from '@datadog/browser-core'
 export { RumUserConfiguration, RumGlobal, datadogRum, InternalContext } from './rum.entry'
 export { RumEventCategory, RumEvent, RumResourceEvent, RumViewEvent, RumUserActionEvent } from './rum'
