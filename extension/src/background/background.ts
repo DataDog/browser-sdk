b157f1966a64c432b828a9e0ff3ff530b7dbf3fc
Bastien Caudan <bastien.caudan@gmail.com>
2020-07-16 18:13:41 +0200

    new icons

--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -2,8 +2,7 @@
 chrome.browserAction.disable()
 
 chrome.runtime.onMessage.addListener((request) => {
-    if (request.sdkEnabled) {
-        chrome.browserAction.enable()
-        chrome.browserAction.setIcon({path:chrome.extension.getURL('/assets/images/bits.png')})
-    }
-});
+  if (request.sdkEnabled) {
+    chrome.browserAction.enable()
+  }
+})
