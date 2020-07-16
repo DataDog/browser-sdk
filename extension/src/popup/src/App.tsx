89161e54e618a1e404f0e823a8a08e44a1c34ad1
Bastien Caudan <bastien.caudan@gmail.com>
2020-07-16 19:15:59 +0200

    message communication proto

--- extension/src/popup/src/App.tsx
+++ extension/src/popup/src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const backgroundPageConnection = chrome.runtime.connect({
+    name: 'name'
+  });
+
+  backgroundPageConnection.postMessage({
+    type: 'hello',
+  });
+
+  backgroundPageConnection.onMessage.addListener((request) => {
+    setCount(request.data)
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{count}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
