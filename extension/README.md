19daef7e1d1d7dcb7a972e713af806a694aa4692
Bastien Caudan <bastien.caudan@gmail.com>
2020-07-16 11:11:00 +0200

    Bootstrap extension

---
+++ extension/README.md
@@ -0,0 +1,14 @@
+# Hackathon extension
+
+## Getting started
+
+    yarn
+    yarn build // or yarn watch
+
+Load `extension/dist` as unpacked extension  
+
+
+## Useful links
+
+- [chrome extension getting started](https://developer.chrome.com/extensions/getstarted)
+- [chrome extension overview](https://developer.chrome.com/extensions/overview)
---
+++ extension/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  devtool: 'inline-source-map',
+  entry: {
+    contentscript: path.resolve(__dirname, 'src/contentscript/contentscript.ts'),
+    background: path.resolve(__dirname, 'src/background/background.ts'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+}
