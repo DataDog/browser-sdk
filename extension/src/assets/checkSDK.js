244d4d5f87e576eceb014141c9ee9780c3c46613
Maxime Quentin <maxime.quentin@datadoghq.com>
2020-07-16 20:14:30 +0200

    pass views to React App.tsx (#451)
    * add type for react popup
    
    * add view list in App.tsx

--- extension/src/assets/checkSDK.js
+++ extension/src/assets/checkSDK.js
@@ -3,10 +3,10 @@ const RUM_SDK_ON = 'rum-sdk-on'
 
 console.log('Looking for the Datadog RUM SDK ...')
 const checkForRumInterval = setInterval(() => {
-    if(window['DD_RUM']) {
-        ROOT_TAG.dispatchEvent(new CustomEvent(RUM_SDK_ON, {}))
-        clearInterval(checkForRumInterval)
-    } else {
-        console.log('keep looking ...')
-    }
+  if (window['DD_RUM']) {
+    ROOT_TAG.dispatchEvent(new CustomEvent(RUM_SDK_ON, {}))
+    clearInterval(checkForRumInterval)
+  } else {
+    console.log('keep looking ...')
+  }
 }, 1000)
--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -1,12 +1,20 @@
 // disable by default
 chrome.browserAction.disable()
 
+import { addOrUpdateViews, View } from '../lib/rumEvents'
+
 const tabsState: { [tabId: number]: boolean } = {}
+let tabsViews: View[] = []
 
 chrome.runtime.onMessage.addListener((request, sender) => {
   if (request.sdkEnabled) {
     chrome.browserAction.enable()
     tabsState[sender.tab.id] = true
+  }
+
+  if (request.view) {
+    chrome.browserAction.enable()
+    tabsViews = addOrUpdateViews(request.view as View, tabsViews)
   }
 })
 
@@ -14,6 +22,11 @@ chrome.runtime.onConnect.addListener((popupConnection) => {
   popupConnection.onMessage.addListener((message) => {
     if (message.type === 'hello') {
       popupConnection.postMessage({ data: Object.keys(tabsState).length })
+    }
+
+    if (message.type === 'refreshViews') {
+      console.log('backGround refreshViews')
+      popupConnection.postMessage({ views: tabsViews })
     }
   })
 })
