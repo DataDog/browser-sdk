00451f00769d31c0a0f454ef0135b9f47547d37b
Bastien Caudan <bastien.caudan@gmail.com>
2020-10-15 11:30:36 +0200

    👌 use early return

--- packages/rum/src/longTaskCollection.ts
+++ packages/rum/src/longTaskCollection.ts
@@ -4,18 +4,19 @@ import { RumEventCategory, RumLongTaskEvent } from './rum'
 
 export function startLongTaskCollection(lifeCycle: LifeCycle) {
   lifeCycle.subscribe(LifeCycleEventType.PERFORMANCE_ENTRY_COLLECTED, (entry) => {
-    if (entry.entryType === 'longtask') {
-      const rawRumEvent: RumLongTaskEvent = {
-        date: getTimestamp(entry.startTime),
-        duration: msToNs(entry.duration),
-        evt: {
-          category: RumEventCategory.LONG_TASK,
-        },
-      }
-      lifeCycle.notify(LifeCycleEventType.RAW_RUM_EVENT_COLLECTED, {
-        rawRumEvent,
-        startTime: entry.startTime,
-      })
+    if (entry.entryType !== 'longtask') {
+      return
     }
+    const rawRumEvent: RumLongTaskEvent = {
+      date: getTimestamp(entry.startTime),
+      duration: msToNs(entry.duration),
+      evt: {
+        category: RumEventCategory.LONG_TASK,
+      },
+    }
+    lifeCycle.notify(LifeCycleEventType.RAW_RUM_EVENT_COLLECTED, {
+      rawRumEvent,
+      startTime: entry.startTime,
+    })
   })
 }
