25fefaeaf77b0690412d75a3def32ac63b6e4bab
Bastien Caudan <bastien.caudan@gmail.com>
2020-11-23 15:38:49 +0100

    💥 remove exported RUM event types

--- packages/rum/src/index.ts
+++ packages/rum/src/index.ts
@@ -1,3 +1,2 @@
 export { Datacenter, ErrorSource } from '@datadog/browser-core'
 export { RumUserConfiguration, RumGlobal, datadogRum } from './boot/rum.entry'
-export { InternalContext, RumEvent, RumEventType, RumActionEvent, RumViewEvent, RumResourceEvent } from './types'
