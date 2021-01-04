c0861eed0618df58fe771e970435d546d3729443
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-01-04 15:22:47 +0100

    ⬆️ update sinon
    Sinon changed its build process and now provide the same code for 'main'
    and 'modules' entry point, only the export syntax changes (as it should
    be!).
    
    We can remove the webpack hack for browserstack unit tests.
    
    This allows running unit tests on IE with the 'local' configuration.

--- test/unit/karma.bs.conf.js
+++ test/unit/karma.bs.conf.js
@@ -2,11 +2,6 @@ const karmaBaseConf = require('./karma.base.conf')
 const browsers = require('../browsers.conf')
 const { getBuildInfos, getIp } = require('../utils')
 
-// force entry resolution to ensure sinon code is in ES5
-// https://github.com/webpack/webpack/issues/5756
-// https://github.com/sinonjs/sinon/blob/894951c/package.json#L113
-karmaBaseConf.webpack.resolve.mainFields = ['cdn', 'main']
-
 module.exports = function (config) {
   config.set({
     ...karmaBaseConf,
