776f3b126e9c15d298c7cca8686ed22af0956a90
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-01-07 16:36:51 +0100

    👷 simplify bundle configuration
    This commit simplifies the bundle process a bit further:
    
    * Each bundled package is now in charge of generating suffixed files on
    its own.
    
    * The WITH_SUFFIX environment variable is removed.
    
    * The replace-build-env script is no more applied to bundled packages.
    Instead, string-replace-loader is used to replace the 'build env', and
    BannerPlugin is used to automatically add a banner to suffixed bundles.
    It is still used for TypeScript generated files though.
    
    * The deploy process is now a bit simpler too, since all bundles are
    generated at once, everything can be deployed at the same time.

--- packages/rum-recorder/webpack.config.js
+++ packages/rum-recorder/webpack.config.js
@@ -2,13 +2,9 @@ const path = require('path')
 
 const webpackBase = require('../../webpack.base')
 
-module.exports = (env, argv) => ({
-  entry: {
-    rum: path.resolve(__dirname, 'src/boot/recorder.entry.ts'),
-  },
-  ...webpackBase(argv.mode),
-  output: {
+module.exports = (env, argv) =>
+  webpackBase({
+    mode: argv.mode,
+    entry: path.resolve(__dirname, 'src/boot/recorder.entry.ts'),
     filename: 'datadog-rum-recorder.js',
-    path: path.resolve(__dirname, 'bundle'),
-  },
-})
+  })
