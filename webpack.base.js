d1fc354c241986cf44afe32d90dda9b104030dd7
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-01-07 11:22:27 +0100

    👷 remove bundle tsconfig files and factorize a bit more

--- webpack.base.js
+++ webpack.base.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
+const tsconfigPath = path.join(__dirname, 'tsconfig.base.json')
+
 module.exports = (mode) => ({
   mode,
   devtool: mode === 'development' ? 'inline-source-map' : 'false',
@@ -10,11 +12,18 @@ module.exports = (mode) => ({
         test: /\.ts$/,
         loader: 'ts-loader',
         exclude: /node_modules/,
+        options: {
+          configFile: tsconfigPath,
+          onlyCompileBundledFiles: true,
+          compilerOptions: {
+            module: 'es6',
+          },
+        },
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin({ configFile: path.join(__dirname, 'tsconfig.base.json') })],
+    plugins: [new TsconfigPathsPlugin({ configFile: tsconfigPath })],
   },
 })
