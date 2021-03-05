d704baa391e7a880758878dd36983ebb4e08500b
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-03-05 15:20:19 +0100

    👌✨ search for a page in the Wikipedia scenario

--- performances/src/main.ts
+++ performances/src/main.ts
@@ -58,10 +58,20 @@ async function profileScenario(
 async function runWikipediaScenario(page: Page, takeMeasurements: () => Promise<void>) {
   await page.goto('https://en.wikipedia.org/wiki/Event_monitoring')
   await takeMeasurements()
+
   await page.goto('https://en.wikipedia.org/wiki/Datadog')
   await takeMeasurements()
+
+  await page.type('[type="search"]', 'median', {
+    // large delay to trigger the autocomplete menu at each key press
+    delay: 400,
+  })
+  await Promise.all([page.waitForNavigation(), page.keyboard.press('Enter')])
+  await takeMeasurements()
+
   await page.goto('https://en.wikipedia.org/wiki/Ubuntu')
   await takeMeasurements()
+
   await page.goto('about:blank')
 }
 
