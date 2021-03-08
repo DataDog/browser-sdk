1ecedcd3273b4bb66d0a27e2c95487a78f04ac45
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-03-08 16:58:37 +0100

    👌 format number with units

--- performances/src/format.ts
+++ performances/src/format.ts
@@ -1,18 +1,33 @@
 import { ProfilingResult, ProfilingResults } from './types'
 
+const DURATION_UNITS = ['μs', 'ms', 's']
+
+const BYTES_UNITS = ['B', 'kB', 'MB']
+
 export function formatProfilingResults(results: ProfilingResults) {
   return `\
-Memory (median): ${formatNumber(results.memory.sdk)} bytes ${formatPercent(results.memory)}
-CPU: ${formatNumber(results.cpu.sdk)} microseconds ${formatPercent(results.cpu)}
+Memory (median): ${formatNumberWithUnit(results.memory.sdk, BYTES_UNITS)} ${formatPercent(results.memory)}
+CPU: ${formatNumberWithUnit(results.cpu.sdk, DURATION_UNITS)} ${formatPercent(results.cpu)}
 Bandwidth:
-  upload: ${formatNumber(results.upload.sdk)} bytes
-  download: ${formatNumber(results.download.sdk)} bytes`
+  upload: ${formatNumberWithUnit(results.upload.sdk, BYTES_UNITS)}
+  download: ${formatNumberWithUnit(results.download.sdk, BYTES_UNITS)}`
 }
 
-function formatNumber(n: number) {
-  return new Intl.NumberFormat('en-US').format(n)
+function formatNumberWithUnit(n: number, units: string[]) {
+  let unit: string
+  for (unit of units) {
+    if (n < 1000) {
+      break
+    }
+    n /= 1000
+  }
+  return `${formatNumber(n)} ${unit!}`
 }
 
 function formatPercent({ total, sdk }: ProfilingResult) {
   return `(${formatNumber((sdk / total) * 100)}%)`
+}
+
+function formatNumber(n: number) {
+  return new Intl.NumberFormat('en-US', { maximumFractionDigits: n < 10 ? 2 : n < 100 ? 1 : 0 }).format(n)
 }
