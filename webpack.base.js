c12c3140e4ea23510d304aea686e019f561da594
Benoît <benoit.zugmeyer@datadoghq.com>
2021-03-29 12:32:43 +0200

    ⬆️ upgrade lerna, webpack and related (#772)
    * ⬆️ upgrade lerna
    
    ssri 6.0.1 is required by a bunch of lerna dependencies. Upgrading ssri
    to 8.0.1 requires to upgrade lerna to 4.0.0 since it is the only version
    which doesn't require ssri 6.
    
    * ⬆️ upgrade webpack and related
    
    webpack 4 requires an old version of terser-webpack-plugin, which
    requires ssri 6. To upgrade ssri to 8.0.1, we need to upgrade webpack to
    5.
    
    This forces us to upgrade a bunch of webpack-related dependencies to
    support the new major version. A bit of configuration changes was also
    needed to support the new version, most importantly for tests
    (karma-webpack).
    
    * ⬆️ upgrade ssri
    
    Finally upgrade ssri to fix https://github.com/advisories/GHSA-vx3p-948g-6vhq
    
    * ⬆️ upgrade webpack in test/app
    
    This will ugrade ssri as well.
    
    Note that `ts-loader` has not been upgraded, because the latest version
    requires typescript >=3.6.
    
    * 🚨 fix lint issue
    
    Somehow, upgrading depencies makes the prefer-regexp-exec rule more
    accurate.
    
    * 🐛 do not include any file by default when bundling with webpack
    
    Even with the [onlyCompileBundledFiles][1] option, ts-loader still
    includes all `.d.ts` files it finds (in all folders, for every build),
    because they may define implicit global types. This cause an issue when
    building packages simultaneously (with lerna stream strategy), because
    some `.d.ts` files may be removed while bundling, resulting in the
    bundle failing because it couldn't load a totally unrelated `.d.ts`
    file.
    
    This issue doesn't seem related to the webpack upgrade, but somehow it
    occurs way more often, probably because bundling takes a bit more time.
    
    This commit makes sure no file is automatically included while bundling,
    only explicitly imported files are taken into account.
    
    [1]: https://github.com/TypeStrong/ts-loader#onlycompilebundledfiles
    When building all bundles with `yarn build`

--- webpack.base.js
+++ webpack.base.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const { BannerPlugin } = require('webpack')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const buildEnv = require('./scripts/build-env')
 
-const tsconfigPath = path.join(__dirname, 'tsconfig.base.json')
+const tsconfigPath = path.join(__dirname, 'tsconfig.webpack.json')
 const SUFFIX_REGEXP = /-(us|eu)/
 
 module.exports = ({ entry, mode, filename, datacenter, types }) => ({
@@ -13,7 +14,8 @@ module.exports = ({ entry, mode, filename, datacenter, types }) => ({
     filename,
     path: path.resolve('./bundle'),
   },
-  devtool: mode === 'development' ? 'inline-source-map' : 'false',
+  target: ['web', 'es5'],
+  devtool: mode === 'development' ? 'inline-source-map' : false,
   module: {
     rules: [
       {
@@ -51,13 +53,25 @@ module.exports = ({ entry, mode, filename, datacenter, types }) => ({
     alias: {
       // The default "pako.esm.js" build is not transpiled to es5
       pako: 'pako/dist/pako.es5.js',
+      // By default, a non-bundled version of sinon is pulled in, which require the nodejs 'util'
+      // module. Since v5, webpack doesn't provide nodejs polyfills. Use a bundled version of sinon
+      // which have its own 'util' module polyfill.
+      sinon: 'sinon/pkg/sinon.js',
     },
+  },
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
   },
 
   plugins: [
     new BannerPlugin({
       banner({ filename }) {
-        const env = filename.match(SUFFIX_REGEXP)[1]
+        const env = SUFFIX_REGEXP.exec(filename)[1]
         const newFileName = filename.replace(SUFFIX_REGEXP, '')
         return `\n${filename} IS DEPRECATED, USE ${newFileName} WITH { site: 'datadoghq.${
           env === 'eu' ? 'eu' : 'com'
