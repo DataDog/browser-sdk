8567ef77f57b467a18c903890f434e648d470027
Bastien Caudan <bastien.caudan@gmail.com>
2021-04-20 15:46:34 +0200

    introduce preferred time

--- packages/core/src/tools/timeUtils.ts
+++ packages/core/src/tools/timeUtils.ts
@@ -1,9 +1,46 @@
-import { isNumber, round } from './utils'
+import { isNumber, round, ONE_SECOND } from './utils'
 
 export type Duration = number & { d: 'Duration in ms' }
 export type ServerDuration = number & { s: 'Duration in ns' }
 export type TimeStamp = number & { t: 'Epoch time' }
 export type RelativeTime = number & { r: 'Time relative to navigation start' } & { d: 'Duration in ms' }
+export type Time = (TimeStamp | RelativeTime) & { p: 'preferred time' }
+
+let isSystemClockPreferred = false
+
+export function preferSystemClock() {
+  isSystemClockPreferred = true
+}
+
+export function preferredNow(): Time {
+  return (isSystemClockPreferred ? timeStampNow() : relativeNow()) as Time
+}
+
+export function preferredTime(timestamp: TimeStamp, relativeTime: RelativeTime): Time {
+  return (isSystemClockPreferred ? timestamp : relativeTime) as Time
+}
+
+export function preferredTimeOrigin() {
+  return (isSystemClockPreferred ? getNavigationStart() : 0) as Time
+}
+
+export function preferredTimeStamp(time: Time) {
+  return isSystemClockPreferred ? (time as TimeStamp) : getTimeStamp(time as RelativeTime)
+}
+
+export function preferredRelativeTime(time: Time) {
+  return isSystemClockPreferred ? getRelativeTime(time as TimeStamp) : (time as RelativeTime)
+}
+
+// TODO test
+export function getCorrectedTimeStamp(relativeTime: RelativeTime) {
+  const correctedOrigin = Date.now() - performance.now()
+  if (isSystemClockPreferred && correctedOrigin > getNavigationStart()) {
+    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+    return Math.floor(correctedOrigin + relativeTime) as TimeStamp
+  }
+  return getTimeStamp(relativeTime)
+}
 
 export function toServerDuration(duration: Duration): ServerDuration
 export function toServerDuration(duration: Duration | undefined): ServerDuration | undefined
@@ -22,6 +59,7 @@ export function relativeNow() {
   return performance.now() as RelativeTime
 }
 
+export function elapsed(start: Time, end: Time): Duration
 export function elapsed(start: TimeStamp, end: TimeStamp): Duration
 export function elapsed(start: RelativeTime, end: RelativeTime): Duration
 export function elapsed(start: number, end: number) {
