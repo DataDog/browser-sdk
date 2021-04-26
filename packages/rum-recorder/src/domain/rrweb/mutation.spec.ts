114c7ca73dcee0bf985d5f56d130907ae078ef6f
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-04-26 15:45:12 +0200

    ✅ [RUMF-902] simplify mutation spec
    Mutation specs will become more important when we'll write more tests
    for it. This commit simplifies the existing tests:
    
    * it removes the MutationObserver mock. Having to mock mutations
    manually was error prone, because we had to make the actual DOM changes,
    then carefully craft mutation records corresponding to the changes we
    made. By using actual MutationObservers, we only need to make DOM
    changes, and the records are generated by the browser for free.
    
    * it uses the "mutationPayloadValidator" to simplify the mutation record
    validation
    
    * it serializes the whole document first, like the actual recording
    process would do during full snapshots

--- packages/rum-recorder/src/domain/rrweb/mutation.spec.ts
+++ packages/rum-recorder/src/domain/rrweb/mutation.spec.ts
@@ -1,13 +1,8 @@
 import { isIE } from '@datadog/browser-core'
-import { serializeNodeWithId, SerializedNodeWithId, IdNodeMap } from '../rrweb-snapshot'
+import { createMutationPayloadValidator } from '../../../test/utils'
+import { snapshot, NodeType } from '../rrweb-snapshot'
 import { MutationObserverWrapper, MutationController } from './mutation'
 import { MutationCallBack } from './types'
-
-const DEFAULT_OPTIONS = {
-  blockClass: 'dd-block',
-  blockSelector: null,
-  skipChild: true,
-}
 
 describe('MutationObserverWrapper', () => {
   let sandbox: HTMLElement
@@ -21,130 +16,57 @@ describe('MutationObserverWrapper', () => {
     }
 
     sandbox = document.createElement('div')
-    sandbox.appendChild(document.createElement('div'))
+    sandbox.id = 'sandbox'
+    document.body.appendChild(sandbox)
+
     mutationCallbackSpy = jasmine.createSpy<MutationCallBack>()
     mutationController = new MutationController()
-    MockMutationObserver.setup()
 
     mutationObserverWrapper = new MutationObserverWrapper(mutationController, mutationCallbackSpy)
   })
 
   afterEach(() => {
-    MockMutationObserver.cleanup()
     mutationObserverWrapper.stop()
     sandbox.remove()
   })
 
   it('generates a mutation when a node is appended to a known node', () => {
-    addNodeToMap(sandbox, {})
+    const serializedDocument = snapshot(document)[0]!
 
-    MockMutationObserver.emitRecords([createMutationRecord()])
+    sandbox.appendChild(document.createElement('div'))
+
+    mutationController.flush()
 
     expect(mutationCallbackSpy).toHaveBeenCalledTimes(1)
-    expect(mutationCallbackSpy).toHaveBeenCalledWith({
-      texts: [],
-      attributes: [],
-      removes: [],
+
+    const { validate, newNode, selectNode } = createMutationPayloadValidator(serializedDocument)
+    validate(mutationCallbackSpy.calls.mostRecent().args[0], {
       adds: [
         {
-          parentId: (jasmine.any(Number) as unknown) as number,
-          nextId: null,
-          node: (jasmine.objectContaining({
-            tagName: 'div',
-          }) as unknown) as SerializedNodeWithId,
+          parent: selectNode({ idAttribute: 'sandbox' }),
+          node: newNode({ type: NodeType.Element, tagName: 'div' }),
         },
       ],
     })
   })
 
   it('does not generate a mutation when a node is appended to a unknown node', () => {
-    MockMutationObserver.emitRecords([createMutationRecord()])
+    sandbox.appendChild(document.createElement('div'))
+
+    mutationController.flush()
+
     expect(mutationCallbackSpy).not.toHaveBeenCalled()
   })
 
   it('emits buffered mutation records on flush', () => {
-    addNodeToMap(sandbox, {})
+    snapshot(document)
 
-    MockMutationObserver.storeRecords([createMutationRecord()])
+    sandbox.appendChild(document.createElement('div'))
+
     expect(mutationCallbackSpy).toHaveBeenCalledTimes(0)
+
     mutationController.flush()
+
     expect(mutationCallbackSpy).toHaveBeenCalledTimes(1)
   })
-
-  function createMutationRecord(): MutationRecord {
-    return {
-      type: 'childList',
-      target: sandbox,
-      addedNodes: createNodeList([sandbox.firstChild!]),
-      removedNodes: createNodeList([]),
-      oldValue: null,
-      attributeName: null,
-      attributeNamespace: null,
-      nextSibling: null,
-      previousSibling: null,
-    }
-  }
 })
-
-function addNodeToMap(node: Node, map: IdNodeMap) {
-  serializeNodeWithId(node, {
-    doc: document,
-    map,
-    ...DEFAULT_OPTIONS,
-  })
-}
-
-function createNodeList(nodes: Node[]): NodeList {
-  return (Object.assign(nodes.slice(), {
-    item(this: Node[], index: number) {
-      return this[index]
-    },
-  }) as unknown) as NodeList
-}
-
-class MockMutationObserver implements MutationObserver {
-  static instance?: MockMutationObserver
-  static originalMutationObserverDescriptor?: PropertyDescriptor
-  private storedRecords: MutationRecord[] = []
-
-  constructor(public readonly callback: (records: MutationRecord[]) => void) {}
-
-  static setup() {
-    if (!this.originalMutationObserverDescriptor) {
-      this.originalMutationObserverDescriptor = Object.getOwnPropertyDescriptor(window, 'MutationObserver')
-      window.MutationObserver = (this as unknown) as typeof MutationObserver
-    }
-  }
-
-  static cleanup() {
-    if (this.originalMutationObserverDescriptor) {
-      Object.defineProperty(window, 'MutationObserver', this.originalMutationObserverDescriptor)
-      this.originalMutationObserverDescriptor = undefined
-    }
-  }
-
-  static emitRecords(records: MutationRecord[]) {
-    this.instance?.callback(records)
-  }
-
-  static storeRecords(records: MutationRecord[]) {
-    this.instance?.storedRecords.push(...records)
-  }
-
-  observe() {
-    if (MockMutationObserver.instance) {
-      throw new Error('Only a single MockMutationObserver can observe at a time')
-    }
-    MockMutationObserver.instance = this
-  }
-
-  disconnect() {
-    if (MockMutationObserver.instance === this) {
-      MockMutationObserver.instance = undefined
-    }
-  }
-
-  takeRecords() {
-    return this.storedRecords.splice(0, this.storedRecords.length)
-  }
-}
