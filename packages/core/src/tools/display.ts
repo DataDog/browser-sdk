b2e52ea76555a4366e07406b61a5064b4447f2db
Bastien Caudan <bastien.caudan@gmail.com>
2021-05-14 16:49:06 +0200

    👌 add extra note on potential patch by other SDK

--- packages/core/src/tools/display.ts
+++ packages/core/src/tools/display.ts
@@ -1,5 +1,9 @@
 /**
  * Keep references on console methods to avoid triggering patched behaviors
+ *
+ * NB: in some setup, console could already be patched by another SDK.
+ * In this case, some display messages can be sent by the other SDK
+ * but we should be safe from infinite loop nonetheless.
  */
 export const display: Pick<typeof console, 'log' | 'warn' | 'error'> = {
   log: console.log,
