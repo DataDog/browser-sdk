8627e0bbd518d749c1d2abdcaa0cb06689e3e223
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2021-05-19 18:10:06 +0200

    Add id to all RUM event view

--- packages/rum-core/src/domain/rumEventsCollection/view/viewCollection.ts
+++ packages/rum-core/src/domain/rumEventsCollection/view/viewCollection.ts
@@ -19,6 +19,7 @@ function processViewUpdate(view: ViewEvent) {
     date: view.startClocks.timeStamp,
     type: RumEventType.VIEW,
     view: {
+      id: view.id,
       action: {
         count: view.eventCounts.userActionCount,
       },
