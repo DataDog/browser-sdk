bd536ccf4187d9e85f5b24e50ba81b99ec7e54b3
Benoît <benoit.zugmeyer@datadoghq.com>
2021-05-26 12:35:13 +0200

    ✨ [RUMF-913] allow masking input values (#860)
    * [RUMF-913] do not collect <input type="radio"> and <input type="checkbox"> values
    
    After asking myself if we should mask values for radios with
    corresponding names, I came to the conclusion that the value of radios and
    checkboxes shouldn't matter during replay, so removing it altogether
    would be simpler.
    
    This commit also removes the 'isChecked' property on inputs other than
    radio or checkbox, making records a bit smaller.
    
    * 🏷️ [RUMF-913] cast 'attributes' mutation.target as "Element"
    
    Also, rename *MutationRecord to Rum*MutationRecord to avoid confusion
    with TS provided types.
    
    * [RUMF-913] introduce the concept of "InputPrivacyMode" and associated functions
    
    * [RUMF-913] implement a function that will be used to get any element value
    
    * ✨ [RUMF-913] use getElementInputValue in "input observer"
    
    * ✨ [RUMF-913] use getElementInputValue during node serialization
    
    Previously, input values where serialized during, regardless of the
    privacy attribute. This commit fixes this issue and adds support for
    "masked" input values as well.
    
    * ✨ [RUMF-913] use getElementInputValue while processing attributes mutation
    
    Previously, input values where taken into account while processing
    attributes mutations regardless of the privacy attribute. This commit
    fixes this issue and bring support for masked inputs
    
    * 👌 use 'include' instead of 'indexOf'
    
    * 👌 simplify 'maskValue'
    
    * 👌 replace "foo" with "unknown-mode" in tests
    
    * 👌 rename test function 'wrapInInputIgnored'
    
    * use "ignored" in priority when two modes are applied on the same node
    
    * 👌 use "ignored" mode in priority over "masked"
    
    * 🎨 rename "InputValue" to "InputState"
    
    as it clashes with my newly introduced function `getElementInputValue`
    
    * 👌 change "InputState" to an union type

--- packages/rum-recorder/src/domain/record/privacy.spec.ts
+++ packages/rum-recorder/src/domain/record/privacy.spec.ts
@@ -1,9 +1,10 @@
 import { isIE } from '../../../../core/test/specHelper'
+import { InputPrivacyMode } from '../../constants'
 import {
   nodeShouldBeHidden,
   nodeOrAncestorsShouldBeHidden,
-  nodeShouldHaveInputIgnored,
-  nodeOrAncestorsShouldHaveInputIgnored,
+  getNodeInputPrivacyMode,
+  getNodeOrAncestorsInputPrivacyMode,
 } from './privacy'
 
 describe('privacy helpers', () => {
@@ -57,58 +58,107 @@ describe('privacy helpers', () => {
       expect(nodeOrAncestorsShouldBeHidden(document)).toBeFalsy()
     })
   })
-  describe('for ignoring input events', () => {
-    it('considers a normal DOM Element as not to be ignored', () => {
-      const node = document.createElement('input')
-      expect(nodeShouldHaveInputIgnored(node)).toBeFalsy()
+
+  describe('input privacy mode', () => {
+    it('use the ancestor privacy mode for a normal DOM Element', () => {
+      const node = document.createElement('div')
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.NONE)
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.IGNORED)).toBe(InputPrivacyMode.IGNORED)
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.MASKED)).toBe(InputPrivacyMode.MASKED)
     })
+
+    it('use the ancestor privacy mode for a DOM Element with a data-dd-privacy="unknown-mode" attribute', () => {
+      const node = document.createElement('input')
+      node.setAttribute('data-dd-privacy', 'unknown-mode')
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.NONE)
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.IGNORED)).toBe(InputPrivacyMode.IGNORED)
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.MASKED)).toBe(InputPrivacyMode.MASKED)
+    })
+
+    it('use the ancestor privacy mode for a DOM HTMLInputElement with a type of "text"', () => {
+      const node = document.createElement('input')
+      node.type = 'text'
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.NONE)
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.IGNORED)).toBe(InputPrivacyMode.IGNORED)
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.MASKED)).toBe(InputPrivacyMode.MASKED)
+    })
+
+    it('considers a DOM Document as not to be ignored', () => {
+      expect(getNodeInputPrivacyMode(document, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.NONE)
+    })
+
     it('considers a DOM Element with a data-dd-privacy="input-ignored" attribute to be ignored', () => {
       const node = document.createElement('input')
       node.setAttribute('data-dd-privacy', 'input-ignored')
-      expect(nodeShouldHaveInputIgnored(node)).toBeTruthy()
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.IGNORED)
     })
-    it('considers a DOM Element with a data-dd-privacy="foo" attribute as not to be ignored', () => {
-      const node = document.createElement('input')
-      node.setAttribute('data-dd-privacy', 'foo')
-      expect(nodeShouldHaveInputIgnored(node)).toBeFalsy()
-    })
+
     it('considers a DOM Element with a dd-privacy-input-ignored class to be ignored', () => {
       const node = document.createElement('input')
       node.className = 'dd-privacy-input-ignored'
-      expect(nodeShouldHaveInputIgnored(node)).toBeTruthy()
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.IGNORED)
     })
+
     it('considers a DOM HTMLInputElement with a type of "password" to be ignored', () => {
       const node = document.createElement('input')
       node.type = 'password'
-      expect(nodeShouldHaveInputIgnored(node)).toBeTruthy()
+      expect(getNodeInputPrivacyMode(node, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.IGNORED)
     })
-    it('considers a DOM HTMLInputElement with a type of "text" as not to be ignored', () => {
-      const node = document.createElement('input')
-      node.type = 'text'
-      expect(nodeShouldHaveInputIgnored(node)).toBeFalse()
+
+    describe('input mode priority', () => {
+      it('consider a DOM Element to be ignored if both modes can apply', () => {
+        const node = document.createElement('input')
+        node.className = 'dd-privacy-input-ignored'
+        node.setAttribute('data-dd-privacy', 'input-masked')
+        expect(getNodeInputPrivacyMode(node, InputPrivacyMode.NONE)).toBe(InputPrivacyMode.IGNORED)
+      })
+
+      it('forces an element to be ignored if an ancestor is ignored', () => {
+        const node = document.createElement('input')
+        node.setAttribute('data-dd-privacy', 'input-masked')
+        expect(getNodeInputPrivacyMode(node, InputPrivacyMode.IGNORED)).toBe(InputPrivacyMode.IGNORED)
+      })
+
+      it('does not force an element to be masked if an ancestor is masked', () => {
+        const node = document.createElement('input')
+        node.setAttribute('data-dd-privacy', 'input-ignored')
+        expect(getNodeInputPrivacyMode(node, InputPrivacyMode.MASKED)).toBe(InputPrivacyMode.IGNORED)
+      })
     })
-    it('considers a normal DOM Element with a normal parent as not to be ignored', () => {
-      const node = document.createElement('input')
-      const parent = document.createElement('form')
-      parent.appendChild(node)
-      expect(nodeOrAncestorsShouldHaveInputIgnored(node)).toBeFalsy()
-    })
-    it('considers a DOM Element with a parent node with a dd-privacy="input-ignored" attribute to be ignored', () => {
-      const node = document.createElement('input')
-      const parent = document.createElement('form')
-      parent.setAttribute('data-dd-privacy', 'input-ignored')
-      parent.appendChild(node)
-      expect(nodeOrAncestorsShouldHaveInputIgnored(node)).toBeTruthy()
-    })
-    it('considers a DOM Element with a parent node with a dd-privacy-input-ignored class to be ignored', () => {
-      const node = document.createElement('input')
-      const parent = document.createElement('form')
-      parent.className = 'dd-privacy-input-ignored'
-      parent.appendChild(node)
-      expect(nodeOrAncestorsShouldHaveInputIgnored(node)).toBeTruthy()
-    })
-    it('considers a DOM Document as not to be ignored', () => {
-      expect(nodeOrAncestorsShouldHaveInputIgnored(document)).toBeFalsy()
+
+    describe('walk through elements ancestors to determine the privacy mode', () => {
+      it('considers a normal DOM Element with a normal parent as not to be ignored', () => {
+        const node = document.createElement('input')
+        const parent = document.createElement('form')
+        parent.appendChild(node)
+        expect(getNodeOrAncestorsInputPrivacyMode(node)).toBe(InputPrivacyMode.NONE)
+      })
+
+      it('considers a DOM Element with a parent node with a dd-privacy="input-ignored" attribute to be ignored', () => {
+        const node = document.createElement('input')
+        const parent = document.createElement('form')
+        parent.setAttribute('data-dd-privacy', 'input-ignored')
+        parent.appendChild(node)
+        expect(getNodeOrAncestorsInputPrivacyMode(node)).toBe(InputPrivacyMode.IGNORED)
+      })
+
+      it('considers a DOM Element with a parent node with a dd-privacy-input-ignored class to be ignored', () => {
+        const node = document.createElement('input')
+        const parent = document.createElement('form')
+        parent.className = 'dd-privacy-input-ignored'
+        parent.appendChild(node)
+        expect(getNodeOrAncestorsInputPrivacyMode(node)).toBe(InputPrivacyMode.IGNORED)
+      })
+
+      // eslint-disable-next-line max-len
+      it('considers a DOM Element with a "masked" privacy mode but within a parent with a "ignored" privacy mode to be ignored', () => {
+        const node = document.createElement('input')
+        const parent = document.createElement('form')
+        parent.setAttribute('data-dd-privacy', 'input-ignored')
+        parent.appendChild(node)
+        node.setAttribute('data-dd-privacy', 'input-masked')
+        expect(getNodeOrAncestorsInputPrivacyMode(node)).toBe(InputPrivacyMode.IGNORED)
+      })
     })
   })
 })
