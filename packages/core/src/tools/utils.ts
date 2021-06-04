6667ae79de447e7aa1afa368622fd7c669b72136
Bastien Caudan <bastien.caudan@gmail.com>
2021-06-04 18:17:17 +0200

    monitor high lcp, fcp, resource.duration values

--- packages/core/src/tools/utils.ts
+++ packages/core/src/tools/utils.ts
@@ -3,6 +3,7 @@ import { monitor } from '../domain/internalMonitoring'
 export const ONE_SECOND = 1000
 export const ONE_MINUTE = 60 * ONE_SECOND
 export const ONE_HOUR = 60 * ONE_MINUTE
+export const ONE_DAY = 24 * ONE_HOUR
 export const ONE_KILO_BYTE = 1024
 
 export const enum DOM_EVENT {
