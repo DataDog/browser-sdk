dd4a71aa6b333fc4e458a42cd600e80d40f20920
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-07-09 17:48:36 +0200

    ♻️ [RUMF-940] use SetupBuilder in rumRecorderPublicApi

--- packages/rum-recorder/src/boot/rumRecorderPublicApi.spec.ts
+++ packages/rum-recorder/src/boot/rumRecorderPublicApi.spec.ts
@@ -1,30 +1,32 @@
-import { Configuration, includes } from '@datadog/browser-core'
 import { StartRum } from '@datadog/browser-rum-core'
 import { createNewEvent } from '../../../core/test/specHelper'
+import { setup, TestSetupBuilder } from '../../../rum-core/test/specHelper'
 import { makeRumRecorderPublicApi, RumRecorderPublicApi, StartRecording } from './rumRecorderPublicApi'
 
 const DEFAULT_INIT_CONFIGURATION = { applicationId: 'xxx', clientToken: 'xxx' }
 
 describe('makeRumRecorderPublicApi', () => {
+  let setupBuilder: TestSetupBuilder
   let rumRecorderPublicApi: RumRecorderPublicApi
   let startRecordingSpy: jasmine.Spy<StartRecording>
   let stopRecordingSpy: jasmine.Spy<() => void>
   let startRumSpy: jasmine.Spy<StartRum>
 
   beforeEach(() => {
-    stopRecordingSpy = jasmine.createSpy('stopRecording')
-    startRecordingSpy = jasmine.createSpy('startRecording').and.callFake(() => ({
-      stop: stopRecordingSpy,
-    }))
-    startRumSpy = jasmine.createSpy<StartRum>('startRum').and.callFake((initConfiguration) => {
-      const configuration: Partial<Configuration> = {
-        isEnabled(feature) {
-          return includes(initConfiguration.enableExperimentalFeatures || [], feature)
-        },
-      }
-      return ({ configuration } as unknown) as ReturnType<StartRum>
+    setupBuilder = setup().beforeBuild(({ lifeCycle, session }) => {
+      stopRecordingSpy = jasmine.createSpy('stopRecording')
+      startRecordingSpy = jasmine.createSpy('startRecording').and.callFake(() => ({
+        stop: stopRecordingSpy,
+      }))
+      startRumSpy = jasmine
+        .createSpy<StartRum>('startRum')
+        .and.callFake(() => (({ session, lifeCycle } as unknown) as ReturnType<StartRum>))
+      rumRecorderPublicApi = makeRumRecorderPublicApi(startRumSpy, startRecordingSpy)
     })
-    rumRecorderPublicApi = makeRumRecorderPublicApi(startRumSpy, startRecordingSpy)
+  })
+
+  afterEach(() => {
+    setupBuilder.cleanup()
   })
 
   function getCommonContext() {
@@ -34,23 +36,27 @@ describe('makeRumRecorderPublicApi', () => {
   describe('boot', () => {
     describe('when tracking views automatically', () => {
       it('starts RUM when init is called', () => {
+        setupBuilder.build()
         expect(startRumSpy).not.toHaveBeenCalled()
         rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
         expect(startRumSpy).toHaveBeenCalled()
       })
 
       it('starts recording when init() is called', () => {
+        setupBuilder.build()
         expect(startRecordingSpy).not.toHaveBeenCalled()
         rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
         expect(startRecordingSpy).toHaveBeenCalled()
       })
 
       it('does not start recording when calling init() with manualSessionReplayRecordingStart: true', () => {
+        setupBuilder.build()
         rumRecorderPublicApi.init({ ...DEFAULT_INIT_CONFIGURATION, manualSessionReplayRecordingStart: true })
         expect(startRecordingSpy).not.toHaveBeenCalled()
       })
 
       it('does not start recording when calling init() with the feature "postpone_start_recording"', () => {
+        setupBuilder.build()
         rumRecorderPublicApi.init({
           ...DEFAULT_INIT_CONFIGURATION,
           enableExperimentalFeatures: ['postpone_start_recording'],
@@ -59,6 +65,7 @@ describe('makeRumRecorderPublicApi', () => {
       })
 
       it('does not start recording before the page "load"', () => {
+        setupBuilder.build()
         const { triggerOnLoad } = mockDocumentReadyState()
         rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
         expect(startRecordingSpy).not.toHaveBeenCalled()
@@ -73,6 +80,7 @@ describe('makeRumRecorderPublicApi', () => {
         trackViewsManually: true,
       }
       it('starts RUM when initial view is started', () => {
+        setupBuilder.build()
         expect(startRumSpy).not.toHaveBeenCalled()
         rumRecorderPublicApi.init(MANUAL_VIEWS_CONFIGURATION)
         expect(startRumSpy).not.toHaveBeenCalled()
@@ -81,6 +89,7 @@ describe('makeRumRecorderPublicApi', () => {
       })
 
       it('starts recording when initial view is started', () => {
+        setupBuilder.build()
         expect(startRecordingSpy).not.toHaveBeenCalled()
         rumRecorderPublicApi.init(MANUAL_VIEWS_CONFIGURATION)
         expect(startRecordingSpy).not.toHaveBeenCalled()
@@ -89,12 +98,14 @@ describe('makeRumRecorderPublicApi', () => {
       })
 
       it('does not start recording when initial view is started with manualSessionReplayRecordingStart: true', () => {
+        setupBuilder.build()
         rumRecorderPublicApi.init({ ...MANUAL_VIEWS_CONFIGURATION, manualSessionReplayRecordingStart: true })
         rumRecorderPublicApi.startView()
         expect(startRecordingSpy).not.toHaveBeenCalled()
       })
 
       it('does not start recording when initial view is started with the feature "postpone_start_recording"', () => {
+        setupBuilder.build()
         rumRecorderPublicApi.init({
           ...MANUAL_VIEWS_CONFIGURATION,
           enableExperimentalFeatures: ['postpone_start_recording'],
@@ -104,6 +115,7 @@ describe('makeRumRecorderPublicApi', () => {
       })
 
       it('does not start recording before the page "load"', () => {
+        setupBuilder.build()
         const { triggerOnLoad } = mockDocumentReadyState()
         rumRecorderPublicApi.init(MANUAL_VIEWS_CONFIGURATION)
         rumRecorderPublicApi.startView()
@@ -116,6 +128,7 @@ describe('makeRumRecorderPublicApi', () => {
 
   describe('startSessionReplayRecording()', () => {
     it('ignores calls while recording is already started', () => {
+      setupBuilder.build()
       rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
       rumRecorderPublicApi.startSessionReplayRecording()
       rumRecorderPublicApi.startSessionReplayRecording()
@@ -124,12 +137,14 @@ describe('makeRumRecorderPublicApi', () => {
     })
 
     it('starts recording if called before init()', () => {
+      setupBuilder.build()
       rumRecorderPublicApi.startSessionReplayRecording()
       rumRecorderPublicApi.init({ ...DEFAULT_INIT_CONFIGURATION, manualSessionReplayRecordingStart: true })
       expect(startRecordingSpy).toHaveBeenCalled()
     })
 
     it('does not start recording multiple times if restarted before onload', () => {
+      setupBuilder.build()
       const { triggerOnLoad } = mockDocumentReadyState()
       rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
       rumRecorderPublicApi.stopSessionReplayRecording()
@@ -141,6 +156,7 @@ describe('makeRumRecorderPublicApi', () => {
 
   describe('stopSessionReplayRecording()', () => {
     it('ignores calls while recording is already stopped', () => {
+      setupBuilder.build()
       rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
       rumRecorderPublicApi.stopSessionReplayRecording()
       rumRecorderPublicApi.stopSessionReplayRecording()
@@ -149,12 +165,14 @@ describe('makeRumRecorderPublicApi', () => {
     })
 
     it('does not start recording if called before init()', () => {
+      setupBuilder.build()
       rumRecorderPublicApi.stopSessionReplayRecording()
       rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
       expect(startRecordingSpy).not.toHaveBeenCalled()
     })
 
     it('prevents recording to start at page "load"', () => {
+      setupBuilder.build()
       const { triggerOnLoad } = mockDocumentReadyState()
       rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
       rumRecorderPublicApi.stopSessionReplayRecording()
@@ -165,6 +183,7 @@ describe('makeRumRecorderPublicApi', () => {
 
   describe('commonContext hasReplay', () => {
     it('is true only if recording', () => {
+      setupBuilder.build()
       rumRecorderPublicApi.init({ ...DEFAULT_INIT_CONFIGURATION, manualSessionReplayRecordingStart: true })
       expect(getCommonContext().hasReplay).toBeUndefined()
       rumRecorderPublicApi.startSessionReplayRecording()
@@ -174,6 +193,7 @@ describe('makeRumRecorderPublicApi', () => {
     })
 
     it('is undefined before page "load"', () => {
+      setupBuilder.build()
       const { triggerOnLoad } = mockDocumentReadyState()
       rumRecorderPublicApi.init(DEFAULT_INIT_CONFIGURATION)
       expect(getCommonContext().hasReplay).toBeUndefined()
