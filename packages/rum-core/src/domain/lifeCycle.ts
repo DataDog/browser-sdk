f1cbdbd8cce5278fb525cfa0ce5007f8e680ca95
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-08-23 18:17:14 +0200

    [RUMF-997] add a SESSION_EXPIRED lifecycle event

--- packages/rum-core/src/domain/lifeCycle.ts
+++ packages/rum-core/src/domain/lifeCycle.ts
@@ -17,6 +17,19 @@ export enum LifeCycleEventType {
   VIEW_ENDED,
   REQUEST_STARTED,
   REQUEST_COMPLETED,
+
+  // The SESSION_EXPIRED lifecycle event has been introduced to represent when a session has expired
+  // and trigger cleanup tasks related to this, prior to renewing the session. Its implementation is
+  // slightly naive: it is not triggered as soon as the session is expired, but rather just before
+  // notifying that the session is renewed. Thus, the session id is already set to the newly renewed
+  // session.
+  //
+  // This implementation is "good enough" for our use-cases. Improving this is not trivial,
+  // primarily because multiple instances of the SDK may be managing the same session cookie at
+  // the same time, for example when using Logs and RUM on the same page, or opening multiple tabs
+  // on the same domain.
+  SESSION_EXPIRED,
+
   SESSION_RENEWED,
   BEFORE_UNLOAD,
   RAW_RUM_EVENT_COLLECTED,
@@ -41,6 +54,7 @@ export class LifeCycle {
   ): void
   notify(
     eventType:
+      | LifeCycleEventType.SESSION_EXPIRED
       | LifeCycleEventType.SESSION_RENEWED
       | LifeCycleEventType.BEFORE_UNLOAD
       | LifeCycleEventType.AUTO_ACTION_DISCARDED
@@ -77,6 +91,7 @@ export class LifeCycle {
   ): Subscription
   subscribe(
     eventType:
+      | LifeCycleEventType.SESSION_EXPIRED
       | LifeCycleEventType.SESSION_RENEWED
       | LifeCycleEventType.BEFORE_UNLOAD
       | LifeCycleEventType.AUTO_ACTION_DISCARDED,
