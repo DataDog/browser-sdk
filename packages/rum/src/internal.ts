3edcf568e7f6ad72ddeb96024f7d38706fa5e81c
Thibaut Gery <thibaut.gery@datadoghq.com>
2021-09-07 17:17:37 +0200

    Export serialise dom function (#1034)
    * Export serialise dom function
    
    * Add constant to be used with

--- packages/rum/src/internal.ts
+++ packages/rum/src/internal.ts
@@ -20,6 +20,9 @@ export {
   PRIVACY_ATTR_VALUE_INPUT_IGNORED,
   PRIVACY_CLASS_HIDDEN,
   PRIVACY_CLASS_INPUT_IGNORED,
+  NodePrivacyLevel,
 } from './constants'
 
 export * from './types'
+
+export { serializeNodeWithId } from './domain/record/serialize'
