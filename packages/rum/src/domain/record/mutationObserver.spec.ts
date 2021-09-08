36982632610d73dbdb107d552f312d056121c0c8
John Gracey <john.gracey@datadoghq.com>
2021-09-08 13:55:30 +0200

    Add test

--- packages/rum/src/domain/record/mutationObserver.spec.ts
+++ packages/rum/src/domain/record/mutationObserver.spec.ts
@@ -479,6 +479,36 @@ describe('startMutationCollection', () => {
         ],
       })
     })
+
+    it('respects the parent privacy level when emitting a text node mutation', () => {
+      const wrapper = document.createElement('div')
+      wrapper.setAttribute('data-dd-privacy', 'allow')
+      document.body.appendChild(wrapper)
+
+      const div = document.createElement('div')
+      div.innerText = 'foo 81'
+      wrapper.appendChild(div)
+
+      const serializedDocument = serializeDocument(document, NodePrivacyLevel.MASK)
+      const { mutationController, mutationCallbackSpy, getLatestMutationPayload } = startMutationCollection(
+        InitialPrivacyLevel.MASK
+      )
+
+      div.firstChild!.textContent = 'bazz 7'
+      mutationController.flush()
+
+      expect(mutationCallbackSpy).toHaveBeenCalledTimes(1)
+
+      const { validate, expectInitialNode } = createMutationPayloadValidator(serializedDocument)
+      validate(getLatestMutationPayload(), {
+        texts: [
+          {
+            node: expectInitialNode({ text: 'foo 81' }),
+            value: 'bazz 7',
+          },
+        ],
+      })
+    })
   })
 
   describe('attributes mutations', () => {
