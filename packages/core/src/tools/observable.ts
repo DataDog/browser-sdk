1f05ca7baf6f73325e25127f38c200d00bd998ee
Bastien Caudan <bastien.caudan@gmail.com>
2021-09-15 18:04:17 +0200

    ♻️  add callback capabilities to observable

--- packages/core/src/tools/observable.ts
+++ packages/core/src/tools/observable.ts
@@ -1,15 +1,35 @@
+import { noop } from './utils'
+
 export interface Subscription {
   unsubscribe: () => void
 }
 
+export interface ObservableParams {
+  onFirstSubscribe?: () => void
+  onLastUnsubscribe?: () => void
+}
+
 export class Observable<T> {
   private observers: Array<(data: T) => void> = []
+  private onFirstSubscribe: () => void
+  private onLastUnsubscribe: () => void
+
+  constructor(params?: ObservableParams) {
+    this.onFirstSubscribe = params?.onFirstSubscribe || noop
+    this.onLastUnsubscribe = params?.onLastUnsubscribe || noop
+  }
 
   subscribe(f: (data: T) => void): Subscription {
+    if (!this.observers.length) {
+      this.onFirstSubscribe()
+    }
     this.observers.push(f)
     return {
       unsubscribe: () => {
         this.observers = this.observers.filter((other) => f !== other)
+        if (!this.observers.length) {
+          this.onLastUnsubscribe()
+        }
       },
     }
   }
