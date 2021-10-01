f714db1e6bb1e12cd074ea6fc2e844ee63372b3c
John Gracey <john.gracey@datadoghq.com>
2021-10-01 16:44:49 +0200

    limitation notice

--- packages/core/src/domain/configuration/experimentalFeatures.ts
+++ packages/core/src/domain/configuration/experimentalFeatures.ts
@@ -1,3 +1,10 @@
+/**
+ * LIMITATION:
+ * For NPM setup, this feature flag singleton is shared between RUM and Logs product.
+ * This means that an experimental flag set on the RUM product will be set on the Logs product.
+ * So keep in mind that in certain configurations, your experimental feature flag may affect other products.
+ */
+
 let enabledExperimentalFeatures: Set<string>
 
 export function updateExperimentalFeatures(enabledFeatures: string[] | undefined): void {
