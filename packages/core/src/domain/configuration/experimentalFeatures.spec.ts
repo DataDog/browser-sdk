e84da617a8eecc8ca8eb61e92c8ddd97b18db7d8
John Gracey <john.gracey@datadoghq.com>
2021-10-01 14:44:26 +0200

    PR review fix

--- packages/core/src/domain/configuration/experimentalFeatures.spec.ts
+++ packages/core/src/domain/configuration/experimentalFeatures.spec.ts
@@ -5,35 +5,30 @@ import {
 } from './experimentalFeatures'
 
 describe('experimentalFeatures', () => {
-  it('initial state is empty', () => {
+  afterEach(() => {
     resetExperimentalFeatures()
-    expect(isExperimentalFeatureEnabled(undefined as any)).toBeFalse()
-    expect(isExperimentalFeatureEnabled('foo')).toBeFalse()
-    expect(isExperimentalFeatureEnabled('bar')).toBeFalse()
-    expect(isExperimentalFeatureEnabled('')).toBeFalse()
   })
 
-  it('updateExperimentalFeatures', () => {
+  it('initial state is empty', () => {
+    expect(isExperimentalFeatureEnabled('foo')).toBeFalse()
+    expect(isExperimentalFeatureEnabled('bar')).toBeFalse()
+  })
+
+  it('should define enabled experimental features', () => {
     updateExperimentalFeatures(['foo'])
     expect(isExperimentalFeatureEnabled('foo')).toBeTrue()
     expect(isExperimentalFeatureEnabled('bar')).toBeFalse()
   })
 
-  it('resetExperimentalFeatures clears state', () => {
-    resetExperimentalFeatures()
-    expect(isExperimentalFeatureEnabled('foo')).toBeFalse()
-    expect(isExperimentalFeatureEnabled('bar')).toBeFalse()
-  })
-
-  it("functions won't throw", () => {
+  it("functions won't throw an error", () => {
     resetExperimentalFeatures()
     resetExperimentalFeatures()
-    expect(isExperimentalFeatureEnabled('foo')).toBeFalse()
     updateExperimentalFeatures(undefined)
     updateExperimentalFeatures(undefined)
     expect(isExperimentalFeatureEnabled('foo')).toBeFalse()
+    
     updateExperimentalFeatures(['foo'])
-    updateExperimentalFeatures(['foo'])
+    updateExperimentalFeatures([11 as any, [] as any, {} as any, null as any, undefined as any])
     expect(isExperimentalFeatureEnabled('foo')).toBeTrue()
   })
 })
