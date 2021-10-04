54ec3287ce60c3c9976cdf07f92f354bd3868e23
John Gracey <john.gracey@datadoghq.com>
2021-10-04 14:53:50 +0200

    scrollX/Y fallback
    return data
    
    support old browsers
    
    add debug console
    
    Debug cross-browser unit test

--- packages/rum/src/domain/record/viewports.spec.ts
+++ packages/rum/src/domain/record/viewports.spec.ts
@@ -1,29 +1,36 @@
 import { getLayoutViewportDimensions } from './viewports'
 
 describe('getLayoutViewportDimensions', () => {
-  it('initially normalizes width and scroll dimention', () => {
+  it('initially normalizes width and scroll dimention', (done) => {
     const SCROLL_DOWN_PX = 100
-    const layoutDimensions = getLayoutViewportDimensions()
-
-    const initialInnerWidth = window.innerWidth
-    const initialInnerHeight = window.innerHeight
-
-    expect(layoutDimensions).toEqual({
-      scrollX: 0,
-      scrollY: 0,
-      innerWidth: window.innerWidth,
-      innerHeight: window.innerHeight,
-    })
 
     document.body.style.setProperty('margin-bottom', '2000px')
-    window.scrollTo(0, SCROLL_DOWN_PX)
+    requestAnimationFrame(() => {
+      const layoutDimensions = getLayoutViewportDimensions()
+      const initialInnerWidth = window.innerWidth
+      const initialInnerHeight = window.innerHeight
 
-    expect(getLayoutViewportDimensions()).toEqual({
-      scrollX: 0,
-      scrollY: SCROLL_DOWN_PX,
-      innerWidth: initialInnerWidth,
-      innerHeight: initialInnerHeight,
+      expect(layoutDimensions).toEqual({
+        scrollX: 0,
+        scrollY: 0,
+        innerWidth: window.innerWidth,
+        innerHeight: window.innerHeight,
+      })
+
+      window.scrollTo(0, SCROLL_DOWN_PX)
+
+      requestAnimationFrame(() => {
+        // eslint-disable-next-line no-console
+        console.log('Debug scrollX:', window.visualViewport, scrollY, getLayoutViewportDimensions())
+        expect(getLayoutViewportDimensions()).toEqual({
+          scrollX: 0,
+          scrollY: SCROLL_DOWN_PX,
+          innerWidth: initialInnerWidth,
+          innerHeight: initialInnerHeight,
+        })
+        document.body.style.removeProperty('margin-bottom')
+        done()
+      })
     })
-    document.body.style.removeProperty('margin-bottom')
   })
 })
