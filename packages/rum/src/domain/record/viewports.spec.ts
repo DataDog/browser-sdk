b51cc10b813aa98565d64392bee0edebfce6ca0f
John Gracey <john.gracey@datadoghq.com>
2021-10-04 18:16:29 +0200

    Safari not handling native scrollTo

--- packages/rum/src/domain/record/viewports.spec.ts
+++ packages/rum/src/domain/record/viewports.spec.ts
@@ -1,36 +1,32 @@
+import { isSafari } from 'packages/core/test/specHelper'
 import { getLayoutViewportDimensions } from './viewports'
 
 describe('getLayoutViewportDimensions', () => {
-  it('initially normalizes width and scroll dimention', (done) => {
+  it('initially normalizes width and scroll dimention', () => {
     const SCROLL_DOWN_PX = 100
 
     document.body.style.setProperty('margin-bottom', '2000px')
-    requestAnimationFrame(() => {
-      const layoutDimensions = getLayoutViewportDimensions()
-      const initialInnerWidth = window.innerWidth
-      const initialInnerHeight = window.innerHeight
+    const layoutDimensions = getLayoutViewportDimensions()
+    const initialInnerWidth = window.innerWidth
+    const initialInnerHeight = window.innerHeight
 
-      expect(layoutDimensions).toEqual({
-        scrollX: 0,
-        scrollY: 0,
-        innerWidth: window.innerWidth,
-        innerHeight: window.innerHeight,
-      })
-
-      window.scrollTo(0, SCROLL_DOWN_PX)
-
-      requestAnimationFrame(() => {
-        // eslint-disable-next-line no-console
-        console.log('Debug scrollX:', window.visualViewport, scrollY, getLayoutViewportDimensions())
-        expect(getLayoutViewportDimensions()).toEqual({
-          scrollX: 0,
-          scrollY: SCROLL_DOWN_PX,
-          innerWidth: initialInnerWidth,
-          innerHeight: initialInnerHeight,
-        })
-        document.body.style.removeProperty('margin-bottom')
-        done()
-      })
+    expect(layoutDimensions).toEqual({
+      scrollX: 0,
+      scrollY: 0,
+      innerWidth: window.innerWidth,
+      innerHeight: window.innerHeight,
     })
+
+    if (!isSafari) {
+      // Mobile Safari 12.0 (iOS 12.1) not responding to native scroll
+      window.scrollTo(0, SCROLL_DOWN_PX)
+      expect(getLayoutViewportDimensions()).toEqual({
+        scrollX: 0,
+        scrollY: SCROLL_DOWN_PX,
+        innerWidth: initialInnerWidth,
+        innerHeight: initialInnerHeight,
+      })
+      document.body.style.removeProperty('margin-bottom')
+    }
   })
 })
