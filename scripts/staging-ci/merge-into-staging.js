77c923bbed8e4a13034e8023ba7599afc0e14e52
Aymeric <aymeric.mortemousque@datadoghq.com>
2021-10-04 11:15:07 +0200

    Remove staging deploy notification + rename chan (#1099)

--- scripts/staging-ci/merge-into-staging.js
+++ scripts/staging-ci/merge-into-staging.js
@@ -5,6 +5,7 @@ const { initGitConfig, executeCommand, printLog, printError, logAndExit } = requ
 const REPOSITORY = process.env.GIT_REPOSITORY
 const CURRENT_STAGING_BRANCH = process.env.CURRENT_STAGING
 const CI_COMMIT_SHA = process.env.CI_COMMIT_SHA
+const CI_COMMIT_SHORT_SHA = process.env.CI_COMMIT_SHORT_SHA
 const CI_COMMIT_REF_NAME = process.env.CI_COMMIT_REF_NAME
 
 async function main() {
@@ -13,7 +14,7 @@ async function main() {
   await executeCommand(`git checkout ${CURRENT_STAGING_BRANCH} -f`)
   await executeCommand(`git pull origin ${CURRENT_STAGING_BRANCH}`)
 
-  printLog(`Merging branch '${CI_COMMIT_REF_NAME}' (${CI_COMMIT_SHA}) into ${CURRENT_STAGING_BRANCH}...`)
+  printLog(`Merging branch '${CI_COMMIT_REF_NAME}' (${CI_COMMIT_SHORT_SHA}) into ${CURRENT_STAGING_BRANCH}...`)
   try {
     await executeCommand(`git merge --no-ff "${CI_COMMIT_SHA}"`)
   } catch (error) {
@@ -23,7 +24,7 @@ async function main() {
   }
 
   const commitMessage = await executeCommand(`git show -s --format=%B`)
-  const newSummary = `Merge branch '${CI_COMMIT_REF_NAME}' (${CI_COMMIT_SHA}) with ${CURRENT_STAGING_BRANCH}`
+  const newSummary = `Merge branch '${CI_COMMIT_REF_NAME}' (${CI_COMMIT_SHORT_SHA}) with ${CURRENT_STAGING_BRANCH}`
   const message = `${newSummary}\n\n${commitMessage}`
 
   await executeCommand(`git commit --amend -m "${message}"`)
