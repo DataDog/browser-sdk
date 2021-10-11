ae2e04a07724eb672b891f901bc31d876eb07bcd
Gitlab staging reset job <aymeric.mortemousque@datadoghq.com>
2021-10-11 10:56:39 +0200

    Add tags separate unit and e2e tests

--- scripts/export-test-result.js
+++ scripts/export-test-result.js
@@ -4,11 +4,20 @@ const { getSecretKey, executeCommand, printLog, logAndExit } = require('./utils'
 
 async function main() {
   const ddCiApiKey = (await getSecretKey('ci.browser-sdk.datadog_ci_api_key')).trim()
-  const testFolders = ['test-report/unit/', 'test-report/e2e/', 'test-report/unit-bs/', 'test-report/e2e-bs/']
-  await executeCommand(`npx @datadog/datadog-ci junit upload --service browser-sdk --env ci ${testFolders.join(' ')}`, {
-    DATADOG_API_KEY: ddCiApiKey,
-  })
-  printLog('Export done.')
+  const unitTestFolders = ['test-report/unit/', 'test-report/unit-bs/']
+  const e2eTestFolders = ['test-report/e2e/', 'test-report/e2e-bs/']
+
+  await executeCommand(
+    `npx @datadog/datadog-ci junit upload --service browser-sdk --env ci --tags type:unit ${unitTestFolders.join(' ')}`,
+    { DATADOG_API_KEY: ddCiApiKey }
+  )
+  printLog('Export unit tests done.')
+
+  await executeCommand(
+    `npx @datadog/datadog-ci junit upload --service browser-sdk --env ci --tags type:e2e ${e2eTestFolders.join(' ')}`,
+    { DATADOG_API_KEY: ddCiApiKey }
+  )
+  printLog('Export e2e tests done.')
 }
 
 main().catch(logAndExit)
