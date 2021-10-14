8b26e61905f4ec23b344c8e7a2905e71db6a3bd3
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-10-14 10:42:49 +0200

    🚚 move tracekit tests in the corresponding module spec
    * `handler.spec` contained test cases that subscribed and threw uncaught
      error to see if they were correctly reported. That's the `report`
      module logic, so it has moved there.
    
    * `parser.spec` contained test cases using `computeStackTrace`, so they
      has been moved to the `computeStackTrace` module.

--- packages/core/src/domain/tracekit/report.spec.ts
+++ packages/core/src/domain/tracekit/report.spec.ts
@@ -1,6 +1,6 @@
 import { disableJasmineUncaughtErrorHandler } from '../../../test/specHelper'
 import { subscribe, unsubscribe, traceKitWindowOnError } from './report'
-import { Handler } from './types'
+import { Handler, StackFrame } from './types'
 
 describe('traceKitWindowOnError', () => {
   const testLineNo = 1337
@@ -103,5 +103,63 @@ describe('traceKitWindowOnError', () => {
       subscribe(subscriptionHandler)
       traceKitWindowOnError({ foo: 'bar' } as any)
     })
+  })
+})
+
+describe('uncaught exception handling', () => {
+  let resetJasmineUncaughtErrorHandler: () => void
+
+  beforeEach(() => {
+    ;({ reset: resetJasmineUncaughtErrorHandler } = disableJasmineUncaughtErrorHandler())
+  })
+
+  afterEach(() => {
+    resetJasmineUncaughtErrorHandler()
+  })
+
+  it('it should not go into an infinite loop', (done) => {
+    const stacks = []
+
+    function handler(stackInfo: StackFrame) {
+      stacks.push(stackInfo)
+    }
+
+    subscribe(handler)
+
+    setTimeout(() => {
+      throw new Error('expected error')
+    })
+
+    setTimeout(() => {
+      unsubscribe(handler)
+      expect(stacks.length).toEqual(1)
+      done()
+    }, 1000)
+  })
+
+  it('should get extra arguments (isWindowError and exception)', (done) => {
+    const handler = jasmine.createSpy()
+
+    const exception = new Error('expected error')
+
+    subscribe(handler)
+
+    setTimeout(() => {
+      throw exception
+    })
+
+    setTimeout(() => {
+      unsubscribe(handler)
+
+      expect(handler).toHaveBeenCalledTimes(1)
+
+      const isWindowError = handler.calls.mostRecent().args[1]
+      expect(isWindowError).toEqual(true)
+
+      const e = handler.calls.mostRecent().args[2]
+      expect(e).toEqual(exception)
+
+      done()
+    }, 1000)
   })
 })
