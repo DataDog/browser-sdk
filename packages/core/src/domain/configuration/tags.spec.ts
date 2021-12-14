5ebb615b40a00cdb47c711eabbf65e0fc66704a4
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-12-14 17:35:43 +0100

    ✨ [RUMF-827] sanitize tags

--- packages/core/src/domain/configuration/tags.spec.ts
+++ packages/core/src/domain/configuration/tags.spec.ts
@@ -1,5 +1,8 @@
+import { display } from '../../tools/display'
 import { InitConfiguration } from './configuration'
-import { buildTags } from './tags'
+import { buildTag, buildTags, TAG_SIZE_LIMIT } from './tags'
+
+const LARGE_VALUE = Array(TAG_SIZE_LIMIT + 10).join('a')
 
 describe('buildTags', () => {
   it('build tags from init configuration', () => {
@@ -10,5 +13,35 @@ describe('buildTags', () => {
         version: 'baz',
       } as InitConfiguration)
     ).toEqual(['env:bar', 'service:foo', 'version:baz'])
+  })
+})
+
+describe('buildTag', () => {
+  let displaySpy: jasmine.Spy<typeof display.warn>
+  beforeEach(() => {
+    displaySpy = spyOn(display, 'warn')
+  })
+
+  it('lowercases tags', () => {
+    expect(buildTag('env', 'BaR')).toBe('env:bar')
+  })
+
+  it('trims large tags', () => {
+    const tag = buildTag('env', LARGE_VALUE)
+    expect(tag.length).toBe(TAG_SIZE_LIMIT)
+    expect(tag).toBe(`env:${LARGE_VALUE.slice(0, TAG_SIZE_LIMIT - 4)}`)
+    expect(displaySpy).toHaveBeenCalledOnceWith(
+      `env value is too big and has been trimmed to ${LARGE_VALUE.slice(0, TAG_SIZE_LIMIT - 4)}`
+    )
+  })
+
+  it('replaces forbidden characters with slashes', () => {
+    expect(buildTag('env', 'b#r')).toBe('env:b_r')
+    expect(displaySpy).toHaveBeenCalledOnceWith('env value contains forbidden characters and has been sanitized to b_r')
+  })
+
+  it('removes ending semicolons', () => {
+    expect(buildTag('env', 'bar:::')).toBe('env:bar')
+    expect(displaySpy).toHaveBeenCalledOnceWith('env value ends with invalid characters and has been sanitized to bar')
   })
 })
--- packages/core/src/domain/configuration/tags.ts
+++ packages/core/src/domain/configuration/tags.ts
@@ -1,4 +1,7 @@
+import { display } from '../../tools/display'
 import { InitConfiguration } from './configuration'
+
+export const TAG_SIZE_LIMIT = 200
 
 export function buildTags(configuration: InitConfiguration): string[] {
   const { env, service, version } = configuration
@@ -17,6 +20,27 @@ export function buildTags(configuration: InitConfiguration): string[] {
   return tags
 }
 
-export function buildTag(tagName: string, rawValue: string) {
-  return `${tagName}:${rawValue}`
+export function buildTag(name: string, value: string) {
+  value = value.toLowerCase()
+
+  // See https://docs.datadoghq.com/getting_started/tagging/#defining-tags for tags syntax.
+  const valueSizeLimit = TAG_SIZE_LIMIT - name.length - 1
+  if (value.length > TAG_SIZE_LIMIT) {
+    value = value.slice(0, valueSizeLimit)
+    display.warn(`${name} value is too big and has been trimmed to ${value}`)
+  }
+
+  const valueWithoutForbiddenCharacters = value.replace(/[^a-z0-9_:./-]/g, '_')
+  if (valueWithoutForbiddenCharacters !== value) {
+    value = valueWithoutForbiddenCharacters
+    display.warn(`${name} value contains forbidden characters and has been sanitized to ${value}`)
+  }
+
+  const valueWithoutEndingColon = value.replace(/:+$/, '')
+  if (valueWithoutEndingColon !== value) {
+    value = valueWithoutEndingColon
+    display.warn(`${name} value ends with invalid characters and has been sanitized to ${value}`)
+  }
+
+  return `${name}:${value}`
 }
