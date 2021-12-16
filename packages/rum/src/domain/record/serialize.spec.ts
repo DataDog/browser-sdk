392534b6af21f228941b03207dd779e7fc567e67
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2021-12-16 15:31:26 +0100

    ✅ revamp privacy.spec tests
    To have a better vision on what's being tested by `privacy.spec`, some
    cleanup was necessary. I took the opportunity to remove some deprecated
    variables to improve things here.
    
    * `getNodeSelfPrivacyLevel`: consolidate tests with more test cases and
      highlight current modes priority
    
    * `getNodePrivacyLevel`: remove tests cases when they are already
      handled by `getNodeSelfPrivacyLevel` and consolidate tests on fallback
      value and inheritance
    
    * `serializeDocumentNode`: move those tests to `serialize.spec` since we
      don't use privacy functions here
    
    As well as a few cosmetic changes.

--- packages/rum/src/domain/record/serialize.spec.ts
+++ packages/rum/src/domain/record/serialize.spec.ts
@@ -24,7 +24,7 @@ import {
   serializeAttribute,
 } from './serialize'
 import { MAX_ATTRIBUTE_VALUE_CHAR_LENGTH } from './privacy'
-import { ElementNode, NodeType } from './types'
+import { ElementNode, NodeType, SerializedNodeWithId, TextNode } from './types'
 
 const DEFAULT_OPTIONS: SerializeOptions = {
   document,
@@ -394,6 +394,78 @@ describe('serializeNodeWithId', () => {
       metaElement.setAttribute('name', 'KeYwOrDs')
       expect(serializeNodeWithId(metaElement, DEFAULT_OPTIONS)).toEqual(null)
     })
+  })
+
+  describe('handles privacy', () => {
+    describe('for privacy tag `hidden`, a DOM tree', () => {
+      it('keeps private info private', () => {
+        const serializedDoc = generateLeanSerializedDoc(HTML, 'hidden')
+        expect(JSON.stringify(serializedDoc)).not.toContain('private')
+      })
+    })
+
+    describe('for privacy tag `mask`, a DOM tree', () => {
+      it("doesn't have innerText alpha numeric", () => {
+        const serializedDoc = generateLeanSerializedDoc(HTML, 'mask')
+        expect({ text: getTextNodesFromSerialized(serializedDoc) }).not.toBe({
+          text: jasmine.stringMatching(/^[*x\s]+\.example {content: "anything";}[*x\s]+$/),
+        })
+      })
+
+      it('keeps private info private', () => {
+        const serializedDoc = generateLeanSerializedDoc(HTML, 'mask')
+        expect(JSON.stringify(serializedDoc)).not.toContain('private')
+      })
+    })
+
+    describe('for privacy tag `mask-user-input`, a DOM tree', () => {
+      it("doesn't mask text content", () => {
+        const serializedDoc = generateLeanSerializedDoc(HTML, 'mask-user-input')
+        expect(JSON.stringify(serializedDoc)).not.toContain('᙮᙮')
+      })
+      it('keeps form fields private', () => {
+        const serializedDoc = generateLeanSerializedDoc(HTML, 'mask-user-input')
+        expect(JSON.stringify(serializedDoc)).toContain('**')
+      })
+    })
+
+    describe('for privacy tag `allow`, a DOM tree', () => {
+      it("doesn't have innerText alpha numeric", () => {
+        const serializedDoc = generateLeanSerializedDoc(HTML, 'allow')
+        const innerText = getTextNodesFromSerialized(serializedDoc)
+        const privateWordMatchCount = innerText.match(/private/g)?.length
+        expect(privateWordMatchCount).toBe(10)
+        expect(innerText).toBe(
+          // eslint-disable-next-line max-len
+          '  \n      .example {content: "anything";}\n       private title \n \n     hello private world \n     Loreum ipsum private text \n     hello private world \n     \n      Click https://private.com/path/nested?query=param#hash\n     \n      \n     \n       private option A \n       private option B \n       private option C \n     \n      \n      \n      \n     inputFoo label \n\n      \n\n           Loreum Ipsum private ...\n     \n\n     editable private div \n'
+        )
+      })
+
+      it('keeps innerText public', () => {
+        const serializedDoc = generateLeanSerializedDoc(HTML, 'allow')
+        expect(JSON.stringify(serializedDoc)).not.toContain('*')
+        expect(JSON.stringify(serializedDoc)).not.toContain('xx')
+      })
+    })
+
+    const getTextNodesFromSerialized = (serializedNode: SerializedNodeWithId | null): string => {
+      try {
+        if (serializedNode === null) {
+          return ''
+        } else if (serializedNode.type === NodeType.Text) {
+          const textNode = serializedNode as TextNode
+          return textNode.textContent
+        } else if (serializedNode.type === NodeType.Element || serializedNode.type === NodeType.Document) {
+          const textNode = serializedNode as ElementNode
+          return textNode.childNodes.map((node: SerializedNodeWithId) => getTextNodesFromSerialized(node)).join(' ')
+        }
+        return ''
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error('caught getTextNodesFromSerialized error:', e, serializedNode)
+        return ''
+      }
+    }
   })
 })
 
