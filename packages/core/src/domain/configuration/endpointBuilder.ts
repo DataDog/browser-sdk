1490fe4bd94fca9ea4c4624bc848ed7ecca21e84
Aymeric <aymeric.mortemousque@datadoghq.com>
2021-12-21 16:43:06 +0100

    🔥 [RUMF-1093] deprecated proxyhost option (#1227)
    * 🔥 Remove deprecated proxyhost option
    
    * Update performance project
    
    * ♻️ Simplidy endpointBuilder
    
    Co-authored-by: Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>

--- packages/core/src/domain/configuration/endpointBuilder.ts
+++ packages/core/src/domain/configuration/endpointBuilder.ts
@@ -29,64 +29,33 @@ export function createEndpointBuilder(
   tags: string[],
   source?: string
 ) {
-  const sdkVersion = buildEnv.sdkVersion
+  const { site = INTAKE_SITE_US, clientToken } = initConfiguration
 
+  const domainParts = site.split('.')
+  const extension = domainParts.pop()
+  const host = `${ENDPOINTS[endpointType]}.browser-intake-${domainParts.join('-')}.${extension!}`
+  const baseUrl = `https://${host}/api/v2/${INTAKE_TRACKS[endpointType]}`
   const proxyUrl = initConfiguration.proxyUrl && normalizeUrl(initConfiguration.proxyUrl)
-  const { site = INTAKE_SITE_US, clientToken, proxyHost } = initConfiguration
-
-  const host = buildHost(endpointType)
-  const path = buildPath(endpointType)
-
-  function build(): string {
-    const queryParameters = buildQueryParameters(endpointType, source)
-    const endpoint = `https://${host}${path}?${queryParameters}`
-    if (proxyUrl) {
-      return `${proxyUrl}?ddforward=${encodeURIComponent(endpoint)}`
-    } else if (proxyHost) {
-      return `https://${proxyHost}${path}?ddhost=${host}&${queryParameters}`
-    }
-    return endpoint
-  }
-
-  function buildIntakeUrl(): string {
-    if (proxyUrl) {
-      return `${proxyUrl}?ddforward`
-    }
-
-    const endpoint = build()
-    return endpoint.slice(0, endpoint.indexOf('?'))
-  }
-
-  function buildHost(endpointType: EndpointType) {
-    const endpoint = ENDPOINTS[endpointType]
-    const domainParts = site.split('.')
-    const extension = domainParts.pop()
-    const suffix = `${domainParts.join('-')}.${extension!}`
-    return `${endpoint}.browser-intake-${suffix}`
-  }
-
-  function buildPath(endpointType: EndpointType) {
-    return `/api/v2/${INTAKE_TRACKS[endpointType]}`
-  }
-
-  function buildQueryParameters(endpointType: EndpointType, source?: string) {
-    let parameters =
-      `ddsource=${source || 'browser'}` +
-      `&ddtags=${encodeURIComponent([`sdk_version:${sdkVersion}`].concat(tags).join(','))}` +
-      `&dd-api-key=${clientToken}` +
-      `&dd-evp-origin-version=${encodeURIComponent(sdkVersion)}` +
-      `&dd-evp-origin=browser` +
-      `&dd-request-id=${generateUUID()}`
-
-    if (endpointType === 'rum') {
-      parameters += `&batch_time=${timeStampNow()}`
-    }
-
-    return parameters
-  }
 
   return {
-    build,
-    buildIntakeUrl,
+    build() {
+      let parameters =
+        `ddsource=${source || 'browser'}` +
+        `&ddtags=${encodeURIComponent([`sdk_version:${buildEnv.sdkVersion}`].concat(tags).join(','))}` +
+        `&dd-api-key=${clientToken}` +
+        `&dd-evp-origin-version=${encodeURIComponent(buildEnv.sdkVersion)}` +
+        `&dd-evp-origin=browser` +
+        `&dd-request-id=${generateUUID()}`
+
+      if (endpointType === 'rum') {
+        parameters += `&batch_time=${timeStampNow()}`
+      }
+      const endpointUrl = `${baseUrl}?${parameters}`
+
+      return proxyUrl ? `${proxyUrl}?ddforward=${encodeURIComponent(endpointUrl)}` : endpointUrl
+    },
+    buildIntakeUrl() {
+      return proxyUrl ? `${proxyUrl}?ddforward` : baseUrl
+    },
   }
 }
