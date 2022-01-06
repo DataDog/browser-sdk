069404f458282a99057f685db87533c535e48772
Benoît <benoit.zugmeyer@datadoghq.com>
2022-01-06 13:53:38 +0100

    🐛 let the backend to most of the tags sanitization (#1252)

--- packages/core/src/domain/configuration/tags.ts
+++ packages/core/src/domain/configuration/tags.ts
@@ -20,17 +20,21 @@ export function buildTags(configuration: InitConfiguration): string[] {
   return tags
 }
 
-export function buildTag(key: string, rawValue: string) {
-  // See https://docs.datadoghq.com/getting_started/tagging/#defining-tags for tags syntax.
-  const valueSizeLimit = TAG_SIZE_LIMIT - key.length - 1
-  const sanitizedValue = rawValue
-    .toLowerCase()
-    .slice(0, valueSizeLimit)
-    .replace(/[^a-z0-9_:./-]/g, '_')
+const FORBIDDEN_CHARACTERS = /[^a-z0-9_:./-]/
 
-  if (sanitizedValue !== rawValue) {
+export function buildTag(key: string, rawValue: string) {
+  // See https://docs.datadoghq.com/getting_started/tagging/#defining-tags for tags syntax. Note
+  // that the backend may not follow the exact same rules, so we only want to display an informal
+  // warning.
+  const valueSizeLimit = TAG_SIZE_LIMIT - key.length - 1
+
+  if (rawValue.length > valueSizeLimit || FORBIDDEN_CHARACTERS.test(rawValue)) {
     display.warn(`${key} value doesn't meet tag requirements and will be sanitized`)
   }
+
+  // Let the backend do most of the sanitization, but still make sure multiple tags can't be crafted
+  // by forging a value containing commas.
+  const sanitizedValue = rawValue.replace(/,/g, '_')
 
   return `${key}:${sanitizedValue}`
 }
