8d5cd524a9c2c9838328cec7b5f73efff6631641
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2022-01-10 15:19:09 +0100

    🐛 [RUMF-1143] fix trackFirstHidden

--- packages/rum-core/src/domain/rumEventsCollection/view/trackFirstHidden.spec.ts
+++ packages/rum-core/src/domain/rumEventsCollection/view/trackFirstHidden.spec.ts
@@ -8,41 +8,69 @@ describe('trackFirstHidden', () => {
     restorePageVisibility()
   })
 
-  it('should return Infinity if the page was not hidden yet', () => {
-    expect(trackFirstHidden().timeStamp).toBe(Infinity as RelativeTime)
+  describe('the page is initially hidden', () => {
+    it('should return 0', () => {
+      setPageVisibility('hidden')
+      expect(trackFirstHidden().timeStamp).toBe(0 as RelativeTime)
+    })
+
+    it('should ignore events', () => {
+      setPageVisibility('hidden')
+      const emitter = document.createElement('div')
+      const firstHidden = trackFirstHidden(emitter)
+
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 100 }))
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.VISIBILITY_CHANGE, { timeStamp: 100 }))
+
+      expect(firstHidden.timeStamp).toBe(0 as RelativeTime)
+    })
   })
 
-  it('should return 0 if the page was hidden when executing trackFirstHidden', () => {
-    setPageVisibility('hidden')
-    expect(trackFirstHidden().timeStamp).toBe(0 as RelativeTime)
-  })
+  describe('the page is initially visible', () => {
+    it('should return Infinity if the page was not hidden yet', () => {
+      expect(trackFirstHidden().timeStamp).toBe(Infinity as RelativeTime)
+    })
 
-  it('should stay to 0 if the page was hidden when executing trackFirstHidden and a pagehide occurs', () => {
-    setPageVisibility('hidden')
-    const emitter = document.createElement('div')
-    const firstHidden = trackFirstHidden(emitter)
+    it('should return the timestamp of the first pagehide event', () => {
+      const emitter = document.createElement('div')
+      const firstHidden = trackFirstHidden(emitter)
 
-    emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 100 }))
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 100 }))
 
-    expect(firstHidden.timeStamp).toBe(0 as RelativeTime)
-  })
+      expect(firstHidden.timeStamp).toBe(100 as RelativeTime)
+    })
 
-  it('should return the timestamp of the first pagehide event', () => {
-    const emitter = document.createElement('div')
-    const firstHidden = trackFirstHidden(emitter)
+    it('should return the timestamp of the first visibilitychange event if the page is hidden', () => {
+      const emitter = document.createElement('div')
+      const firstHidden = trackFirstHidden(emitter)
 
-    emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 100 }))
+      setPageVisibility('hidden')
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.VISIBILITY_CHANGE, { timeStamp: 100 }))
 
-    expect(firstHidden.timeStamp).toBe(100 as RelativeTime)
-  })
+      expect(firstHidden.timeStamp).toBe(100 as RelativeTime)
+    })
 
-  it('should stay to the first value if multiple pagehide event occurs', () => {
-    const emitter = document.createElement('div')
-    const firstHidden = trackFirstHidden(emitter)
+    it('should ignore visibilitychange event if the page is visible', () => {
+      const emitter = document.createElement('div')
+      const firstHidden = trackFirstHidden(emitter)
 
-    emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 100 }))
-    emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 200 }))
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.VISIBILITY_CHANGE, { timeStamp: 100 }))
 
-    expect(firstHidden.timeStamp).toBe(100 as RelativeTime)
+      expect(firstHidden.timeStamp).toBe(Infinity as RelativeTime)
+    })
+
+    it('should ignore subsequent events', () => {
+      const emitter = document.createElement('div')
+      const firstHidden = trackFirstHidden(emitter)
+
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 100 }))
+
+      // Subsequent events:
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.PAGE_HIDE, { timeStamp: 200 }))
+      setPageVisibility('hidden')
+      emitter.dispatchEvent(createNewEvent(DOM_EVENT.VISIBILITY_CHANGE, { timeStamp: 200 }))
+
+      expect(firstHidden.timeStamp).toBe(100 as RelativeTime)
+    })
   })
 })
