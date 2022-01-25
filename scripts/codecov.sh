0445d5b3d751e8f3d8363ba14dc3eea6ddc6fbbc
Bastien Caudan <bastien.caudan@datadoghq.com>
2022-01-25 17:52:16 +0100

    👷 [RUMF-1160] Use new codecov uploader (#1297)
    * 🔥 remove deprecated codecov package
    
    * Setup new codecov uploader

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -6,4 +6,4 @@ if ! grep -q '"s":{[^}]\+}' coverage/coverage-final.json; then
 fi
 
 export CODECOV_TOKEN=$(aws ssm get-parameter --region us-east-1 --name ci.browser-sdk.codecov_token --with-decryption --query "Parameter.Value" --out text)
-yarn codecov -t "${CODECOV_TOKEN}" -f coverage/coverage-final.json
+codecov -t "${CODECOV_TOKEN}" -f coverage/coverage-final.json
