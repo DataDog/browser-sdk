ed7e3a5d7c6bf12ec494ff63d8817a0d78448880
Bastien Caudan <bastien.caudan@gmail.com>
2022-02-11 15:23:15 +0100

    👷 tweak dd ci configuration

--- scripts/export-test-result.js
+++ scripts/export-test-result.js
@@ -3,17 +3,18 @@
 const { getSecretKey, executeCommand, printLog, logAndExit } = require('./utils')
 
 async function main() {
-  await exportTestResult('unit', ['test-report/unit/', 'test-report/unit-bs/'])
-  await exportTestResult('e2e', ['test-report/e2e/', 'test-report/e2e-bs/'])
+  await exportTestResult('unit', 'test-report/unit/')
+  await exportTestResult('unit-bs', 'test-report/unit-bs/')
+  await exportTestResult('e2e', 'test-report/e2e/')
+  await exportTestResult('e2e-bs', 'test-report/e2e-bs/')
 }
 
-async function exportTestResult(type, folders) {
+async function exportTestResult(type, folder) {
   const DATADOG_API_KEY = await getSecretKey('ci.browser-sdk.datadog_ci_api_key')
 
-  await executeCommand(
-    `datadog-ci junit upload --service browser-sdk --env ci --tags type:${type} ${folders.join(' ')}`,
-    { DATADOG_API_KEY }
-  )
+  await executeCommand(`datadog-ci junit upload --service browser-sdk --env ci --tags test.type:${type} ${folder}`, {
+    DATADOG_API_KEY,
+  })
 
   printLog(`Export ${type} tests done.`)
 }
