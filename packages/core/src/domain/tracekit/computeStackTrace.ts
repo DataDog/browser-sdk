37eac681bc20c15d54fc14dfac951c0a7f11a22b
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2022-02-11 17:15:56 +0100

    🔥 remove old opera support

--- packages/core/src/domain/tracekit/computeStackTrace.ts
+++ packages/core/src/domain/tracekit/computeStackTrace.ts
@@ -1,4 +1,4 @@
-import type { StackFrame, StackTrace } from './types'
+import type { StackTrace } from './types'
 
 const UNKNOWN_FUNCTION = '?'
 
@@ -68,48 +68,11 @@ const UNKNOWN_FUNCTION = '?'
  * @param {(string|number)=} depth
  * @memberof computeStackTrace
  */
-export function computeStackTrace(ex: unknown, depth?: string | number): StackTrace {
+export function computeStackTrace(ex: unknown): StackTrace {
   let stack
-  const normalizedDepth = depth === undefined ? 0 : +depth
-
-  try {
-    // This must be tried first because Opera 10 *destroys*
-    // its stacktrace property if you try to access the stack
-    // property first!!
-    stack = computeStackTraceFromStacktraceProp(ex)
-    if (stack) {
-      return stack
-    }
-  } catch (e) {
-    if (debug) {
-      throw e
-    }
-  }
 
   try {
     stack = computeStackTraceFromStackProp(ex)
-    if (stack) {
-      return stack
-    }
-  } catch (e) {
-    if (debug) {
-      throw e
-    }
-  }
-
-  try {
-    stack = computeStackTraceFromOperaMultiLineMessage(ex)
-    if (stack) {
-      return stack
-    }
-  } catch (e) {
-    if (debug) {
-      throw e
-    }
-  }
-
-  try {
-    stack = computeStackTraceByWalkingCallerChain(ex, normalizedDepth + 1)
     if (stack) {
       return stack
     }
@@ -269,290 +232,6 @@ export function computeStackTraceFromStackProp(ex: unknown) {
   }
 }
 
-/**
- * Computes stack trace information from the stacktrace property.
- * Opera 10+ uses this property.
- * @param {Error} ex
- * @return {?StackTrace} Stack trace information.
- * @memberof computeStackTrace
- */
-function computeStackTraceFromStacktraceProp(ex: unknown) {
-  // Access and store the stacktrace property before doing ANYTHING
-  // else to it because Opera is not very good at providing it
-  // reliably in other circumstances.
-  const stacktrace = tryToGetString(ex, 'stacktrace')
-  if (!stacktrace) {
-    return
-  }
-
-  const opera10Regex = / line (\d+).*script (?:in )?(\S+)(?:: in function (\S+))?$/i
-  const opera11Regex =
-    / line (\d+), column (\d+)\s*(?:in (?:<anonymous function: ([^>]+)>|([^)]+))\((.*)\))? in (.*):\s*$/i
-  const lines = stacktrace.split('\n')
-  const stack = []
-  let parts
-
-  for (let line = 0; line < lines.length; line += 2) {
-    let element: StackFrame | undefined
-    if (opera10Regex.exec(lines[line])) {
-      parts = opera10Regex.exec(lines[line])!
-      element = {
-        args: [],
-        column: undefined,
-        func: parts[3],
-        line: +parts[1],
-        url: parts[2],
-      }
-    } else if (opera11Regex.exec(lines[line])) {
-      parts = opera11Regex.exec(lines[line])!
-      element = {
-        args: parts[5] ? parts[5].split(',') : [],
-        column: +parts[2],
-        func: parts[3] || parts[4],
-        line: +parts[1],
-        url: parts[6],
-      }
-    }
-
-    if (element) {
-      if (!element.func && element.line) {
-        element.func = UNKNOWN_FUNCTION
-      }
-      element.context = [lines[line + 1]]
-
-      stack.push(element)
-    }
-  }
-
-  if (!stack.length) {
-    return
-  }
-
-  return {
-    stack,
-    message: tryToGetString(ex, 'message'),
-    name: tryToGetString(ex, 'name'),
-  }
-}
-
-/**
- * NOT TESTED.
- * Computes stack trace information from an error message that includes
- * the stack trace.
- * Opera 9 and earlier use this method if the option to show stack
- * traces is turned on in opera:config.
- * @param {Error} ex
- * @return {?StackTrace} Stack information.
- * @memberof computeStackTrace
- */
-function computeStackTraceFromOperaMultiLineMessage(ex: unknown): StackTrace | undefined {
-  // TODO: Clean this function up
-  // Opera includes a stack trace into the exception message. An example is:
-  //
-  // Statement on line 3: Undefined variable: undefinedFunc
-  // Backtrace:
-  //   Line 3 of linked script file://localhost/Users/andreyvit/Projects/TraceKit/javascript-client/sample.js:
-  //   In function zzz
-  //         undefinedFunc(a);
-  //   Line 7 of inline#1 script in file://localhost/Users/andreyvit/Projects/TraceKit/javascript-client/sample.html:
-  //   In function yyy
-  //           zzz(x, y, z);
-  //   Line 3 of inline#1 script in file://localhost/Users/andreyvit/Projects/TraceKit/javascript-client/sample.html:
-  //   In function xxx
-  //           yyy(a, a, a);
-  //   Line 1 of function script
-  //     try { xxx('hi'); return false; } catch(ex) { report(ex); }
-  //   ...
-
-  const message = tryToGetString(ex, 'message')
-  if (!message) {
-    return
-  }
-  const lines = message.split('\n')
-  if (lines.length < 4) {
-    return
-  }
-
-  const lineRE1 = /^\s*Line (\d+) of linked script ((?:file|https?|blob)\S+)(?:: in function (\S+))?\s*$/i
-  const lineRE2 = /^\s*Line (\d+) of inline#(\d+) script in ((?:file|https?|blob)\S+)(?:: in function (\S+))?\s*$/i
-  const lineRE3 = /^\s*Line (\d+) of function script\s*$/i
-  const stack = []
-  const scripts = window && window.document && window.document.getElementsByTagName('script')
-  const inlineScriptBlocks = []
-  let parts
-
-  for (const s in scripts) {
-    if (has(scripts, s) && !scripts[s].src) {
-      inlineScriptBlocks.push(scripts[s])
-    }
-  }
-
-  for (let line = 2; line < lines.length; line += 2) {
-    let item: StackFrame | undefined
-    if (lineRE1.exec(lines[line])) {
-      parts = lineRE1.exec(lines[line])!
-      item = {
-        args: [],
-        column: undefined,
-        func: parts[3],
-        line: +parts[1],
-        url: parts[2],
-      }
-    } else if (lineRE2.exec(lines[line])) {
-      parts = lineRE2.exec(lines[line])!
-      item = {
-        args: [],
-        column: undefined, // TODO: Check to see if inline#1 (+parts[2]) points to the script number or column number.
-        func: parts[4],
-        line: +parts[1],
-        url: parts[3],
-      }
-    } else if (lineRE3.exec(lines[line])) {
-      parts = lineRE3.exec(lines[line])!
-      const url = window.location.href.replace(/#.*$/, '')
-      item = {
-        url,
-        args: [],
-        column: undefined,
-        func: '',
-        line: +parts[1],
-      }
-    }
-
-    if (item) {
-      if (!item.func) {
-        item.func = UNKNOWN_FUNCTION
-      }
-      item.context = [lines[line + 1]]
-      stack.push(item)
-    }
-  }
-  if (!stack.length) {
-    return // could not parse multiline exception message as Opera stack trace
-  }
-
-  return {
-    stack,
-    message: lines[0],
-    name: tryToGetString(ex, 'name'),
-  }
-}
-
-/**
- * Adds information about the first frame to incomplete stack traces.
- * Safari and IE require this to get complete data on the first frame.
- * @param {StackTrace} stackInfo Stack trace information from
- * one of the compute* methods.
- * @param {string=} url The URL of the script that caused an error.
- * @param {(number|string)=} lineNo The line number of the script that
- * caused an error.
- * @param {string=} message The error generated by the browser, which
- * hopefully contains the name of the object that caused the error.
- * @return {boolean} Whether or not the stack information was
- * augmented.
- * @memberof computeStackTrace
- */
-function augmentStackTraceWithInitialElement(stackInfo: StackTrace, url?: string, lineNo?: string | number) {
-  const initial: StackFrame = {
-    url,
-    line: lineNo ? +lineNo : undefined,
-  }
-
-  if (initial.url && initial.line) {
-    stackInfo.incomplete = false
-
-    const stack = stackInfo.stack
-    if (stack.length > 0) {
-      if (stack[0].url === initial.url) {
-        if (stack[0].line === initial.line) {
-          return false // already in stack trace
-        }
-        if (!stack[0].line && stack[0].func === initial.func) {
-          stack[0].line = initial.line
-          stack[0].context = initial.context
-          return false
-        }
-      }
-    }
-
-    stack.unshift(initial)
-    stackInfo.partial = true
-    return true
-  }
-  stackInfo.incomplete = true
-
-  return false
-}
-
-/**
- * Computes stack trace information by walking the arguments.caller
- * chain at the time the exception occurred. This will cause earlier
- * frames to be missed but is the only way to get any stack trace in
- * Safari and IE. The top frame is restored by
- * {@link augmentStackTraceWithInitialElement}.
- * @param {Error} ex
- * @param {number} depth
- * @return {StackTrace} Stack trace information.
- * @memberof computeStackTrace
- */
-function computeStackTraceByWalkingCallerChain(ex: unknown, depth: number) {
-  const functionName = /function\s+([_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*)?\s*\(/i
-  const stack = []
-  const funcs: any = {}
-  let recursion = false
-  let parts
-  let item: StackFrame
-
-  for (let curr = computeStackTraceByWalkingCallerChain.caller; curr && !recursion; curr = curr.caller) {
-    if (curr === computeStackTrace) {
-      continue
-    }
-
-    item = {
-      args: [],
-      column: undefined,
-      func: UNKNOWN_FUNCTION,
-      line: undefined,
-      url: undefined,
-    }
-
-    parts = functionName.exec(curr.toString())
-    if ((curr as any).name) {
-      item.func = (curr as any).name
-    } else if (parts) {
-      item.func = parts[1]
-    }
-
-    if (typeof item.func === 'undefined') {
-      item.func = parts ? parts.input.substring(0, parts.input.indexOf('{')) : undefined
-    }
-
-    if (funcs[curr.toString()]) {
-      recursion = true
-    } else {
-      funcs[curr.toString()] = true
-    }
-
-    stack.push(item)
-  }
-
-  if (depth) {
-    stack.splice(0, depth)
-  }
-
-  const result: StackTrace = {
-    stack,
-    message: tryToGetString(ex, 'message'),
-    name: tryToGetString(ex, 'name'),
-  }
-  augmentStackTraceWithInitialElement(
-    result,
-    tryToGetString(ex, 'sourceURL') || tryToGetString(ex, 'fileName'),
-    tryToGetString(ex, 'line') || tryToGetString(ex, 'lineNumber')
-  )
-  return result
-}
-
 function tryToGetString(candidate: unknown, property: string) {
   if (typeof candidate !== 'object' || !candidate || !(property in candidate)) {
     return undefined
@@ -567,25 +246,13 @@ function tryToGetString(candidate: unknown, property: string) {
  * @return {StackTrace} Stack trace information.
  * @memberof computeStackTrace
  */
-export function computeStackTraceOfCaller(depth?: number) {
-  const currentDepth = (depth === undefined ? 0 : +depth) + 1 // "+ 1" because "ofCaller" should drop one frame
+export function computeStackTraceOfCaller() {
   try {
+    // Throw error for IE
     throw new Error()
   } catch (ex) {
-    return computeStackTrace(ex, currentDepth + 1)
+    return computeStackTrace(ex)
   }
-}
-
-/**
- * A better form of hasOwnProperty<br/>
- * Example: `has(MainHostObject, property) === true/false`
- *
- * @param {Object} object to check property
- * @param {string} key to check
- * @return {Boolean} true if the object has the key and it is not inherited
- */
-function has(object: object, key: string) {
-  return Object.prototype.hasOwnProperty.call(object, key)
 }
 
 /**
