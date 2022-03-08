615edbdabb99ec79157a9307a6d7841dae15ef82
Gitlab staging reset job <aymeric.mortemousque@datadoghq.com>
2022-03-08 10:39:20 +0100

    Use slack API for staging reset job

--- scripts/staging-ci/staging-reset.js
+++ scripts/staging-ci/staging-reset.js
@@ -1,19 +1,26 @@
 'use strict'
 
 const fs = require('fs')
-const { CI_FILE, initGitConfig, executeCommand, printLog, logAndExit, replaceCiVariable } = require('../utils')
+const {
+  CI_FILE,
+  initGitConfig,
+  executeCommand,
+  printLog,
+  logAndExit,
+  replaceCiVariable,
+  sendSlackMessage,
+} = require('../utils')
 
 const REPOSITORY = process.env.GIT_REPOSITORY
 const MAIN_BRANCH = process.env.MAIN_BRANCH
+const CI_PROJECT_NAME = process.env.CI_PROJECT_NAME
+const BUILD_URL = `${process.env.CI_PROJECT_URL}/pipelines/${process.env.CI_PIPELINE_ID}`
 
 const CURRENT_STAGING_BRANCH = process.env.CURRENT_STAGING
 const NEW_STAGING_NUMBER = getWeekNumber().toString().padStart(2, '0')
 const NEW_STAGING_BRANCH = `staging-${NEW_STAGING_NUMBER}`
 
 async function main() {
-  // used to share the new staging name to the notification jobs
-  await executeCommand(`echo "NEW_STAGING=${NEW_STAGING_BRANCH}" >> build.env`)
-
   await initGitConfig(REPOSITORY)
   await executeCommand(`git fetch --no-tags origin ${MAIN_BRANCH} ${CURRENT_STAGING_BRANCH}`)
   await executeCommand(`git checkout ${MAIN_BRANCH} -f`)
@@ -52,6 +59,13 @@ async function main() {
   }
 
   printLog('Reset done.')
+
+  const commitMessage = await executeCommand('git show-branch --no-name HEAD')
+  await sendSlackMessage(
+    '#browser-sdk-deploy',
+    `:white_check_mark: [*${CI_PROJECT_NAME}*] Staging has been reset from *${CURRENT_STAGING_BRANCH}* ` +
+      `to *${NEW_STAGING_BRANCH}* on pipeline <${BUILD_URL}|${commitMessage}>.`
+  )
 }
 
 function getWeekNumber() {
@@ -60,4 +74,12 @@ function getWeekNumber() {
   return Math.ceil(((today - yearStart) / 86400000 + yearStart.getUTCDay() + 1) / 7)
 }
 
-main().catch(logAndExit)
+main().catch(async (error) => {
+  const commitMessage = await executeCommand('git show-branch --no-name HEAD')
+  await sendSlackMessage(
+    '#browser-sdk-deploy',
+    `:x: [*${CI_PROJECT_NAME}*] Staging failed to reset from *${CURRENT_STAGING_BRANCH}* ` +
+      `to *${NEW_STAGING_BRANCH}* on pipeline <${BUILD_URL}|${commitMessage}>.`
+  )
+  logAndExit(error)
+})
