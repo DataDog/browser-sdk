1de0bd45e595192b5e6ce03adacf51bccf2e3aa4
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2022-03-14 11:43:16 +0100

    👷 [RUMF-1156] fix BrowserStack configuration
    The previous browsers configuration was not
    working because the new wdio packages are validating it (both with
    typechecking and runtime checks).
    
    With this commit, I try to improve how we define and pass browser
    configuration to both karma and e2e tests. I also made sure that we are
    running on the latest appium and selenium versions, to keep things up to
    date.

--- test/e2e/wdio.bs.conf.js
+++ test/e2e/wdio.bs.conf.js
@@ -1,20 +1,41 @@
 const { getBuildInfos } = require('../utils')
+const browsers = require('../browsers.conf')
 const baseConf = require('./wdio.base.conf')
-const browsers = require('./browsers.conf')
 
 exports.config = {
   ...baseConf,
 
   specFileRetries: 1,
 
-  capabilities: Object.values(browsers).map((browser) => ({
-    ...browser,
-    browserName: `${browser.browser} ${browser.browser_version || ''}`,
-    'browserstack.video': false,
-    'browserstack.local': true,
-    project: 'browser sdk e2e',
-    build: getBuildInfos(),
-  })),
+  capabilities: browsers
+    .filter(
+      (browser) =>
+        browser.sessionName !== 'IE' &&
+        // Safari mobile on iOS 12.0 does not support
+        // the way we flush events on page change
+        // TODO check newer version on browserstack
+        browser.sessionName !== 'Safari mobile'
+    )
+    .map((browser) =>
+      // See https://www.browserstack.com/automate/capabilities?tag=selenium-4
+      // Make sure to look at the "W3C Protocol" tab
+      ({
+        browserName: browser.name,
+        browserVersion: browser.version,
+        'bstack:options': {
+          os: browser.os,
+          osVersion: browser.osVersion,
+          deviceName: browser.device,
+
+          appiumVersion: '1.22.0',
+          seleniumVersion: '4.1.2',
+
+          sessionName: browser.sessionName,
+          projectName: 'browser sdk e2e',
+          buildName: getBuildInfos(),
+        },
+      })
+    ),
   logLevels: {
     '@wdio/browserstack-service': 'info',
   },
