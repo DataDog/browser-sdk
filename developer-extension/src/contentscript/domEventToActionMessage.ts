93aa495cea2e17b001bd6f140ceae50e3876dff7
Aymeric <aymeric.mortemousque@datadoghq.com>
2022-04-04 15:52:06 +0200

    ♻️ Simplify developer extension sdks config panel (#1470)

--- developer-extension/src/contentscript/domEventToActionMessage.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Receive an HTML event from the page
- * and forward it as an action message to extension logic
- */
-
-document.documentElement.addEventListener('extension', (event: any) => {
-  chrome.runtime.sendMessage({ action: event.detail.action, payload: event.detail.payload })
-})
