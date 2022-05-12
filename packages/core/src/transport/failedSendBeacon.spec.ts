816140adb8ae5310de65d6d4aca6bd5c4b6a111d
Gitlab staging reset job <aymeric.mortemousque@datadoghq.com>
2022-05-12 10:43:28 +0200

    👌 only writing in local storage at unload and only reading from it at init

--- packages/core/src/transport/failedSendBeacon.spec.ts
+++ packages/core/src/transport/failedSendBeacon.spec.ts
@@ -1,12 +1,20 @@
+import type { Clock } from '../../test/specHelper'
 import { mockClock } from '../../test/specHelper'
-import { ONE_SECOND } from '../tools/utils'
+import { startsWith } from '../tools/utils'
 import { resetExperimentalFeatures, updateExperimentalFeatures } from '../domain/configuration'
 import { addFailedSendBeacon, LOCAL_STORAGE_KEY, startFlushFailedSendBeacons } from './failedSendBeacon'
 
 describe('failedSendBeacon', () => {
+  let clock: Clock
+
+  beforeEach(() => {
+    clock = mockClock()
+  })
+
   afterEach(() => {
     resetExperimentalFeatures()
     window.localStorage.clear()
+    clock.cleanup()
   })
 
   describe('when ff lower-batch-size enabled', () => {
@@ -14,32 +22,34 @@ describe('failedSendBeacon', () => {
       updateExperimentalFeatures(['lower-batch-size'])
     })
 
-    it('should flush failed sendBeacon after 2 second', () => {
-      const clock = mockClock()
-
+    it('should flush failed sendBeacon asynchronously', () => {
       addFailedSendBeacon('foo', 100)
       startFlushFailedSendBeacons()
-      clock.tick(2 * ONE_SECOND)
-      expect(window.localStorage.getItem(LOCAL_STORAGE_KEY)).toEqual(null)
-      clock.cleanup()
+      clock.tick(0)
+      expect(localStorageHasFailedBeacon()).toEqual(false)
     })
 
     it('should add failed sendBeacon', () => {
       addFailedSendBeacon('foo', 100)
-      const failedSendBeacons = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || '[]')
-      expect(failedSendBeacons.length).toEqual(1)
+      expect(localStorageHasFailedBeacon()).toEqual(true)
     })
   })
 
   describe('when ff lower-batch-size disabled', () => {
-    it('should not flush failed sendBeacon ', () => {
+    it('should not flush failed sendBeacon asynchronously', () => {
       startFlushFailedSendBeacons()
+      clock.tick(0)
       expect(spyOn(window.localStorage, 'removeItem')).not.toHaveBeenCalled()
     })
 
     it('should not add failed sendBeacon', () => {
+      window.localStorage.clear()
       addFailedSendBeacon('foo', 100)
-      expect(window.localStorage.getItem(LOCAL_STORAGE_KEY)).toEqual(null)
+      expect(localStorageHasFailedBeacon()).toEqual(false)
     })
   })
 })
+
+function localStorageHasFailedBeacon() {
+  return Object.keys(window.localStorage).some((key) => startsWith(key, LOCAL_STORAGE_KEY))
+}
--- packages/core/src/transport/failedSendBeacon.ts
+++ packages/core/src/transport/failedSendBeacon.ts
@@ -1,38 +1,43 @@
 import { isExperimentalFeatureEnabled } from '../domain/configuration'
 import { addMonitoringMessage, monitor } from '../domain/internalMonitoring'
+import { generateUUID, startsWith } from '../tools/utils'
 
 // replaced at build time
 declare const __BUILD_ENV__SDK_VERSION__: string
 
-const FAILED_SEND_BEACON_FLUSH_INTERVAL = 2000
 export const LOCAL_STORAGE_KEY = 'datadog-browser-sdk-failed-send-beacon'
 
 export function startFlushFailedSendBeacons() {
   if (!isExperimentalFeatureEnabled('lower-batch-size')) return
 
-  setInterval(monitor(flushFailedSendBeacon), FAILED_SEND_BEACON_FLUSH_INTERVAL)
+  setTimeout(monitor(flushFailedSendBeacon))
 }
 
 export function addFailedSendBeacon(endpointType: string, size: number, reason?: string) {
   if (!isExperimentalFeatureEnabled('lower-batch-size')) return
 
-  const requests = getFailedSendBeacons()
-  requests.push({
-    reason,
-    endpointType,
-    version: __BUILD_ENV__SDK_VERSION__,
-    connection: navigator.connection ? (navigator.connection as any).effectiveType : undefined,
-    onLine: navigator.onLine,
-    size,
-  })
-  window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(requests))
-}
-
-function getFailedSendBeacons(): any[] {
-  return JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || '[]') as any[]
+  window.localStorage.setItem(
+    `${LOCAL_STORAGE_KEY}-${generateUUID()}`,
+    JSON.stringify({
+      reason,
+      endpointType,
+      version: __BUILD_ENV__SDK_VERSION__,
+      connection: navigator.connection ? (navigator.connection as any).effectiveType : undefined,
+      onLine: navigator.onLine,
+      size,
+    })
+  )
 }
 
 function flushFailedSendBeacon() {
-  getFailedSendBeacons().forEach((result) => addMonitoringMessage('failed sendBeacon', result))
-  window.localStorage.removeItem(LOCAL_STORAGE_KEY)
+  const keys = Object.keys(localStorage)
+  for (const key of keys) {
+    if (startsWith(key, LOCAL_STORAGE_KEY)) {
+      const value = localStorage.getItem(key)
+      if (value) {
+        addMonitoringMessage('failed sendBeacon', JSON.parse(value))
+        window.localStorage.removeItem(key)
+      }
+    }
+  }
 }
