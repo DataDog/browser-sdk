22dd9a81971599ec2a7a217099c4ff607fbe8e31
Bastien Caudan <bastien.caudan@gmail.com>
2022-05-18 13:47:31 +0200

    🐛 Fix dependency issue

--- packages/core/src/domain/configuration/index.ts
+++ packages/core/src/domain/configuration/index.ts
@@ -5,17 +5,10 @@ export {
   DefaultPrivacyLevel,
   validateAndBuildConfiguration,
 } from './configuration'
-export {
-  createEndpointBuilder,
-  EndpointBuilder,
-  INTAKE_SITE_STAGING,
-  INTAKE_SITE_US5,
-  INTAKE_SITE_US,
-  INTAKE_SITE_US3,
-  INTAKE_SITE_EU,
-} from './endpointBuilder'
+export { createEndpointBuilder, EndpointBuilder } from './endpointBuilder'
 export {
   isExperimentalFeatureEnabled,
   updateExperimentalFeatures,
   resetExperimentalFeatures,
 } from './experimentalFeatures'
+export * from './intakeSites'
