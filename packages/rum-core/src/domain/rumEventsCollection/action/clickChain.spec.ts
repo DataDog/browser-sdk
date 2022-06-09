ef4d25b96a8d29c88ac1aca089862d0d1dbafbea
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2022-06-09 10:38:28 +0200

    🚚 [RUMF-1297] rename rageClickChain to clickChain

--- packages/rum-core/src/domain/rumEventsCollection/action/clickChain.spec.ts
+++ packages/rum-core/src/domain/rumEventsCollection/action/clickChain.spec.ts
@@ -2,17 +2,12 @@ import { Observable, ONE_SECOND, timeStampNow } from '@datadog/browser-core'
 import type { Clock } from '@datadog/browser-core/test/specHelper'
 import { mockClock, createNewEvent } from '@datadog/browser-core/test/specHelper'
 import { FrustrationType } from '../../../rawRumEvent.types'
-import type { RageClickChain } from './rageClickChain'
-import {
-  MAX_DISTANCE_BETWEEN_CLICKS,
-  MAX_DURATION_BETWEEN_CLICKS,
-  createRageClickChain,
-  isRage,
-} from './rageClickChain'
+import type { ClickChain } from './clickChain'
+import { MAX_DISTANCE_BETWEEN_CLICKS, MAX_DURATION_BETWEEN_CLICKS, createClickChain, isRage } from './clickChain'
 import type { Click } from './trackClickActions'
 
-describe('createRageClickChain', () => {
-  let clickChain: RageClickChain | undefined
+describe('createClickChain', () => {
+  let clickChain: ClickChain | undefined
   let clock: Clock
 
   beforeEach(() => {
@@ -25,7 +20,7 @@ describe('createRageClickChain', () => {
   })
 
   it('creates a click chain', () => {
-    clickChain = createRageClickChain(createFakeClick())
+    clickChain = createClickChain(createFakeClick())
     expect(clickChain).toEqual({
       tryAppend: jasmine.any(Function),
       stop: jasmine.any(Function),
@@ -33,14 +28,14 @@ describe('createRageClickChain', () => {
   })
 
   it('appends a click', () => {
-    clickChain = createRageClickChain(createFakeClick())
+    clickChain = createClickChain(createFakeClick())
     expect(clickChain.tryAppend(createFakeClick())).toBe(true)
   })
 
   describe('finalize', () => {
     it('finalizes if we try to append a non-similar click', () => {
       const firstClick = createFakeClick({ target: document.documentElement })
-      clickChain = createRageClickChain(firstClick)
+      clickChain = createClickChain(firstClick)
       firstClick.stop()
       clickChain.tryAppend(createFakeClick({ target: document.body }))
       expect(firstClick.validate).toHaveBeenCalled()
@@ -48,7 +43,7 @@ describe('createRageClickChain', () => {
 
     it('does not finalize until it waited long enough to ensure no other click can be appended', () => {
       const firstClick = createFakeClick()
-      clickChain = createRageClickChain(firstClick)
+      clickChain = createClickChain(firstClick)
       firstClick.stop()
       clock.tick(MAX_DURATION_BETWEEN_CLICKS - 1)
       expect(firstClick.validate).not.toHaveBeenCalled()
@@ -58,7 +53,7 @@ describe('createRageClickChain', () => {
 
     it('does not finalize until all clicks are stopped', () => {
       const firstClick = createFakeClick()
-      clickChain = createRageClickChain(firstClick)
+      clickChain = createClickChain(firstClick)
       clock.tick(MAX_DURATION_BETWEEN_CLICKS)
       expect(firstClick.validate).not.toHaveBeenCalled()
       firstClick.stop()
@@ -67,7 +62,7 @@ describe('createRageClickChain', () => {
 
     it('finalizes when stopping the click chain', () => {
       const firstClick = createFakeClick({ target: document.documentElement })
-      clickChain = createRageClickChain(firstClick)
+      clickChain = createClickChain(firstClick)
       firstClick.stop()
       clickChain.stop()
       expect(firstClick.validate).toHaveBeenCalled()
@@ -76,25 +71,25 @@ describe('createRageClickChain', () => {
 
   describe('clicks similarity', () => {
     it('does not accept a click if its timestamp is long after the previous one', () => {
-      clickChain = createRageClickChain(createFakeClick())
+      clickChain = createClickChain(createFakeClick())
       clock.tick(MAX_DURATION_BETWEEN_CLICKS)
       expect(clickChain.tryAppend(createFakeClick())).toBe(false)
     })
 
     it('does not accept a click if its target is different', () => {
-      clickChain = createRageClickChain(createFakeClick({ target: document.documentElement }))
+      clickChain = createClickChain(createFakeClick({ target: document.documentElement }))
       expect(clickChain.tryAppend(createFakeClick({ target: document.body }))).toBe(false)
     })
 
     it('does not accept a click if its location is far from the previous one', () => {
-      clickChain = createRageClickChain(createFakeClick({ clientX: 100, clientY: 100 }))
+      clickChain = createClickChain(createFakeClick({ clientX: 100, clientY: 100 }))
       expect(
         clickChain.tryAppend(createFakeClick({ clientX: 100, clientY: 100 + MAX_DISTANCE_BETWEEN_CLICKS + 1 }))
       ).toBe(false)
     })
 
     it('considers clicks relative to the previous one', () => {
-      clickChain = createRageClickChain(createFakeClick())
+      clickChain = createClickChain(createFakeClick())
       clock.tick(MAX_DURATION_BETWEEN_CLICKS - 1)
       clickChain.tryAppend(createFakeClick())
       clock.tick(MAX_DURATION_BETWEEN_CLICKS - 1)
@@ -105,20 +100,20 @@ describe('createRageClickChain', () => {
   describe('when rage is detected', () => {
     it('discards individual clicks', () => {
       const clicks = [createFakeClick(), createFakeClick(), createFakeClick()]
-      createValidatedRageClickChain(clicks)
+      createValidatedClickChain(clicks)
       clicks.forEach((click) => expect(click.discard).toHaveBeenCalled())
     })
 
     it('uses a clone of the first click to represent the rage click', () => {
       const clicks = [createFakeClick(), createFakeClick(), createFakeClick()]
-      createValidatedRageClickChain(clicks)
+      createValidatedClickChain(clicks)
       expect(clicks[0].clonedClick).toBeTruthy()
       expect(clicks[0].clonedClick?.validate).toHaveBeenCalled()
     })
 
     it('the rage click should have a "rage" frustration', () => {
       const clicks = [createFakeClick(), createFakeClick(), createFakeClick()]
-      createValidatedRageClickChain(clicks)
+      createValidatedClickChain(clicks)
       const expectedFrustrations = new Set()
       expectedFrustrations.add(FrustrationType.RAGE_CLICK)
       expect(clicks[0].clonedClick?.getFrustrations()).toEqual(expectedFrustrations)
@@ -127,12 +122,12 @@ describe('createRageClickChain', () => {
     it('the rage click should contains other clicks frustration', () => {
       const clicks = [createFakeClick(), createFakeClick(), createFakeClick()]
       clicks[1].addFrustration(FrustrationType.DEAD_CLICK)
-      createValidatedRageClickChain(clicks)
+      createValidatedClickChain(clicks)
       expect(clicks[0].clonedClick?.getFrustrations().has(FrustrationType.RAGE_CLICK)).toBe(true)
     })
 
-    function createValidatedRageClickChain(clicks: Click[]) {
-      clickChain = createRageClickChain(clicks[0])
+    function createValidatedClickChain(clicks: Click[]) {
+      clickChain = createClickChain(clicks[0])
       clicks.slice(1).forEach((click) => clickChain!.tryAppend(click))
       clicks.forEach((click) => click.stop())
       clock.tick(MAX_DURATION_BETWEEN_CLICKS)
