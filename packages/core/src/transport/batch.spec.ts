93e055a1bc32737e8bd0e027ae77e91f98d68b96
Aymeric <aymeric.mortemousque@datadoghq.com>
2022-06-20 14:01:46 +0200

    🔊  Clear batch before send to allow telemetry in httpRequest.send (#1594)

--- packages/core/src/transport/batch.spec.ts
+++ packages/core/src/transport/batch.spec.ts
@@ -150,4 +150,21 @@ describe('batch', () => {
 
     expect(transport.send).toHaveBeenCalledWith('{"message":"10"}\n{"message":"11"}', jasmine.any(Number), undefined)
   })
+
+  it('should be able to use telemetry in the httpRequest.send', () => {
+    const fakeRequest = {
+      send(data: string) {
+        addTelemetryDebugFake()
+        transport.send(data, BATCH_BYTES_LIMIT)
+      },
+    } as unknown as HttpRequest
+    const batch = new Batch(fakeRequest, BATCH_MESSAGES_LIMIT, BATCH_BYTES_LIMIT, MESSAGE_BYTES_LIMIT, FLUSH_TIMEOUT)
+    const addTelemetryDebugFake = () => batch.add({ message: 'telemetry message' })
+
+    batch.add({ message: 'normal message' })
+    batch.flush()
+    expect(transport.send).toHaveBeenCalledTimes(1)
+    batch.flush()
+    expect(transport.send).toHaveBeenCalledTimes(2)
+  })
 })
