6a5613b6490a0787e6b1751f6e5cc6d8926b86dc
William Lacroix <william.lacroix@datadoghq.com>
2022-07-05 15:22:42 +0100

    simplify internal context unit test

--- packages/logs/src/domain/internalContext.spec.ts
+++ packages/logs/src/domain/internalContext.spec.ts
@@ -1,38 +1,22 @@
-import type { RelativeTime } from '@datadog/browser-core'
-// Note: we are using parts of rum-core in the tests
-import { createRumSessionManagerMock } from '../../../rum-core/test/mockRumSessionManager'
-import { setup } from '../../../rum-core/test/specHelper'
-import type { TestSetupBuilder } from '../../../rum-core/test/specHelper'
-
-import type { LogsSessionManager } from './logsSessionManager'
 import { startInternalContext } from './internalContext'
 
 describe('internal context', () => {
-  let setupBuilder: TestSetupBuilder
-  let findSessionSpy: jasmine.Spy<LogsSessionManager['findTrackedSession']>
-  let internalContext: ReturnType<typeof startInternalContext>
-
-  beforeEach(() => {
-    setupBuilder = setup()
-      .withSessionManager(createRumSessionManagerMock().setId('456'))
-      .beforeBuild(({ sessionManager }) => {
-        findSessionSpy = spyOn(sessionManager, 'findTrackedSession').and.callThrough()
-        internalContext = startInternalContext(sessionManager)
-      })
-  })
-
-  afterEach(() => {
-    setupBuilder.cleanup()
-  })
-
-  it("should return undefined if the session isn't tracked", () => {
-    setupBuilder.withSessionManager(createRumSessionManagerMock().setNotTracked()).build()
-    expect(internalContext.get()).toEqual(undefined)
+  it('should return undefined if sessionManager cannot find session', () => {
+    const sessionManagerMock = {
+      findTrackedSession: () => undefined,
+    }
+    expect(startInternalContext(sessionManagerMock).get()).toEqual(undefined)
   })
 
   it('should return internal context corresponding to startTime', () => {
-    setupBuilder.build()
-    internalContext.get(123)
-    expect(findSessionSpy).toHaveBeenCalledWith(123 as RelativeTime)
+    const sessionIdMock = '123'
+    const sessionManagerMock = {
+      findTrackedSession: () => ({
+        id: sessionIdMock,
+      }),
+    }
+    expect(startInternalContext(sessionManagerMock).get()).toEqual({
+      session_id: sessionIdMock,
+    })
   })
 })
