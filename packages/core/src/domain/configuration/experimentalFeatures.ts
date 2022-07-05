87add9dff29eef409a08abfd457aaed80924cfd0
Gitlab staging reset job <aymeric.mortemousque@datadoghq.com>
2022-07-05 16:11:11 +0200

    👌 fix empty set

--- packages/core/src/domain/configuration/experimentalFeatures.ts
+++ packages/core/src/domain/configuration/experimentalFeatures.ts
@@ -5,7 +5,7 @@
  * So keep in mind that in certain configurations, your experimental feature flag may affect other products.
  */
 
-let enabledExperimentalFeatures: Set<string>
+let enabledExperimentalFeatures: Set<string> | undefined
 
 export function updateExperimentalFeatures(enabledFeatures: string[] | undefined): void {
   // Safely handle external data
@@ -20,7 +20,7 @@ export function updateExperimentalFeatures(enabledFeatures: string[] | undefined
   enabledFeatures
     .filter((flag) => typeof flag === 'string')
     .forEach((flag: string) => {
-      enabledExperimentalFeatures.add(flag)
+      enabledExperimentalFeatures!.add(flag)
     })
 }
 
@@ -33,5 +33,5 @@ export function resetExperimentalFeatures(): void {
 }
 
 export function getExperimentalFeatures(): Set<string> {
-  return enabledExperimentalFeatures || []
+  return enabledExperimentalFeatures || new Set()
 }
