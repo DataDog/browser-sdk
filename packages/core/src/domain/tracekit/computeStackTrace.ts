ec929311d9192d7531a070fceb74535a2d53682c
William Lacroix <william.lacroix@datadoghq.com>
2022-07-26 10:55:52 +0100

    compose chrome regex into strings

--- packages/core/src/domain/tracekit/computeStackTrace.ts
+++ packages/core/src/domain/tracekit/computeStackTrace.ts
@@ -29,12 +29,14 @@ export function computeStackTrace(ex: unknown): StackTrace {
     stack,
   }
 }
-
+const url = '((?:file|https?|blob|chrome-extension|native|eval|webpack|<anonymous>|\\w+\\.|\\/).*?)'
+const position = '(?::(\\d+))'
 const CHROME_LINE_RE =
-  // eslint-disable-next-line max-len
-  /^\s*at (.*?) ?\(((?:file|https?|blob|chrome-extension|native|eval|webpack|<anonymous>|\w+\.|\/).*?)(?::(\d+))?(?::(\d+))?\)?\s*$/i
+  // eslint-disable-next-line local-rules/disallow-side-effects
+  new RegExp(`^\\s*at (.*?) ?\\(${url}${position}?${position}?\\)?\\s*$`, 'i')
 
-const CHROME_EVAL_RE = /\((\S*)(?::(\d+))(?::(\d+))\)/
+// eslint-disable-next-line local-rules/disallow-side-effects
+const CHROME_EVAL_RE = new RegExp(`\\((\\S*)${position}${position}\\)`)
 
 function parseChromeLine(line: string): StackFrame | undefined {
   const parts = CHROME_LINE_RE.exec(line)
@@ -64,8 +66,8 @@ function parseChromeLine(line: string): StackFrame | undefined {
 }
 
 const CHROME_ANONYMOUS_FUNCTION_RE =
-  // eslint-disable-next-line max-len
-  /^\s*at ?((?:file|https?|blob|chrome-extension|native|eval|webpack|<anonymous>|\w+\.|\/).*?)(?::(\d+))?(?::(\d+))??\s*$/i
+  // eslint-disable-next-line local-rules/disallow-side-effects
+  new RegExp(`^\\s*at ?${url}${position}?${position}??\\s*$`, 'i')
 
 function parseChromeAnonymousLine(line: string): StackFrame | undefined {
   const parts = CHROME_ANONYMOUS_FUNCTION_RE.exec(line)
