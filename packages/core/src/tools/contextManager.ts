b4061467b950e9c28948b5a6b1b42a157db5784d
William Lacroix <william.lacroix@datadoghq.com>
2022-07-27 17:22:02 +0200

    extend APIs of ContextManager

--- packages/core/src/tools/contextManager.ts
+++ packages/core/src/tools/contextManager.ts
@@ -1,21 +1,53 @@
+import { deepClone } from './utils'
+
 import type { Context, ContextValue } from './context'
 
 export function createContextManager() {
   let context: Context = {}
 
   return {
+    /**
+     * @deprecated use getContext instead
+     */
     get: () => context,
 
+    /**
+     * @deprecated use setContextProperty instead
+     */
     add: (key: string, value: any) => {
       context[key] = value as ContextValue
     },
 
+    /**
+     * @deprecated use removeContextProperty instead
+     */
     remove: (key: string) => {
       delete context[key]
     },
 
+    /**
+     * @deprecated use setContext instead
+     */
     set: (newContext: object) => {
       context = newContext as Context
+    },
+
+    getContext: () => deepClone(context),
+
+    setContext: (newContext: Context) => {
+      context = deepClone(newContext)
+    },
+
+    setContextProperty: (key: string, property: any) => {
+      context[key] = deepClone(property)
+    },
+
+    removeContextProperty: (key: string) => {
+      delete context[key]
+    },
+
+    clearContext: () => {
+      context = {}
     },
   }
 }
