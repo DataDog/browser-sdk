570cc2db73807eb40881e4000c3705c660c13e7b
Bastien Caudan <bastien.caudan@gmail.com>
2022-08-11 12:38:43 +0200

    🐛 when not available try to find document scrolling after scroll

--- packages/rum/src/domain/record/elementsScrollPositions.ts
+++ packages/rum/src/domain/record/elementsScrollPositions.ts
@@ -1,19 +1,24 @@
 import { addTelemetryDebug } from '@datadog/browser-core'
+import { forEach } from './utils'
 
 export type ElementsScrollPositions = ReturnType<typeof createElementsScrollPositions>
 export type ScrollPositions = { scrollLeft: number; scrollTop: number }
 
 export function createElementsScrollPositions() {
   const scrollPositionsByElement = new WeakMap<Element, ScrollPositions>()
-  const documentScrollingElement = document.scrollingElement!
-  if (!documentScrollingElement) {
-    addTelemetryDebug('document without scrollingElement')
-  }
+  let documentScrollingElement = document.scrollingElement
   return {
     set(element: Element | Document, scrollPositions: ScrollPositions) {
+      if (element === document && !documentScrollingElement) {
+        documentScrollingElement = findDocumentScrollingElement(document.childNodes, scrollPositions)
+        if (!documentScrollingElement) {
+          addTelemetryDebug('unable to find document scrolling element')
+          return
+        }
+      }
       try {
         scrollPositionsByElement.set(
-          element === document ? documentScrollingElement : (element as Element),
+          element === document ? documentScrollingElement! : (element as Element),
           scrollPositions
         )
       } catch (e) {
@@ -27,4 +32,22 @@ export function createElementsScrollPositions() {
       return scrollPositionsByElement.has(element)
     },
   }
+}
+
+function findDocumentScrollingElement(childNodes: NodeList, scrollPositions: ScrollPositions) {
+  let scrollingElement: Element | null = null
+  forEach(childNodes, (node) => {
+    if (!scrollingElement && node.nodeType === node.ELEMENT_NODE) {
+      const element = node as Element
+      if (
+        Math.round(element.scrollTop) === scrollPositions.scrollTop &&
+        Math.round(element.scrollLeft) === scrollPositions.scrollLeft
+      ) {
+        scrollingElement = element
+      } else {
+        findDocumentScrollingElement(element.childNodes, scrollPositions)
+      }
+    }
+  })
+  return scrollingElement
 }
