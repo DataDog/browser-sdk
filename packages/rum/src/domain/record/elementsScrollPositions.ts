fba33d20b751d315bf4af54035f6053418aff5ec
Bastien Caudan <bastien.caudan@gmail.com>
2022-08-11 17:03:04 +0200

    🐛 [RUMF-1344] try to improve document scrolling element handling (#1688)
    * 🔊 [RUMF-1344] Add extra context to scrollPositionsByElement failures
    
    * 🐛 [RUMF-1344] try to improve document scrolling element handling
    
    * add extra debug log when no scrolling element

--- packages/rum/src/domain/record/elementsScrollPositions.ts
+++ packages/rum/src/domain/record/elementsScrollPositions.ts
@@ -5,16 +5,23 @@ export type ScrollPositions = { scrollLeft: number; scrollTop: number }
 
 export function createElementsScrollPositions() {
   const scrollPositionsByElement = new WeakMap<Element, ScrollPositions>()
-  const documentScrollingElement = document.scrollingElement!
-  if (!documentScrollingElement) {
-    addTelemetryDebug('document without scrollingElement')
-  }
+  let documentScrollingElement: Element | null
   return {
     set(element: Element | Document, scrollPositions: ScrollPositions) {
-      scrollPositionsByElement.set(
-        element === document ? documentScrollingElement : (element as Element),
-        scrollPositions
-      )
+      if (element === document && !documentScrollingElement) {
+        documentScrollingElement = tryToFindScrollingElement(scrollPositions)
+        if (!documentScrollingElement) {
+          return
+        }
+      }
+      try {
+        scrollPositionsByElement.set(
+          element === document ? documentScrollingElement! : (element as Element),
+          scrollPositions
+        )
+      } catch (e) {
+        addTelemetryDebug(`invalid element: ${String(element)}`)
+      }
     },
     get(element: Element) {
       return scrollPositionsByElement.get(element)
@@ -23,4 +30,23 @@ export function createElementsScrollPositions() {
       return scrollPositionsByElement.has(element)
     },
   }
+}
+
+function tryToFindScrollingElement(scrollPositions: ScrollPositions) {
+  if (document.scrollingElement) {
+    return document.scrollingElement
+  }
+  addTelemetryDebug('null document scrolling element')
+  if (scrollPositions.scrollLeft === 0 && scrollPositions.scrollTop === 0) {
+    addTelemetryDebug('Unable to find scrolling element for scroll (0,0)')
+    return null
+  }
+  if (
+    Math.round(document.documentElement.scrollLeft) === scrollPositions.scrollLeft &&
+    Math.round(document.documentElement.scrollTop) === scrollPositions.scrollTop
+  ) {
+    return document.documentElement
+  }
+  addTelemetryDebug('Unable to find scrolling element')
+  return null
 }
