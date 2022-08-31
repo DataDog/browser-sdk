51452a395d3e2e3aea8d3d0779dfed3d5fa37c7c
Bastien Caudan <bastien.caudan@gmail.com>
2022-08-31 10:23:02 +0200

    👌 fix

--- packages/core/src/transport/httpRequest.ts
+++ packages/core/src/transport/httpRequest.ts
@@ -34,7 +34,7 @@ export function createHttpRequest(endpointBuilder: EndpointBuilder, bytesLimit: 
 
   function fetchKeepAliveStrategy(data: string | FormData, bytesCount: number) {
     const url = endpointBuilder.build()
-    const canUseKeepAlive = isKeepAliveSupported(url) && bytesCount < bytesLimit
+    const canUseKeepAlive = isKeepAliveSupported() && bytesCount < bytesLimit
     if (canUseKeepAlive) {
       fetch(url, { method: 'POST', body: data, keepalive: true }).catch(
         monitor(() => {
@@ -47,13 +47,10 @@ export function createHttpRequest(endpointBuilder: EndpointBuilder, bytesLimit: 
     }
   }
 
-  /**
-   * Request can throw
-   * cf https://developer.mozilla.org/en-US/docs/Web/API/Request/Request#errors
-   */
-  function isKeepAliveSupported(url: string) {
+  function isKeepAliveSupported() {
+    // Request can throw, cf https://developer.mozilla.org/en-US/docs/Web/API/Request/Request#errors
     try {
-      return window.Request && 'keepalive' in new Request(url)
+      return window.Request && 'keepalive' in new Request('http://a')
     } catch {
       return false
     }
