3ba85c607a19618c3710c77f25ec4d731109a0c8
Bastien Caudan <bastien.caudan@datadoghq.com>
2022-09-28 15:41:57 +0200

    🐛 fix `lerna: command not found`

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
 export NPM_TOKEN=$(aws ssm get-parameter --region us-east-1 --name ci.browser-sdk.npm_token --with-decryption --query "Parameter.Value" --out text)
 BUILD_MODE=release yarn build
-lerna publish from-package
+yarn lerna publish from-package
