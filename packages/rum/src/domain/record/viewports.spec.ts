fccab7de513a09e1be11c0aec36b9e40f44e3feb
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2022-10-27 15:37:09 +0200

    ✅ fix flaky test caused by an async scroll event

--- packages/rum/src/domain/record/viewports.spec.ts
+++ packages/rum/src/domain/record/viewports.spec.ts
@@ -1,3 +1,4 @@
+import { addEventListener, DOM_EVENT } from '@datadog/browser-core'
 import { getScrollX, getScrollY } from './viewports'
 
 function isMobileSafari12() {
@@ -5,14 +6,27 @@ function isMobileSafari12() {
 }
 
 describe('layout viewport', () => {
+  let shouldWaitForWindowScrollEvent: boolean
+
   const addVerticalScrollBar = () => {
     document.body.style.setProperty('margin-bottom', '5000px')
   }
 
-  afterEach(() => {
+  beforeEach(() => {
+    shouldWaitForWindowScrollEvent = false
+  })
+
+  afterEach((done) => {
     document.body.style.removeProperty('margin-bottom')
-    document.body.style.removeProperty('margin-right')
     window.scrollTo(0, 0)
+
+    // Those tests are triggering asynchronous scroll events that might impact tests run after them.
+    // To avoid that, we wait for the next scroll event before continuing to the next one.
+    if (shouldWaitForWindowScrollEvent) {
+      addEventListener(window, DOM_EVENT.SCROLL, done, { passive: true, once: true, capture: true })
+    } else {
+      done()
+    }
   })
 
   describe('getScrollX/Y', () => {
@@ -23,6 +37,7 @@ describe('layout viewport', () => {
       expect(getScrollX()).toBe(window.scrollX || window.pageXOffset)
       expect(getScrollY()).toBe(window.scrollY || window.pageYOffset)
     })
+
     it('normalized scroll updates when scrolled', () => {
       if (isMobileSafari12()) {
         // Mobile Safari 12 doesn't support scrollTo() within an iframe
@@ -32,7 +47,10 @@ describe('layout viewport', () => {
       }
       addVerticalScrollBar()
       const SCROLL_DOWN_PX = 100
+
       window.scrollTo(0, SCROLL_DOWN_PX)
+      shouldWaitForWindowScrollEvent = true
+
       expect(getScrollX()).toBe(0)
       expect(getScrollY()).toBe(100)
       expect(getScrollX()).toBe(window.scrollX || window.pageXOffset)
