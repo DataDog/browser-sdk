25feb3adac7d935833b3f9d482f322735fb5aa74
William Lacroix <william.lacroix@datadoghq.com>
2022-10-28 13:18:54 +0200

    ✅ add e2e tests to check performance entries are pulled through

--- test/e2e/scenario/rum/resources.scenario.ts
+++ test/e2e/scenario/rum/resources.scenario.ts
@@ -196,6 +196,72 @@ describe('rum resources', () => {
       })
   })
 
+  describe('fetch should have associated performance entry', () => {
+    createTest('track fetch performance entries')
+      .withRum()
+      .withSetup(bundleSetup)
+      .run(async ({ serverEvents }) => {
+        await browserExecuteAsync((done) => {
+          fetch('/ok').then(
+            () => done(undefined),
+            () => {
+              throw Error('Issue with fetch call')
+            }
+          )
+        })
+
+        await flushEvents()
+
+        const resourceEvent = serverEvents.rumResources.find((event) => event.resource.type === 'fetch')
+
+        expect(resourceEvent?.resource.size).toBeDefined()
+        expect(resourceEvent?.resource.download).toBeDefined()
+        expect(resourceEvent?.resource.first_byte).toBeDefined()
+      })
+  })
+
+  createTest('track redirect fetch timings')
+    .withRum()
+    .run(async ({ serverEvents }) => {
+      await browserExecuteAsync((done) => {
+        fetch(`/redirect?duration=${200}`).then(
+          () => done(undefined),
+          () => {
+            throw Error('Issue with fetch call')
+          }
+        )
+      })
+      await flushEvents()
+      const resourceEvent = serverEvents.rumResources.find((r) => r.resource.url.includes('/redirect'))!
+      expect(resourceEvent).not.toBeUndefined()
+      expect(resourceEvent.resource.method).toEqual('GET')
+      expect(resourceEvent.resource.status_code).toEqual(200)
+      expectToHaveValidTimings(resourceEvent)
+      expect(resourceEvent.resource.redirect).not.toBeUndefined()
+      expect(resourceEvent.resource.redirect!.duration).toBeGreaterThan(0)
+    })
+
+  createTest('track dns lookup fetch timings')
+    .withRum()
+    .run(async ({ serverEvents }) => {
+      await browserExecuteAsync((done) => {
+        fetch(`/redirect?duration=${200}`).then(
+          () => done(undefined),
+          () => {
+            throw Error('Issue with fetch call')
+          }
+        )
+      })
+      await flushEvents()
+      const resourceEvent = serverEvents.rumResources.find((r) => r.resource.url.includes('/redirect'))!
+      expect(resourceEvent).not.toBeUndefined()
+      expect(resourceEvent.resource.method).toEqual('GET')
+      expect(resourceEvent.resource.status_code).toEqual(200)
+      expectToHaveValidTimings(resourceEvent)
+      expect(resourceEvent.resource.redirect).not.toBeUndefined()
+      expect(resourceEvent.resource.redirect!.duration).toBeGreaterThan(0)
+    })
+
   describe('support XHRs with same XMLHttpRequest instance', () => {
     createTest('track XHRs when calling requests one after another')
       .withRum()
