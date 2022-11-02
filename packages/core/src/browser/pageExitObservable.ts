a1966d72b03c2545f1ecfb2d4dd90f9c1b42e456
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2022-11-02 17:26:02 +0100

    👌 use capture for the visibilitychange event

--- packages/core/src/browser/pageExitObservable.ts
+++ packages/core/src/browser/pageExitObservable.ts
@@ -16,11 +16,17 @@ export function createPageExitObservable(): Observable<PageExitEvent> {
      * Only event that guarantee to fire on mobile devices when the page transitions to background state
      * (e.g. when user switches to a different application, goes to homescreen, etc), or is being unloaded.
      */
-    const { stop: stopVisibilityChangeListener } = addEventListener(document, DOM_EVENT.VISIBILITY_CHANGE, () => {
-      if (document.visibilityState === 'hidden') {
-        observable.notify({ reason: PageExitReason.HIDDEN })
-      }
-    })
+    const { stop: stopVisibilityChangeListener } = addEventListener(
+      document,
+      DOM_EVENT.VISIBILITY_CHANGE,
+      () => {
+        if (document.visibilityState === 'hidden') {
+          observable.notify({ reason: PageExitReason.HIDDEN })
+        }
+      },
+      { capture: true }
+    )
+
     /**
      * Safari does not support yet to send a request during:
      * - a visibility change during doc unload (cf: https://bugs.webkit.org/show_bug.cgi?id=194897)
