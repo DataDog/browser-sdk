f4431e327722fa52a035e4fc8828bcf444249de6
Bastien Caudan <bastien.caudan@datadoghq.com>
2022-11-09 17:59:05 +0100

    ♻️ extract some tests specific to endpointBuilder

--- packages/core/src/domain/configuration/transportConfiguration.spec.ts
+++ packages/core/src/domain/configuration/transportConfiguration.spec.ts
@@ -1,12 +1,7 @@
-import type { BuildEnvWindow } from '../../../test/specHelper'
 import { computeTransportConfiguration } from './transportConfiguration'
 
 describe('transportConfiguration', () => {
   const clientToken = 'some_client_token'
-
-  beforeEach(() => {
-    ;(window as unknown as BuildEnvWindow).__BUILD_ENV__SDK_VERSION__ = 'some_version'
-  })
 
   describe('site', () => {
     it('should use US site by default', () => {
@@ -22,43 +17,9 @@ describe('transportConfiguration', () => {
     })
   })
 
-  describe('query parameters', () => {
-    it('should add intake query parameters', () => {
-      const configuration = computeTransportConfiguration({ clientToken })
-      expect(configuration.rumEndpointBuilder.build()).toMatch(
-        `&dd-api-key=${clientToken}&dd-evp-origin-version=(.*)&dd-evp-origin=browser&dd-request-id=(.*)`
-      )
-    })
-
-    it('should add batch_time for rum endpoint', () => {
-      const configuration = computeTransportConfiguration({ clientToken })
-      expect(configuration.rumEndpointBuilder.build()).toContain('&batch_time=')
-    })
-
-    it('should not add batch_time for logs and replay endpoints', () => {
-      const configuration = computeTransportConfiguration({ clientToken })
-      expect(configuration.logsEndpointBuilder.build()).not.toContain('&batch_time=')
-      expect(configuration.sessionReplayEndpointBuilder.build()).not.toContain('&batch_time=')
-    })
-  })
-
-  describe('proxyUrl', () => {
-    it('should replace the full intake endpoint by the proxyUrl and set it in the attribute ddforward', () => {
-      const configuration = computeTransportConfiguration({ clientToken, proxyUrl: 'https://proxy.io/path' })
-      expect(configuration.rumEndpointBuilder.build()).toMatch(
-        `https://proxy.io/path\\?ddforward=${encodeURIComponent(
-          `https://rum.browser-intake-datadoghq.com/api/v2/rum?ddsource=(.*)&ddtags=(.*)&dd-api-key=${clientToken}` +
-            '&dd-evp-origin-version=(.*)&dd-evp-origin=browser&dd-request-id=(.*)&batch_time=(.*)'
-        )}`
-      )
-    })
-  })
-
   describe('sdk_version, env, version and service', () => {
     it('should not modify the logs and rum endpoints tags when not defined', () => {
       const configuration = computeTransportConfiguration({ clientToken })
-      expect(decodeURIComponent(configuration.rumEndpointBuilder.build())).toContain('&ddtags=sdk_version:some_version')
-
       expect(decodeURIComponent(configuration.rumEndpointBuilder.build())).not.toContain(',env:')
       expect(decodeURIComponent(configuration.rumEndpointBuilder.build())).not.toContain(',service:')
       expect(decodeURIComponent(configuration.rumEndpointBuilder.build())).not.toContain(',version:')
@@ -72,25 +33,8 @@ describe('transportConfiguration', () => {
 
     it('should be set as tags in the logs and rum endpoints', () => {
       const configuration = computeTransportConfiguration({ clientToken, env: 'foo', service: 'bar', version: 'baz' })
-      expect(decodeURIComponent(configuration.rumEndpointBuilder.build())).toContain(
-        '&ddtags=sdk_version:some_version,env:foo,service:bar,version:baz'
-      )
-      expect(decodeURIComponent(configuration.logsEndpointBuilder.build())).toContain(
-        '&ddtags=sdk_version:some_version,env:foo,service:bar,version:baz'
-      )
-    })
-  })
-
-  describe('tags', () => {
-    it('should be encoded', () => {
-      const configuration = computeTransportConfiguration({
-        clientToken,
-        service: 'bar:foo',
-        datacenter: 'us1.prod.dog',
-      })
-      expect(configuration.rumEndpointBuilder.build()).toContain(
-        'ddtags=sdk_version%3Asome_version%2Cservice%3Abar%3Afoo%2Cdatacenter%3Aus1.prod.dog'
-      )
+      expect(decodeURIComponent(configuration.rumEndpointBuilder.build())).toContain('env:foo,service:bar,version:baz')
+      expect(decodeURIComponent(configuration.logsEndpointBuilder.build())).toContain('env:foo,service:bar,version:baz')
     })
   })
 
