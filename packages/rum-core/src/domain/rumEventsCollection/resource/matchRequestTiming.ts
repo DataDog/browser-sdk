df9c12f7bd3302c9e315d4dde9cf36271f867673
William Lacroix <william.lacroix@datadoghq.com>
2022-11-10 13:07:12 +0000

    💡 remove comment about options preflight

--- packages/rum-core/src/domain/rumEventsCollection/resource/matchRequestTiming.ts
+++ packages/rum-core/src/domain/rumEventsCollection/resource/matchRequestTiming.ts
@@ -15,12 +15,10 @@ interface Timing {
  * Observations:
  * - Timing (start, end) are nested inside the request (start, end)
  * - Some timing can be not exactly nested, being off by < 1 ms
- * - Browsers generate a timing entry for OPTIONS request
  *
  * Strategy:
  * - from valid nested entries (with 1 ms error margin)
  * - if a single timing match, return the timing
- * - if two following timings match (OPTIONS request), return the timing for the actual request
  * - otherwise we can't decide, return undefined
  */
 export function matchRequestTiming(request: RequestCompleteEvent) {
