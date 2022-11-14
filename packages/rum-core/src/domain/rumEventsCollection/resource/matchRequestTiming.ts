150a7c62b491da4805e6d01d17004731949b70b0
William Lacroix <liywjl@users.noreply.github.com>
2022-11-14 13:03:20 +0000

    🗑️ [RUMF-1433] Remove Preflight request Performance Entry check (#1813)
    * 🗑️ [RUMF-1433] Remove Preflight request Performance Entry check
    
    * 💡 remove comment about options preflight

--- packages/rum-core/src/domain/rumEventsCollection/resource/matchRequestTiming.ts
+++ packages/rum-core/src/domain/rumEventsCollection/resource/matchRequestTiming.ts
@@ -15,12 +15,10 @@ interface Timing {
  * Observations:
  * - Timing (start, end) are nested inside the request (start, end)
  * - Some timing can be not exactly nested, being off by < 1 ms
- * - Browsers generate a timing entry for OPTIONS request
  *
  * Strategy:
  * - from valid nested entries (with 1 ms error margin)
  * - if a single timing match, return the timing
- * - if two following timings match (OPTIONS request), return the timing for the actual request
  * - otherwise we can't decide, return undefined
  */
 export function matchRequestTiming(request: RequestCompleteEvent) {
@@ -48,15 +46,7 @@ export function matchRequestTiming(request: RequestCompleteEvent) {
     return candidates[0]
   }
 
-  if (candidates.length === 2 && firstCanBeOptionRequest(candidates)) {
-    return candidates[1]
-  }
-
   return
-}
-
-function firstCanBeOptionRequest(correspondingEntries: RumPerformanceResourceTiming[]) {
-  return endTime(correspondingEntries[0]) <= correspondingEntries[1].startTime
 }
 
 function endTime(timing: Timing) {
