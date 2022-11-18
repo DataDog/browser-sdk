f551178e4f2125f6e55b5deb85c0cb06d472fb86
William Lacroix <liywjl@users.noreply.github.com>
2022-11-18 15:44:20 +0000

    ✨ Auto Flush for SDK extension (#1824)
    * ✨ auto flush for SDK extension
    
    * ♻️ use useInterval hook from usehooks-ts
    
    * ♻️ useState for autoFlush boolean instead of store
    
    * ♻️ remove usehooks-ts and use native hook instead

--- developer-extension/src/panel/components/actionsBar.tsx
+++ developer-extension/src/panel/components/actionsBar.tsx
@@ -1,64 +1,62 @@
 import { Group, Checkbox, Badge, Button } from '@mantine/core'
-import React from 'react'
+import React, { useState } from 'react'
 import { evalInWindow } from '../evalInWindow'
+import { flushEvents } from '../flushEvents'
 import { useStore } from '../hooks/useStore'
+import { useAutoFlushEvents } from '../hooks/useAutoFlushEvents'
 
 export function ActionsBar() {
   const [{ useDevBundles, useRumSlim, devServerStatus, blockIntakeRequests }, setStore] = useStore()
+  const [autoFlush, setAutoFlush] = useState<boolean>(true)
+
+  useAutoFlushEvents(autoFlush)
+
   return (
     <Group>
-      <Group spacing="sm">
+      <Group>
+        <Group spacing="sm">
+          <Checkbox
+            label="Use dev bundles"
+            checked={useDevBundles}
+            onChange={(e) => setStore({ useDevBundles: isChecked(e.target) })}
+            color="violet"
+          />
+          {devServerStatus === 'available' ? (
+            <Badge color="green">Available</Badge>
+          ) : devServerStatus === 'checking' ? (
+            <Badge color="yellow">Checking...</Badge>
+          ) : (
+            <Badge color="red">Unavailable</Badge>
+          )}
+        </Group>
         <Checkbox
-          label="Use dev bundles"
-          checked={useDevBundles}
-          onChange={(e) => setStore({ useDevBundles: isChecked(e.target) })}
+          label="Use RUM Slim"
+          checked={useRumSlim}
+          onChange={(e) => setStore({ useRumSlim: isChecked(e.target) })}
           color="violet"
         />
-        {devServerStatus === 'available' ? (
-          <Badge color="green">Available</Badge>
-        ) : devServerStatus === 'checking' ? (
-          <Badge color="yellow">Checking...</Badge>
-        ) : (
-          <Badge color="red">Unavailable</Badge>
-        )}
+        <Checkbox
+          label="Block intake requests"
+          checked={blockIntakeRequests}
+          onChange={(e) => setStore({ blockIntakeRequests: isChecked(e.target) })}
+          color="violet"
+        />
       </Group>
-      <Checkbox
-        label="Use RUM Slim"
-        checked={useRumSlim}
-        onChange={(e) => setStore({ useRumSlim: isChecked(e.target) })}
-        color="violet"
-      />
-      <Checkbox
-        label="Block intake requests"
-        checked={blockIntakeRequests}
-        onChange={(e) => setStore({ blockIntakeRequests: isChecked(e.target) })}
-        color="violet"
-      />
-      <Button color="violet" variant="light" compact onClick={() => flushEvents()}>
-        Flush buffered events
-      </Button>
-      <Button color="violet" variant="light" compact onClick={() => endSession()}>
-        End current session
-      </Button>
+      <Group>
+        <Checkbox label="Auto Flush" checked={autoFlush} onChange={() => setAutoFlush((b) => !b)} color="violet" />
+        <Button color="violet" variant="light" compact onClick={() => flushEvents()}>
+          Flush buffered events
+        </Button>
+        <Button color="violet" variant="light" compact onClick={() => endSession()}>
+          End current session
+        </Button>
+      </Group>
     </Group>
   )
 }
 
 function isChecked(target: EventTarget) {
   return target instanceof HTMLInputElement && target.checked
-}
-
-function flushEvents() {
-  evalInWindow(
-    `
-      const descriptor = Object.getOwnPropertyDescriptor(Document.prototype, 'visibilityState')
-      console.log(descriptor)
-      Object.defineProperty(Document.prototype, 'visibilityState', { value: 'hidden' })
-      document.dispatchEvent(new Event('visibilitychange', { bubbles: true }))
-      Object.defineProperty(Document.prototype, 'visibilityState', descriptor)
-      document.dispatchEvent(new Event('visibilitychange', { bubbles: true }))
-    `
-  ).catch((error) => console.error('Error while flushing events:', error))
 }
 
 function endSession() {
