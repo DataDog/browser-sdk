dd9b5e877534adfe82205b2a65d0de1f61d0a94f
Benoît <benoit.zugmeyer@datadoghq.com>
2022-12-13 14:31:52 +0100

    👌 clearer documentation
    Co-authored-by: William Lacroix <liywjl@users.noreply.github.com>

--- packages/rum-core/src/domain/rumEventsCollection/view/trackViewEventCounts.ts
+++ packages/rum-core/src/domain/rumEventsCollection/view/trackViewEventCounts.ts
@@ -2,10 +2,13 @@ import { ONE_MINUTE } from '@datadog/browser-core'
 import type { LifeCycle } from '../../lifeCycle'
 import { trackEventCounts } from '../../trackEventCounts'
 
-// Arbitrary delay for stopping event counting after the view ends. Ideally, we would not stop and
-// keep counting events until the end of the session. But this might have a small performance impact
-// if there are many many views: we would need to go through each event to see if the related view
-// matches. So let's have a fairly short delay to avoid impacting performances too much.
+// Some events are not being counted as they transcend views. To reduce the occurrence; 
+// an arbitrary delay is added for stopping event counting after the view ends. 
+// 
+// Ideally, we would not stop and keep counting events until the end of the session.
+// But this might have a small performance impact if there are many many views: 
+// we would need to go through each event to see if the related view matches.
+// So let's have a fairly short delay to avoid impacting performances too much.
 //
 // In the future, we could have views stored in a data structure similar to ContextHistory. Whenever
 // a child event is collected, we could look into this history to find the matching view and
