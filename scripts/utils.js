0f29a00dbe61ec73f3cd05803ed3e6d4c478cb55
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2023-01-03 16:49:32 +0100

    ✨ [RUMF-1330] introduce the `command` helper

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,6 +2,7 @@ const util = require('util')
 const path = require('path')
 const fsPromises = require('fs/promises')
 const fs = require('fs')
+const childProcess = require('child_process')
 const execute = util.promisify(require('child_process').exec)
 const spawn = require('child_process').spawn
 // node-fetch v3.x only support ESM syntax.
@@ -85,6 +86,87 @@ async function spawnCommand(command, args) {
   })
 }
 
+/**
+ * Helper to run executables. This has been introduced to work around Shell injections[0] while
+ * keeping a good developer experience. The template string is split on white spaces and passed to
+ * a child_process method that don't use a shell.
+ *
+ * Prefer this helper over using other child_process methods. Avoid using `child_process.exec`,
+ * `child_process.execSync` or the `shell` option of other `child_process` functions.
+ *
+ * Use this as a tagged string template. Example:
+ *
+ * > command`git ls-files ${directory}`.run()
+ *
+ * [0]: https://matklad.github.io/2021/07/30/shell-injection.html
+ */
+function command(...templateArguments) {
+  const [commandName, ...commandArguments] = parseCommandArguments(...templateArguments)
+
+  let input = ''
+  let env
+
+  return {
+    withInput(newInput) {
+      input = newInput
+      return this
+    },
+
+    withEnvironment(newEnv) {
+      env = newEnv
+      return this
+    },
+
+    run() {
+      const commandResult = childProcess.spawnSync(commandName, commandArguments, {
+        input,
+        env,
+        encoding: 'utf-8',
+      })
+
+      if (commandResult.status !== 0) {
+        const formattedCommand = `${commandName} ${commandArguments.join(' ')}`
+        const formattedStderr = `----\n${commandResult.stderr || ''}\n----`
+        const exitCause = commandResult.signal ? `signal ${commandResult.signal}` : `status ${commandResult.status}`
+        const error = new Error(`Command failed with exit ${exitCause}: ${formattedCommand}\n${formattedStderr}`)
+        throw error
+      }
+
+      if (commandResult.stderr) {
+        printError(commandResult.stderr)
+      }
+
+      return commandResult.stdout
+    },
+  }
+}
+
+function parseCommandArguments(templateStrings, ...templateVariables) {
+  let parsedArguments = []
+  for (let i = 0; i < templateStrings.length; i += 1) {
+    if (i > 0) {
+      // Interleave variables with template strings
+      if (!parsedArguments.length || templateStrings[i - 1].match(/\s$/)) {
+        // If the latest string ends with a space, consider the variable as a separate argument
+        parsedArguments.push(templateVariables[i - 1])
+      } else {
+        // Else, append the variable to the latest argument
+        parsedArguments[parsedArguments.length - 1] += templateVariables[i - 1]
+      }
+    }
+
+    const words = templateStrings[i].split(/\s+/).filter((word) => word)
+
+    if (parsedArguments.length && words.length && !templateStrings[i].match(/^\s/)) {
+      // If the string does not start with a space, append it to the latest argument
+      parsedArguments[parsedArguments.length - 1] += words.shift()
+    }
+
+    parsedArguments.push(...words)
+  }
+  return parsedArguments
+}
+
 function runMain(mainFunction) {
   Promise.resolve()
     // The main function can be either synchronous or asynchronous, so let's wrap it in an async
@@ -137,6 +219,7 @@ module.exports = {
   CI_FILE,
   getSecretKey,
   initGitConfig,
+  command,
   executeCommand,
   spawnCommand,
   printError,
