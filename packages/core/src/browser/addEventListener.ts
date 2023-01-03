c264097428bd5541dbeed6741ca1e3364e258561
Benoît <benoit.zugmeyer@datadoghq.com>
2023-01-03 14:43:49 +0100

    🔊 [RUMF-1345] add telemetry logs on untrusted events (#1910)
    * 🔊 [RUMF-1345] log first untrusted events of each type
    
    * ♻️ lift runOnReadyState to resolve circular dependency
    
    * 👌 rename variable and consider Set as side-effect free
    
    Co-authored-by: William Lacroix <william.lacroix@datadoghq.com>

--- packages/core/src/browser/addEventListener.ts
+++ packages/core/src/browser/addEventListener.ts
@@ -1,5 +1,6 @@
 import { monitor } from '../tools/monitor'
 import { getZoneJsOriginalValue } from '../tools/getZoneJsOriginalValue'
+import { addTelemetryDebug } from '../domain/telemetry'
 
 export const enum DOM_EVENT {
   BEFORE_UNLOAD = 'beforeunload',
@@ -77,14 +78,11 @@ export function addEventListeners<E extends Event>(
   listener: (event: E) => void,
   { once, capture, passive }: AddEventListenerOptions = {}
 ) {
-  const wrappedListener = monitor(
-    once
-      ? (event: Event) => {
-          stop()
-          listener(event as E)
-        }
-      : (listener as (event: Event) => void)
-  )
+  const wrappedListener = monitor((event: Event) => {
+    if (once) stop()
+    reportUntrustedEvent(event)
+    listener(event as E)
+  })
 
   const options = passive ? { capture, passive } : capture
 
@@ -99,4 +97,21 @@ export function addEventListeners<E extends Event>(
   return {
     stop,
   }
+}
+
+const reportedUntrustedEventTypes = new Set<string>()
+
+function reportUntrustedEvent(event: Event) {
+  const eventType = event.type
+
+  if (event.isTrusted || (window as any).jasmine || reportedUntrustedEventTypes.has(eventType)) {
+    return
+  }
+
+  reportedUntrustedEventTypes.add(eventType)
+  addTelemetryDebug('Untrusted event', { event_type: eventType })
+}
+
+export function resetUntrustedEventsCount() {
+  reportedUntrustedEventTypes.clear()
 }
