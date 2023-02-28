cda1afb95f5f1e1006d19f72f66b57b3dd8884fb
Aymeric <aymeric@mortemousque.com>
2023-02-28 16:25:53 +0100

    Check cancelIdleCallback

--- packages/core/src/tools/utils.ts
+++ packages/core/src/tools/utils.ts
@@ -527,13 +527,16 @@ export function combine(...sources: any[]): unknown {
   return destination as unknown
 }
 
+/**
+ * Use 'requestIdleCallback' when available: it will throttle the mutation processing if the
+ * browser is busy rendering frames (ex: when frames are below 60fps). When not available, the
+ * fallback on 'requestAnimationFrame' will still ensure the mutations are processed after any
+ * browser rendering process (Layout, Recalculate Style, etc.), so we can serialize DOM nodes efficiently.
+ *
+ * Note: check both 'requestIdleCallback' and 'cancelIdleCallback' existence because some polyfills only implement 'requestIdleCallback'.
+ */
 export function requestIdleCallback(callback: () => void, opts?: { timeout?: number }) {
-  // Use 'requestIdleCallback' when available: it will throttle the mutation processing if the
-  // browser is busy rendering frames (ex: when frames are below 60fps). When not available, the
-  // fallback on 'requestAnimationFrame' will still ensure the mutations are processed after any
-  // browser rendering process (Layout, Recalculate Style, etc.), so we can serialize DOM nodes
-  // efficiently.
-  if (window.requestIdleCallback) {
+  if (window.requestIdleCallback && window.cancelIdleCallback) {
     const id = window.requestIdleCallback(monitor(callback), opts)
     return () => window.cancelIdleCallback(id)
   }
