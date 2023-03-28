5efe6de2852fd4c1bdd94e8b656fc70e4dc34a72
Aymeric <aymeric@mortemousque.com>
2023-03-28 15:08:57 +0200

    Fix comment

--- packages/core/src/tools/heavyCustomerDataWarning.ts
+++ packages/core/src/tools/heavyCustomerDataWarning.ts
@@ -1,9 +1,9 @@
 import { display } from './display'
 import { ONE_KIBI_BYTE } from './utils'
 
-// RUM batch bytes limit is 16KB
+// RUM and logs batch bytes limit is 16KB
 // ensure that we leave room for other event attributes and maintain a decent amount of event per batch
-// 3KB (customer data) + 1KB (other attributes) * 4 (events per batch) = 16KB
+// (3KB (customer data) + 1KB (other attributes)) * 4 (events per batch) = 16KB
 export const CUSTOMER_DATA_BYTES_LIMIT = 3 * ONE_KIBI_BYTE
 
 export const enum CustomerDataType {
