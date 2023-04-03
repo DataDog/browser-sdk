12b8c61eff4a7ab37425a2668dc874db5f786842
Yannick Adam <yannick.adam@datadoghq.com>
2023-04-03 14:25:28 +0200

    🎨 Add type definitions for usage from TS

---
+++ test/browsers.conf.d.ts
@@ -0,0 +1,11 @@
+declare const browsers: Array<{
+  sessionName: string
+  name: string
+  version?: string
+  os: string
+  osVersion: string
+  device?: string
+}>
+
+// eslint-disable-next-line import/no-default-export
+export default browsers
