64ccaffb707c0befa6ac473b527080c3485477b0
Bastien Caudan <bastien.caudan@datadoghq.com>
2023-04-03 11:49:06 +0200

    ♻️move requestIdleCallback to mutation batch

--- packages/core/src/tools/utils.ts
+++ packages/core/src/tools/utils.ts
@@ -1,5 +1,4 @@
 import { display } from './display'
-import { monitor } from './monitor'
 import { startsWith, arrayFrom } from './polyfills'
 
 /**
@@ -42,23 +41,6 @@ export function getType(value: unknown) {
     return 'array'
   }
   return typeof value
-}
-
-/**
- * Use 'requestIdleCallback' when available: it will throttle the mutation processing if the
- * browser is busy rendering frames (ex: when frames are below 60fps). When not available, the
- * fallback on 'requestAnimationFrame' will still ensure the mutations are processed after any
- * browser rendering process (Layout, Recalculate Style, etc.), so we can serialize DOM nodes efficiently.
- *
- * Note: check both 'requestIdleCallback' and 'cancelIdleCallback' existence because some polyfills only implement 'requestIdleCallback'.
- */
-export function requestIdleCallback(callback: () => void, opts?: { timeout?: number }) {
-  if (window.requestIdleCallback && window.cancelIdleCallback) {
-    const id = window.requestIdleCallback(monitor(callback), opts)
-    return () => window.cancelIdleCallback(id)
-  }
-  const id = window.requestAnimationFrame(monitor(callback))
-  return () => window.cancelAnimationFrame(id)
 }
 
 export function removeDuplicates<T>(array: T[]) {
