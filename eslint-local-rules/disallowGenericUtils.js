84f6fd56dd1ad6f0515015a4578de99bdee28b34
Bastien Caudan <1331991+bcaudan@users.noreply.github.com>
2023-04-05 16:18:12 +0200

    Add an eslint rule to disallow the use of too generic utility file names (#2101)

---
+++ eslint-local-rules/disallowGenericUtils.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'Disallow the use of too generic utility file names',
+      recommended: false,
+    },
+    schema: [],
+  },
+  create: (context) => ({
+    Program: (node) => {
+      const filename = path.basename(context.getFilename())
+      if (isGenericUtilsFileName(filename)) {
+        context.report({
+          node,
+          message: 'Consider having a more specific file name to reflect the domain of this utility file',
+        })
+      }
+    },
+  }),
+}
+
+function isGenericUtilsFileName(filename) {
+  return /^(utils|specHelper)\..*/.test(filename)
+}
