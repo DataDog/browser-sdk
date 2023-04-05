7f85aa9b252288873b9f66802ec970b2dba530fa
Aymeric <aymeric@mortemousque.com>
2023-04-05 16:26:28 +0200

    Disallow url constructor patched value

---
+++ eslint-local-rules/disallowUrlConstructorPatchValues.js
@@ -0,0 +1,29 @@
+module.exports = {
+  meta: {
+    docs: {
+      description: 'Disallow problematic URL constructor patched values.',
+      recommended: false,
+    },
+    schema: [],
+  },
+
+  create(context) {
+    return {
+      'Program:exit'(node) {
+        const globalScope = context.getScope(node)
+        const variable = globalScope.set.get('URL')
+
+        if (variable && variable.defs.length === 0) {
+          variable.references.forEach((ref) => {
+            const idNode = ref.identifier
+            const parent = idNode.parent
+
+            if (parent && parent.type === 'NewExpression' && parent.callee === idNode) {
+              context.report(idNode, 'This value might be patched. Use `buildUrl` from @datadog/browser-core instead')
+            }
+          })
+        }
+      },
+    }
+  },
+}
