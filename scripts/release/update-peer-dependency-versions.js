c46ed29875e1f9a515ee8e5052b2e762a24bc25f
Bastien Caudan <bastien.caudan@datadoghq.com>
2023-05-03 13:47:38 +0200

    👷 update peer dependencies for yaml files

--- scripts/release/update-peer-dependency-versions.js
+++ scripts/release/update-peer-dependency-versions.js
@@ -2,6 +2,9 @@ const lernaConfig = require('../../lerna.json')
 const { runMain } = require('../lib/execution-utils')
 const { modifyFile } = require('../lib/files-utils')
 
+const JSON_FILES = ['rum', 'rum-slim', 'logs'].map((packageName) => `./packages/${packageName}/package.json`)
+const YAML_FILES = ['yarn.lock']
+
 // This script updates the peer dependency versions between rum and logs packages to match the new
 // version during a release.
 //
@@ -12,13 +15,21 @@ const { modifyFile } = require('../lib/files-utils')
 // [1]: https://github.com/lerna/lerna/commit/bdbfc62966e5351abfeac77830f9d47b6d69f1b1
 // [2]: https://github.com/lerna/lerna/issues/1575
 runMain(async () => {
-  for (const packageName of ['rum', 'rum-slim', 'logs']) {
-    await modifyFile(`./packages/${packageName}/package.json`, (content) => {
-      const json = JSON.parse(content)
-      Object.keys(json.peerDependencies).forEach((key) => {
-        json.peerDependencies[key] = lernaConfig.version
-      })
-      return `${JSON.stringify(json, null, 2)}\n`
-    })
+  for (const jsonFile of JSON_FILES) {
+    await modifyFile(jsonFile, updateJsonPeerDependencies)
+  }
+  for (const yamlFile of YAML_FILES) {
+    await modifyFile(yamlFile, updateYamlPeerDependencies)
   }
 })
+function updateJsonPeerDependencies(content) {
+  const json = JSON.parse(content)
+  Object.keys(json.peerDependencies).forEach((key) => {
+    json.peerDependencies[key] = lernaConfig.version
+  })
+  return `${JSON.stringify(json, null, 2)}\n`
+}
+
+function updateYamlPeerDependencies(content) {
+  return content.replaceAll(/(peerDependencies:\s+"@datadog\/[^"]+":\s)[\d.]+/g, `$1${lernaConfig.version}`)
+}
