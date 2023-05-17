2573addc142430f4b691246c67b070518c7978d7
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2023-05-17 14:26:33 +0200

    👌📝 [devext] add comments to the `useSettings` hook

--- developer-extension/src/panel/hooks/useSettings.ts
+++ developer-extension/src/panel/hooks/useSettings.ts
@@ -45,9 +45,17 @@ function setSetting<Name extends keyof Settings>(name: Name, value: Settings[Nam
 }
 
 export function useSettings() {
+  // If we don't have settings yet, it means that we are still loading them from the storage. Throw
+  // the promise so it'll be caught by the Suspense boundary.
   if (!settings) {
     throw storageLoadingPromise
   }
+
+  // We want to have a single 'settings' object shared between all components (it's easier to have
+  // a single source of truth and simplifies persistence implementation). Usually, we would use
+  // something like redux or one of the many other alternatives. Since we have a single use-case in
+  // this project, let's implement a shared state manually by forcing an update when the 'settings'
+  // object changes.
 
   // https://legacy.reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
   const [, forceUpdate] = useReducer((x: number) => x + 1, 0)
