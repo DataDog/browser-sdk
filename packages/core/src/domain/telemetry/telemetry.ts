827a06670dcab07aae6891dcccdf6a7f0823409d
Bastien Caudan <bastien.caudan@datadoghq.com>
2023-05-30 14:14:07 +0200

    🐛 Telemetry: do not scrub staging and canary frames

--- packages/core/src/domain/telemetry/telemetry.ts
+++ packages/core/src/domain/telemetry/telemetry.ts
@@ -25,6 +25,8 @@ declare const __BUILD_ENV__SDK_VERSION__: string
 const ALLOWED_FRAME_URLS = [
   'https://www.datadoghq-browser-agent.com',
   'https://www.datad0g-browser-agent.com',
+  'https://d3uc069fcn7uxw.cloudfront.net',
+  'https://d20xtzwzcl0ceb.cloudfront.net',
   'http://localhost',
   '<anonymous>',
 ]
