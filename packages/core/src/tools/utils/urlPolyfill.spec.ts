81cbe25457c622f91ead0ee1f1fa2a2fd58e6c67
Aymeric <aymeric@mortemousque.com>
2023-07-03 11:24:28 +0200

    Update test

--- packages/core/src/tools/utils/urlPolyfill.spec.ts
+++ packages/core/src/tools/utils/urlPolyfill.spec.ts
@@ -30,7 +30,7 @@ describe('normalize url', () => {
   it('should keep file url unchanged', () => {
     if (isFirefox()) {
       // On firefox, URL host is empty for file URI: 'https://bugzilla.mozilla.org/show_bug.cgi?id=1578787'
-      expect(normalizeUrl('file://foo.com/my/path')).toMatch(/^file:\/\/.*\/my\/path$/)
+      expect(normalizeUrl('file://foo.com/my/path')).toEqual('file:///my/path')
     } else {
       expect(normalizeUrl('file://foo.com/my/path')).toEqual('file://foo.com/my/path')
     }
