682d40b505850f72b7d358ecc6814b3007f30e7e
Aymeric <aymeric.mortemousque@datadoghq.com>
2023-07-03 15:40:47 +0200

    💥 [RUMF-1473] Ignore untrusted event (#2308)

--- packages/core/src/tools/timer.ts
+++ packages/core/src/tools/timer.ts
@@ -2,20 +2,20 @@ import { getZoneJsOriginalValue } from './getZoneJsOriginalValue'
 import { monitor } from './monitor'
 import { getGlobalObject } from './getGlobalObject'
 
-export type TimeoutId = ReturnType<typeof window.setTimeout>
+export type TimeoutId = ReturnType<typeof globalThis.setTimeout>
 
 export function setTimeout(callback: () => void, delay?: number): TimeoutId {
-  return getZoneJsOriginalValue(getGlobalObject<Window>(), 'setTimeout')(monitor(callback), delay)
+  return getZoneJsOriginalValue(getGlobalObject(), 'setTimeout')(monitor(callback), delay)
 }
 
 export function clearTimeout(timeoutId: TimeoutId | undefined) {
-  getZoneJsOriginalValue(getGlobalObject<Window>(), 'clearTimeout')(timeoutId)
+  getZoneJsOriginalValue(getGlobalObject(), 'clearTimeout')(timeoutId)
 }
 
 export function setInterval(callback: () => void, delay?: number): TimeoutId {
-  return getZoneJsOriginalValue(window, 'setInterval')(monitor(callback), delay)
+  return getZoneJsOriginalValue(getGlobalObject(), 'setInterval')(monitor(callback), delay)
 }
 
 export function clearInterval(timeoutId: TimeoutId | undefined) {
-  getZoneJsOriginalValue(window, 'clearInterval')(timeoutId)
+  getZoneJsOriginalValue(getGlobalObject(), 'clearInterval')(timeoutId)
 }
