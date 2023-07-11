d65041c4b475da9062489a9ddd7872abfc6278eb
Aymeric <aymeric@mortemousque.com>
2023-07-11 13:48:17 +0200

    Remove useless m

--- packages/core/src/domain/tracekit/tracekit.ts
+++ packages/core/src/domain/tracekit/tracekit.ts
@@ -4,7 +4,7 @@ import type { UnhandledErrorCallback, StackTrace } from './types'
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Error_types
 const ERROR_TYPES_RE =
-  /^(?:[Uu]ncaught (?:exception: )?)?(?:((?:Eval|Internal|Range|Reference|Syntax|Type|URI|)Error): )?([\s\S]*)$/m
+  /^(?:[Uu]ncaught (?:exception: )?)?(?:((?:Eval|Internal|Range|Reference|Syntax|Type|URI|)Error): )?([\s\S]*)$/
 
 /**
  * Cross-browser collection of unhandled errors
