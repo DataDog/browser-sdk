a3ebcb0d3c8baf06bf44cd7c0c5add250b326add
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2023-07-11 09:47:47 +0200

    👷 [RUM-232] export cjs, esm and types for the /string package

--- packages/worker/scripts/build-worker-string.js
+++ packages/worker/scripts/build-worker-string.js
@@ -5,7 +5,14 @@ const fs = require('fs')
 
 const bundlePath = path.resolve(__dirname, '../bundle')
 const workerBundlePath = path.join(bundlePath, 'worker.js')
-const workerStringPath = path.join(bundlePath, 'workerString.ts')
 const workerBundleContent = fs.readFileSync(workerBundlePath, { encoding: 'utf-8' })
 
-fs.writeFileSync(workerStringPath, `export const workerString = ${JSON.stringify(workerBundleContent)}`)
+writeOutput('cjs', `exports.workerString = ${JSON.stringify(workerBundleContent)}\n`)
+writeOutput('esm', `export const workerString = ${JSON.stringify(workerBundleContent)}\n`)
+
+function writeOutput(moduleType, content) {
+  const outputPath = path.resolve(__dirname, path.join('..', 'string', moduleType))
+  fs.mkdirSync(outputPath, { recursive: true })
+  fs.writeFileSync(path.join(outputPath, 'main.js'), content)
+  fs.writeFileSync(path.join(outputPath, 'main.d.ts'), 'export declare const workerString: string\n')
+}
