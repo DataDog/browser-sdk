31deb9e24544d37fcaa7602c6105319b164c040d
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2023-08-03 19:01:24 +0200

    ✨ better event description

--- developer-extension/src/panel/formatNumber.ts
+++ developer-extension/src/panel/formatNumber.ts
@@ -1,3 +1,7 @@
 export function formatNumber(n: number): string {
   return new Intl.NumberFormat('en-US', {}).format(n)
 }
+
+export function formatDuration(ns: number): string {
+  return new Intl.NumberFormat('en-US', { style: 'unit', unit: 'millisecond' }).format(ns / 1_000_000)
+}
