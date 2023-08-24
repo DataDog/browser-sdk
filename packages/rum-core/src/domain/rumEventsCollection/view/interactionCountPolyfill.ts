9bc714fe8b23187343180f2cfa4e687134637e31
Aymeric <aymeric@mortemousque.com>
2023-08-24 10:30:59 +0200

    Add more detail about the polyfill in comment

--- packages/rum-core/src/domain/rumEventsCollection/view/interactionCountPolyfill.ts
+++ packages/rum-core/src/domain/rumEventsCollection/view/interactionCountPolyfill.ts
@@ -4,7 +4,9 @@
  * The interactionCount is an integer which counts the total number of distinct user interactions,
  * for which there was a unique interactionId.
  *
- * The interactionCount polyfill is an estimate based on a convention specific to Chrome
+ * The interactionCount polyfill is an estimate based on a convention specific to Chrome. Cf: https://github.com/GoogleChrome/web-vitals/pull/213
+ * This is currently no an issue as the polyfill is only used for INP which is currently only supported on Chrome.
+ * Hopefully when/if other browsers will support INP, they will also implement performance.interactionCount at the same time, so we won't need that polyfill.
  *
  * Reference implementation: https://github.com/GoogleChrome/web-vitals/blob/main/src/lib/polyfills/interactionCountPolyfill.ts
  */
