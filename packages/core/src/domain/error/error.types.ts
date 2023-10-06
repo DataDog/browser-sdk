2c4fac433470455d08e8e241e8eeea2d223ebfd2
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2023-10-06 15:33:32 +0200

    [RUM-1211] fix compatibility with TS4.6/4.7 using ES2022

--- packages/core/src/domain/error/error.types.ts
+++ packages/core/src/domain/error/error.types.ts
@@ -1,6 +1,15 @@
 import type { ClocksState } from '../../tools/utils/timeUtils'
 
-export interface ErrorWithCause extends Error {
+// TS v4.6 introduced Error.cause[1] typed as `Error`. TS v4.8 changed Error.cause to be
+// `unknown`[2].
+//
+// Because we still support TS 3.8, we need to declare our own type. We can remove it once we drop
+// support for TS v4.7 and before. The 'cause' property defined by TS needs to be omitted because
+// we define it with a type `unknown` which is incompatible with TS 4.6 and 4.7.
+//
+// [1]: https://devblogs.microsoft.com/typescript/announcing-typescript-4-6/#target-es2022
+// [2]: https://devblogs.microsoft.com/typescript/announcing-typescript-4-8/#lib-d-ts-updates
+export interface ErrorWithCause extends Omit<Error, 'cause'> {
   cause?: unknown
 }
 
