a14e7cc4fcc50613ed413f880338b1ccc8563068
Benoît <benoit.zugmeyer@datadoghq.com>
2023-10-09 09:31:34 +0200

    [RUM-1062] remove message prefix when using the console handler (#2452)
    * [RUM-1062] remove message prefix when using the console handler
    
    * add e2e test
    
    * remove fallback to console.log as it cannot happen

--- packages/logs/src/domain/logger/loggerCollection.ts
+++ packages/logs/src/domain/logger/loggerCollection.ts
@@ -1,5 +1,12 @@
-import type { TimeStamp } from '@datadog/browser-core'
-import { includes, display, combine, ErrorSource, timeStampNow } from '@datadog/browser-core'
+import type { Context, TimeStamp } from '@datadog/browser-core'
+import {
+  includes,
+  combine,
+  ErrorSource,
+  timeStampNow,
+  originalConsoleMethods,
+  globalConsole,
+} from '@datadog/browser-core'
 import type { CommonContext } from '../../rawLogsEvent.types'
 import type { LifeCycle } from '../lifeCycle'
 import { LifeCycleEventType } from '../lifeCycle'
@@ -23,7 +30,7 @@ export function startLoggerCollection(lifeCycle: LifeCycle) {
     const messageContext = logsMessage.context
 
     if (isAuthorized(logsMessage.status, HandlerType.console, logger)) {
-      display(logsMessage.status, logsMessage.message, combine(logger.getContext(), messageContext))
+      displayInConsole(logsMessage, combine(logger.getContext(), messageContext))
     }
 
     lifeCycle.notify(LifeCycleEventType.RAW_LOG_COLLECTED, {
@@ -50,4 +57,8 @@ export function isAuthorized(status: StatusType, handlerType: HandlerType, logge
   return (
     STATUS_PRIORITIES[status] >= STATUS_PRIORITIES[logger.getLevel()] && includes(sanitizedHandlerType, handlerType)
   )
+}
+
+function displayInConsole(logsMessage: LogsMessage, messageContext: Context | undefined) {
+  originalConsoleMethods[logsMessage.status].call(globalConsole, logsMessage.message, messageContext)
 }
