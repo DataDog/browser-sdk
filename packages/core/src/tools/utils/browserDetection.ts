5fa8c25c69dbbd955a851673dd0e096c5d0326ba
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2023-10-30 12:32:16 +0100

    ✅ [RUM-1863] add tests on browser detection

--- packages/core/src/tools/utils/browserDetection.ts
+++ packages/core/src/tools/utils/browserDetection.ts
@@ -1,17 +1,42 @@
-let browserIsIE: boolean | undefined
+// Exported only for tests
+export const enum Browser {
+  IE,
+  CHROMIUM,
+  SAFARI,
+  OTHER,
+}
+
 export function isIE() {
-  return browserIsIE ?? (browserIsIE = Boolean((document as any).documentMode))
+  return detectBrowserCached() === Browser.IE
 }
 
-let browserIsChromium: boolean | undefined
 export function isChromium() {
-  return (
-    browserIsChromium ??
-    (browserIsChromium = !!(window as any).chrome || /HeadlessChrome/.test(window.navigator.userAgent))
-  )
+  return detectBrowserCached() === Browser.CHROMIUM
 }
 
-let browserIsSafari: boolean | undefined
 export function isSafari() {
-  return browserIsSafari ?? (browserIsSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent))
+  return detectBrowserCached() === Browser.SAFARI
+}
+
+let browserCache: Browser | undefined
+function detectBrowserCached() {
+  return browserCache ?? (browserCache = detectBrowser())
+}
+
+// Exported only for tests
+export function detectBrowser(browserWindow: Window = window) {
+  const userAgent = browserWindow.navigator.userAgent
+  if ((browserWindow as any).chrome || /HeadlessChrome/.test(userAgent)) {
+    return Browser.CHROMIUM
+  }
+
+  if (/safari/i.test(userAgent) && !/chrome|android/i.test(userAgent)) {
+    return Browser.SAFARI
+  }
+
+  if ((browserWindow.document as any).documentMode) {
+    return Browser.IE
+  }
+
+  return Browser.OTHER
 }
