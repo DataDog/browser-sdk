f611ff5257fb344d8b196c46f0cb0731817deed2
Bastien Caudan <1331991+bcaudan@users.noreply.github.com>
2023-12-08 10:19:30 +0100

    ♻️ [Logs] reorganise some components (#2519)
    * ♻️ extract reportError
    
    * ♻️ extract logsTelemetry
    
    * ♻️ extract rumInternalContext
    
    * ♻️ move telemetry configuration to logsTelemetry
    
    * ♻️ more consistent dependency
    
    * 👌 extract rumInternalContext spec

---
+++ packages/logs/src/domain/logsTelemetry.ts
@@ -0,0 +1,72 @@
+import type { RawError, Observable, PageExitEvent, TelemetryEvent, Context } from '@datadog/browser-core'
+import {
+  startTelemetry,
+  TelemetryService,
+  canUseEventBridge,
+  getEventBridge,
+  startBatchWithReplica,
+  createIdentityEncoder,
+  isTelemetryReplicationAllowed,
+  addTelemetryConfiguration,
+} from '@datadog/browser-core'
+import type { LogsConfiguration, LogsInitConfiguration } from './configuration'
+import { getRUMInternalContext } from './rumInternalContext'
+import type { LogsSessionManager } from './logsSessionManager'
+import { serializeLogsConfiguration } from './configuration'
+
+export function startLogsTelemetry(
+  initConfiguration: LogsInitConfiguration,
+  configuration: LogsConfiguration,
+  reportError: (error: RawError) => void,
+  pageExitObservable: Observable<PageExitEvent>,
+  session: LogsSessionManager
+) {
+  const telemetry = startTelemetry(TelemetryService.LOGS, configuration)
+  telemetry.setContextProvider(() => ({
+    application: {
+      id: getRUMInternalContext()?.application_id,
+    },
+    session: {
+      id: session.findTrackedSession()?.id,
+    },
+    view: {
+      id: (getRUMInternalContext()?.view as Context)?.id,
+    },
+    action: {
+      id: (getRUMInternalContext()?.user_action as Context)?.id,
+    },
+  }))
+  const cleanupTasks: Array<() => void> = []
+  if (canUseEventBridge()) {
+    const bridge = getEventBridge<'internal_telemetry', TelemetryEvent>()!
+    const telemetrySubscription = telemetry.observable.subscribe((event) => bridge.send('internal_telemetry', event))
+    cleanupTasks.push(() => telemetrySubscription.unsubscribe())
+  } else {
+    const telemetryBatch = startBatchWithReplica(
+      configuration,
+      {
+        endpoint: configuration.rumEndpointBuilder,
+        encoder: createIdentityEncoder(),
+      },
+      configuration.replica && {
+        endpoint: configuration.replica.rumEndpointBuilder,
+        encoder: createIdentityEncoder(),
+      },
+      reportError,
+      pageExitObservable,
+      session.expireObservable
+    )
+    cleanupTasks.push(() => telemetryBatch.stop())
+    const telemetrySubscription = telemetry.observable.subscribe((event) =>
+      telemetryBatch.add(event, isTelemetryReplicationAllowed(configuration))
+    )
+    cleanupTasks.push(() => telemetrySubscription.unsubscribe())
+  }
+  addTelemetryConfiguration(serializeLogsConfiguration(initConfiguration))
+  return {
+    telemetry,
+    stop: () => {
+      cleanupTasks.forEach((task) => task())
+    },
+  }
+}
---
+++ packages/logs/src/domain/rumInternalContext.spec.ts
@@ -0,0 +1,75 @@
+import type { TelemetryEvent } from '@datadog/browser-core'
+import { startTelemetry, TelemetryService } from '@datadog/browser-core'
+import { mockSyntheticsWorkerValues, cleanupSyntheticsWorkerValues } from '@datadog/browser-core/test'
+import { resetRUMInternalContext, getRUMInternalContext } from './rumInternalContext'
+import { validateAndBuildLogsConfiguration } from './configuration'
+
+const initConfiguration = { clientToken: 'xxx', service: 'service' }
+
+describe('getRUMInternalContext', () => {
+  afterEach(() => {
+    delete window.DD_RUM
+    delete window.DD_RUM_SYNTHETICS
+    resetRUMInternalContext()
+  })
+
+  it('returns undefined if no RUM instance is present', () => {
+    expect(getRUMInternalContext()).toBeUndefined()
+  })
+
+  it('returns undefined if the global variable does not have a `getInternalContext` method', () => {
+    window.DD_RUM = {} as any
+    expect(getRUMInternalContext()).toBeUndefined()
+  })
+
+  it('returns the internal context from the `getInternalContext` method', () => {
+    window.DD_RUM = {
+      getInternalContext: () => ({ foo: 'bar' }),
+    }
+    expect(getRUMInternalContext()).toEqual({ foo: 'bar' })
+  })
+
+  describe('when RUM is injected by Synthetics', () => {
+    let telemetrySpy: jasmine.Spy<(event: TelemetryEvent) => void>
+
+    beforeEach(() => {
+      mockSyntheticsWorkerValues({ injectsRum: true, publicId: 'test-id', resultId: 'result-id' })
+      const telemetry = startTelemetry(
+        TelemetryService.LOGS,
+        validateAndBuildLogsConfiguration({ ...initConfiguration, telemetrySampleRate: 100 })!
+      )
+      telemetrySpy = jasmine.createSpy()
+      telemetry.observable.subscribe(telemetrySpy)
+    })
+
+    afterEach(() => {
+      cleanupSyntheticsWorkerValues()
+    })
+
+    it('uses the global variable created when the synthetics worker is injecting RUM', () => {
+      window.DD_RUM_SYNTHETICS = {
+        getInternalContext: () => ({ foo: 'bar' }),
+      }
+      expect(getRUMInternalContext()).toEqual({ foo: 'bar' })
+    })
+
+    it('adds a telemetry debug event when RUM has not been injected yet', () => {
+      getRUMInternalContext()
+      expect(telemetrySpy.calls.mostRecent().args[0].telemetry).toEqual(
+        jasmine.objectContaining({
+          message: 'Logs sent before RUM is injected by the synthetics worker',
+          status: 'debug',
+          type: 'log',
+          testId: 'test-id',
+          resultId: 'result-id',
+        })
+      )
+    })
+
+    it('adds the telemetry debug event only once', () => {
+      getRUMInternalContext()
+      getRUMInternalContext()
+      expect(telemetrySpy).toHaveBeenCalledTimes(1)
+    })
+  })
+})
