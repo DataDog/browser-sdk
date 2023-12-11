c9f2a50261d0c5b75c91f6bf3c9542ba2c476956
Benoît <benoit.zugmeyer@datadoghq.com>
2023-12-11 18:59:49 +0100

    ✅ import JSON schemas dynamically in tests (#2521)
    * ✅ import JSON schemas dynamically in unit tests
    
    * ✅ import JSON schemas dynamically in e2e tests

--- packages/rum-core/test/formatValidation.ts
+++ packages/rum-core/test/formatValidation.ts
@@ -1,30 +1,15 @@
 import type { Context } from '@datadog/browser-core'
 import ajv from 'ajv'
-import rumEventsSchemaJson from '../../../rum-events-format/schemas/rum-events-schema.json'
-import _commonSchemaJson from '../../../rum-events-format/schemas/rum/_common-schema.json'
-import _actionChildSchemaJson from '../../../rum-events-format/schemas/rum/_action-child-schema.json'
-import _perfMetricSchemaJson from '../../../rum-events-format/schemas/rum/_perf-metric-schema.json'
-import actionSchemaJson from '../../../rum-events-format/schemas/rum/action-schema.json'
-import errorSchemaJson from '../../../rum-events-format/schemas/rum/error-schema.json'
-import longTaskSchemaJson from '../../../rum-events-format/schemas/rum/long_task-schema.json'
-import resourceSchemaJson from '../../../rum-events-format/schemas/rum/resource-schema.json'
-import viewSchemaJson from '../../../rum-events-format/schemas/rum/view-schema.json'
+import { allJsonSchemas } from './allJsonSchemas'
 
 export function validateRumFormat(rumEvent: Context) {
   const instance = new ajv({
     allErrors: true,
   })
-  void instance
-    .addSchema(_commonSchemaJson, 'rum/_common-schema.json')
-    .addSchema(_actionChildSchemaJson, 'rum/_action-child-schema.json')
-    .addSchema(_perfMetricSchemaJson, 'rum/_perf-metric-schema.json')
-    .addSchema(viewSchemaJson, 'rum/view-schema.json')
-    .addSchema(actionSchemaJson, 'rum/action-schema.json')
-    .addSchema(resourceSchemaJson, 'rum/resource-schema.json')
-    .addSchema(longTaskSchemaJson, 'rum/long_task-schema.json')
-    .addSchema(errorSchemaJson, 'rum/error-schema.json')
-    .addSchema(rumEventsSchemaJson, 'rum-events-schema.json')
-    .validate('rum-events-schema.json', rumEvent)
+
+  instance.addSchema(allJsonSchemas)
+
+  void instance.validate('rum-events-schema.json', rumEvent)
 
   if (instance.errors) {
     const errors = instance.errors
--- test/e2e/lib/helpers/validation.ts
+++ test/e2e/lib/helpers/validation.ts
@@ -1,31 +1,19 @@
+import fs from 'fs'
 import type { RumEvent } from '@datadog/browser-rum'
 import ajv from 'ajv'
-import rumEventsSchemaJson from '../../../../rum-events-format/schemas/rum-events-schema.json'
-import _commonSchemaJson from '../../../../rum-events-format/schemas/rum/_common-schema.json'
-import _actionChildSchemaJson from '../../../../rum-events-format/schemas/rum/_action-child-schema.json'
-import _perfMetricSchemaJson from '../../../../rum-events-format/schemas/rum/_perf-metric-schema.json'
-import actionSchemaJson from '../../../../rum-events-format/schemas/rum/action-schema.json'
-import errorSchemaJson from '../../../../rum-events-format/schemas/rum/error-schema.json'
-import longTaskSchemaJson from '../../../../rum-events-format/schemas/rum/long_task-schema.json'
-import resourceSchemaJson from '../../../../rum-events-format/schemas/rum/resource-schema.json'
-import viewSchemaJson from '../../../../rum-events-format/schemas/rum/view-schema.json'
+import { globSync } from 'glob'
 
 export function validateRumFormat(events: RumEvent[]) {
+  const instance = new ajv({
+    allErrors: true,
+  })
+  const allJsonSchemas = globSync('./rum-events-format/schemas/**/*.json').map(
+    (path) => JSON.parse(fs.readFileSync(path, 'utf8')) as object
+  )
+  instance.addSchema(allJsonSchemas)
+
   events.forEach((rumEvent) => {
-    const instance = new ajv({
-      allErrors: true,
-    })
-    void instance
-      .addSchema(_commonSchemaJson, 'rum/_common-schema.json')
-      .addSchema(_actionChildSchemaJson, 'rum/_action-child-schema.json')
-      .addSchema(_perfMetricSchemaJson, 'rum/_perf-metric-schema.json')
-      .addSchema(viewSchemaJson, 'rum/view-schema.json')
-      .addSchema(actionSchemaJson, 'rum/action-schema.json')
-      .addSchema(resourceSchemaJson, 'rum/resource-schema.json')
-      .addSchema(longTaskSchemaJson, 'rum/long_task-schema.json')
-      .addSchema(errorSchemaJson, 'rum/error-schema.json')
-      .addSchema(rumEventsSchemaJson, 'rum-events-schema.json')
-      .validate('rum-events-schema.json', rumEvent)
+    void instance.validate('rum-events-schema.json', rumEvent)
     if (instance.errors) {
       instance.errors.map((error) => fail(`${error.dataPath || 'event'} ${error.message!}`))
     }
