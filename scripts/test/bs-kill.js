7a21ffbc9ecfb9014c188e0ab9be905f5330ef3e
Bastien Caudan <bastien.caudan@datadoghq.com>
2024-01-30 14:51:09 +0100

    👷 add script to kill bs workers

---
+++ scripts/test/bs-kill.js
@@ -0,0 +1,35 @@
+'use strict'
+/**
+ * This script stops all BrowserStack workers that are currently running.
+ * Useful when troubleshooting BrowserStack issues locally.
+ */
+const { printLog, runMain, timeout } = require('../lib/execution-utils')
+const { browserStackRequest } = require('../lib/bs-utils')
+
+const MINIMUM_WORKER_LIFE = 30_000
+
+const BS_WORKERS_URL = 'https://api.browserstack.com/5/workers'
+const BS_WORKER_URL = (workerId) => `https://api.browserstack.com/5/worker/${workerId}`
+
+runMain(async () => {
+  const workerIds = await getActiveWorkerIds()
+  printLog(`Stopping ${workerIds.length} workers`)
+  await Promise.all(workerIds.map((workerId) => stopWorker(workerId)))
+  process.exit(0)
+})
+
+async function getActiveWorkerIds() {
+  return (await browserStackRequest(BS_WORKERS_URL)).map((worker) => worker.id)
+}
+
+async function stopWorker(workerId) {
+  const stopResponse = await browserStackRequest(BS_WORKER_URL(workerId), { method: 'DELETE' })
+  // stopResponse:
+  // - when stopped: {"time":X.Y}
+  // - when worker not old enough: {"message":"worker is running for X.Y secs, minimum life is 30 sec"}
+  if (stopResponse.time === undefined) {
+    printLog(`Worker ${workerId} not stopped, retrying in 30s`)
+    await timeout(MINIMUM_WORKER_LIFE)
+    await stopWorker(workerId)
+  }
+}
