179840f49de0dbf45352d806748fce88b293bc7b
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-01-31 15:31:16 +0100

    Add comment

--- packages/rum/src/domain/startRecordBridge.ts
+++ packages/rum/src/domain/startRecordBridge.ts
@@ -7,6 +7,9 @@ export function startRecordBridge(viewContexts: ViewContexts) {
 
   return {
     addRecord: (record: BrowserRecord) => {
+      // Get the current active view, not at the time of the record, aligning with the segment logic.
+      // This approach could potentially associate the record to an incorrect view, in case the record date is in the past (e.g. frustration records).
+      // However the risk is minimal. We could address the issue when potential negative impact are identified.
       const view = viewContexts.findView()!
       bridge.send('record', record, view.id)
     },
