25d5c0442132bd3a3228413a7d5a2db1d75e15f7
Bastien Caudan <bastien.caudan@datadoghq.com>
2024-02-01 17:37:07 +0100

    duration vital logic

--- packages/rum-core/src/domain/vital/vitalCollection.ts
+++ packages/rum-core/src/domain/vital/vitalCollection.ts
@@ -1,5 +1,9 @@
 import type { ClocksState } from '@datadog/browser-core'
-import type { LifeCycle } from '../lifeCycle'
+import { elapsed, generateUUID } from '@datadog/browser-core'
+import { LifeCycleEventType } from '../lifeCycle'
+import type { LifeCycle, RawRumEventCollectedData } from '../lifeCycle'
+import type { RawRumVitalEvent } from '../../rawRumEvent.types'
+import { RumEventType, VitalType } from '../../rawRumEvent.types'
 
 export interface DurationVitalStart {
   name: string
@@ -11,9 +15,49 @@ export interface DurationVitalStop {
   stopClocks: ClocksState
 }
 
-export function startVitalCollection(lifecycle: LifeCycle) {
+interface Vital {
+  name: string
+  type: VitalType
+  startClocks: ClocksState
+  value: number
+}
+
+export function startVitalCollection(lifeCycle: LifeCycle) {
+  const vitalStartsByName: { [name: string]: DurationVitalStart } = {}
   return {
-    startDurationVital: (vitalStart: DurationVitalStart) => {},
-    stopDurationVital: (vitalStop: DurationVitalStop) => {},
+    startDurationVital: (vitalStart: DurationVitalStart) => {
+      vitalStartsByName[vitalStart.name] = vitalStart
+    },
+    stopDurationVital: (vitalStop: DurationVitalStop) => {
+      const vitalStart = vitalStartsByName[vitalStop.name]
+      if (!vitalStart) {
+        return
+      }
+      const vital = {
+        name: vitalStart.name,
+        type: VitalType.DURATION,
+        startClocks: vitalStart.startClocks,
+        value: elapsed(vitalStart.startClocks.timeStamp, vitalStop.stopClocks.timeStamp),
+      }
+      lifeCycle.notify(LifeCycleEventType.RAW_RUM_EVENT_COLLECTED, processVital(vital))
+    },
+  }
+}
+
+function processVital(vital: Vital): RawRumEventCollectedData<RawRumVitalEvent> {
+  return {
+    rawRumEvent: {
+      date: vital.startClocks.timeStamp,
+      vital: {
+        id: generateUUID(),
+        type: vital.type,
+        custom: {
+          [vital.name]: vital.value,
+        },
+      },
+      type: RumEventType.VITAL,
+    },
+    startTime: vital.startClocks.relative,
+    domainContext: {},
   }
 }
