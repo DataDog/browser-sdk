06ba60f3f0dc7f06d55e28504e179b5f950c752c
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-02-12 11:45:54 +0100

    Flush pending clicks at BEFORE_VIEW_ENDED

--- packages/rum-core/src/domain/action/trackClickActions.spec.ts
+++ packages/rum-core/src/domain/action/trackClickActions.spec.ts
@@ -39,7 +39,6 @@ describe('trackClickActions', () => {
   let input: HTMLInputElement
   let setupBuilder: TestSetupBuilder
   let findActionId: ActionContexts['findActionId']
-
   beforeEach(() => {
     button = document.createElement('button')
     button.type = 'button'
@@ -176,13 +175,13 @@ describe('trackClickActions', () => {
     expect(findActionId()).toEqual([])
   })
 
-  it('ongoing click action is stopped on view end', () => {
+  it('ongoing click action is stopped before the view is ended', () => {
     const { lifeCycle, clock } = setupBuilder.build()
     emulateClick({ activity: { delay: BEFORE_PAGE_ACTIVITY_VALIDATION_DELAY } })
 
     clock.tick(BEFORE_PAGE_ACTIVITY_VALIDATION_DELAY)
 
-    lifeCycle.notify(LifeCycleEventType.VIEW_ENDED, {
+    lifeCycle.notify(LifeCycleEventType.BEFORE_VIEW_ENDED, {
       endClocks: clocksNow(),
     })
 
