ed481844e1d44e1d7adf6758f0aa3c4e81f219f7
Benoît <benoit.zugmeyer@datadoghq.com>
2024-02-12 11:49:24 +0100

    ⚗️✨ [RUM-2445] implement Tracking Consent management (#2589)
    * ✨ [RUM-2445] introduce init param
    
    * ✨ [RUM-2445] wait for consent before starting SDKs
    
    * ✨ [RUM-2445] implement setTrackingConsent public API
    
    * ♻️✅ [RUM-2445] modify session manager specs
    
    Define "start(Rum|Logs)SessionManagerWithDefault" to avoid having to
    pass new parameters everywhere.
    
    * ✨ [RUM-2445] expire and renew session based on tracking consent state
    
    * ✅ [RUM-2445] add e2e tests
    
    * ✅👌 add an e2e test
    
    * 👌 add jsdoc (also the logs public API I forgot)
    
    * 👌 rename tracking consent state methods
    
    * 👌 re-use TrackingConsentState instance
    
    * ✅👌 add test on unsubscribe
    
    * 👌✅ add some e2e tests for logs
    
    * 📝👌 adjust comment

--- packages/logs/src/domain/logsSessionManager.spec.ts
+++ packages/logs/src/domain/logsSessionManager.spec.ts
@@ -7,6 +7,8 @@ import {
   stopSessionManager,
   ONE_SECOND,
   DOM_EVENT,
+  createTrackingConsentState,
+  TrackingConsent,
 } from '@datadog/browser-core'
 import type { Clock } from '@datadog/browser-core/test'
 import { createNewEvent, mockClock } from '@datadog/browser-core/test'
@@ -21,16 +23,9 @@ import {
 
 describe('logs session manager', () => {
   const DURATION = 123456
-  const configuration: Partial<LogsConfiguration> = {
-    sessionSampleRate: 0.5,
-    sessionStoreStrategyType: { type: 'Cookie', cookieOptions: {} },
-  }
   let clock: Clock
-  let tracked: boolean
 
   beforeEach(() => {
-    tracked = true
-    spyOn(Math, 'random').and.callFake(() => (tracked ? 0 : 1))
     clock = mockClock()
   })
 
@@ -43,18 +38,14 @@ describe('logs session manager', () => {
   })
 
   it('when tracked should store tracking type and session id', () => {
-    tracked = true
-
-    startLogsSessionManager(configuration as LogsConfiguration)
+    startLogsSessionManagerWithDefaults()
 
     expect(getCookie(SESSION_STORE_KEY)).toContain(`${LOGS_SESSION_KEY}=${LoggerTrackingType.TRACKED}`)
     expect(getCookie(SESSION_STORE_KEY)).toMatch(/id=[a-f0-9-]+/)
   })
 
   it('when not tracked should store tracking type', () => {
-    tracked = false
-
-    startLogsSessionManager(configuration as LogsConfiguration)
+    startLogsSessionManagerWithDefaults({ configuration: { sessionSampleRate: 0 } })
 
     expect(getCookie(SESSION_STORE_KEY)).toContain(`${LOGS_SESSION_KEY}=${LoggerTrackingType.NOT_TRACKED}`)
     expect(getCookie(SESSION_STORE_KEY)).not.toContain('id=')
@@ -63,7 +54,7 @@ describe('logs session manager', () => {
   it('when tracked should keep existing tracking type and session id', () => {
     setCookie(SESSION_STORE_KEY, 'id=abcdef&logs=1', DURATION)
 
-    startLogsSessionManager(configuration as LogsConfiguration)
+    startLogsSessionManagerWithDefaults()
 
     expect(getCookie(SESSION_STORE_KEY)).toContain(`${LOGS_SESSION_KEY}=${LoggerTrackingType.TRACKED}`)
     expect(getCookie(SESSION_STORE_KEY)).toContain('id=abcdef')
@@ -72,19 +63,18 @@ describe('logs session manager', () => {
   it('when not tracked should keep existing tracking type', () => {
     setCookie(SESSION_STORE_KEY, 'logs=0', DURATION)
 
-    startLogsSessionManager(configuration as LogsConfiguration)
+    startLogsSessionManagerWithDefaults()
 
     expect(getCookie(SESSION_STORE_KEY)).toContain(`${LOGS_SESSION_KEY}=${LoggerTrackingType.NOT_TRACKED}`)
   })
 
   it('should renew on activity after expiration', () => {
-    startLogsSessionManager(configuration as LogsConfiguration)
+    startLogsSessionManagerWithDefaults()
 
     setCookie(SESSION_STORE_KEY, '', DURATION)
     expect(getCookie(SESSION_STORE_KEY)).toBeUndefined()
     clock.tick(STORAGE_POLL_DELAY)
 
-    tracked = true
     document.body.dispatchEvent(createNewEvent(DOM_EVENT.CLICK))
 
     expect(getCookie(SESSION_STORE_KEY)).toMatch(/id=[a-f0-9-]+/)
@@ -94,18 +84,18 @@ describe('logs session manager', () => {
   describe('findSession', () => {
     it('should return the current session', () => {
       setCookie(SESSION_STORE_KEY, 'id=abcdef&logs=1', DURATION)
-      const logsSessionManager = startLogsSessionManager(configuration as LogsConfiguration)
+      const logsSessionManager = startLogsSessionManagerWithDefaults()
       expect(logsSessionManager.findTrackedSession()!.id).toBe('abcdef')
     })
 
     it('should return undefined if the session is not tracked', () => {
       setCookie(SESSION_STORE_KEY, 'id=abcdef&logs=0', DURATION)
-      const logsSessionManager = startLogsSessionManager(configuration as LogsConfiguration)
+      const logsSessionManager = startLogsSessionManagerWithDefaults()
       expect(logsSessionManager.findTrackedSession()).toBe(undefined)
     })
 
     it('should return undefined if the session has expired', () => {
-      const logsSessionManager = startLogsSessionManager(configuration as LogsConfiguration)
+      const logsSessionManager = startLogsSessionManagerWithDefaults()
       setCookie(SESSION_STORE_KEY, '', DURATION)
       clock.tick(STORAGE_POLL_DELAY)
       expect(logsSessionManager.findTrackedSession()).toBe(undefined)
@@ -113,7 +103,7 @@ describe('logs session manager', () => {
 
     it('should return session corresponding to start time', () => {
       setCookie(SESSION_STORE_KEY, 'id=abcdef&logs=1', DURATION)
-      const logsSessionManager = startLogsSessionManager(configuration as LogsConfiguration)
+      const logsSessionManager = startLogsSessionManagerWithDefaults()
       clock.tick(10 * ONE_SECOND)
       setCookie(SESSION_STORE_KEY, '', DURATION)
       clock.tick(STORAGE_POLL_DELAY)
@@ -121,6 +111,17 @@ describe('logs session manager', () => {
       expect(logsSessionManager.findTrackedSession(0 as RelativeTime)!.id).toBe('abcdef')
     })
   })
+
+  function startLogsSessionManagerWithDefaults({ configuration }: { configuration?: Partial<LogsConfiguration> } = {}) {
+    return startLogsSessionManager(
+      {
+        sessionSampleRate: 100,
+        sessionStoreStrategyType: { type: 'Cookie', cookieOptions: {} },
+        ...configuration,
+      } as LogsConfiguration,
+      createTrackingConsentState(TrackingConsent.GRANTED)
+    )
+  }
 })
 
 describe('logger session stub', () => {
