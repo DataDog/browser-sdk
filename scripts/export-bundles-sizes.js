369a184ac1e4617c26a1bf6e141e5db884f7fc2a
roman.gaignault <roman.gaignault@datadoghq.com>
2024-02-12 11:48:12 +0100

    added  function getGitInformation, branch name in request and error handling

--- scripts/export-bundles-sizes.js
+++ scripts/export-bundles-sizes.js
@@ -1,14 +1,20 @@
 const fs = require('fs')
 const path = require('path')
-const childProcess = require('child_process')
+const { execSync } = require('child_process')
 const { getOrg2ApiKey } = require('./lib/secrets')
-const { runMain } = require('./lib/execution-utils')
-const url = 'https://http-intake.logs.datadoghq.com/api/v2/logs'
+const { runMain, fetch: fetchWrapper } = require('./lib/execution-utils')
+
 const rumPath = path.join(__dirname, '../packages/rum/bundle/datadog-rum.js')
 const logsPath = path.join(__dirname, '../packages/logs/bundle/datadog-logs.js')
 const rumSlimPath = path.join(__dirname, '../packages/rum-slim/bundle/datadog-rum-slim.js')
 const workerPath = path.join(__dirname, '../packages/worker/bundle/worker.js')
 const versionPath = path.join(__dirname, '../packages/rum/package.json')
+
+const URL = 'https://http-intake.logs.datadoghq.com/api/v2/logs'
+const HEADERS = {
+  'DD-API-KEY': getOrg2ApiKey(),
+  'Content-Type': 'application/json',
+}
 
 runMain(async () => {
   const logData = [
@@ -24,32 +30,48 @@ runMain(async () => {
         worker: getBundleSize(workerPath),
       },
       version: getVersion(),
-      commit: getCommitId(),
+      commit: getGitInformation('git rev-parse HEAD'),
+      branch: getGitInformation('git rev-parse --abbrev-ref HEAD'),
     },
   ]
-  await postBundleSize(url, logData)
+  await postBundleSize(URL, logData)
 })
 
-const getCommitId = () => childProcess.execSync('git rev-parse HEAD').toString().trim()
+function getGitInformation(command) {
+  try {
+    return execSync(command)
+      .toString('utf8')
+      .replace(/[\n\r\s]+$/, '')
+  } catch (error) {
+    console.error('Failed to execute git command:', error)
+    return null
+  }
+}
 
 function getVersion() {
-  const versionJson = fs.readFileSync(versionPath, 'utf8')
-  return JSON.parse(versionJson).version
+  try {
+    const versionJson = fs.readFileSync(versionPath, 'utf8')
+    return JSON.parse(versionJson).version
+  } catch (error) {
+    console.error('Failed to get version:', error)
+    return null
+  }
 }
 
 function getBundleSize(pathBundle) {
-  const file = fs.statSync(pathBundle)
-  return file.size
+  try {
+    const file = fs.statSync(pathBundle)
+    return file.size
+  } catch (error) {
+    console.error('Failed to get bundle size:', error)
+    return null
+  }
 }
 
 async function postBundleSize(url = '', bundleData = {}) {
-  const response = await fetch(url, {
+  await fetchWrapper(url, {
     method: 'POST',
-    headers: {
-      'DD-API-KEY': getOrg2ApiKey(),
-      'Content-Type': 'application/json',
-    },
+    headers: HEADERS,
     body: JSON.stringify(bundleData),
   })
-  console.log(await response.text())
 }
