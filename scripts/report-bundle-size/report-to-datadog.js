b3cc81166dacfa9c58be5927adf7d4b481e6f4de
RomanGaignault <158156364+RomanGaignault@users.noreply.github.com>
2024-03-11 15:24:36 +0100

    ✨[RUM-3255] Report the bundle sizes differences as a PR comment (#2629)
    * first commit
    
    * bumped chrome version
    
    * added fetchwrapper
    
    * Error fetch
    
    * changed difference name
    
    * chore : changed names
    
    * Update Dockerfile
    
    Co-authored-by: Bastien Caudan <1331991+bcaudan@users.noreply.github.com>
    
    * nitpick / command instead of execSync / KB conversion / filename
    
    * timeout
    
    * fetch origin
    
    * pr header
    
    * prcommentheader
    
    * throw error if more than 1 pr / nitpick and formatSize function
    
    * format size update
    
    * exit instead of throw error
    
    * extracting retries as a const
    
    * deleted getPR function
    
    * revert fetch PR
    
    * Deleted the buildQuery function and set packageName as a constant to use it directly in the query.
    
    * extract query as a constant
    
    * deleted name in create message
    
    * revert query
    
    * extract query as a constant
    
    * call report bundle size from export bundle size. Deleted retries because we don't need to fetch local metrics anymore.
    
    * update name
    
    * update import
    
    * export
    
    * fixed compare
    
    * create message update
    
    * Divided script into 3 files : index.js / report-to-datadog / report-as-a-pr-comment.
    
    * deleted old scripts
    
    * update path import
    
    * added main function in report to datadog, more precise name to report as a pr comment function
    
    ---------
    
    Co-authored-by: Bastien Caudan <1331991+bcaudan@users.noreply.github.com>

---
+++ scripts/report-bundle-size/report-to-datadog.js
@@ -0,0 +1,39 @@
+const { fetch } = require('../lib/execution-utils')
+const { getOrg2ApiKey } = require('../lib/secrets')
+const { browserSdkVersion } = require('../lib/browser-sdk-version')
+
+const LOG_INTAKE_URL = 'https://http-intake.logs.datadoghq.com/api/v2/logs'
+const LOG_INTAKE_REQUEST_HEADERS = {
+  'DD-API-KEY': getOrg2ApiKey(),
+  'Content-Type': 'application/json',
+}
+
+async function reportBundleSizesToDatadog(bundleSizes) {
+  const logData = createLogData(bundleSizes, browserSdkVersion)
+  await sendLogToOrg2(logData)
+}
+
+function createLogData(bundleSizes, browserSdkVersion) {
+  return [
+    {
+      message: 'Browser SDK bundles sizes',
+      service: 'browser-sdk',
+      ddsource: 'browser-sdk',
+      env: 'ci',
+      bundle_sizes: bundleSizes,
+      version: browserSdkVersion,
+      commit: process.env.CI_COMMIT_SHORT_SHA,
+      branch: process.env.CI_COMMIT_REF_NAME,
+    },
+  ]
+}
+
+async function sendLogToOrg2(bundleData = {}) {
+  await fetch(LOG_INTAKE_URL, {
+    method: 'POST',
+    headers: LOG_INTAKE_REQUEST_HEADERS,
+    body: JSON.stringify(bundleData),
+  })
+}
+
+module.exports = { reportBundleSizesToDatadog }
