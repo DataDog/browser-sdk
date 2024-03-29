1659a6ec43ee6b8f23ab76e6e7df781d14cb888d
roman.gaignault <roman.gaignault@datadoghq.com>
2024-03-29 15:45:22 +0100

    set cpu performance

--- scripts/performance/index.js
+++ scripts/performance/index.js
@@ -2,11 +2,11 @@ const { runMain } = require('../lib/execution-utils')
 const { reportAsPrComment } = require('./report-as-a-pr-comment')
 const { reportToDatadog } = require('./report-to-datadog')
 const { calculateBundleSizes } = require('./bundle-size/bundle-size-calculator')
-const { calculateCpuPerformance } = require('./cpu-performance/cpu-performance-calculator.js')
+// const { calculateCpuPerformance } = require('./cpu-performance/cpu-performance-calculator.js')
 
 runMain(async () => {
   const bundleSizes = calculateBundleSizes()
-  const cpuPerformance = calculateCpuPerformance()
+  const cpuPerformance = []
 
   await reportToDatadog(bundleSizes)
   await reportAsPrComment(bundleSizes, cpuPerformance)
