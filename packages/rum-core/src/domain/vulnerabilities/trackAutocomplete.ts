cf171466c3813093b80db0993d318e4311ddf993
Igor Unanua <igor.unanua@datadoghq.com>
2024-04-02 11:52:32 +0200

    Search for autocomplete attribute

--- packages/rum-core/src/domain/vulnerabilities/trackAutocomplete.ts
+++ packages/rum-core/src/domain/vulnerabilities/trackAutocomplete.ts
@@ -1,20 +1,37 @@
 
-import type { Observable, Vulnerability } from '@datadog/browser-core';
+import { VulnerabilityType, type Observable, type Vulnerability } from '@datadog/browser-core';
 import type { LifeCycle} from '../lifeCycle';
 import { LifeCycleEventType } from '../lifeCycle';
 
 
 export function trackAutocomplete(lifeCycle: LifeCycle, vulnerabilityObservable: Observable<Vulnerability>) {
-  lifeCycle.subscribe(LifeCycleEventType.VIEW_UPDATED, (view) => {
+  const subscription = lifeCycle.subscribe(LifeCycleEventType.VIEW_UPDATED, () => {
     const forms = document.querySelectorAll<HTMLFormElement>('form')
 
     forms.forEach(form => {
-      console.log(`form ${form.name} autocomplete: ${form.getAttribute('autocomplete')}`, form)
+      notifyAutocomplete(form.getAttribute('autocomplete'), form, vulnerabilityObservable)
 
-      const inputs = form.querySelectorAll('input')
-      inputs.forEach(input => {
-        console.log(` - input ${input.name} autocomplete: ${input.getAttribute('autocomplete')}`, input)
+      const elements = form.querySelectorAll<HTMLElement>('input, textarea, select')
+      elements.forEach(element => {
+        notifyAutocomplete(element.getAttribute('autocomplete'), element, vulnerabilityObservable)
       })
     })
   })
+
+  return {
+    stop: () => {
+      subscription.unsubscribe()
+    },
+  }
+}
+
+function notifyAutocomplete(autocompleteValue: string | null, element: HTMLElement, vulnerabilityObservable: Observable<Vulnerability>) {
+  if (autocompleteValue === 'on') {
+    vulnerabilityObservable.notify({
+      type: VulnerabilityType.AUTOCOMPLETE,
+
+      // TODO: obtain location: file, line, column...
+      element
+    })
+  }
 }
