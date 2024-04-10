7c6fccfcdca5acd6939b5ea13c9f9ec3ecdb2e2a
roman.gaignault <roman.gaignault@datadoghq.com>
2024-04-10 14:53:16 +0200

    update waiting time synthetic

--- scripts/performance/cpu-performance/synthetic-trigger.js
+++ scripts/performance/cpu-performance/synthetic-trigger.js
@@ -1,31 +1,53 @@
 const { fetchHandlingError } = require('../../lib/execution-utils')
 const { getOrg2ApiKey, getOrg2AppKey } = require('../../lib/secrets')
-const apiKey = getOrg2ApiKey()
-const appKey = getOrg2AppKey()
-
-const url = 'https://api.datadoghq.com/api/v1/synthetics/tests/trigger/ci'
+const { timeout } = require('../../lib/execution-utils')
+const API_KEY = getOrg2ApiKey()
+const APP_KEY = getOrg2AppKey()
+const TIMEOUT_IN_MS = 10000
+const TEST_PUBLIC_ID = 'vcg-7rk-5av'
 
 async function syntheticTrigger(prNumber, commitId) {
   const body = {
     tests: [
       {
-        public_id: 'vcg-7rk-5av',
+        public_id: `${TEST_PUBLIC_ID}`,
         startUrl: `https://datadoghq.dev/browser-sdk-test-playground/performance/?prNumber=${prNumber}&commitId=${commitId}`,
       },
     ],
   }
-
-  await fetchHandlingError(url, {
+  const url = 'https://api.datadoghq.com/api/v1/synthetics/tests/trigger/ci'
+  const response = await fetchHandlingError(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'DD-API-KEY': apiKey,
-      'DD-APPLICATION-KEY': appKey,
+      'DD-API-KEY': API_KEY,
+      'DD-APPLICATION-KEY': APP_KEY,
     },
     body: JSON.stringify(body),
   })
+  const data = await response.json()
+  return data.results[0].result_id
 }
 
+async function getSyntheticTestResult(resultId, RETRIES_NUMBER) {
+  const url = `https://api.datadoghq.com/api/v1/synthetics/tests/${TEST_PUBLIC_ID}/results/${resultId}`
+  for (let i = 0; i < RETRIES_NUMBER; i++) {
+    const response = await fetchHandlingError(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'DD-API-KEY': API_KEY,
+        'DD-APPLICATION-KEY': APP_KEY,
+      },
+    })
+    const data = await response.json()
+    if (data.length !== 0 && data.status === 0) {
+      break
+    }
+    await timeout(TIMEOUT_IN_MS)
+  }
+}
 module.exports = {
+  getSyntheticTestResult,
   syntheticTrigger,
 }
--- scripts/performance/index.js
+++ scripts/performance/index.js
@@ -3,14 +3,15 @@ const { fetchPR, LOCAL_BRANCH } = require('../lib/git-utils')
 const { reportAsPrComment, LOCAL_COMMIT_SHA } = require('./report-as-a-pr-comment')
 const { reportToDatadog } = require('./report-to-datadog')
 const { calculateBundleSizes } = require('./bundle-size/bundle-size-calculator')
-const { syntheticTrigger } = require('./cpu-performance/synthetic-trigger')
-const ONE_MINUTE_IN_SECOND = 60 * 1000
+const { syntheticTrigger, getSyntheticTestResult } = require('./cpu-performance/synthetic-trigger')
+
+const RETRIES_NUMBER = 6 // Number of retries to get the synthetic test result
 
 runMain(async () => {
-  const PR_NUMBER = (await fetchPR(LOCAL_BRANCH)).number
+  const prNumber = (await fetchPR(LOCAL_BRANCH)).number
   const bundleSizes = calculateBundleSizes()
-  await syntheticTrigger(PR_NUMBER, LOCAL_COMMIT_SHA)
-  await new Promise((resolve) => setTimeout(resolve, ONE_MINUTE_IN_SECOND)) // Waiting for synthetic test to finish
+  const resultId = await syntheticTrigger(prNumber, LOCAL_COMMIT_SHA)
+  await getSyntheticTestResult(resultId, RETRIES_NUMBER) // Waiting for synthetic test to finish
   await reportToDatadog(bundleSizes)
   await reportAsPrComment(bundleSizes)
 })
