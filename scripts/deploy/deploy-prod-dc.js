10b7915460a76651c513043254ecf6285d29bf46
Bastien Caudan <bastien.caudan@datadoghq.com>
2024-04-22 16:56:54 +0200

    👌 develop deploy-auto script into gitlab jobs

--- scripts/deploy/deploy-prod-dc.js
+++ scripts/deploy/deploy-prod-dc.js
@@ -2,33 +2,27 @@ const { printLog, runMain, timeout } = require('../lib/execution-utils')
 const { command } = require('../lib/command')
 
 /**
- * Orchestrate the deployments of all the artifacts of a release
+ * Orchestrate the deployments of the artifacts for specific DCs
  * Usage:
- * node deploy-auto.js vXXX
+ * node deploy-prod-dc.js vXXX us1,eu1,... true|false
  */
 const ONE_MINUTE_IN_SECOND = 60
 const GATE_DURATION = 30 * ONE_MINUTE_IN_SECOND
 const GATE_INTERVAL = ONE_MINUTE_IN_SECOND
 
 const version = process.argv[2]
+const uploadPath = process.argv[3]
+const withGateMonitors = process.argv[4] === 'true'
 
 runMain(async () => {
-  for await (const uploadPath of ['us3,us5,ap1', 'eu1', 'us1']) {
-    deploy(uploadPath)
-    await gateMonitors(uploadPath)
-  }
-
-  deploy('root') // gov
-  command`node ./scripts/deploy/publish-npm.js`.withLogs().run()
-  command`node ./scripts/deploy/publish-developer-extension.js`.withLogs().run()
-})
-
-function deploy(uploadPath) {
-  printLog(`Deploy artifacts for ${uploadPath}`)
   command`node ./scripts/deploy/check-monitors.js ${uploadPath}`.withLogs().run()
   command`node ./scripts/deploy/deploy.js prod v${version} ${uploadPath}`.withLogs().run()
   command`node ./scripts/deploy/upload-source-maps.js prod v${version} ${uploadPath}`.withLogs().run()
-}
+
+  if (withGateMonitors && uploadPath !== 'root') {
+    await gateMonitors(uploadPath)
+  }
+})
 
 async function gateMonitors(uploadPath) {
   printLog(`Check monitors for ${uploadPath} during ${GATE_DURATION / ONE_MINUTE_IN_SECOND} minutes`)
