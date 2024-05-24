60e4d27125958c6d5165555f08b4a2d64e114039
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2024-05-24 14:29:25 +0200

    ✅ fix clearing cookies between tests

--- packages/core/test/forEach.spec.ts
+++ packages/core/test/forEach.spec.ts
@@ -9,10 +9,13 @@ beforeEach(() => {
   // prevent 'Some of your tests did a full page reload!' issue
   window.onbeforeunload = () => 'stop'
   startLeakDetection()
+  // Note: clearing cookies should be done in `beforeEach` rather than `afterEach`, because in some
+  // cases the test patches the `document.cookie` getter (ex: `spyOnProperty(document, 'cookie',
+  // 'get')`), which would prevent the `clearAllCookies` function from working properly.
+  clearAllCookies()
 })
 
 afterEach(() => {
-  clearAllCookies()
   stopLeakDetection()
 })
 
