7cfb9e070ffb7d6ad35833f55b215589e0c32e59
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2024-05-24 10:29:13 +0200

    👌 add type to e2e tests

--- test/e2e/scenario/microfrontend.scenario.ts
+++ test/e2e/scenario/microfrontend.scenario.ts
@@ -1,11 +1,12 @@
+import type { RumEvent, RumEventDomainContext, RumInitConfiguration } from '@datadog/browser-rum-core/src'
 import { withBrowserLogs } from '../lib/helpers/browser'
 import { flushEvents, createTest } from '../lib/framework'
 
 const HANDLING_STACK_REGEX = /^Error: \n\s+at testHandlingStack @/
 
-const CONFIG = {
+const CONFIG: Partial<RumInitConfiguration> = {
   enableExperimentalFeatures: ['micro_frontend'],
-  beforeSend: (event: any, domainContext: any) => {
+  beforeSend: (event: RumEvent, domainContext: RumEventDomainContext) => {
     if ('handlingStack' in domainContext) {
       event.context!.handlingStack = domainContext.handlingStack
     }
@@ -98,30 +99,28 @@ describe('microfrontend', () => {
       expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
     })
 
-  describe('expose handling stack for console errors', () => {
-    createTest('expose handling stack for console errors')
-      .withRum(CONFIG)
-      .withRumInit((configuration) => {
-        window.DD_RUM!.init(configuration)
+  createTest('expose handling stack for console errors')
+    .withRum(CONFIG)
+    .withRumInit((configuration) => {
+      window.DD_RUM!.init(configuration)
 
-        function testHandlingStack() {
-          console.error('foo')
-        }
+      function testHandlingStack() {
+        console.error('foo')
+      }
 
-        testHandlingStack()
+      testHandlingStack()
+    })
+    .run(async ({ intakeRegistry }) => {
+      await flushEvents()
+
+      const event = intakeRegistry.rumErrorEvents[0]
+
+      await withBrowserLogs((logs) => {
+        expect(logs.length).toBe(1)
+        expect(logs[0].message).toMatch(/"foo"$/)
       })
-      .run(async ({ intakeRegistry }) => {
-        await flushEvents()
 
-        const event = intakeRegistry.rumErrorEvents[0]
-
-        await withBrowserLogs((logs) => {
-          expect(logs.length).toBe(1)
-          expect(logs[0].message).toMatch(/"foo"$/)
-        })
-
-        expect(event).toBeTruthy()
-        expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
-      })
-  })
+      expect(event).toBeTruthy()
+      expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
+    })
 })
