39276a7d025754c196d33cad38ad43fe90b3795e
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2024-05-30 10:29:02 +0200

    ✨ [RUM-4013] DD_RUM: add handling stack in beforeSend context (#2730)
    * ✨ [RUM-4013] add handling stack in beforeSend context
    
    * add handling stack for addErrors and addAction
    
    * 🧪 add e2e tests
    
    * add e2e and loose up unit tests
    
    * remove unused variable
    
    * 🧪 add missing test coverage
    
    * ✅ add e2e test for console errors
    
    * fix missing imports
    
    * remove unused imports
    
    * 👌 add type to e2e tests
    
    * 👌 fix pr comments

--- test/e2e/scenario/microfrontend.scenario.ts
+++ test/e2e/scenario/microfrontend.scenario.ts
@@ -123,6 +123,129 @@ describe('microfrontend', () => {
       expect(event).toBeTruthy()
       expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
     })
+})
+
+const HANDLING_STACK_REGEX = /^Error: \n\s+at testHandlingStack @/
+
+const CONFIG: Partial<RumInitConfiguration> = {
+  enableExperimentalFeatures: ['micro_frontend'],
+  beforeSend: (event: RumEvent, domainContext: RumEventDomainContext) => {
+    if ('handlingStack' in domainContext) {
+      event.context!.handlingStack = domainContext.handlingStack
+    }
+
+    return true
+  },
+}
+
+describe('microfrontend', () => {
+  createTest('expose handling stack for fetch requests')
+    .withRum(CONFIG)
+    .withRumInit((configuration) => {
+      window.DD_RUM!.init(configuration)
+
+      const noop = () => {}
+      function testHandlingStack() {
+        fetch('/ok').then(noop, noop)
+      }
+
+      testHandlingStack()
+    })
+    .run(async ({ intakeRegistry }) => {
+      await flushEvents()
+
+      const event = intakeRegistry.rumResourceEvents.find((event) => event.resource.type === 'fetch')
+
+      expect(event).toBeTruthy()
+      expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
+    })
+
+  createTest('expose handling stack for xhr requests')
+    .withRum(CONFIG)
+    .withRumInit((configuration) => {
+      window.DD_RUM!.init(configuration)
+
+      function testHandlingStack() {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', '/ok')
+        xhr.send()
+      }
+
+      testHandlingStack()
+    })
+    .run(async ({ intakeRegistry }) => {
+      await flushEvents()
+
+      const event = intakeRegistry.rumResourceEvents.find((event) => event.resource.type === 'xhr')
+
+      expect(event).toBeTruthy()
+      expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
+    })
+
+  createTest('expose handling stack for DD_RUM.addAction')
+    .withRum(CONFIG)
+    .withRumInit((configuration) => {
+      window.DD_RUM!.init(configuration)
+
+      function testHandlingStack() {
+        window.DD_RUM!.addAction('foo')
+      }
+
+      testHandlingStack()
+    })
+    .run(async ({ intakeRegistry }) => {
+      await flushEvents()
+
+      const event = intakeRegistry.rumActionEvents[0]
+
+      expect(event).toBeTruthy()
+      expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
+    })
+
+  createTest('expose handling stack for DD_RUM.addError')
+    .withRum(CONFIG)
+    .withRumInit((configuration) => {
+      window.DD_RUM!.init(configuration)
+
+      function testHandlingStack() {
+        window.DD_RUM!.addError(new Error('foo'))
+      }
+
+      testHandlingStack()
+    })
+    .run(async ({ intakeRegistry }) => {
+      await flushEvents()
+
+      const event = intakeRegistry.rumErrorEvents[0]
+
+      expect(event).toBeTruthy()
+      expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
+    })
+
+  createTest('expose handling stack for console errors')
+    .withRum(CONFIG)
+    .withRumInit((configuration) => {
+      window.DD_RUM!.init(configuration)
+
+      function testHandlingStack() {
+        console.error('foo')
+      }
+
+      testHandlingStack()
+    })
+    .run(async ({ intakeRegistry }) => {
+      await flushEvents()
+
+      const event = intakeRegistry.rumErrorEvents[0]
+
+      await withBrowserLogs((logs) => {
+        expect(logs.length).toBe(1)
+        expect(logs[0].message).toMatch(/"foo"$/)
+      })
+
+      expect(event).toBeTruthy()
+      expect(event?.context?.handlingStack).toMatch(HANDLING_STACK_REGEX)
+    })
 
   createTest('allow to modify service and version')
     .withRum(CONFIG)
