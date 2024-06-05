0c3d32fca91e0648cc5545945b1f84d2053b29a6
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-06-05 16:06:58 +0200

    Improve createPostStartStrategy

--- packages/rum-core/src/boot/rumPublicApi.ts
+++ packages/rum-core/src/boot/rumPublicApi.ts
@@ -382,7 +382,7 @@ export function makeRumPublicApi(
         deflateWorker
       )
 
-      strategy = createPostStartStrategy(strategy.initConfiguration!, startRumResult)
+      strategy = createPostStartStrategy(strategy, startRumResult)
 
       return startRumResult
     }
@@ -500,13 +500,13 @@ export function makeRumPublicApi(
   return rumPublicApi
 }
 
-function createPostStartStrategy(initConfiguration: RumInitConfiguration, startRumResult: StartRumResult): Strategy {
+function createPostStartStrategy(preStartStrategy: Strategy, startRumResult: StartRumResult): Strategy {
   return assign(
     {
       init: (initConfiguration: RumInitConfiguration) => {
         displayAlreadyInitializedError('DD_RUM', initConfiguration)
       },
-      initConfiguration,
+      initConfiguration: preStartStrategy.initConfiguration,
     },
     startRumResult
   )
