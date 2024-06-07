5b37fd43fa59656427a91c506642e149fe407ece
Aymeric <aymeric.mortemousque@datadoghq.com>
2024-06-07 15:53:10 +0200

    ♻️ Sends the conf telemetry from preStartRum (#2795)

--- packages/rum-core/src/boot/preStartRum.ts
+++ packages/rum-core/src/boot/preStartRum.ts
@@ -9,6 +9,7 @@ import {
   clocksNow,
   assign,
   getEventBridge,
+  addTelemetryConfiguration,
   initFeatureFlags,
 } from '@datadog/browser-core'
 import type { TrackingConsentState, DeflateWorker } from '@datadog/browser-core'
@@ -19,6 +20,7 @@ import {
 } from '../domain/configuration'
 import type { CommonContext } from '../domain/contexts/commonContext'
 import type { ViewOptions } from '../domain/view/trackViews'
+import { serializeRumConfiguration } from '../domain/configuration'
 import type { RumPublicApiOptions, Strategy } from './rumPublicApi'
 import type { StartRumResult } from './startRum'
 
@@ -27,7 +29,6 @@ export function createPreStartStrategy(
   getCommonContext: () => CommonContext,
   trackingConsentState: TrackingConsentState,
   doStartRum: (
-    initConfiguration: RumInitConfiguration,
     configuration: RumConfiguration,
     deflateWorker: DeflateWorker | undefined,
     initialViewOptions?: ViewOptions
@@ -67,7 +68,7 @@ export function createPreStartStrategy(
       initialViewOptions = firstStartViewCall.options
     }
 
-    const startRumResult = doStartRum(cachedInitConfiguration, cachedConfiguration, deflateWorker, initialViewOptions)
+    const startRumResult = doStartRum(cachedConfiguration, deflateWorker, initialViewOptions)
 
     bufferApiCalls.drain(startRumResult)
   }
@@ -89,6 +90,7 @@ export function createPreStartStrategy(
 
       // Expose the initial configuration regardless of initialization success.
       cachedInitConfiguration = initConfiguration
+      addTelemetryConfiguration(serializeRumConfiguration(initConfiguration))
 
       if (cachedConfiguration) {
         displayAlreadyInitializedError('DD_RUM', initConfiguration)
