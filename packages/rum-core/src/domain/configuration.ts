0582b8a006bcc3b3d55b111992685620439425e1
cy-moi <congyao119@gmail.com>
2024-06-10 16:35:15 +0200

    Fix typo

--- packages/rum-core/src/domain/configuration.ts
+++ packages/rum-core/src/domain/configuration.ts
@@ -135,7 +135,7 @@ export function validateAndBuildRumConfiguration(
         : DefaultPrivacyLevel.MASK,
       enablePrivacyForActionName:
         isExperimentalFeatureEnabled(ExperimentalFeature.ENABLE_PRIVACY_FOR_ACTION_NAME) &&
-          !!initConfiguration.enablePrivacyForActionName
+        !!initConfiguration.enablePrivacyForActionName,
       customerDataTelemetrySampleRate: 1,
       traceContextInjection: objectHasValue(TraceContextInjection, initConfiguration.traceContextInjection)
         ? initConfiguration.traceContextInjection
