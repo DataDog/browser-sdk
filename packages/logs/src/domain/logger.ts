6285b92ff801befbb9029f480fdbecfd7015c2dc
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2024-06-11 12:37:02 +0200

    ♻️ refactor Logger to reduce bundle size

--- packages/logs/src/domain/logger.ts
+++ packages/logs/src/domain/logger.ts
@@ -49,145 +49,8 @@ export class Logger {
     }
   }
 
-  log(message: string, messageContext?: object, status: StatusType = StatusType.info, error?: Error) {
-    // note: generating the handling stack is intentionally duplicated in each of the logger method to save a frame in the stack
-    let handlingStack: string | undefined
-
-    if (isAuthorized(status, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, status, error, handlingStack)
-  }
-
-  ok(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.ok, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.ok, error, handlingStack)
-  }
-
-  debug(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.debug, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.debug, error, handlingStack)
-  }
-
-  info(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.info, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.info, error, handlingStack)
-  }
-
-  notice(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.notice, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.notice, error, handlingStack)
-  }
-
-  warn(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.warn, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.warn, error, handlingStack)
-  }
-
-  error(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.error, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.error, error, handlingStack)
-  }
-
-  critical(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.critical, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.critical, error, handlingStack)
-  }
-
-  alert(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.alert, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.alert, error, handlingStack)
-  }
-
-  emerg(message: string, messageContext?: object, error?: Error) {
-    let handlingStack: string | undefined
-
-    if (isAuthorized(StatusType.emerg, HandlerType.http, this)) {
-      handlingStack = createHandlingStack()
-    }
-
-    this.logImplementation(message, messageContext, StatusType.emerg, error, handlingStack)
-  }
-
-  setContext(context: object) {
-    this.contextManager.setContext(context as Context)
-  }
-
-  getContext() {
-    return this.contextManager.getContext()
-  }
-
-  setContextProperty(key: string, value: any) {
-    this.contextManager.setContextProperty(key, value)
-  }
-
-  removeContextProperty(key: string) {
-    this.contextManager.removeContextProperty(key)
-  }
-
-  clearContext() {
-    this.contextManager.clearContext()
-  }
-
-  setHandler(handler: HandlerType | HandlerType[]) {
-    this.handlerType = handler
-  }
-
-  getHandler() {
-    return this.handlerType
-  }
-
-  setLevel(level: StatusType) {
-    this.level = level
-  }
-
-  getLevel() {
-    return this.level
-  }
-
   @monitored
-  private logImplementation(
+  logImplementation(
     message: string,
     messageContext?: object,
     status: StatusType = StatusType.info,
@@ -230,5 +93,87 @@ export class Logger {
       this,
       handlingStack
     )
+  }
+
+  log(message: string, messageContext?: object, status: StatusType = StatusType.info, error?: Error) {
+    let handlingStack: string | undefined
+
+    if (isAuthorized(status, HandlerType.http, this)) {
+      handlingStack = createHandlingStack()
+    }
+
+    this.logImplementation(message, messageContext, status, error, handlingStack)
+  }
+
+  // plaheholders for type definition, the actual implementation is done in the prototype level
+  ok(_message: string, _messageContext?: object, _error?: Error): void {}
+  debug(_message: string, _messageContext?: object, _error?: Error): void {}
+  info(_message: string, _messageContext?: object, _error?: Error): void {}
+  notice(_message: string, _messageContext?: object, _error?: Error): void {}
+  warn(_message: string, _messageContext?: object, _error?: Error): void {}
+  error(_message: string, _messageContext?: object, _error?: Error): void {}
+  critical(_message: string, _messageContext?: object, _error?: Error): void {}
+  alert(_message: string, _messageContext?: object, _error?: Error): void {}
+  emerg(_message: string, _messageContext?: object, _error?: Error): void {}
+
+  setContext(context: object) {
+    this.contextManager.setContext(context as Context)
+  }
+
+  getContext() {
+    return this.contextManager.getContext()
+  }
+
+  setContextProperty(key: string, value: any) {
+    this.contextManager.setContextProperty(key, value)
+  }
+
+  removeContextProperty(key: string) {
+    this.contextManager.removeContextProperty(key)
+  }
+
+  clearContext() {
+    this.contextManager.clearContext()
+  }
+
+  setHandler(handler: HandlerType | HandlerType[]) {
+    this.handlerType = handler
+  }
+
+  getHandler() {
+    return this.handlerType
+  }
+
+  setLevel(level: StatusType) {
+    this.level = level
+  }
+
+  getLevel() {
+    return this.level
+  }
+}
+
+/* eslint-disable local-rules/disallow-side-effects */
+Logger.prototype.ok = createLoggerMethod(StatusType.ok)
+Logger.prototype.debug = createLoggerMethod(StatusType.debug)
+Logger.prototype.info = createLoggerMethod(StatusType.info)
+Logger.prototype.notice = createLoggerMethod(StatusType.notice)
+Logger.prototype.warn = createLoggerMethod(StatusType.warn)
+Logger.prototype.error = createLoggerMethod(StatusType.error)
+Logger.prototype.critical = createLoggerMethod(StatusType.critical)
+Logger.prototype.alert = createLoggerMethod(StatusType.alert)
+Logger.prototype.emerg = createLoggerMethod(StatusType.emerg)
+
+/* eslint-enable local-rules/disallow-side-effects */
+
+function createLoggerMethod(status: StatusType) {
+  return function (this: Logger, message: string, messageContext?: object, error?: Error) {
+    let handlingStack: string | undefined
+
+    if (isAuthorized(status, HandlerType.http, this)) {
+      handlingStack = createHandlingStack()
+    }
+
+    this.logImplementation(message, messageContext, status, error, handlingStack)
   }
 }
