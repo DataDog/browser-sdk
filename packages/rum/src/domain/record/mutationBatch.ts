a644eadb8a8bea5233323337b6492843530cf74e
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-06-12 15:00:24 +0200

    Add comment

--- packages/rum/src/domain/record/mutationBatch.ts
+++ packages/rum/src/domain/record/mutationBatch.ts
@@ -8,6 +8,11 @@ import type { RumMutationRecord } from './trackers'
  * browser is busy executing a longer task, mutations will be processed after this task.
  */
 const MUTATION_PROCESS_MAX_DELAY = 100
+/**
+ * Minimum duration to wait before processing mutations. This is used to batch mutations together
+ * and be able to deduplicate them to save processing time and bandwidth.
+ * 16ms is the duration of a frame at 60fps that ensure fluid UI.
+ */
 export const MUTATION_PROCESS_MIN_DELAY = 16
 
 export function createMutationBatch(processMutationBatch: (mutations: RumMutationRecord[]) => void) {
