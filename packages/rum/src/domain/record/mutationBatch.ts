bafeb7aa15957c1aed2479748e61404e98bd3093
Aymeric <aymeric.mortemousque@datadoghq.com>
2024-06-14 09:45:46 +0200

    ⚡️[RUM-3570] Batch the records for 16ms minimum before processing them (#2807)

--- packages/rum/src/domain/record/mutationBatch.ts
+++ packages/rum/src/domain/record/mutationBatch.ts
@@ -1,4 +1,4 @@
-import { noop, monitor } from '@datadog/browser-core'
+import { noop, monitor, throttle } from '@datadog/browser-core'
 import type { RumMutationRecord } from './trackers'
 
 /**
@@ -8,6 +8,12 @@ import type { RumMutationRecord } from './trackers'
  * browser is busy executing a longer task, mutations will be processed after this task.
  */
 const MUTATION_PROCESS_MAX_DELAY = 100
+/**
+ * Minimum duration to wait before processing mutations. This is used to batch mutations together
+ * and be able to deduplicate them to save processing time and bandwidth.
+ * 16ms is the duration of a frame at 60fps that ensure fluid UI.
+ */
+export const MUTATION_PROCESS_MIN_DELAY = 16
 
 export function createMutationBatch(processMutationBatch: (mutations: RumMutationRecord[]) => void) {
   let cancelScheduledFlush = noop
@@ -19,10 +25,14 @@ export function createMutationBatch(processMutationBatch: (mutations: RumMutatio
     pendingMutations = []
   }
 
+  const { throttled: throttledFlush, cancel: cancelThrottle } = throttle(flush, MUTATION_PROCESS_MIN_DELAY, {
+    leading: false,
+  })
+
   return {
     addMutations: (mutations: RumMutationRecord[]) => {
       if (pendingMutations.length === 0) {
-        cancelScheduledFlush = requestIdleCallback(flush, { timeout: MUTATION_PROCESS_MAX_DELAY })
+        cancelScheduledFlush = requestIdleCallback(throttledFlush, { timeout: MUTATION_PROCESS_MAX_DELAY })
       }
       pendingMutations.push(...mutations)
     },
@@ -31,6 +41,7 @@ export function createMutationBatch(processMutationBatch: (mutations: RumMutatio
 
     stop: () => {
       cancelScheduledFlush()
+      cancelThrottle()
     },
   }
 }
