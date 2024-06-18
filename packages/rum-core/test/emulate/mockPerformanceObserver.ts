8c66535b1ad848d547ebc0272c867f618e51ba67
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-06-18 15:31:22 +0200

    fixup! Introduce performanceObservable

---
+++ packages/rum-core/test/emulate/mockPerformanceObserver.ts
@@ -0,0 +1,51 @@
+import { registerCleanupTask } from '@datadog/browser-core/test'
+import type { RumPerformanceEntry } from '../../src/browser/performanceObservable'
+
+export function mockPerformanceObserver() {
+  const originalPerformanceObserver = window.PerformanceObserver
+  const instances = new Set<{
+    callback: PerformanceObserverCallback
+    entryTypes: string[]
+  }>()
+  let performanceObserver: PerformanceObserver
+
+  window.PerformanceObserver = function (callback: PerformanceObserverCallback) {
+    const instance = { callback, entryTypes: [] as string[] }
+
+    performanceObserver = {
+      disconnect() {
+        instances.delete(instance)
+      },
+      observe({ entryTypes, type }: PerformanceObserverInit) {
+        instance.entryTypes = entryTypes || (type ? [type] : [])
+        instances.add(instance)
+      },
+      takeRecords() {
+        return []
+      },
+    }
+    return performanceObserver
+  } as unknown as typeof originalPerformanceObserver
+
+  registerCleanupTask(() => {
+    window.PerformanceObserver = originalPerformanceObserver
+    instances.clear()
+  })
+
+  return {
+    notifyPerformanceEntry: (entry: RumPerformanceEntry) => {
+      instances.forEach(({ callback, entryTypes }) => {
+        if (entryTypes && entryTypes.includes(entry.entryType)) {
+          callback(
+            {
+              getEntries: () => [entry] as PerformanceEntryList,
+              getEntriesByName: () => [entry] as PerformanceEntryList,
+              getEntriesByType: () => [entry] as PerformanceEntryList,
+            },
+            performanceObserver
+          )
+        }
+      })
+    },
+  }
+}
