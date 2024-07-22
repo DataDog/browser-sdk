6c2ed51b10c2500ae95d1f187866e8335899c758
Najib Boutaib <najib.boutaib@datadoghq.com>
2024-07-22 16:33:01 +0200

    Discard Segment class in favour of function

--- packages/rum/src/domain/segmentCollection/segment.ts
+++ packages/rum/src/domain/segmentCollection/segment.ts
@@ -8,55 +8,60 @@ export type FlushReason = Exclude<CreationReason, 'init'> | 'stop'
 export type FlushCallback = (metadata: BrowserSegmentMetadata, encoderResult: EncoderResult<Uint8Array>) => void
 export type AddRecordCallback = (encodedBytesCount: number) => void
 
-export class Segment {
-  private metadata: BrowserSegmentMetadata
-  private encodedBytesCount = 0
+export interface Segment {
+  addRecord: (record: BrowserRecord, callback: AddRecordCallback) => void
+  flush: (callback: FlushCallback) => void
+}
 
-  constructor(
-    private encoder: Encoder<Uint8Array>,
-    context: SegmentContext,
-    creationReason: CreationReason
-  ) {
-    const viewId = context.view.id
+export function segmentFactory({
+  context,
+  creationReason,
+  encoder,
+}: {
+  context: SegmentContext
+  creationReason: CreationReason
+  encoder: Encoder<Uint8Array>
+}): Segment {
+  let encodedBytesCount = 0
+  const viewId = context.view.id
+  const metadata: BrowserSegmentMetadata = assign(
+    {
+      start: Infinity,
+      end: -Infinity,
+      creation_reason: creationReason,
+      records_count: 0,
+      has_full_snapshot: false,
+      index_in_view: replayStats.getSegmentsCount(viewId),
+      source: 'browser' as const,
+    },
+    context
+  )
+  replayStats.addSegment(viewId)
 
-    this.metadata = assign(
-      {
-        start: Infinity,
-        end: -Infinity,
-        creation_reason: creationReason,
-        records_count: 0,
-        has_full_snapshot: false,
-        index_in_view: replayStats.getSegmentsCount(viewId),
-        source: 'browser' as const,
-      },
-      context
-    )
+  function addRecord(record: BrowserRecord, callback: AddRecordCallback): void {
+    metadata.start = Math.min(metadata.start, record.timestamp)
+    metadata.end = Math.max(metadata.end, record.timestamp)
+    metadata.records_count += 1
+    metadata.has_full_snapshot ||= record.type === RecordType.FullSnapshot
 
-    replayStats.addSegment(viewId)
-  }
-
-  addRecord(record: BrowserRecord, callback: AddRecordCallback): void {
-    this.metadata.start = Math.min(this.metadata.start, record.timestamp)
-    this.metadata.end = Math.max(this.metadata.end, record.timestamp)
-    this.metadata.records_count += 1
-    this.metadata.has_full_snapshot ||= record.type === RecordType.FullSnapshot
-
-    const prefix = this.encoder.isEmpty ? '{"records":[' : ','
-    this.encoder.write(prefix + JSON.stringify(record), (additionalEncodedBytesCount) => {
-      this.encodedBytesCount += additionalEncodedBytesCount
-      callback(this.encodedBytesCount)
+    const prefix = encoder.isEmpty ? '{"records":[' : ','
+    encoder.write(prefix + JSON.stringify(record), (additionalEncodedBytesCount) => {
+      encodedBytesCount += additionalEncodedBytesCount
+      callback(encodedBytesCount)
     })
   }
 
-  flush(callback: FlushCallback) {
-    if (this.encoder.isEmpty) {
+  function flush(callback: FlushCallback) {
+    if (encoder.isEmpty) {
       throw new Error('Empty segment flushed')
     }
 
-    this.encoder.write(`],${JSON.stringify(this.metadata).slice(1)}\n`)
-    this.encoder.finish((encoderResult) => {
-      replayStats.addWroteData(this.metadata.view.id, encoderResult.rawBytesCount)
-      callback(this.metadata, encoderResult)
+    encoder.write(`],${JSON.stringify(metadata).slice(1)}\n`)
+    encoder.finish((encoderResult) => {
+      replayStats.addWroteData(metadata.view.id, encoderResult.rawBytesCount)
+      callback(metadata, encoderResult)
     })
   }
+
+  return { addRecord, flush }
 }
