e3a178e426cfde3166505bd3ffae9a0c328c11f9
cy-moi <congyao119@gmail.com>
2024-08-28 14:08:38 +0200

    remove unused field in event types

--- packages/rum-core/src/rawRumEvent.types.ts
+++ packages/rum-core/src/rawRumEvent.types.ts
@@ -90,7 +90,6 @@ export interface RawRumErrorEvent {
 export interface RawRumViewEvent {
   date: TimeStamp
   type: RumEventType.VIEW
-  context?: Context
   view: {
     loading_type: ViewLoadingType
     first_byte?: ServerDuration
