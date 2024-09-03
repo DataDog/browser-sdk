4e210618847878b601928ea3d7187c8092542036
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2024-09-03 16:39:01 +0200

    🐛 Allow both history and History prototype to be patched by 3rd party

--- packages/core/test/emulate/mockLocation.ts
+++ packages/core/test/emulate/mockLocation.ts
@@ -1,25 +1,4 @@
-import { assign, buildUrl } from '../../src'
-
-export function mockLocation(initialUrl: string) {
-  const fakeLocation = buildLocation(initialUrl)
-  spyOn(History.prototype, 'pushState').and.callFake((_: any, __: string, pathname: string) => {
-    assign(fakeLocation, buildLocation(pathname, fakeLocation.href))
-  })
-
-  function hashchangeCallBack() {
-    fakeLocation.hash = window.location.hash
-    fakeLocation.href = fakeLocation.href.replace(/#.*/, '') + window.location.hash
-  }
-
-  window.addEventListener('hashchange', hashchangeCallBack)
-  return {
-    location: fakeLocation,
-    cleanup: () => {
-      window.removeEventListener('hashchange', hashchangeCallBack)
-      window.location.hash = ''
-    },
-  }
-}
+import { buildUrl } from '../../src'
 
 export function buildLocation(url: string, base = location.href) {
   const urlObject = buildUrl(url, base)
