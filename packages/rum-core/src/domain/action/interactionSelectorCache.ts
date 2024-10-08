e4a24941aea209e95d26a43e12978770cdb84799
cy-moi <congyao119@gmail.com>
2024-10-08 10:10:49 +0200

    Avoid using arrow functions

--- packages/rum-core/src/domain/action/interactionSelectorCache.ts
+++ packages/rum-core/src/domain/action/interactionSelectorCache.ts
@@ -5,13 +5,13 @@ import type { RelativeTime } from '@datadog/browser-core'
 export const CLICK_ACTION_MAX_DURATION = 10 * ONE_SECOND
 export const interactionSelectorCache = new Map<RelativeTime, string>()
 
-export const getInteractionSelector = (relativeTimestamp: RelativeTime) => {
+export function getInteractionSelector(relativeTimestamp: RelativeTime) {
   const selector = interactionSelectorCache.get(relativeTimestamp)
   interactionSelectorCache.delete(relativeTimestamp)
   return selector
 }
 
-export const updateInteractionSelector = (relativeTimestamp: RelativeTime, selector: string) => {
+export function updateInteractionSelector(relativeTimestamp: RelativeTime, selector: string) {
   interactionSelectorCache.set(relativeTimestamp, selector)
   interactionSelectorCache.forEach((_, relativeTimestamp) => {
     if (elapsed(relativeTimestamp, relativeNow()) > CLICK_ACTION_MAX_DURATION) {
