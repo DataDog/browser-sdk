ad91e28ca3b106b5e2d4b16ee02067f3605b5bc9
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2024-10-31 17:46:29 +0100

    👌 review suggestions (comments and variable name fine tuning)

--- packages/core/src/tools/taskQueue.spec.ts
+++ packages/core/src/tools/taskQueue.spec.ts
@@ -1,5 +1,5 @@
 import { mockClock, mockRequestIdleCallback, registerCleanupTask } from '../../test'
-import { createTaskQueue, TIMED_OUT_TIME_REMAINING } from './taskQueue'
+import { createTaskQueue, MAX_EXECUTION_TIME_ON_TIMEOUT } from './taskQueue'
 
 describe('createTaskQueue', () => {
   it('runs the task using an idle callback', () => {
@@ -14,7 +14,7 @@ describe('createTaskQueue', () => {
     expect(task).toHaveBeenCalled()
   })
 
-  it('runs as much task as possible in a single idle callback', () => {
+  it('runs as many tasks as possible in a single idle callback', () => {
     const clock = mockClock()
     registerCleanupTask(clock.cleanup)
     const requestIdleCallbackMock = mockRequestIdleCallback()
@@ -44,7 +44,7 @@ describe('createTaskQueue', () => {
     registerCleanupTask(clock.cleanup)
     const requestIdleCallbackMock = mockRequestIdleCallback()
 
-    const task1 = jasmine.createSpy().and.callFake(() => clock.tick(TIMED_OUT_TIME_REMAINING - 10))
+    const task1 = jasmine.createSpy().and.callFake(() => clock.tick(MAX_EXECUTION_TIME_ON_TIMEOUT - 10))
     const task2 = jasmine.createSpy().and.callFake(() => clock.tick(20))
     const task3 = jasmine.createSpy().and.callFake(() => clock.tick(20))
 
