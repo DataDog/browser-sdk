4480edcbd01e068374d1fb48ac1e735396f2ad79
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-11-18 16:36:36 +0100

    Fix wrong update

--- packages/core/test/emulate/mockReportingObserver.ts
+++ packages/core/test/emulate/mockReportingObserver.ts
@@ -50,16 +50,8 @@ export function mockReportingObserver() {
 export type MockCspEventListener = ReturnType<typeof mockCspEventListener>
 
 export function mockCspEventListener() {
-  // eslint-disable-next-line @typescript-eslint/unbound-method
-  const originalAddEventListener = EventTarget.prototype.addEventListener
-  EventTarget.prototype.addEventListener = jasmine
-    .createSpy()
-    .and.callFake((_type: string, listener: EventListener) => {
-      listeners.push(listener)
-    })
-
-  registerCleanupTask(() => {
-    EventTarget.prototype.addEventListener = originalAddEventListener
+  spyOn(document, 'addEventListener').and.callFake((_type: string, listener: EventListener) => {
+    listeners.push(listener)
   })
 
   const listeners: EventListener[] = []
