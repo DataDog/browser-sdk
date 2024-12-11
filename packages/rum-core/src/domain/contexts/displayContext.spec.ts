68d9d06b0042118cbb3c732f386fb4774387e143
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-12-11 15:21:30 +0100

    Delay the viewport dimension collection

--- packages/rum-core/src/domain/contexts/displayContext.spec.ts
+++ packages/rum-core/src/domain/contexts/displayContext.spec.ts
@@ -4,8 +4,12 @@ import { startDisplayContext } from './displayContext'
 
 describe('displayContext', () => {
   let displayContext: DisplayContext
-
+  let requestAnimationFrameSpy: jasmine.Spy
   beforeEach(() => {
+    requestAnimationFrameSpy = spyOn(window, 'requestAnimationFrame').and.callFake((callback) => {
+      callback(1)
+      return 1
+    })
     displayContext = startDisplayContext(mockRumConfiguration())
   })
 
@@ -13,7 +17,8 @@ describe('displayContext', () => {
     displayContext.stop()
   })
 
-  it('should return current display context', () => {
+  it('should return current display context using requestAnimationFrame', () => {
+    expect(requestAnimationFrameSpy).toHaveBeenCalledTimes(1)
     expect(displayContext.get()).toEqual({
       viewport: {
         width: jasmine.any(Number),
--- packages/rum-core/src/domain/contexts/displayContext.ts
+++ packages/rum-core/src/domain/contexts/displayContext.ts
@@ -1,16 +1,26 @@
 import type { RumConfiguration } from '../configuration'
+import type { ViewportDimension } from '../../browser/viewportObservable'
 import { getViewportDimension, initViewportObservable } from '../../browser/viewportObservable'
 
 export type DisplayContext = ReturnType<typeof startDisplayContext>
 
 export function startDisplayContext(configuration: RumConfiguration) {
-  let viewport = getViewportDimension()
+  let viewport: ViewportDimension | undefined
+
+  // Use requestAnimationFrame to delay the calculation of viewport dimensions until after SDK initialization, preventing long tasks.
+  const animationFrameId = requestAnimationFrame(() => {
+    viewport = getViewportDimension()
+  })
+
   const unsubscribeViewport = initViewportObservable(configuration).subscribe((viewportDimension) => {
     viewport = viewportDimension
   }).unsubscribe
 
   return {
-    get: () => ({ viewport }),
-    stop: unsubscribeViewport,
+    get: () => (viewport ? { viewport } : undefined),
+    stop: () => {
+      unsubscribeViewport()
+      animationFrameId && cancelAnimationFrame(animationFrameId)
+    },
   }
 }
