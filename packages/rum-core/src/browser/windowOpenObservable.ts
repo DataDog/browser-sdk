5eb198c8225e848a646447dea9cefde47a2e1fa3
Nicolas Ulrich <nicolas.ulrich@datadoghq.com>
2024-12-13 09:47:30 +0100

    Cleaning instrument

--- packages/rum-core/src/browser/windowOpenObservable.ts
+++ packages/rum-core/src/browser/windowOpenObservable.ts
@@ -1,9 +1,10 @@
 import { instrumentMethod, Observable } from '@datadog/browser-core'
 
 export function createWindowOpenObservable() {
-  const observable = new Observable<void>()
-  instrumentMethod(window, 'open', () => {
-    observable.notify()
+  return new Observable<void>((observable) => {
+    const { stop } = instrumentMethod(window, 'open', () => {
+      observable.notify()
+    })
+    return stop
   })
-  return observable
 }
