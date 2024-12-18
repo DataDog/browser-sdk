884c544cc87dcbaf73bb9a0cba93187f59010eb6
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2024-12-18 12:17:29 +0100

    Add feature flag

--- packages/rum-core/src/domain/contexts/displayContext.spec.ts
+++ packages/rum-core/src/domain/contexts/displayContext.spec.ts
@@ -1,3 +1,4 @@
+import { addExperimentalFeatures, ExperimentalFeature, resetExperimentalFeatures } from '@datadog/browser-core'
 import { mockRumConfiguration } from '../../../test'
 import type { DisplayContext } from './displayContext'
 import { startDisplayContext } from './displayContext'
@@ -10,15 +11,30 @@ describe('displayContext', () => {
       callback(1)
       return 1
     })
-    displayContext = startDisplayContext(mockRumConfiguration())
   })
 
   afterEach(() => {
     displayContext.stop()
+    resetExperimentalFeatures()
   })
 
-  it('should return current display context using requestAnimationFrame', () => {
+  it('should return current display context using requestAnimationFrame if FF enabled', () => {
+    addExperimentalFeatures([ExperimentalFeature.DELAY_VIEWPORT_COLLECTION])
+    displayContext = startDisplayContext(mockRumConfiguration())
+
     expect(requestAnimationFrameSpy).toHaveBeenCalledTimes(1)
+    expect(displayContext.get()).toEqual({
+      viewport: {
+        width: jasmine.any(Number),
+        height: jasmine.any(Number),
+      },
+    })
+  })
+
+  it('should return current display context without using requestAnimationFrame if FF disabled', () => {
+    expect(requestAnimationFrameSpy).toHaveBeenCalledTimes(0)
+    displayContext = startDisplayContext(mockRumConfiguration())
+
     expect(displayContext.get()).toEqual({
       viewport: {
         width: jasmine.any(Number),
