b221b4847b2258b53cdeeaa22d9fcd972899a11f
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2025-01-06 14:55:04 +0100

    Use monitor

--- packages/rum-core/src/domain/contexts/displayContext.ts
+++ packages/rum-core/src/domain/contexts/displayContext.ts
@@ -1,3 +1,4 @@
+import { monitor } from '@datadog/browser-core'
 import type { RumConfiguration } from '../configuration'
 import type { ViewportDimension } from '../../browser/viewportObservable'
 import { getViewportDimension, initViewportObservable } from '../../browser/viewportObservable'
@@ -7,9 +8,11 @@ export type DisplayContext = ReturnType<typeof startDisplayContext>
 export function startDisplayContext(configuration: RumConfiguration) {
   let viewport: ViewportDimension | undefined
   // Use requestAnimationFrame to delay the calculation of viewport dimensions until after SDK initialization, preventing long tasks.
-  const animationFrameId = requestAnimationFrame(() => {
-    viewport = getViewportDimension()
-  })
+  const animationFrameId = requestAnimationFrame(
+    monitor(() => {
+      viewport = getViewportDimension()
+    })
+  )
 
   const unsubscribeViewport = initViewportObservable(configuration).subscribe((viewportDimension) => {
     viewport = viewportDimension
