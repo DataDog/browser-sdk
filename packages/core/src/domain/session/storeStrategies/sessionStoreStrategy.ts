5f995a4bac4c6ba3d1b3bee641a3e3c843a7272d
Benoît <benoit.zugmeyer@datadoghq.com>
2025-01-07 14:04:44 +0100

    ✨ [RUM-5001] introduce a `sessionPersistence` config option to force using local storage (#3244)
    * 🏷️ update rum-events-format
    
    * ♻️ [RUM-5001] introduce a constant for session persistence types
    
    * ✨ [RUM-5001] implement the sessionPersistence config option
    
    * 🔧 use sessionPersistence: local-storage in the extension
    
    * ✅ remove `allowFallbackToLocalStorage` tests from configuration.spec.ts
    
    There is no point in testing the session store strategy selection twice
    
    * ✅ update e2e tests
    
    * ✅ fix e2e tests

--- packages/core/src/domain/session/storeStrategies/sessionStoreStrategy.ts
+++ packages/core/src/domain/session/storeStrategies/sessionStoreStrategy.ts
@@ -1,9 +1,12 @@
 import type { CookieOptions } from '../../../browser/cookie'
+import type { SessionPersistence } from '../sessionConstants'
 import type { SessionState } from '../sessionState'
 
 export const SESSION_STORE_KEY = '_dd_s'
 
-export type SessionStoreStrategyType = { type: 'Cookie'; cookieOptions: CookieOptions } | { type: 'LocalStorage' }
+export type SessionStoreStrategyType =
+  | { type: typeof SessionPersistence.COOKIE; cookieOptions: CookieOptions }
+  | { type: typeof SessionPersistence.LOCAL_STORAGE }
 
 export interface SessionStoreStrategy {
   isLockEnabled: boolean
