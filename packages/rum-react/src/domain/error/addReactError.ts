f5108e39b8a8f5ce7357dc22f8ef62ddd7e8269d
Eloy Toro <eloy.toro@datadoghq.com>
2025-01-07 10:33:23 +0100

    use createReporter API in react plugin

--- packages/rum-react/src/domain/error/addReactError.ts
+++ packages/rum-react/src/domain/error/addReactError.ts
@@ -1,13 +1,11 @@
 import type { ErrorInfo } from 'react'
-import type { ErrorWithCause } from '@datadog/browser-core'
 import { onReactPluginInit } from '../reactPlugin'
 
 export function addReactError(error: Error, info: ErrorInfo) {
-  const renderingError = new Error(error.message)
-  renderingError.name = 'ReactRenderingError'
-  renderingError.stack = info.componentStack ?? undefined
-  ;(renderingError as ErrorWithCause).cause = error
   onReactPluginInit((_, rumPublicApi) => {
-    rumPublicApi.addError(renderingError, { framework: 'react' })
+    const reporter = rumPublicApi.createReporter('@datadog/browser-rum-react', {
+      handlingStack: info.componentStack ?? undefined,
+    })
+    reporter.addError(error, { framework: 'react' })
   })
 }
