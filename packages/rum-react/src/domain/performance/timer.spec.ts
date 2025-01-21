8c065629096496970586e1f5945be6aef3570cc9
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-01-21 14:13:03 +0100

    👌 fix typecheck

--- packages/rum-react/src/domain/performance/timer.spec.ts
+++ packages/rum-react/src/domain/performance/timer.spec.ts
@@ -1,4 +1,5 @@
 import { mockClock, registerCleanupTask } from '@datadog/browser-core/test'
+import type { Duration } from '@datadog/browser-core'
 import { createTimer } from './timer'
 
 describe('createTimer', () => {
@@ -10,6 +11,6 @@ describe('createTimer', () => {
     timer.startTimer()
     clock.tick(1000)
     timer.stopTimer()
-    expect(timer.getDuration()).toBe(1000)
+    expect(timer.getDuration()).toBe(1000 as Duration)
   })
 })
