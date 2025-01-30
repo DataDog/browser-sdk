97c308f4354d49c60449cbb72b59aa2a8be2a54b
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2025-01-30 08:15:54 +0100

    👌 improve test:e2e workflow

--- test/e2e/playwright.local.config.ts
+++ test/e2e/playwright.local.config.ts
@@ -6,24 +6,19 @@ const projects = [
     name: 'chromium',
     use: { ...devices['Desktop Chrome'] },
   },
+  {
+    name: 'firefox',
+    use: { ...devices['Desktop Firefox'] },
+  },
+  {
+    name: 'webkit',
+    use: { ...devices['Desktop Safari'] },
+  },
+  {
+    name: 'android',
+    use: { ...devices['Pixel 7'] },
+  },
 ]
-
-if (!process.env.CI) {
-  projects.push(
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    {
-      name: 'android',
-      use: { ...devices['Pixel 7'] },
-    }
-  )
-}
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
