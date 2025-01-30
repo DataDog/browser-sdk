293712a9295f6a6d8bb977c3f30786f12716bf26
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-01-30 17:17:22 +0100

    ✅ consolidate tests a bit

--- packages/rum-core/src/domain/resource/retrieveInitialDocumentResourceTiming.spec.ts
+++ packages/rum-core/src/domain/resource/retrieveInitialDocumentResourceTiming.spec.ts
@@ -1,10 +1,12 @@
-import { mockRumConfiguration } from '../../../test'
+import { mockDocumentReadyState, mockRumConfiguration } from '../../../test'
+import { FAKE_INITIAL_DOCUMENT } from './resourceUtils'
 import { retrieveInitialDocumentResourceTiming } from './retrieveInitialDocumentResourceTiming'
 
 describe('rum initial document resource', () => {
   it('creates a resource timing for the initial document', (done) => {
     retrieveInitialDocumentResourceTiming(mockRumConfiguration(), (timing) => {
       expect(timing.entryType).toBe('resource')
+      expect(timing.initiatorType).toBe(FAKE_INITIAL_DOCUMENT)
       expect(timing.duration).toBeGreaterThan(0)
 
       // generate a performance entry like structure
@@ -14,5 +16,14 @@ describe('rum initial document resource', () => {
       expect((toJsonTiming as any).toJSON).toBeUndefined()
       done()
     })
+  })
+
+  it('waits until the document is interactive to notify the resource', () => {
+    const { triggerOnDomLoaded } = mockDocumentReadyState()
+    const spy = jasmine.createSpy()
+    retrieveInitialDocumentResourceTiming(mockRumConfiguration(), spy)
+    expect(spy).not.toHaveBeenCalled()
+    triggerOnDomLoaded()
+    expect(spy).toHaveBeenCalled()
   })
 })
