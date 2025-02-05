5990dc15131c35a25277b8da8d4e702a45004179
Seth Fowler <seth.fowler@datadoghq.com>
2025-02-05 14:15:39 +0000

    🐛 [RUM-8429] Report an error when lazy loading the recorder module fails

---
+++ packages/rum/src/domain/scriptLoadingError.ts
@@ -0,0 +1,37 @@
+import { addTelemetryError, display, DOCS_ORIGIN } from '@datadog/browser-core'
+
+export function reportScriptLoadingError({
+  configuredUrl,
+  error,
+  source,
+  scriptType,
+}: {
+  configuredUrl?: string | undefined
+  error: unknown
+  source: string
+  scriptType: 'module' | 'Worker'
+}) {
+  const verb = scriptType === 'Worker' ? 'creating' : 'importing'
+  display.error(`${source} failed to start: an error occurred while ${verb} the ${scriptType}:`, error)
+  if (error instanceof Event || (error instanceof Error && isMessageCspRelated(error.message))) {
+    let baseMessage
+    if (configuredUrl) {
+      baseMessage = `Please make sure the ${scriptType} URL ${configuredUrl} is correct and CSP is correctly configured.`
+    } else {
+      baseMessage = 'Please make sure CSP is correctly configured.'
+    }
+    display.error(
+      `${baseMessage} See documentation at ${DOCS_ORIGIN}/integrations/content_security_policy_logs/#use-csp-with-real-user-monitoring-and-session-replay`
+    )
+  } else {
+    addTelemetryError(error)
+  }
+}
+
+function isMessageCspRelated(message: string) {
+  return (
+    message.includes('Content Security Policy') ||
+    // Related to `require-trusted-types-for` CSP: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/require-trusted-types-for
+    message.includes("requires 'TrustedScriptURL'")
+  )
+}
