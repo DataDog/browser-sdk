89097569167c51edba96da8657c3699f4e8f5258
Rômulo Ruas <romulo.fernandes@gmail.com>
2025-02-07 18:00:15 +0100

    Use a more specific chunk name for recorder

--- packages/rum/src/boot/lazyLoadRecorder.ts
+++ packages/rum/src/boot/lazyLoadRecorder.ts
@@ -1,6 +1,6 @@
 export async function lazyLoadRecorder() {
   try {
-    const module = await import(/* webpackChunkName: "recorder" */ './startRecording')
+    const module = await import(/* webpackChunkName: "datadog-recorder" */ './startRecording')
     return module.startRecording
   } catch {
     /* Prevent collecting the webpack ChunkLoadError as it is already collected as a RUM resource. */
