d42dfc5b7851bf6846c9f15debd34a3cd1a762c7
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-02-14 11:34:03 +0100

    🔥 remove support for bigint identifiers

--- packages/rum-core/src/domain/tracing/identifier.spec.ts
+++ packages/rum-core/src/domain/tracing/identifier.spec.ts
@@ -1,18 +1,7 @@
-import { ExperimentalFeature } from '@datadog/browser-core'
-import { mockExperimentalFeatures } from '../../../../core/test'
 import { getCrypto } from '../../browser/crypto'
-import {
-  createSpanIdentifier,
-  createTraceIdentifier,
-  toPaddedHexadecimalString,
-  clearIdentifierImplementationCache,
-} from './identifier'
+import { createSpanIdentifier, createTraceIdentifier, toPaddedHexadecimalString } from './identifier'
 
 describe('identifier', () => {
-  beforeEach(() => {
-    clearIdentifierImplementationCache()
-  })
-
   describe('TraceIdentifier', () => {
     it('generates a random id', () => {
       const identifier = createTraceIdentifier()
@@ -37,41 +26,6 @@ describe('identifier', () => {
       mockRandomValues((buffer) => fill(buffer, 0xff))
       const identifier = createSpanIdentifier()
       expect(identifier.toString(16)).toEqual('7fffffffffffffff')
-    })
-  })
-
-  // Run the same tests again with consistent trace sampling enabled, which uses the BigInt
-  // implementation
-  describe('with CONSISTENT_TRACE_SAMPLING enabled', () => {
-    beforeEach(() => {
-      mockExperimentalFeatures([ExperimentalFeature.CONSISTENT_TRACE_SAMPLING])
-    })
-
-    describe('TraceIdentifier', () => {
-      it('generates a random id', () => {
-        const identifier = createTraceIdentifier()
-        expect(identifier.toString()).toMatch(/^\d+$/)
-      })
-
-      it('formats using base 16', () => {
-        mockRandomValues((buffer) => (buffer[0] = 0xff))
-        const identifier = createTraceIdentifier()
-        expect(identifier.toString(16)).toEqual('ff')
-      })
-
-      it('should generate a max value of 64 bits', () => {
-        mockRandomValues((buffer) => fill(buffer, 0xff))
-        const identifier = createTraceIdentifier()
-        expect(identifier.toString(16)).toEqual('ffffffffffffffff')
-      })
-    })
-
-    describe('SpanIdentifier', () => {
-      it('generates a max value of 63 bits', () => {
-        mockRandomValues((buffer) => fill(buffer, 0xff))
-        const identifier = createSpanIdentifier()
-        expect(identifier.toString(16)).toEqual('7fffffffffffffff')
-      })
     })
   })
 })
