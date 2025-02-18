d9c3b15ecaee0bc9e08e535e68436a260b5ea5c4
Nicolas Ulrich <nicolas.ulrich@datadoghq.com>
2025-02-18 10:55:27 +0100

    Remove unused context function

--- packages/core/src/domain/context/contextUtils.ts
+++ packages/core/src/domain/context/contextUtils.ts
@@ -3,23 +3,6 @@ import { display } from '../../tools/display'
 import { getType } from '../../tools/utils/typeUtils'
 
 /**
- * Clone input data and ensure known context properties
- * are strings, as defined here:
- * https://docs.datadoghq.com/logs/log_configuration/attributes_naming_convention/#user-related-attributes
- */
-export function sanitizeContext(newContext: Context, keys: string[]): Context {
-  // We shallow clone only to prevent mutation of context data.
-  const context = { ...newContext }
-  keys.forEach((key) => {
-    if (key in context) {
-      /* eslint-disable @typescript-eslint/no-base-to-string */
-      context[key] = String(context[key])
-    }
-  })
-  return context
-}
-
-/**
  * Simple check to ensure an object is a valid context
  */
 export function checkContext(maybeContext: unknown): maybeContext is Context {
