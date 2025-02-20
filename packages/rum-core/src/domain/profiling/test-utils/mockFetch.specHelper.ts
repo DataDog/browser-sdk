fde6ceba0600522bfc8b833578c1fe5439a3a7a1
Thomas Bertet <thomas.bertet@datadoghq.com>
2025-02-20 16:18:13 +0100

    [RUM Profiler] format

--- packages/rum-core/src/domain/profiling/test-utils/mockFetch.specHelper.ts
+++ packages/rum-core/src/domain/profiling/test-utils/mockFetch.specHelper.ts
@@ -1,39 +1,34 @@
-
 export function mockFetch() {
-    const originalFetch = globalThis.fetch;
+  const originalFetch = globalThis.fetch
 
-    const fetchMock = jasmine.createSpy('fetchMock').and.callFake(
-        (_input: RequestInfo | URL, _init?: RequestInit) =>
-            Promise.resolve(new Response())
-    );
-    
-    globalThis.fetch = fetchMock as unknown as typeof globalThis.fetch;
+  const fetchMock = jasmine
+    .createSpy('fetchMock')
+    .and.callFake((_input: RequestInfo | URL, _init?: RequestInit) => Promise.resolve(new Response()))
 
-    afterEach(() => {
-        fetchMock.calls.reset();
-    });
-    afterAll(() => {
-        globalThis.fetch = originalFetch;
-    });
+  globalThis.fetch = fetchMock as unknown as typeof globalThis.fetch
 
-    function extractIntakeUrlAndFormDataFromFetch(callIndex = 0) {
-        const calls = fetchMock.calls.all();
-        if (!calls[callIndex]) {
-            throw new Error(`No fetch call found at index ${callIndex}`);
-        }
+  afterEach(() => {
+    fetchMock.calls.reset()
+  })
+  afterAll(() => {
+    globalThis.fetch = originalFetch
+  })
 
-        const [intakeUrl, request] = calls[callIndex] as unknown as [
-            string,
-            { body: FormData; method: string },
-        ];
-
-        return {
-            // eslint-disable-next-line local-rules/disallow-url-constructor-patched-values
-            intakeUrl: new URL(intakeUrl),
-            formData: request.body,
-            method: request.method,
-        };
+  function extractIntakeUrlAndFormDataFromFetch(callIndex = 0) {
+    const calls = fetchMock.calls.all()
+    if (!calls[callIndex]) {
+      throw new Error(`No fetch call found at index ${callIndex}`)
     }
 
-    return { fetchMock, extractIntakeUrlAndFormDataFromFetch };
+    const [intakeUrl, request] = calls[callIndex] as unknown as [string, { body: FormData; method: string }]
+
+    return {
+      // eslint-disable-next-line local-rules/disallow-url-constructor-patched-values
+      intakeUrl: new URL(intakeUrl),
+      formData: request.body,
+      method: request.method,
+    }
+  }
+
+  return { fetchMock, extractIntakeUrlAndFormDataFromFetch }
 }
--- packages/rum-core/src/domain/profiling/utils/getNumberOfSamples.ts
+++ packages/rum-core/src/domain/profiling/utils/getNumberOfSamples.ts
@@ -1,4 +1,4 @@
-import type { ProfilerSample } from '../types';
+import type { ProfilerSample } from '../types'
 
 /**
  * Counts number of samples when the thread was not idle (stackId is defined)
@@ -6,11 +6,11 @@ import type { ProfilerSample } from '../types';
  * @returns Number of samples
  */
 export function getNumberOfSamples(samples: ProfilerSample[]): number {
-    let numberOfSamples = 0;
-    for (const sample of samples) {
-        if (sample.stackId !== undefined) {
-            numberOfSamples++;
-        }
+  let numberOfSamples = 0
+  for (const sample of samples) {
+    if (sample.stackId !== undefined) {
+      numberOfSamples++
     }
-    return numberOfSamples;
+  }
+  return numberOfSamples
 }
