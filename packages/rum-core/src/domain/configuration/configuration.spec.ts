de91db5cca1cef02a1d29e1b0c2affcf52c2818a
Thomas Bertet <thomas.bertet@datadoghq.com>
2025-02-20 18:04:48 +0100

    [RUM Profiler] format files

--- packages/rum-core/src/domain/configuration/configuration.spec.ts
+++ packages/rum-core/src/domain/configuration/configuration.spec.ts
@@ -547,7 +547,7 @@ describe('serializeRumConfiguration', () => {
         ? `use_${CamelToSnakeCase<Key>}`
         : Key extends 'trackLongTasks'
           ? 'track_long_task' // oops
-          : Key extends 'applicationId' | 'subdomain' | 'remoteConfigurationId'  | 'profilingSampleRate'
+          : Key extends 'applicationId' | 'subdomain' | 'remoteConfigurationId' | 'profilingSampleRate'
             ? never
             : CamelToSnakeCase<Key>
     // By specifying the type here, we can ensure that serializeConfiguration is returning an
@@ -575,7 +575,7 @@ describe('serializeRumConfiguration', () => {
       use_worker_url: true,
       compress_intake_requests: true,
       plugins: [{ name: 'foo', bar: true }],
-      track_feature_flags_for_events: ['vital']
+      track_feature_flags_for_events: ['vital'],
     })
   })
 })
