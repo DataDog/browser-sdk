1ceb0fa4953393dc73a4929b1f27b3bcfe405b0c
Thomas Bertet <thomas.bertet@datadoghq.com>
2025-02-27 15:24:09 +0100

    [RUM Profiler] fix formatting

--- packages/rum-core/src/domain/profiling/profilingSupported.ts
+++ packages/rum-core/src/domain/profiling/profilingSupported.ts
@@ -2,10 +2,10 @@ import { getGlobalObject } from '@datadog/browser-core'
 import type { Profiler } from './types'
 
 // eslint-disable-next-line local-rules/disallow-side-effects
-const globalThis = getGlobalObject();
+const globalThis = getGlobalObject()
 
 // This API might be unavailable in some browsers
-const globalThisProfiler: Profiler | undefined = (globalThis as any).Profiler;
+const globalThisProfiler: Profiler | undefined = (globalThis as any).Profiler
 
 export function isProfilingSupported(): boolean {
   return globalThisProfiler !== undefined
