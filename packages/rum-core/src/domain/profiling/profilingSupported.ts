1a3eeb2eda167f09c362ca2a32718b55759810c1
Thomas Bertet <thomas.bertet@datadoghq.com>
2025-02-27 11:32:58 +0100

    [RUM Profiler] Adjust globalThis to not fail unit test

--- packages/rum-core/src/domain/profiling/profilingSupported.ts
+++ packages/rum-core/src/domain/profiling/profilingSupported.ts
@@ -1,7 +1,11 @@
+import { getGlobalObject } from '@datadog/browser-core'
 import type { Profiler } from './types'
 
-// These APIs might be unavailable in some browsers
-const globalThisProfiler: Profiler | undefined = (globalThis as any).Profiler
+// eslint-disable-next-line local-rules/disallow-side-effects
+const globalThis = getGlobalObject();
+
+// This API might be unavailable in some browsers
+const globalThisProfiler: Profiler | undefined = (globalThis as any).Profiler;
 
 export function isProfilingSupported(): boolean {
   return globalThisProfiler !== undefined
