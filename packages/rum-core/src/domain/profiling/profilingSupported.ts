d2d75cb3a8b1aece0d5a55cf2477421adddcf9f6
Thomas Bertet <thomas.bertet@datadoghq.com>
2025-03-06 20:55:45 +0100

    [RUM Profiler] improve code readability

--- packages/rum-core/src/domain/profiling/profilingSupported.ts
+++ packages/rum-core/src/domain/profiling/profilingSupported.ts
@@ -1,12 +1,10 @@
 import { getGlobalObject } from '@datadog/browser-core'
 import type { Profiler } from './types'
 
-// eslint-disable-next-line local-rules/disallow-side-effects
-const globalThis = getGlobalObject()
-
-// This API might be unavailable in some browsers
-const globalThisProfiler: Profiler | undefined = (globalThis as any).Profiler
-
 export function isProfilingSupported(): boolean {
+  const globalThis = getGlobalObject()
+
+  // This API might be unavailable in some browsers
+  const globalThisProfiler: Profiler | undefined = (globalThis as any).Profiler
   return globalThisProfiler !== undefined
 }
