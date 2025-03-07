297afc25adc521689a668456bca6c4325f21215f
Thomas Bertet <thomas.bertet@datadoghq.com>
2025-03-07 10:33:52 +0100

    [RUM Profiler] Rollback change to chunk, name it profiler

--- packages/rum-core/src/domain/profiling/lazyLoadProfiler.ts
+++ packages/rum-core/src/domain/profiling/lazyLoadProfiler.ts
@@ -1,6 +1,6 @@
 export async function lazyLoadProfiler() {
   try {
-    const module = await import('./datadogProfiler')
+    const module = await import(/* webpackChunkName: "profiler" */ './profiler')
     return module.createRumProfiler
   } catch {
     /* Prevent collecting the webpack ChunkLoadError as it is already collected as a RUM resource. */
