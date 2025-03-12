9ed67afd28d441ed1a1f3207e40006f45d43cf3e
Thomas Bertet <thomas.bertet@datadoghq.com>
2025-03-12 13:23:33 +0100

    [RUM Profiler] Update formatting, fix deploy test

--- packages/rum-slim/src/boot/stubProfilerApi.ts
+++ packages/rum-slim/src/boot/stubProfilerApi.ts
@@ -1,7 +1,7 @@
 import { noop } from '@datadog/browser-core'
 import type { ProfilerApi } from '@datadog/browser-rum-core'
 
-export function makeProfilerApiStub() :ProfilerApi {
+export function makeProfilerApiStub(): ProfilerApi {
   return {
     onRumStart: noop,
     stop: noop,
