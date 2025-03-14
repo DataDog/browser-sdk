224d6459eb660d1dc376fafb300f0e769ee473ca
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2025-03-14 07:10:11 +0100

    👌 remove error monitoring for fetchStrategy

--- packages/core/src/transport/httpRequest.ts
+++ packages/core/src/transport/httpRequest.ts
@@ -110,10 +110,7 @@ export function fetchStrategy(
 
   fetch(fetchUrl, { method: 'POST', body: payload.data, mode: 'cors' })
     .then(monitor((response: Response) => onResponse?.({ status: response.status, type: response.type })))
-    .catch((error) => {
-      monitorError(error)
-      onResponse?.({ status: 0 })
-    })
+    .catch(() => onResponse?.({ status: 0 }))
 }
 
 function isKeepAliveSupported() {
