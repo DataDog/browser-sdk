864b40d87dc6dc66f209577b535c2ca6eee6bec3
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2025-04-09 11:12:24 +0200

    ✨ refactor: improve transportManager by consolidating batch sending logic and enhancing configuration

--- packages/rum-extra-slim/src/domain/transportManager.ts
+++ packages/rum-extra-slim/src/domain/transportManager.ts
@@ -1,32 +1,66 @@
-import { dateNow, monitorError } from '@datadog/browser-core'
+import type { Configuration, Context, Duration, Payload } from '@datadog/browser-core'
+import {
+  createFlushController,
+  createIdentityEncoder,
+  createPageMayExitObservable,
+  dateNow,
+  monitorError,
+  Observable,
+  ONE_KIBI_BYTE,
+  ONE_SECOND,
+} from '@datadog/browser-core'
+import { createBatch } from '@datadog/browser-core/src/transport/batch'
 import type { SessionManager } from './sessionManager'
 import type { Event } from './event'
 
 const BASE_URL = 'https://d1vqwqwipxy36n.cloudfront.net'
 
+// Flush controller config
+const MESSAGE_LIMIT = 50
+const BYTES_LIMIT = 16 * ONE_KIBI_BYTE
+const DURATION_LIMIT = (30 * ONE_SECOND) as Duration
+
+// Batch config
+const MESSAGE_BYTES_LIMIT = 256 * ONE_KIBI_BYTE
+
 export type TransportManager = ReturnType<typeof startTransportManager>
 
-export function startTransportManager(sessionManager: SessionManager) {
-  function send(data: Event) {
-    console.log(`[${data.type}]`, data) // eslint-disable-line no-console
+function sendBatch(payload: Payload) {
+  fetch(`${BASE_URL}`, {
+    method: 'POST',
+    body: payload.data,
+  }).catch((e) => {
+    // @ts-expect-error wrong types in vscode?
+    monitorError(new Error('[RUM XS] Failed to send event', { cause: e }))
+  })
+}
 
+export function startTransportManager(sessionManager: SessionManager) {
+  const batch = createBatch({
+    encoder: createIdentityEncoder(),
+    request: {
+      send: sendBatch,
+      sendOnExit: sendBatch,
+    },
+    flushController: createFlushController({
+      messagesLimit: MESSAGE_LIMIT,
+      bytesLimit: BYTES_LIMIT,
+      durationLimit: DURATION_LIMIT,
+      pageMayExitObservable: createPageMayExitObservable({} as Configuration),
+      sessionExpireObservable: new Observable<void>(() => () => void {}),
+    }),
+    messageBytesLimit: MESSAGE_BYTES_LIMIT,
+  })
+
+  function send(data: Event) {
     const { clientId, contextId } = sessionManager
 
-    fetch(`${BASE_URL}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        clientId,
-        contextId,
-        timestamp: dateNow(),
-        data,
-      }),
-    }).catch((e) => {
-      // @ts-expect-error wrong types in vscode?
-      monitorError(new Error('[RUM XS] Failed to send event', { cause: e }))
-    })
+    batch.add({
+      clientId,
+      contextId,
+      timestamp: dateNow(),
+      data,
+    } as any as Context)
   }
 
   return {
