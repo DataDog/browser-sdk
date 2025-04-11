1a1acbcf5041e80049720b59e04c079f7229d09e
Aymeric <aymeric.mortemousque@datadoghq.com>
2025-04-11 16:04:27 +0200

    ♻️ [RUM-8767] Use assembly hooks for global, user and account context (#3457)

--- packages/rum-core/src/domain/contexts/accountContext.ts
+++ packages/rum-core/src/domain/contexts/accountContext.ts
@@ -1,12 +1,28 @@
-import { createContextManager, CustomerDataType, storeContextManager } from '@datadog/browser-core'
+import type { Account } from '@datadog/browser-core'
+import { createContextManager, CustomerDataType, isEmptyObject, storeContextManager } from '@datadog/browser-core'
 import type { RumConfiguration } from '../configuration'
+import type { Hooks, PartialRumEvent } from '../../hooks'
+import { HookNames } from '../../hooks'
 
-export function startAccountContext(configuration: RumConfiguration) {
+export function startAccountContext(hooks: Hooks, configuration: RumConfiguration) {
   const accountContextManager = buildAccountContextManager()
 
   if (configuration.storeContextsAcrossPages) {
     storeContextManager(configuration, accountContextManager, 'rum', CustomerDataType.Account)
   }
+
+  hooks.register(HookNames.Assemble, ({ eventType }): PartialRumEvent | undefined => {
+    const account = accountContextManager.getContext() as Account
+
+    if (isEmptyObject(account) || !account.id) {
+      return
+    }
+
+    return {
+      type: eventType,
+      account,
+    }
+  })
 
   return accountContextManager
 }
--- packages/rum-core/src/domain/contexts/userContext.ts
+++ packages/rum-core/src/domain/contexts/userContext.ts
@@ -1,12 +1,33 @@
-import { createContextManager, CustomerDataType, storeContextManager } from '@datadog/browser-core'
+import { createContextManager, CustomerDataType, isEmptyObject, storeContextManager } from '@datadog/browser-core'
 import type { RumConfiguration } from '../configuration'
+import type { Hooks, PartialRumEvent } from '../../hooks'
+import { HookNames } from '../../hooks'
+import type { RumSessionManager } from '../rumSessionManager'
 
-export function startUserContext(configuration: RumConfiguration) {
+export function startUserContext(hooks: Hooks, configuration: RumConfiguration, sessionManager: RumSessionManager) {
   const userContextManager = buildUserContextManager()
 
   if (configuration.storeContextsAcrossPages) {
     storeContextManager(configuration, userContextManager, 'rum', CustomerDataType.User)
   }
+
+  hooks.register(HookNames.Assemble, ({ eventType, startTime }): PartialRumEvent | undefined => {
+    const user = userContextManager.getContext()
+    const session = sessionManager.findTrackedSession(startTime)
+
+    if (session && session.anonymousId && !user.anonymous_id && !!configuration.trackAnonymousUser) {
+      user.anonymous_id = session.anonymousId
+    }
+
+    if (isEmptyObject(user)) {
+      return
+    }
+
+    return {
+      type: eventType,
+      usr: user,
+    }
+  })
 
   return userContextManager
 }
