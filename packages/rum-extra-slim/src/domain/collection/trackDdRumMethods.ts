ef81c87212be45b2682da6575ccdf18054df4737
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-04-14 15:23:23 +0200

    use onReady to instrument DD_RUM APIs

--- packages/rum-extra-slim/src/domain/collection/trackDdRumMethods.ts
+++ packages/rum-extra-slim/src/domain/collection/trackDdRumMethods.ts
@@ -1,5 +1,6 @@
 import type { Context } from '@datadog/browser-core'
 import { instrumentMethod } from '@datadog/browser-core'
+import type { RumPublicApi } from '@datadog/browser-rum-core'
 import type { TransportManager } from '../transportManager'
 import { addError } from './addError'
 import type { ContextType } from './setContext'
@@ -9,32 +10,39 @@ import { addFeatureFlagEvaluation } from './addFeatureFlagEvaluation'
 
 type Stoppable = { stop: () => void }
 
+type GlobalRum = {
+  onReady?(fn: () => void): void
+} & RumPublicApi
+
 export function trackDDRumMethods(transportManager: TransportManager) {
-  if (!('DD_RUM' in window)) {
+  const globalRum = (window as any).DD_RUM as GlobalRum
+  if (!globalRum || !globalRum.onReady) {
     return () => void {}
   }
 
   const subscriptions: Stoppable[] = []
 
-  for (const contextType of Object.values(CONTEXT_TYPE)) {
-    subscriptions.push(
-      instrumentMethod(window.DD_RUM as any, getContextMethod(contextType), ({ parameters }) =>
-        setContext(transportManager, contextType, parameters[0] as Context)
+  globalRum.onReady(() => {
+    for (const contextType of Object.values(CONTEXT_TYPE)) {
+      subscriptions.push(
+        instrumentMethod(globalRum as any, getContextMethod(contextType), ({ parameters }) =>
+          setContext(transportManager, contextType, parameters[0] as Context)
+        )
       )
-    )
-  }
+    }
 
-  subscriptions.push(
-    instrumentMethod(window.DD_RUM as any, 'addError', ({ parameters }) =>
-      addError(transportManager, parameters[0], parameters[1])
-    ),
-    instrumentMethod(window.DD_RUM as any, 'addFeatureFlagEvaluation', ({ parameters }) => {
-      addFeatureFlagEvaluation(transportManager, parameters[0], parameters[1])
-    }),
-    instrumentMethod(window.DD_RUM as any, 'addAction', ({ parameters }) => {
-      addAction(transportManager, parameters[0], parameters[1])
-    })
-  )
+    subscriptions.push(
+      instrumentMethod(globalRum, 'addError', ({ parameters }) =>
+        addError(transportManager, parameters[0], parameters[1])
+      ),
+      instrumentMethod(globalRum, 'addFeatureFlagEvaluation', ({ parameters }) => {
+        addFeatureFlagEvaluation(transportManager, parameters[0], parameters[1])
+      }),
+      instrumentMethod(globalRum, 'addAction', ({ parameters }) => {
+        addAction(transportManager, parameters[0], parameters[1])
+      })
+    )
+  })
 
   return () => subscriptions.forEach((subscription) => subscription.stop())
 }
