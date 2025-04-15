36f33ad9699416c27a7198a6c370b57509d36b5e
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-04-15 15:03:51 +0200

    more generic rule to handle any sdk/chunk

--- developer-extension/src/background/domain/syncRules.ts
+++ developer-extension/src/background/domain/syncRules.ts
@@ -1,11 +1,5 @@
 import type { NetRequestRulesOptions } from '../../common/extension.types'
-import {
-  DEV_LOGS_URL,
-  DEV_RUM_PROFILER_CHUNK_URL,
-  DEV_RUM_RECORDER_CHUNK_URL,
-  DEV_RUM_SLIM_URL,
-  DEV_RUM_URL,
-} from '../../common/packagesUrlConstants'
+import { DEV_RUM_SLIM_URL, DEV_RUM_URL, DEV_SERVER_ORIGIN } from '../../common/packagesUrlConstants'
 import { INTAKE_DOMAINS } from '../../common/intakeDomainConstants'
 import { createLogger } from '../../common/logger'
 import { onDevtoolsDisconnection, onDevtoolsMessage } from '../devtoolsPanelConnection'
@@ -68,16 +62,11 @@ function buildRules(
     const devRumUrl = useRumSlim ? DEV_RUM_SLIM_URL : DEV_RUM_URL
     logger.log('add redirect to dev bundles rules')
     rules.push(
-      createRedirectRule(/^https:\/\/.*\/datadog-rum-slim(-[\w-]+)?\.js$/, {
-        url: DEV_RUM_SLIM_URL,
+      createRedirectRule(/^https:\/\/.*\/datadog-(:?rum|rum-slim|logs)(:?-[\w-]+)?\.js$/, {
+        regexSubstitution: `${DEV_SERVER_ORIGIN}/datadog-\\1.js`,
       }),
-      createRedirectRule(/^https:\/\/.*\/datadog-rum(-[\w-]+)?\.js$/, { url: devRumUrl }),
-      createRedirectRule(/^https:\/\/.*\/datadog-logs(-[\w-]+)?\.js$/, { url: DEV_LOGS_URL }),
-      createRedirectRule(/^https:\/\/.*\/chunks\/recorder(-[\w-]+)?-datadog-rum.js$/, {
-        url: DEV_RUM_RECORDER_CHUNK_URL,
-      }),
-      createRedirectRule(/^https:\/\/.*\/chunks\/profiler(-[\w-]+)?-datadog-rum.js$/, {
-        url: DEV_RUM_PROFILER_CHUNK_URL,
+      createRedirectRule(/^https:\/\/.*\/chunks\/(\w+)(:?-\w+)?-datadog-rum.js$/, {
+        regexSubstitution: `${DEV_SERVER_ORIGIN}/chunks/\\1-datadog-rum.js`,
       }),
       createRedirectRule('https://localhost:8443/static/datadog-rum-hotdog.js', { url: devRumUrl })
     )
