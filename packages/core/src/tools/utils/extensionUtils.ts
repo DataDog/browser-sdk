f64f7b664055a53c13668fa4c6050d07300b9a3f
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2025-04-15 17:54:54 +0200

    changed wording

--- packages/core/src/tools/utils/extensionUtils.ts
+++ packages/core/src/tools/utils/extensionUtils.ts
@@ -1,11 +1,11 @@
-export const EXTENSION_PREFIXES = ['chrome-extension://', 'moz-extension://', 'safari-extension://']
+export const EXTENSION_PREFIXES = ['chrome-extension://', 'moz-extension://']
 
 export function containsExtensionUrl(str: string): boolean {
   return EXTENSION_PREFIXES.some((prefix) => str.includes(prefix))
 }
 
 /**
- * Utility function to detect if the current environment is a browser extension
+ * Utility function to detect if the SDK is being initialized in an unsupported browser extension environment.
  * @returns {boolean} true if running in an unsupported browser extension environment
  */
 export function isUnsupportedExtensionEnvironment(errorStack: string, windowLocation: string): boolean {
