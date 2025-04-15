32734fabbc3b29101387bb20d3dd587c7c59962f
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2025-04-15 16:08:00 +0200

    🐛 Add profiler chunk URL to redirect rules and constants (#3485)
    * 🐛 Add profiler chunk URL to redirect rules and constants
    
    * more generic rule to handle any sdk/chunk
    
    * remove unneeded non-capturing groups
    
    ---------
    
    Co-authored-by: Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>

--- developer-extension/src/common/packagesUrlConstants.ts
+++ developer-extension/src/common/packagesUrlConstants.ts
@@ -1,4 +1,4 @@
-export const DEV_LOGS_URL = 'http://localhost:8080/datadog-logs.js'
-export const DEV_RUM_RECORDER_CHUNK_URL = 'http://localhost:8080/chunks/recorder-datadog-rum.js'
-export const DEV_RUM_SLIM_URL = 'http://localhost:8080/datadog-rum-slim.js'
-export const DEV_RUM_URL = 'http://localhost:8080/datadog-rum.js'
+export const DEV_SERVER_ORIGIN = 'http://localhost:8080'
+export const DEV_LOGS_URL = `${DEV_SERVER_ORIGIN}/datadog-logs.js`
+export const DEV_RUM_SLIM_URL = `${DEV_SERVER_ORIGIN}/datadog-rum-slim.js`
+export const DEV_RUM_URL = `${DEV_SERVER_ORIGIN}/datadog-rum.js`
