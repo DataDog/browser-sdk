f25d53b2c412b7364b0467a580a7f3a9250d743f
Thomas BERTET <thomas.bertet@gmail.com>
2025-05-05 15:03:01 +0200

    [RUM Profiler] Use clocks for start/end time (#3510)
    * [RUM Profiler] Use clocks for start/end time
    
    * [RUM Profiler] Use clocks instead of relative time
    
    * [RUM Profiler] use more clocks.
    
    * [RUM Profiler] format
    
    * [RUM Profiler] Remove unused import
    
    * [RUM Profiler] Long task registry improvements

--- packages/rum/src/domain/profiling/utils/longTaskRegistry.ts
+++ packages/rum/src/domain/profiling/utils/longTaskRegistry.ts
@@ -1,45 +1,48 @@
-type PerformanceEntryStartTimeMs = number
+import type { ClocksState, RelativeTime } from '@datadog/browser-core'
+import { clocksNow } from '@datadog/browser-core'
+
+type PerformanceEntryStartTime = RelativeTime
 
 // Maps PerformanceEntry start-time to the corresponding long task id (from RUM LongTaskEvent),
 // We need this to link RUM Long Tasks with RUM Profiler stack traces
 // Given that long task takes at least 50ms and we export profile at least every 60 seconds, we can have up to 1200 entries (60s / 50ms = 1200).
-const registry = new Map<PerformanceEntryStartTimeMs, string>()
+const registry = new Map<PerformanceEntryStartTime, string>()
 
 // Enable Long Task Registry only if RUM Profiler has been activated
-let enabledTime: false | number = false
+let enabledClocks: false | ClocksState = false
 
 export function enableLongTaskRegistry() {
-  enabledTime = performance.now()
+  enabledClocks = clocksNow()
 }
 
 export function disableLongTaskRegistry() {
-  enabledTime = false
+  enabledClocks = false
   registry.clear() // Free-up the memory
 }
 
 /**
  * Store the long task ID in the registry for the Profiler to link it with the corresponding Profile.
  */
-export function setLongTaskId(longTaskId: string, performanceEntryStartTime: number) {
-  registry.set(performanceEntryStartTime, longTaskId)
+export function setLongTaskId(longTaskId: string, startTime: RelativeTime) {
+  registry.set(startTime, longTaskId)
 }
 
-export function getLongTaskId(longTaskEntry: { startTime: DOMHighResTimeStamp }): string | undefined {
+export function getLongTaskId(startTime: RelativeTime): string | undefined {
   // Don't return if it's not enabled or the long task has been reported before the activation
-  if (enabledTime === false || longTaskEntry.startTime < enabledTime) {
+  if (enabledClocks === false || startTime < enabledClocks.relative) {
     return undefined
   }
 
-  return registry.get(longTaskEntry.startTime)
+  return registry.get(startTime)
 }
 
-export function deleteLongTaskIdsBefore(collectionTime: number) {
-  if (enabledTime === false || collectionTime < enabledTime) {
+export function deleteLongTaskIdsBefore(collectionClocks: ClocksState) {
+  if (enabledClocks === false || collectionClocks.relative < enabledClocks.relative) {
     return undefined
   }
 
   for (const performanceEntryStartTime of registry.keys()) {
-    if (performanceEntryStartTime < collectionTime) {
+    if (performanceEntryStartTime < collectionClocks.relative) {
       registry.delete(performanceEntryStartTime)
     }
   }
