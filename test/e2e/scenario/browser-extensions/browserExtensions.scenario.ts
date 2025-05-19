498e01736bc125ab0ae53fb538d1a2aa26c3a838
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2025-05-19 13:43:27 +0200

    fix cli format, removed extra build file, renamed base extension

--- test/e2e/scenario/browser-extensions/browserExtensions.scenario.ts
+++ test/e2e/scenario/browser-extensions/browserExtensions.scenario.ts
@@ -3,11 +3,11 @@ import { test, expect } from '@playwright/test'
 import { DEFAULT_RUM_CONFIGURATION, createTest } from '../../lib/framework'
 
 // Different extension build paths for different configurations
-const pathToDefaultExtension = path.join(__dirname, '../../../../test/apps/extensions/extension-default')
+const pathToDefaultExtension = path.join(__dirname, '../../../../test/apps/extensions/base')
 // Contains allowedTrackingOrigins parameter with chrome-extension origin
-const pathToChromeExtension = path.join(__dirname, '../../../../test/apps/extensions/extension-chrome')
+const pathToChromeExtension = path.join(__dirname, '../../../../test/apps/extensions/allowed-tracking-origin')
 // Contains allowedTrackingOrigins parameter with app.example.com origin
-const pathToExampleExtension = path.join(__dirname, '../../../../test/apps/extensions/extension-example')
+const pathToExampleExtension = path.join(__dirname, '../../../../test/apps/extensions/invalid-tracking-origin')
 
 test.describe('browser extensions', () => {
   console.log('test')
