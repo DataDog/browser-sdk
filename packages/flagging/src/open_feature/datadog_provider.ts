16fc2dde922da2232d68c09bc1336318e9e0ab4b
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2025-05-19 16:43:02 -0700

    wire up the SDK

--- packages/flagging/src/open_feature/datadog_provider.ts
+++ packages/flagging/src/open_feature/datadog_provider.ts
@@ -1,7 +1,6 @@
 import {
     Hook,
     Provider,
-    ProviderEvents,
 } from '@openfeature/web-sdk'
 import {
     EvaluationContext,
@@ -14,6 +13,7 @@ import {
 } from '@openfeature/core'
 import Strategy from '../strategy/strategy';
 import { EppoStrategy } from '../strategy/eppo_strategy';
+import { MemoryOnlyConfigurationStore } from '../strategy/eppo/internal/configuration-store/memory.store';
 
 export class DatadogProvider implements Provider {
     // Adds runtime validation that the provider is used with the expected SDK
@@ -32,7 +32,20 @@ export class DatadogProvider implements Provider {
         this.datadogClientToken = clientToken;
 
         // todo: map datadog client token to eppo
-        this.strategy = new EppoStrategy();
+        this.strategy = new EppoStrategy({
+            precomputedFlagStore: new MemoryOnlyConfigurationStore(),
+            requestParameters: {
+                apiKey: this.datadogClientToken,
+                sdkVersion: '1.0.0',
+                sdkName: 'datadog-provider',
+            },
+            subject: {
+                subjectKey: 'datadog-provider',
+                subjectAttributes: {
+                    environment: 'datadog-provider',
+                }
+            }
+        });
     }
 
     resolveBooleanEvaluation(flagKey: string, defaultValue: boolean, context: EvaluationContext, logger: Logger): ResolutionDetails<boolean> {
