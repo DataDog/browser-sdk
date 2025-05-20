28820d16a2c71814cc6fc44eb62f06fa45b49a9f
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-05-20 14:46:34 +0200

    refactor: move first hidden check history from trackFirstHidden

--- packages/rum-core/src/domain/view/viewMetrics/trackFirstHidden.ts
+++ packages/rum-core/src/domain/view/viewMetrics/trackFirstHidden.ts
@@ -1,22 +1,12 @@
 import type { RelativeTime } from '@datadog/browser-core'
 import { addEventListeners, DOM_EVENT, noop } from '@datadog/browser-core'
 import type { RumConfiguration } from '../../configuration'
-import { supportPerformanceTimingEvent, RumPerformanceEntryType } from '../../../browser/performanceObservable'
 
 export type FirstHidden = ReturnType<typeof trackFirstHidden>
 
 export function trackFirstHidden(configuration: RumConfiguration, eventTarget: Window = window) {
   if (document.visibilityState === 'hidden') {
     return { timeStamp: 0 as RelativeTime, stop: noop }
-  }
-
-  if (supportPerformanceTimingEvent(RumPerformanceEntryType.VISIBILITY_STATE)) {
-    const firstHiddenEntry = performance
-      .getEntriesByType(RumPerformanceEntryType.VISIBILITY_STATE)
-      .find((entry) => entry.name === 'hidden')
-    if (firstHiddenEntry) {
-      return { timeStamp: firstHiddenEntry.startTime as RelativeTime, stop: noop }
-    }
   }
 
   let timeStamp: RelativeTime = Infinity as RelativeTime
