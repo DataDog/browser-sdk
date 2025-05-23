8d4ccb5a48b07ec48b7576c17c23b4ef4963d3b6
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-05-23 15:28:43 +0200

    ⚡️ [RUM-10104] Revert "♻️ Refactor cookieObservable to not rely on cookieStore (#3456)"
    This reverts commit 6423b6d4863b3e54e9bc1697078de75a12e96315.

--- packages/rum-core/src/browser/cookieObservable.spec.ts
+++ packages/rum-core/src/browser/cookieObservable.spec.ts
@@ -2,6 +2,7 @@ import type { Subscription } from '@datadog/browser-core'
 import { ONE_MINUTE, deleteCookie, setCookie } from '@datadog/browser-core'
 import type { Clock } from '@datadog/browser-core/test'
 import { mockClock } from '@datadog/browser-core/test'
+import { mockRumConfiguration } from '../../test'
 import { WATCH_COOKIE_INTERVAL_DELAY, createCookieObservable } from './cookieObservable'
 
 const COOKIE_NAME = 'cookie_name'
@@ -26,7 +27,7 @@ describe('cookieObservable', () => {
   })
 
   it('should notify observers on cookie change', (done) => {
-    const observable = createCookieObservable(COOKIE_NAME)
+    const observable = createCookieObservable(mockRumConfiguration(), COOKIE_NAME)
 
     subscription = observable.subscribe((cookieChange) => {
       expect(cookieChange).toEqual('foo')
@@ -37,8 +38,22 @@ describe('cookieObservable', () => {
     clock.tick(WATCH_COOKIE_INTERVAL_DELAY)
   })
 
-  it('should not notify observers on cookie change when the cookie value has not changed', () => {
-    const observable = createCookieObservable(COOKIE_NAME)
+  it('should notify observers on cookie change when cookieStore is not supported', () => {
+    Object.defineProperty(window, 'cookieStore', { get: () => undefined, configurable: true })
+    const observable = createCookieObservable(mockRumConfiguration(), COOKIE_NAME)
+
+    let cookieChange: string | undefined
+    subscription = observable.subscribe((change) => (cookieChange = change))
+
+    setCookie(COOKIE_NAME, 'foo', COOKIE_DURATION)
+    clock.tick(WATCH_COOKIE_INTERVAL_DELAY)
+
+    expect(cookieChange).toEqual('foo')
+  })
+
+  it('should not notify observers on cookie change when the cookie value as not changed when cookieStore is not supported', () => {
+    Object.defineProperty(window, 'cookieStore', { get: () => undefined, configurable: true })
+    const observable = createCookieObservable(mockRumConfiguration(), COOKIE_NAME)
 
     setCookie(COOKIE_NAME, 'foo', COOKIE_DURATION)
 
