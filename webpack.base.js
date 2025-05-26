2c54f668004d8a6edbc392d0c3e74c88044cbb27
leomelki <leo.melki@datadoghq.com>
2025-05-26 13:40:53 +0200

    not minify classnames starting with _

--- webpack.base.js
+++ webpack.base.js
@@ -63,6 +63,7 @@ module.exports = ({ entry, mode, filename, types, keepBuildEnvVariables, plugins
             unsafe: true,
             unsafe_methods: true,
           },
+          keep_classnames: /^_/
         },
       }),
     ],
