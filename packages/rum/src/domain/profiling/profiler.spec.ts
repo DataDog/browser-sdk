bfd619e27be2ca296f20fea688f95874f6961044
Thomas BERTET <thomas.bertet@gmail.com>
2025-05-27 13:58:42 +0200

    [RUM Profiler] Simplify & fix logic for Long Task registry (#3569)
    * [RUM Profiler] Simplify & fix logic for Long Task registry
    
    * [RUM Profiler] Add unit test for long task collection and correlation
    
    * [RUM Profiler] Add unit test to cover long task registry logic

--- packages/rum/src/domain/profiling/profiler.spec.ts
+++ packages/rum/src/domain/profiling/profiler.spec.ts
@@ -1,11 +1,19 @@
-import { LifeCycle } from '@datadog/browser-rum-core'
+import { LifeCycle, LifeCycleEventType, RumEventType, RumPerformanceEntryType } from '@datadog/browser-rum-core'
+import type { RelativeTime } from '@datadog/browser-core'
 import { relativeNow, timeStampNow } from '@datadog/browser-core'
 import { setPageVisibility, restorePageVisibility, createNewEvent } from '@datadog/browser-core/test'
-import { createRumSessionManagerMock, mockPerformanceObserver, mockRumConfiguration } from '../../../../rum-core/test'
+import {
+  createPerformanceEntry,
+  createRawRumEvent,
+  createRumSessionManagerMock,
+  mockPerformanceObserver,
+  mockRumConfiguration,
+} from '../../../../rum-core/test'
 import { mockProfiler } from '../../../test'
 import { mockedTrace } from './test-utils/mockedTrace'
 import { transport } from './transport/transport'
 import { createRumProfiler } from './profiler'
+import type { RumProfilerTrace } from './types'
 
 describe('profiler', () => {
   let sendProfileSpy: jasmine.Spy
@@ -25,7 +33,7 @@ describe('profiler', () => {
     const sessionManager = createRumSessionManagerMock().setId('session-id-1')
     lifeCycle = new LifeCycle()
 
-    mockPerformanceObserver()
+    const { notifyPerformanceEntries } = mockPerformanceObserver()
 
     // Replace Browser's Profiler with a mock for testing purpose.
     mockProfiler(mockedTrace)
@@ -43,11 +51,11 @@ describe('profiler', () => {
         minProfileDurationMs: 0,
       }
     )
-    return profiler
+    return { profiler, notifyPerformanceEntries }
   }
 
   it('should start profiling collection and collect data on stop', async () => {
-    const profiler = setupProfiler()
+    const { profiler } = setupProfiler()
 
     profiler.start({
       id: 'view-id-1',
@@ -74,7 +82,7 @@ describe('profiler', () => {
   })
 
   it('should pause profiling collection on hidden visibility and restart on visible visibility', async () => {
-    const profiler = setupProfiler()
+    const { profiler } = setupProfiler()
 
     profiler.start({
       id: 'view-id-1',
@@ -114,6 +122,57 @@ describe('profiler', () => {
 
     // Check the the sendProfilesSpy was called with the mocked trace
     expect(sendProfileSpy).toHaveBeenCalledWith(mockedTrace, jasmine.any(Object), jasmine.any(String), 'session-id-1')
+  })
+
+  it('should collect long task from core and then attach long task id to the Profiler trace', async () => {
+    const { profiler, notifyPerformanceEntries } = setupProfiler()
+
+    profiler.start({
+      id: 'view-id-1',
+      name: 'view-name-1',
+      startClocks: {
+        relative: relativeNow(),
+        timeStamp: timeStampNow(),
+      },
+    })
+
+    await waitForBoolean(() => profiler.isRunning())
+
+    // Generate a Long Task RUM event
+    const longTaskRumEvent = createRawRumEvent(RumEventType.LONG_TASK, {
+      long_task: {
+        id: 'long-task-id-1',
+        duration: 1000,
+        startTime: 12345 as RelativeTime,
+      },
+    })
+
+    // Notify Profiler that a Long Task RUM event has been collected (via its LifeCycle)
+    lifeCycle.notify(LifeCycleEventType.RAW_RUM_EVENT_COLLECTED, {
+      rawRumEvent: longTaskRumEvent,
+      startTime: 12345 as RelativeTime,
+      domainContext: {},
+    })
+
+    // Notify Profiler that some long tasks have been collected (via its PerformanceObserver)
+    notifyPerformanceEntries([
+      createPerformanceEntry(RumPerformanceEntryType.LONG_ANIMATION_FRAME, {
+        startTime: 12345 as RelativeTime,
+        entryType: RumPerformanceEntryType.LONG_ANIMATION_FRAME,
+      }),
+    ])
+
+    // Stop collection of profile.
+    await profiler.stop()
+
+    // Wait for stop of collection.
+    await waitForBoolean(() => profiler.isStopped())
+
+    const lastCall: RumProfilerTrace = sendProfileSpy.calls.mostRecent().args[0] as unknown as RumProfilerTrace
+
+    expect(lastCall.longTasks.length).toBe(1)
+    expect(lastCall.longTasks[0].id).toBeDefined()
+    expect(lastCall.longTasks[0].startClocks.relative).toBe(12345 as RelativeTime)
   })
 })
 
