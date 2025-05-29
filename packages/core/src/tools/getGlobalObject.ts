013e07442a7255b759961ad792f17705ddb9a039
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2025-05-29 08:48:00 -0700

    polyfill getGlobalObject

--- packages/core/src/tools/getGlobalObject.ts
+++ packages/core/src/tools/getGlobalObject.ts
@@ -6,26 +6,39 @@ export function getGlobalObject<T = typeof globalThis>(): T {
   if (typeof globalThis === 'object') {
     return globalThis as unknown as T
   }
-  Object.defineProperty(Object.prototype, '_dd_temp_', {
-    get() {
-      return this as object
-    },
-    configurable: true,
-  })
-  // @ts-ignore _dd_temp is defined using defineProperty
-  let globalObject: unknown = _dd_temp_
-  // @ts-ignore _dd_temp is defined using defineProperty
-  delete Object.prototype._dd_temp_
-  if (typeof globalObject !== 'object') {
-    // on safari _dd_temp_ is available on window but not globally
-    // fallback on other browser globals check
-    if (typeof self === 'object') {
-      globalObject = self
-    } else if (typeof window === 'object') {
-      globalObject = window
-    } else {
-      globalObject = {}
+
+  // Polyfill globalThis if it doesn't exist
+  if (typeof globalThis === 'undefined') {
+    Object.defineProperty(Object.prototype, '_dd_temp_', {
+      get() {
+        return this as object
+      },
+      configurable: true,
+    })
+    // @ts-ignore _dd_temp is defined using defineProperty
+    let globalObject: unknown = _dd_temp_
+    // @ts-ignore _dd_temp is defined using defineProperty
+    delete Object.prototype._dd_temp_
+    if (typeof globalObject !== 'object') {
+      // on safari _dd_temp_ is available on window but not globally
+      // fallback on other browser globals check
+      if (typeof self === 'object') {
+        globalObject = self
+      } else if (typeof window === 'object') {
+        globalObject = window
+      } else {
+        globalObject = {}
+      }
     }
+
+    // Define globalThis if it doesn't exist
+    Object.defineProperty(globalObject as object, 'globalThis', {
+      value: globalObject,
+      writable: true,
+      enumerable: false,
+      configurable: true,
+    })
   }
-  return globalObject as T
+
+  return globalThis as unknown as T
 }
--- packages/flagging/src/entries/main.ts
+++ packages/flagging/src/entries/main.ts
@@ -1,9 +1,16 @@
 import { defineGlobal, getGlobalObject } from '@datadog/browser-core'
 
+// Apply globalThis polyfill before importing OpenFeature
+// The OpenFeature web SDK uses globalThis which is not available in older browsers (like Chrome 63).
+// Our getGlobalObject() utility provides a polyfill that makes globalThis available by using
+// fallbacks like window or self. We need to call it before importing OpenFeature to ensure
+// the polyfill is in place when OpenFeature tries to use globalThis.
+getGlobalObject()
+
 import { DatadogProvider } from '../openfeature/provider'
 
-export { DatadogProvider }
 export { configurationFromString, configurationToString } from '../configuration'
+export { DatadogProvider }
 
 interface BrowserWindow extends Window {
   DD_FLAGGING?: {
