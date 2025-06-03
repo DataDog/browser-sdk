c5128b4b7ab29c63707b3baea0500bc4ccbbb7ab
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2025-06-03 18:34:19 +0200

    fix add exact match for extension origin

--- packages/core/src/domain/allowedTrackingOrigins.ts
+++ packages/core/src/domain/allowedTrackingOrigins.ts
@@ -21,7 +21,7 @@ export function isAllowedTrackingOrigins(
     return true
   }
 
-  const isAllowed = matchList(allowedTrackingOrigins, windowOrigin, true)
+  const isAllowed = matchList(allowedTrackingOrigins, windowOrigin)
   if (!isAllowed) {
     display.error(ERROR_NOT_ALLOWED_TRACKING_ORIGIN)
   }
