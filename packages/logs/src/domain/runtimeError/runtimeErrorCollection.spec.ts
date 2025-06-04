d14a29fe07206a6891ef3d9f22d0ae794eb95daa
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-06-04 18:43:45 +0200

    ✅ refactor logs runtimeErrorCollection.spec

--- packages/logs/src/domain/runtimeError/runtimeErrorCollection.spec.ts
+++ packages/logs/src/domain/runtimeError/runtimeErrorCollection.spec.ts
@@ -1,5 +1,6 @@
 import type { ErrorWithCause } from '@datadog/browser-core'
 import { ErrorSource, ErrorHandling } from '@datadog/browser-core'
+import { registerCleanupTask } from '../../../../core/test'
 import type { RawRuntimeLogsEvent } from '../../rawLogsEvent.types'
 import type { LogsConfiguration } from '../configuration'
 import { StatusType } from '../logger/isAuthorized'
@@ -7,11 +8,21 @@ import type { RawLogsEventCollectedData } from '../lifeCycle'
 import { LifeCycle, LifeCycleEventType } from '../lifeCycle'
 import { startRuntimeErrorCollection } from './runtimeErrorCollection'
 
+function startRuntimeErrorCollectionWithDefaults({
+  forwardErrorsToLogs = true,
+}: { forwardErrorsToLogs?: boolean } = {}) {
+  const rawLogsEvents: Array<RawLogsEventCollectedData<RawRuntimeLogsEvent>> = []
+  const lifeCycle = new LifeCycle()
+  lifeCycle.subscribe(LifeCycleEventType.RAW_LOG_COLLECTED, (rawLogsEvent) =>
+    rawLogsEvents.push(rawLogsEvent as RawLogsEventCollectedData<RawRuntimeLogsEvent>)
+  )
+  const { stop } = startRuntimeErrorCollection({ forwardErrorsToLogs } as LogsConfiguration, lifeCycle)
+  registerCleanupTask(stop)
+
+  return { rawLogsEvents }
+}
+
 describe('runtime error collection', () => {
-  const configuration = { forwardErrorsToLogs: true } as LogsConfiguration
-  let lifeCycle: LifeCycle
-  let stopRuntimeErrorCollection: () => void
-  let rawLogsEvents: Array<RawLogsEventCollectedData<RawRuntimeLogsEvent>>
   let onErrorSpy: jasmine.Spy
   let originalOnErrorHandler: OnErrorEventHandler
 
@@ -19,20 +30,14 @@ describe('runtime error collection', () => {
     originalOnErrorHandler = window.onerror
     onErrorSpy = jasmine.createSpy()
     window.onerror = onErrorSpy
-    rawLogsEvents = []
-    lifeCycle = new LifeCycle()
-    lifeCycle.subscribe(LifeCycleEventType.RAW_LOG_COLLECTED, (rawLogsEvent) =>
-      rawLogsEvents.push(rawLogsEvent as RawLogsEventCollectedData<RawRuntimeLogsEvent>)
-    )
   })
 
   afterEach(() => {
-    stopRuntimeErrorCollection()
     window.onerror = originalOnErrorHandler
   })
 
   it('should send runtime errors', (done) => {
-    ;({ stop: stopRuntimeErrorCollection } = startRuntimeErrorCollection(configuration, lifeCycle))
+    const { rawLogsEvents } = startRuntimeErrorCollectionWithDefaults()
     setTimeout(() => {
       throw new Error('error!')
     })
@@ -68,7 +73,7 @@ describe('runtime error collection', () => {
 
     nestedError.cause = deepNestedError
     error.cause = nestedError
-    ;({ stop: stopRuntimeErrorCollection } = startRuntimeErrorCollection(configuration, lifeCycle))
+    const { rawLogsEvents } = startRuntimeErrorCollectionWithDefaults()
     setTimeout(() => {
       // eslint-disable-next-line @typescript-eslint/only-throw-error
       throw error
@@ -107,10 +112,7 @@ describe('runtime error collection', () => {
   })
 
   it('should not send runtime errors when forwardErrorsToLogs is false', (done) => {
-    ;({ stop: stopRuntimeErrorCollection } = startRuntimeErrorCollection(
-      { ...configuration, forwardErrorsToLogs: false },
-      lifeCycle
-    ))
+    const { rawLogsEvents } = startRuntimeErrorCollectionWithDefaults({ forwardErrorsToLogs: false })
 
     setTimeout(() => {
       throw new Error('error!')
@@ -129,7 +131,7 @@ describe('runtime error collection', () => {
 
     const error = new Error('Error with dd_context') as DatadogError
     error.dd_context = { foo: 'barr' }
-    ;({ stop: stopRuntimeErrorCollection } = startRuntimeErrorCollection(configuration, lifeCycle))
+    const { rawLogsEvents } = startRuntimeErrorCollectionWithDefaults()
     setTimeout(() => {
       throw error
     })
