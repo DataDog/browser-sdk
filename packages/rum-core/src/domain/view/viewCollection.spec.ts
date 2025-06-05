d95939618987cc753a5f7fdc55ad4cc3fc72758a
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-06-05 12:06:16 +0200

    fix: add device info to the right place

--- packages/rum-core/src/domain/view/viewCollection.spec.ts
+++ packages/rum-core/src/domain/view/viewCollection.spec.ts
@@ -201,6 +201,11 @@ describe('viewCollection', () => {
         },
       },
       privacy: { replay_level: 'mask' },
+      device: {
+        current_locale: jasmine.any(String),
+        locales: jasmine.any(Array),
+        time_zone: jasmine.any(String),
+      },
     })
   })
 
