e5f565ea6ab6ecc9ab9669b9c97423385e2aa330
Aymeric <aymeric.mortemousque@datadoghq.com>
2025-06-06 10:34:14 +0200

    ♻️ [RUM-10256] Use hooks for Logs SDK to decouple global context (#3594)

--- packages/rum-core/src/boot/startRum.ts
+++ packages/rum-core/src/boot/startRum.ts
@@ -16,6 +16,7 @@ import {
   addTelemetryDebug,
   drainPreStartTelemetry,
   startAccountContext,
+  startGlobalContext,
 } from '@datadog/browser-core'
 import type { RumMutationRecord } from '../browser/domMutationObservable'
 import { createDOMMutationObservable } from '../browser/domMutationObservable'
@@ -47,7 +48,6 @@ import { startLongAnimationFrameCollection } from '../domain/longAnimationFrame/
 import { RumPerformanceEntryType, supportPerformanceTimingEvent } from '../browser/performanceObservable'
 import { startLongTaskCollection } from '../domain/longTask/longTaskCollection'
 import { startSyntheticsContext } from '../domain/contexts/syntheticsContext'
-import { startGlobalContext } from '../domain/contexts/globalContext'
 import { startUserContext } from '../domain/contexts/userContext'
 import { startRumAssembly } from '../domain/assembly'
 import { startSessionContext } from '../domain/contexts/sessionContext'
@@ -140,7 +140,7 @@ export function startRum(
   const featureFlagContexts = startFeatureFlagContexts(lifeCycle, hooks, configuration)
   startSessionContext(hooks, session, recorderApi, viewHistory)
   startConnectivityContext(hooks)
-  const globalContext = startGlobalContext(hooks, configuration)
+  const globalContext = startGlobalContext(hooks, configuration, 'rum', true)
   const userContext = startUserContext(hooks, configuration, session)
   const accountContext = startAccountContext(hooks, configuration, 'rum')
 
