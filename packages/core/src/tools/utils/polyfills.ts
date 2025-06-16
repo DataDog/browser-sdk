cfbc2e000162419cbaf1b1ec16e80c3779939991
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-06-16 14:20:25 +0200

    refactor: add guards to access timezone

--- packages/core/src/tools/utils/polyfills.ts
+++ packages/core/src/tools/utils/polyfills.ts
@@ -21,3 +21,17 @@ export function objectValues<T = unknown>(object: { [key: string]: T }) {
 export function objectEntries<T = unknown>(object: { [key: string]: T }): Array<[string, T]> {
   return Object.entries(object)
 }
+
+export function getTimeZone() {
+  // Use Intl.DateTimeFormat().resolvedOptions().timeZone to get the time zone
+  // This is supported in all modern browsers and Node.js
+  // Making sure to handle cases where the time zone cannot be determined
+  try {
+    const intl = new Intl.DateTimeFormat()
+
+    return intl.resolvedOptions().timeZone
+  } catch {
+    // Fallback to a default value if the time zone cannot be determined
+    return undefined
+  }
+}
