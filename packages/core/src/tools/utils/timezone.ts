d149bdd2c35383fb39bbcf8794f65b74653d3cce
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-06-17 11:04:17 +0200

    fix: pr feedback - remove documentation

--- packages/core/src/tools/utils/timezone.ts
+++ packages/core/src/tools/utils/timezone.ts
@@ -1,13 +1,9 @@
 export function getTimeZone() {
-  // Use Intl.DateTimeFormat().resolvedOptions().timeZone to get the time zone
-  // This is supported in all modern browsers and Node.js
-  // Making sure to handle cases where the time zone cannot be determined
   try {
     const intl = new Intl.DateTimeFormat()
 
     return intl.resolvedOptions().timeZone
   } catch {
-    // Fallback to a default value if the time zone cannot be determined
     return undefined
   }
 }
