f52c9a34ba9ac3eb6d93a163677b6124a226c00e
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-07-14 17:00:42 +0200

    🐛 warnings

--- packages/logs/src/domain/contexts/trackingConsentContext.ts
+++ packages/logs/src/domain/contexts/trackingConsentContext.ts
@@ -1,10 +1,10 @@
-import { DISCARDED, HookNames } from '@datadog/browser-core'
+import { DISCARDED, HookNames, SKIPPED } from '@datadog/browser-core'
 import type { TrackingConsentState } from '@datadog/browser-core'
 import type { Hooks } from '../hooks'
 
 export function startTrackingConsentContext(hooks: Hooks, trackingConsentState: TrackingConsentState) {
-  hooks.register(HookNames.Assemble, ({ startTime }) => {
-    const wasConsented = trackingConsentState.isGranted(startTime)
+  hooks.register(HookNames.Assemble, () => {
+    const wasConsented = trackingConsentState.isGranted()
 
     if (!wasConsented) {
       return DISCARDED
