1c3b8ada7d0955e34ae40c2fef6544ce76e59a5f
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-07-14 16:59:37 +0200

    ✨ create consent context

---
+++ packages/logs/src/domain/contexts/trackingConsentContext.ts
@@ -0,0 +1,15 @@
+import { DISCARDED, HookNames } from '@datadog/browser-core'
+import type { TrackingConsentState } from '@datadog/browser-core'
+import type { Hooks } from '../hooks'
+
+export function startTrackingConsentContext(hooks: Hooks, trackingConsentState: TrackingConsentState) {
+  hooks.register(HookNames.Assemble, ({ startTime }) => {
+    const wasConsented = trackingConsentState.isGranted(startTime)
+
+    if (!wasConsented) {
+      return DISCARDED
+    }
+
+    return SKIPPED
+  })
+}
