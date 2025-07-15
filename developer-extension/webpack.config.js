a2ac278112b2e6569244024b3318ffabc79bac8f
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2025-07-15 13:46:43 +0200

    added hot-reload

--- developer-extension/webpack.config.js
+++ developer-extension/webpack.config.js
@@ -1,51 +1,100 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { default: WebextensionPlugin } = require('@webextension-toolbox/webpack-webextension-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const { HotModuleReplacementPlugin } = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const { createDefinePlugin } = require('../webpack.base')
 
 module.exports = (_env, argv) => {
+  const isDevelopment = argv.mode === 'development'
+
   return [
     baseConfig({
+      name: 'background',
       entry: './src/background',
       output: {
         filename: 'background.js',
       },
       plugins: [
-        new WebextensionPlugin({
-          manifestDefaults: {
-            ...getVersion(),
-          },
-        }),
+        ...(isDevelopment
+          ? []
+          : [
+              new WebextensionPlugin({
+                manifestDefaults: {
+                  ...getVersion(),
+                },
+              }),
+            ]),
         new CopyPlugin({
-          patterns: [{ from: './icons/' }],
+          patterns: [
+            { from: './icons/' },
+            ...(isDevelopment
+              ? [{ from: './manifest-dev.json', to: 'manifest.json', force: true }]
+              : [{ from: './manifest.json', to: 'manifest.json' }]),
+          ],
         }),
       ],
     }),
     baseConfig({
-      entry: './src/panel',
+      name: 'panel',
+      entry: './src/panel/index.tsx',
+      devServer: isDevelopment
+        ? {
+            hot: true,
+            port: 3001,
+            static: false,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+            },
+            allowedHosts: 'all',
+            liveReload: false,
+            client: {
+              overlay: {
+                errors: true,
+                warnings: false,
+              },
+              // Disable webpack hot client injection to avoid CSP issues
+              webSocketTransport: 'ws',
+            },
+            devMiddleware: {
+              writeToDisk: (filePath) => filePath.endsWith('panel.html'),
+            },
+          }
+        : undefined,
       output: {
         filename: 'panel.js',
+        publicPath: isDevelopment ? 'http://localhost:3001/' : './',
       },
       plugins: [
         new HtmlWebpackPlugin({
           filename: 'panel.html',
+          template: 'src/panel/panel.html.template',
+          inject: false, // Disable auto script injection - we handle it manually
+          templateParameters: {
+            isDevelopment,
+            devServerUrl: 'http://localhost:3001/',
+          },
         }),
         createDefinePlugin(),
+        ...(isDevelopment ? [new HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()] : []),
       ],
     }),
     baseConfig({
+      name: 'content-main',
       entry: './src/content-scripts/main.ts',
       output: {
         filename: 'content-script-main.js',
       },
     }),
     baseConfig({
+      name: 'content-isolated',
       entry: './src/content-scripts/isolated.ts',
       output: {
         filename: 'content-script-isolated.js',
       },
     }),
     baseConfig({
+      name: 'devtools',
       entry: './src/devtools',
       output: {
         filename: 'devtools.js',
@@ -58,11 +107,14 @@ module.exports = (_env, argv) => {
     }),
   ]
 
-  function baseConfig({ entry, output, plugins }) {
+  function baseConfig({ name, entry, output, plugins, devServer }) {
     return {
+      name,
       entry,
       output,
-      devtool: argv.mode === 'development' ? 'inline-source-map' : false,
+      // Use source-map instead of eval-based devtools for CSP compatibility
+      devtool: isDevelopment ? 'source-map' : false,
+      devServer,
 
       module: {
         rules: [
@@ -72,6 +124,12 @@ module.exports = (_env, argv) => {
             exclude: /node_modules/,
             options: {
               onlyCompileBundledFiles: true,
+              ...(isDevelopment && {
+                getCustomTransformers: () => ({
+                  before: [require('react-refresh-typescript').default()],
+                }),
+                transpileOnly: true,
+              }),
             },
           },
           {
