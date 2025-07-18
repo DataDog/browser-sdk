1568983996e177a931a710135b089808de699464
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-07-18 12:42:46 +0200

    ♻️ remove blocking regular events in RUM

--- packages/rum-core/src/domain/contexts/trackingConsentContext.spec.ts
+++ packages/rum-core/src/domain/contexts/trackingConsentContext.spec.ts
@@ -11,56 +11,6 @@ describe('tracking consent context', () => {
     hooks = createHooks()
   })
 
-  describe('for regular RUM events (Assemble hook)', () => {
-    it('should discard RUM events if consent is not granted', () => {
-      const trackingConsentState = createTrackingConsentState(TrackingConsent.NOT_GRANTED)
-      startTrackingConsentContext(hooks, trackingConsentState)
-
-      const defaultLogAttributes = hooks.triggerHook(HookNames.Assemble, {
-        eventType: 'view' as const,
-        startTime: 0 as RelativeTime,
-      })
-
-      expect(defaultLogAttributes).toBe(DISCARDED)
-    })
-
-    it('should not discard RUM events if consent is granted and no startTime is provided', () => {
-      const trackingConsentState = createTrackingConsentState(TrackingConsent.GRANTED)
-      startTrackingConsentContext(hooks, trackingConsentState)
-
-      const defaultLogAttributes = hooks.triggerHook(HookNames.Assemble, {
-        eventType: 'view' as const,
-        startTime: undefined as any,
-      }) as DefaultRumEventAttributes
-
-      expect(defaultLogAttributes).toBeUndefined()
-    })
-
-    it('should not discard RUM events when startTime is provided (due to empty history)', () => {
-      const trackingConsentState = createTrackingConsentState(TrackingConsent.GRANTED)
-      startTrackingConsentContext(hooks, trackingConsentState)
-
-      const defaultLogAttributes = hooks.triggerHook(HookNames.Assemble, {
-        eventType: 'view' as const,
-        startTime: 100 as RelativeTime,
-      })
-
-      expect(defaultLogAttributes).toBeUndefined()
-    })
-
-    it('should discard RUM events when startTime is provided and consent was not granted initially', () => {
-      const trackingConsentState = createTrackingConsentState(TrackingConsent.NOT_GRANTED)
-      startTrackingConsentContext(hooks, trackingConsentState)
-
-      const defaultLogAttributes = hooks.triggerHook(HookNames.Assemble, {
-        eventType: 'view' as const,
-        startTime: 100 as RelativeTime,
-      })
-
-      expect(defaultLogAttributes).toBe(DISCARDED)
-    })
-  })
-
   describe('for telemetry (AssembleTelemetry hook)', () => {
     it('should discard telemetry if consent is not granted', () => {
       const trackingConsentState = createTrackingConsentState(TrackingConsent.NOT_GRANTED)
