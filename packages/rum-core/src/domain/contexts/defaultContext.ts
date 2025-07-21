f3ab7ba8261e19ef7bb384baabc33e6e94e8ad95
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-07-21 15:51:16 +0200

    ✨ override source

--- packages/rum-core/src/domain/contexts/defaultContext.ts
+++ packages/rum-core/src/domain/contexts/defaultContext.ts
@@ -1,6 +1,6 @@
 import { canUseEventBridge, currentDrift, HookNames, round, timeStampNow } from '@datadog/browser-core'
 import type { RumConfiguration } from '../configuration'
-import type { DefaultRumEventAttributes, DefaultTelemetryEventAttributes, Hooks } from '../hooks'
+import type { DefaultRumEventAttributes, Hooks } from '../hooks'
 
 // replaced at build time
 declare const __BUILD_ENV__SDK_VERSION__: string
@@ -10,9 +10,12 @@ export function startDefaultContext(
   configuration: RumConfiguration,
   sdkName: 'rum' | 'rum-slim' | 'rum-synthetics' | undefined
 ) {
-  hooks.register(
-    HookNames.Assemble,
-    ({ eventType }): DefaultRumEventAttributes => ({
+  hooks.register(HookNames.Assemble, ({ eventType }): DefaultRumEventAttributes => {
+    const source = configuration.source
+    const variant = configuration.variant
+    const isSourceOverridden = source !== 'browser'
+
+    return {
       type: eventType,
       _dd: {
         format_version: 2,
@@ -22,21 +25,15 @@ export function startDefaultContext(
           session_replay_sample_rate: round(configuration.sessionReplaySampleRate, 3),
           profiling_sample_rate: round(configuration.profilingSampleRate, 3),
         },
-        browser_sdk_version: canUseEventBridge() ? __BUILD_ENV__SDK_VERSION__ : undefined,
+        browser_sdk_version: canUseEventBridge() || isSourceOverridden ? __BUILD_ENV__SDK_VERSION__ : undefined,
         sdk_name: sdkName,
+        ...(variant ? { variant } : {}),
       },
       application: {
         id: configuration.applicationId,
       },
       date: timeStampNow(),
-      source: 'browser',
-    })
-  )
-
-  hooks.register(
-    HookNames.AssembleTelemetry,
-    (): DefaultTelemetryEventAttributes => ({
-      application: { id: configuration.applicationId },
-    })
-  )
+      source: source as DefaultRumEventAttributes['source'],
+    }
+  })
 }
