fce552a9fc33ead65f4e7d099dde5f2d7ccc02d0
roman.gaignault <roman.gaignault@datadoghq.com>
2025-07-25 15:28:46 +0200

    fix ci

--- webpack.base.js
+++ webpack.base.js
@@ -1,3 +1,6 @@
+// Register ts-node to be able to import TypeScript files
+require('ts-node/register')
+
 const path = require('path')
 const webpack = require('webpack')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
