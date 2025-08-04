3e0ba55d2f5318172c3aba0b2cb5b932a773389a
roman.gaignault <roman.gaignault@datadoghq.com>
2025-08-04 11:13:12 +0200

    transpilation to old browsers and license

--- test/unit/karma.base.conf.js
+++ test/unit/karma.base.conf.js
@@ -112,7 +112,7 @@ function overrideTsLoaderRule(module) {
   // We use swc-loader to transpile some dependencies that are using syntax not compatible with browsers we use for testing
   module.rules.push({
     test: /\.m?js$/,
-    include: /node_modules\/(react-router-dom|turbo-stream)/,
+    include: /node_modules\/(react-router-dom|react-router|turbo-stream)/,
     use: {
       loader: 'swc-loader',
       options: {
