8cd66b59e78d356edffb0f029c2c2e6fb7d6f441
Seth Fowler <seth.fowler@datadoghq.com>
2025-08-19 15:37:59 +0100

    Address review comments

--- packages/rum-core/src/domain/view/viewMetrics/startInitialViewMetricsTelemetry.ts
+++ packages/rum-core/src/domain/view/viewMetrics/startInitialViewMetricsTelemetry.ts
@@ -1,6 +1,7 @@
 import type { Context, Telemetry } from '@datadog/browser-core'
 import { performDraw, addTelemetryMetrics, noop } from '@datadog/browser-core'
-import { LifeCycleEventType, type LifeCycle } from '../../lifeCycle'
+import { LifeCycleEventType } from '../../lifeCycle'
+import type { LifeCycle } from '../../lifeCycle'
 import type { RumConfiguration } from '../../configuration'
 import type { LargestContentfulPaint } from './trackLargestContentfulPaint'
 import type { NavigationTimings } from './trackNavigationTimings'
@@ -35,6 +36,14 @@ export function startInitialViewMetricsTelemetry(
     if (!initialViewMetrics.largestContentfulPaint || !initialViewMetrics.navigationTimings) {
       return
     }
+
+    // The navigation timings become available shortly after the load event fires, so
+    // we're snapshotting the LCP value available at that point. However, more LCP values
+    // can be emitted until the page is scrolled or interacted with, so it's possible that
+    // the final LCP value may differ. These metrics are intended to help diagnose
+    // performance issues early in the page load process, and using LCP-at-page-load is a
+    // good fit for that use case, but it's important to be aware that this is not
+    // necessarily equivalent to the normal LCP metric.
 
     addTelemetryMetrics(INITIAL_VIEW_METRICS_TELEMETRY_NAME, {
       metrics: createCoreInitialViewMetrics(
