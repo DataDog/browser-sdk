d2df59743ffc19a8c1f7ed01f0a68e53ac4a97e8
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-09-01 14:32:44 +0200

    ✨ mock session strategy

--- packages/core/src/domain/session/sessionStore.ts
+++ packages/core/src/domain/session/sessionStore.ts
@@ -16,6 +16,7 @@ import {
   isSessionStarted,
 } from './sessionState'
 import { initLocalStorageStrategy, selectLocalStorageStrategy } from './storeStrategies/sessionInLocalStorage'
+import { initMockStrategy, selectMockStrategy } from './storeStrategies/sessionInMock'
 import { processSessionStoreOperations } from './sessionStoreOperations'
 import { SESSION_NOT_TRACKED, SessionPersistence } from './sessionConstants'
 
@@ -53,9 +54,12 @@ export function selectSessionStoreStrategyType(
     case SessionPersistence.LOCAL_STORAGE:
       return selectLocalStorageStrategy()
 
+    case SessionPersistence.NONE:
+      return selectMockStrategy()
+
     case undefined: {
       let sessionStoreStrategyType = isWorkerEnvironment
-        ? selectLocalStorageStrategy()
+        ? selectMockStrategy()
         : selectCookieStrategy(initConfiguration)
       if (!sessionStoreStrategyType && initConfiguration.allowFallbackToLocalStorage) {
         sessionStoreStrategyType = selectLocalStorageStrategy()
@@ -72,9 +76,17 @@ export function getSessionStoreStrategy(
   sessionStoreStrategyType: SessionStoreStrategyType,
   configuration: Configuration
 ) {
-  return sessionStoreStrategyType.type === SessionPersistence.COOKIE
-    ? initCookieStrategy(configuration, sessionStoreStrategyType.cookieOptions)
-    : initLocalStorageStrategy(configuration)
+  switch (sessionStoreStrategyType.type) {
+    case SessionPersistence.COOKIE:
+      return initCookieStrategy(configuration, sessionStoreStrategyType.cookieOptions)
+
+    case SessionPersistence.NONE:
+      return initMockStrategy()
+
+    case SessionPersistence.LOCAL_STORAGE:
+    default:
+      return initLocalStorageStrategy(configuration)
+  }
 }
 
 /**
