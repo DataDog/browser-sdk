dff17562fcfe23a8aaf2183a82dbfd69c9ed852d
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-09-17 10:13:35 +0200

    🐛 more removing transition

--- packages/rum-core/src/domain/trackEventCounts.ts
+++ packages/rum-core/src/domain/trackEventCounts.ts
@@ -30,7 +30,7 @@ export function trackEventCounts({
   }
 
   const subscription = lifeCycle.subscribe(LifeCycleEventType.RUM_EVENT_COLLECTED, (event): void => {
-    if (event.type === 'view' || event.type === 'vital' || event.type === 'transition' || !isChildEvent(event)) {
+    if (event.type === 'view' || event.type === 'vital' || !isChildEvent(event)) {
       return
     }
     switch (event.type) {
