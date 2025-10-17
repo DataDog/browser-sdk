c4f60e955e2d9794653f6a88ac6434f0c6e09c5d
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2025-10-17 13:01:16 +0200

    👷 improve node detection

--- packages/core/src/tools/globalObject.ts
+++ packages/core/src/tools/globalObject.ts
@@ -49,6 +49,6 @@ export function getGlobalObject<T = typeof globalThis>(): T {
 export const globalObject = getGlobalObject<GlobalObject>()
 
 export const isWorkerEnvironment = 'WorkerGlobalScope' in globalObject
-// @see https://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js
-// @ts-expect-error module is undefined outside of Node.
-export const isNodeEnvironment = typeof module !== 'undefined' && module.exports
+export const isNodeEnvironment =
+  // @ts-expect-error for Node.js-specific globals that are not present in browser environments
+  typeof process !== 'undefined' && process.versions !== null && process.versions.node !== null
