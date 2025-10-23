f7b676c1dee56fc5c399f1b22081c9a644b5c134
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2025-10-23 16:46:22 +0200

    Fix test

--- benchmark/scenarios/heavy.scenario.ts
+++ benchmark/scenarios/heavy.scenario.ts
@@ -3,34 +3,36 @@ import { createBenchmarkTest } from '../helpers'
 
 test.describe('benchmark', () => {
   createBenchmarkTest('heavy').run(async (page, takeMeasurements) => {
-    await page.goto('http://localhost:5173/', {
-      waitUntil: 'networkidle',
-    })
-
+    await page.goto('http://localhost:5173/');
     await takeMeasurements()
 
-    await page.getByRole('textbox', { name: 'Search...' }).fill('test search')
-    await page.getByRole('textbox', { name: 'Search...' }).press('Enter')
-    await page.getByText('Requests:8.0KErrors:3Latency:').click()
-    await page.getByRole('button', { name: '🔔' }).click()
-    await page.getByRole('link', { name: '📝 Logs' }).click()
-    await page.getByRole('textbox', { name: 'Search logs...' }).click()
-    await page.getByRole('textbox', { name: 'Search logs...' }).fill('')
-    await page.locator('label').filter({ hasText: 'ERROR' }).locator('span').click()
-    await page.getByRole('combobox').first().selectOption('api-gateway')
-    await page.getByRole('combobox').nth(1).selectOption('prod-api-02')
-    await page.getByRole('combobox').nth(1).selectOption('')
-    await page.getByRole('combobox').first().selectOption('')
-    await page.locator('.log-cell.log-cell-level').first().click()
-    await page.getByText('version:').click()
-    await page.getByRole('button', { name: '✕' }).click()
-    await page.getByRole('link', { name: '🔍 APM Traces' }).click()
-    await page.getByText('Status:AllOKWarningErrorService:All Servicesanalytics-serviceapi-gatewayauth-').click()
-    await page.getByLabel('Service:').selectOption('api-gateway')
-    await page.getByText('POST /api/checkout').click()
-    await page.getByRole('link', { name: '🖥️ Infrastructure' }).click()
-    await page.getByRole('link', { name: '⚙️ Settings' }).click()
-
+   await page.getByRole('textbox', { name: 'Search...' }).click();
+    await page.getByRole('textbox', { name: 'Search...' }).fill('test super');
+    await page.getByText('APM Requests↑1.23M').click();
+    await page.getByRole('button', { name: '🔔' }).click();
+    await page.getByText('New dashboard available2').click();
+    await page.getByRole('button', { name: '🔔' }).click();
+    await page.getByRole('link', { name: '📝 Logs' }).click();
+    await page.getByRole('textbox', { name: 'Search log messages...' }).click();
+    await page.getByRole('textbox', { name: 'Search log messages...' }).fill('war');
+    await page.getByRole('button', { name: 'Clear search' }).click();
+    await page.locator('label').filter({ hasText: 'ERROR' }).click();
+    await page.locator('[data-test-id="log-row-log-1940"]').getByText('logging-service').click();
+    await page.getByRole('button', { name: 'Copy' }).click();
+    await page.getByRole('link', { name: '🖥️ Infrastructure' }).click();
+    await page.getByTitle('prod-analytics-01\nCPU: 52%\nMemory: 66%\nDisk: 59%').click();
+    await page.getByTitle('prod-analytics-02\nCPU: 49%\nMemory: 61%\nDisk: 65%').click();
+    await page.getByText('CPU49%Memory61%Disk65%').click();
+    await page.getByRole('button', { name: 'List' }).click();
+    await page.getByRole('cell', { name: '61%' }).first().click();
+    await page.locator('div').filter({ hasText: /^49%$/ }).first().click();
+    await page.getByRole('link', { name: '⚙️ Settings' }).click();
+    await page.getByRole('textbox', { name: 'Full Name *' }).click();
+    await page.getByRole('textbox', { name: 'Full Name *' }).fill('John Do');
+    await page.getByRole('textbox', { name: 'Full Name *' }).press('ControlOrMeta+m');
+    await page.getByRole('textbox', { name: 'Full Name *' }).fill('Merci jhon');
+    await page.getByRole('button', { name: 'Team' }).click();
+    await page.getByRole('button', { name: 'Integrations' }).click();
     expect(true).toBe(true)
   })
 })
