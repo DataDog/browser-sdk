35eaf1f9e598cb06cfc9c20031bb0d38c2076251
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2025-10-24 13:20:25 +0200

    👷 Simplify cleanup task in appendComponent by removing unnecessary act wrapper

--- packages/rum-react/test/appendComponent.ts
+++ packages/rum-react/test/appendComponent.ts
@@ -14,9 +14,7 @@ export function appendComponent(component: React.ReactNode) {
     root.render(component)
   })
   registerCleanupTask(() => {
-    act(() => {
-      root.unmount()
-    })
+    root.unmount()
   })
   return container
 }
