20a1fd989137080ce19176c113fc9a78a8eb5892
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2025-10-31 14:45:40 +0100

    fix e2e

--- test/e2e/lib/helpers/playwright.ts
+++ test/e2e/lib/helpers/playwright.ts
@@ -1,7 +1,7 @@
 import type { PlaywrightWorkerOptions } from '@playwright/test'
 import type { BrowserConfiguration } from '../../../browsers.conf'
-import { getBuildInfos } from '../../../envUtils'
-import packageJson from '../../../../package.json'
+import { getBuildInfos } from '../../../envUtils.cjs'
+import packageJson from '../../../../package.json' with { type: 'json' }
 
 export const DEV_SERVER_BASE_URL = 'http://localhost:8080'
 
