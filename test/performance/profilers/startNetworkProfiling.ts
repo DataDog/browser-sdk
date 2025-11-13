76bc2920ce86a30829b9f4e8c205227d0268489d
Aymeric Mortemousque <aymeric.mortemousque@datadoghq.com>
2025-11-13 10:14:40 +0100

    Fix Target page, context or browser has been closed

--- test/performance/profilers/startNetworkProfiling.ts
+++ test/performance/profilers/startNetworkProfiling.ts
@@ -1,19 +1,24 @@
-import type { Page } from '@playwright/test'
+import type { Page, Request } from '@playwright/test'
 
 export function startNetworkProfiling(page: Page) {
   let uploadBytes = 0
   let downloadBytes = 0
 
-  page.on('requestfinished', async (request) => {
+  async function onRequestFinished(request: Request) {
     const sizes = await request.sizes()
     downloadBytes += sizes.responseBodySize + sizes.responseHeadersSize
     uploadBytes += sizes.requestBodySize + sizes.requestHeadersSize
-  })
+  }
+
+  page.on('requestfinished', onRequestFinished)
 
   return {
-    stopNetworkProfiling: () => ({
-      upload: uploadBytes,
-      download: downloadBytes,
-    }),
+    stopNetworkProfiling: () => {
+      page.off('requestfinished', onRequestFinished)
+      return {
+        upload: uploadBytes,
+        download: downloadBytes,
+      }
+    },
   }
 }
