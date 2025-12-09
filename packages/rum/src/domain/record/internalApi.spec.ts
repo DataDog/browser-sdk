7dc5b2e0fca50ef1702b9f56816b174c0074f2a3
Seth Fowler <seth.fowler@datadoghq.com>
2025-12-09 15:32:45 +0000

    ✨[PANA-5156] Expose better session replay internal API

---
+++ packages/rum/src/domain/record/internalApi.spec.ts
@@ -0,0 +1,122 @@
+import { NodeType, RecordType } from '../../types'
+import { appendElement } from '../../../../rum-core/test'
+import { takeFullSnapshot, takeNodeSnapshot } from './internalApi'
+
+describe('takeFullSnapshot', () => {
+  it('should produce Meta, Focus, and FullSnapshot records', () => {
+    expect(takeFullSnapshot()).toEqual(
+      jasmine.arrayContaining([
+        {
+          data: {
+            height: jasmine.any(Number),
+            href: window.location.href,
+            width: jasmine.any(Number),
+          },
+          type: RecordType.Meta,
+          timestamp: jasmine.any(Number),
+        },
+        {
+          data: {
+            has_focus: document.hasFocus(),
+          },
+          type: RecordType.Focus,
+          timestamp: jasmine.any(Number),
+        },
+        {
+          data: {
+            node: jasmine.any(Object),
+            initialOffset: {
+              left: jasmine.any(Number),
+              top: jasmine.any(Number),
+            },
+          },
+          type: RecordType.FullSnapshot,
+          timestamp: jasmine.any(Number),
+        },
+      ])
+    )
+  })
+
+  it('should produce VisualViewport records when supported', () => {
+    if (!window.visualViewport) {
+      pending('visualViewport not supported')
+    }
+
+    expect(takeFullSnapshot()).toEqual(
+      jasmine.arrayContaining([
+        {
+          data: jasmine.any(Object),
+          type: RecordType.VisualViewport,
+          timestamp: jasmine.any(Number),
+        },
+      ])
+    )
+  })
+})
+
+describe('takeNodeSnapshot', () => {
+  it('should serialize nodes', () => {
+    const node = appendElement('<div>Hello <b>world</b></div>', document.body)
+    expect(takeNodeSnapshot(node)).toEqual({
+      type: NodeType.Element,
+      id: 0,
+      tagName: 'div',
+      isSVG: undefined,
+      attributes: {},
+      childNodes: [
+        {
+          type: NodeType.Text,
+          id: 1,
+          textContent: 'Hello ',
+        },
+        {
+          type: NodeType.Element,
+          id: 2,
+          tagName: 'b',
+          isSVG: undefined,
+          attributes: {},
+          childNodes: [
+            {
+              type: NodeType.Text,
+              id: 3,
+              textContent: 'world',
+            },
+          ],
+        },
+      ],
+    })
+  })
+
+  it('should serialize shadow hosts', () => {
+    const node = appendElement('<div>Hello</div>', document.body)
+    const shadowRoot = node.attachShadow({ mode: 'open' })
+    shadowRoot.appendChild(document.createTextNode('world'))
+    expect(takeNodeSnapshot(node)).toEqual({
+      type: NodeType.Element,
+      id: 0,
+      tagName: 'div',
+      isSVG: undefined,
+      attributes: {},
+      childNodes: [
+        {
+          type: NodeType.Text,
+          id: 1,
+          textContent: 'Hello',
+        },
+        {
+          type: NodeType.DocumentFragment,
+          id: 2,
+          isShadowRoot: true,
+          adoptedStyleSheets: undefined,
+          childNodes: [
+            {
+              type: NodeType.Text,
+              id: 3,
+              textContent: 'world',
+            },
+          ],
+        },
+      ],
+    })
+  })
+})
