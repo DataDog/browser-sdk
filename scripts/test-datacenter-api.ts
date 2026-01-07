db6bc46fbf3be0fb5c097bf6391f3867ff233e8b
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2026-01-07 11:09:09 +0100

    ✨ add test script for datacenter API in CI

---
+++ scripts/test-datacenter-api.ts
@@ -0,0 +1,21 @@
+import { runMain, printLog } from './lib/executionUtils.ts'
+import { getAllDatacenters, getSite } from './lib/datacenter.ts'
+
+/**
+ * Test script to verify datacenter API works in CI
+ * This is temporary and will be removed before merging
+ */
+
+runMain(async () => {
+  printLog('Fetching datacenters from runtime-metadata-service...')
+
+  const datacenters = await getAllDatacenters()
+  printLog(`Found ${datacenters.length} datacenters:`)
+
+  for (const dc of datacenters) {
+    const site = await getSite(dc)
+    printLog(`  - ${dc}: ${site}`)
+  }
+
+  printLog('✅ Datacenter API test successful!')
+})
