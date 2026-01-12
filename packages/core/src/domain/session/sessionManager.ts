cf26f2ba67a7a00de6446fad6a3a773408197c7b
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-01-12 14:20:10 +0100

    fix: tracking consent notify

--- packages/core/src/domain/session/sessionManager.ts
+++ packages/core/src/domain/session/sessionManager.ts
@@ -93,7 +93,7 @@ export function startSessionManager<TrackingType extends string>(
       if (trackingConsentState.isGranted()) {
         sessionStore.expandOrRenewSession()
       } else {
-        sessionStore.expire()
+        sessionStore.expire(false)
       }
     })
 
