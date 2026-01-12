de7e507bae92a5015cae0089c0812c20320fd188
Bastien Caudan <bastien.caudan@datadoghq.com>
2026-01-12 12:43:35 +0100

    Remove workarounds for dd-trace reduction

--- packages/electron/src/domain/trace/tracer.ts
+++ packages/electron/src/domain/trace/tracer.ts
@@ -1,4 +1,3 @@
-process.env.AWS_LAMBDA_FUNCTION_NAME = 'fake-electron'
 import tracer from 'dd-trace'
 
 export function initTracer(service: string, env: string, version: string) {
@@ -6,9 +5,6 @@ export function initTracer(service: string, env: string, version: string) {
     service,
     env,
     version,
-    experimental: {
-      exporter: 'agent',
-    },
   })
 }
 // initialized in a different file to avoid hoisting.
