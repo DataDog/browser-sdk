33c1f46c7982b86e9a3f6f9f5eeb448339434d18
Daniel Oyewole <daniel.oyewole@multiverse.io>
2026-01-26 13:11:27 +0000

    update the regex to allow underscores in the key for the name changes

--- packages/core/src/domain/session/sessionStateValidation.ts
+++ packages/core/src/domain/session/sessionStateValidation.ts
@@ -1,4 +1,4 @@
-export const SESSION_ENTRY_REGEXP = /^([a-zA-Z]+)=([a-z0-9-]+)$/
+export const SESSION_ENTRY_REGEXP = /^([a-zA-Z_]+)=([a-z0-9-]+)$/
 export const SESSION_ENTRY_SEPARATOR = '&'
 
 export function isValidSessionString(sessionString: string | undefined | null): sessionString is string {
