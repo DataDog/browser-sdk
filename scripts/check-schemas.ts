7bbfa3a865291a9903dbd9a2390b655259f3a4e1
Benoit Zugmeyer <benoit.zugmeyer@datadoghq.com>
2026-01-26 13:58:23 +0100

    adjust check-schemas script

--- scripts/check-schemas.ts
+++ scripts/check-schemas.ts
@@ -2,17 +2,26 @@ import { printLog, printError, runMain } from './lib/executionUtils.ts'
 import { command } from './lib/command.ts'
 
 runMain(() => {
+  if (command`git status --porcelain`.run()) {
+    throw new Error('This script should be run from a clean working tree')
+  }
+
   printLog('Regenerating schemas...')
   command`scripts/cli build_json2type`.run()
   command`node scripts/generate-schema-types.ts`.run()
-  command`yarn format -w`.run()
 
   printLog('Checking untracked changes...')
   const diff = command`git diff --color`.run()
-
   if (diff) {
     printLog(diff)
-    printError('\nUntracked changes detected, ensure that schemas and types are in-sync.\n')
+    printError(`
+Untracked changes detected.
+* If your PR requires rum-events-format changes, make sure to run \`yarn json-schemas:sync\`.
+* Else, you probably updated the rum-events-format submodule by mistake. To revert it, run something like:
+  git checkout $(git merge-base main HEAD) rum-events-format
+  git submodule update rum-events-format
+`)
+
     process.exit(1)
   }
 })
