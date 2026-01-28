91820764b32d961b531a425c16e4fbf737d92e19
Benoit Zugmeyer <benoit.zugmeyer@datadoghq.com>
2026-01-28 10:45:31 +0100

    introduce a new way to mock values

--- packages/rum-core/src/domain/resource/retrieveInitialDocumentResourceTiming.ts
+++ packages/rum-core/src/domain/resource/retrieveInitialDocumentResourceTiming.ts
@@ -1,4 +1,4 @@
-import { runOnReadyState } from '@datadog/browser-core'
+import { mockable, runOnReadyState } from '@datadog/browser-core'
 import { RumPerformanceEntryType } from '../../browser/performanceObservable'
 import type { RumPerformanceResourceTiming } from '../../browser/performanceObservable'
 import type { RumConfiguration } from '../configuration'
@@ -8,11 +8,10 @@ import { FAKE_INITIAL_DOCUMENT } from './resourceUtils'
 
 export function retrieveInitialDocumentResourceTiming(
   configuration: RumConfiguration,
-  callback: (timing: RumPerformanceResourceTiming) => void,
-  getNavigationEntryImpl = getNavigationEntry
+  callback: (timing: RumPerformanceResourceTiming) => void
 ) {
   runOnReadyState(configuration, 'interactive', () => {
-    const navigationEntry = getNavigationEntryImpl()
+    const navigationEntry = mockable(getNavigationEntry)()
     const entry: RumPerformanceResourceTiming = Object.assign(navigationEntry.toJSON(), {
       entryType: RumPerformanceEntryType.RESOURCE as const,
       initiatorType: FAKE_INITIAL_DOCUMENT,
