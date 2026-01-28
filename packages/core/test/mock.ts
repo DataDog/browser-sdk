2fbac022bcff8bee393e99dc5600e8aeb66247db
Benoit Zugmeyer <benoit.zugmeyer@datadoghq.com>
2026-01-28 10:45:31 +0100

    introduce a new way to mock values

---
+++ packages/core/test/mock.ts
@@ -0,0 +1,42 @@
+import { mockableValues } from '../src/tools/mockable'
+import { registerCleanupTask } from './registerCleanupTask'
+
+/**
+ * Registers a mock replacement for a mockable value. The mock is automatically
+ * cleaned up after each test via registerCleanupTask.
+ *
+ * @param value - The original value (must be the same reference passed to mockable())
+ * @param replacement - The mock replacement
+ * @example
+ * import { mockValue } from '@datadog/browser-core/test'
+ * import { trackRuntimeError } from '../domain/error/trackRuntimeError'
+ *
+ * mockValue(trackRuntimeError, () => new Observable<RawError>())
+ */
+export function replaceMockable<T>(value: T, replacement: T): void {
+  mockableValues.set(value, replacement)
+  registerCleanupTask(() => {
+    mockableValues.delete(value)
+  })
+}
+
+/**
+ * Creates a Jasmine spy and registers it as a mock replacement for a mockable function.
+ * The mock is automatically cleaned up after each test via registerCleanupTask.
+ *
+ * @param value - The original function (must be the same reference passed to mockable())
+ * @returns A Jasmine spy that can be used for assertions
+ * @example
+ * import { mockWithSpy } from '@datadog/browser-core/test'
+ * import { trackRuntimeError } from '../domain/error/trackRuntimeError'
+ *
+ * const spy = mockWithSpy(trackRuntimeError)
+ * spy.and.returnValue(new Observable<RawError>())
+ * // ... test code ...
+ * expect(spy).toHaveBeenCalled()
+ */
+export function replaceMockableWithSpy<T extends (...args: any[]) => any>(value: T): jasmine.Spy<T> {
+  const spy = jasmine.createSpy<T>()
+  replaceMockable(value, spy as unknown as T)
+  return spy
+}
