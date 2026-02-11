3fa9bcb302dca24daca2359ea613e841cea1504a
Benoît <benoit.zugmeyer@datadoghq.com>
2026-02-11 09:54:32 +0100

    ✅ improve unit test setup (#4149)

--- packages/core/test/forEach.spec.ts
+++ packages/core/test/forEach.spec.ts
@@ -1,11 +1,17 @@
-import type { BuildEnvWindow } from './buildEnv'
+import { resetValueHistoryGlobals } from '../src/tools/valueHistory'
+import { resetFetchObservable } from '../src/browser/fetchObservable'
+import { resetConsoleObservable } from '../src/domain/console/consoleObservable'
+import { resetXhrObservable } from '../src/browser/xhrObservable'
+import { resetGetCurrentSite } from '../src/browser/cookie'
+import { resetReplayStats } from '../../rum/src/domain/replayStats'
+import { resetInteractionCountPolyfill } from '../../rum-core/src/domain/view/viewMetrics/interactionCountPolyfill'
+import { resetMonitor } from '../src/tools/monitor'
+import { resetTelemetry } from '../src/domain/telemetry'
 import { startLeakDetection } from './leakDetection'
+import type { BuildEnvWindow } from './buildEnv'
 
 beforeEach(() => {
   ;(window as unknown as BuildEnvWindow).__BUILD_ENV__SDK_VERSION__ = 'test'
-  // reset globals
-  ;(window as any).DD_LOGS = {}
-  ;(window as any).DD_RUM = {}
   ;(window as any).IS_REACT_ACT_ENVIRONMENT = true
   // prevent 'Some of your tests did a full page reload!' issue
   window.onbeforeunload = () => 'stop'
@@ -16,8 +22,23 @@ beforeEach(() => {
   clearAllCookies()
 })
 
+afterEach(() => {
+  // reset globals
+  delete (window as any).DD_LOGS
+  delete (window as any).DD_RUM
+  resetValueHistoryGlobals()
+  resetFetchObservable()
+  resetConsoleObservable()
+  resetXhrObservable()
+  resetGetCurrentSite()
+  resetReplayStats()
+  resetMonitor()
+  resetTelemetry()
+  resetInteractionCountPolyfill()
+})
+
 function clearAllCookies() {
   document.cookie.split(';').forEach((c) => {
-    document.cookie = c.replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/;samesite=strict`)
+    document.cookie = c.replace(/=.*/, `=;expires=${new Date(0).toUTCString()};path=/;samesite=strict`)
   })
 }
