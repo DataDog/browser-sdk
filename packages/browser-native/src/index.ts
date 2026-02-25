e78d80b343c3416fd8fd2e9a77551d0422017448
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-02-25 15:09:52 +0100

    🔧 add @datadog/browser-native dependency to browser-core

--- packages/browser-native/src/index.ts
+++ packages/browser-native/src/index.ts
@@ -1,4 +1,3 @@
-// exports added in later tasks
 export { fetch, sendBeacon } from './network'
 export { addEventListener, removeEventListener } from './events'
 export { getCookie, setCookie, getReadyState, getVisibilityState } from './document'
