2110730af2532573e19cf3b7430f6a611a5ec66f
Benoit Zugmeyer <benoit.zugmeyer@datadoghq.com>
2026-02-26 16:22:20 +0100

    rename forEach file

--- test/unit/karma.base.conf.js
+++ test/unit/karma.base.conf.js
@@ -31,7 +31,7 @@ const FILES = [
   // Make sure 'forEach.spec' is the first file to be loaded, so its `beforeEach` hook is executed
   // before all other `beforeEach` hooks, and its `afterEach` hook is executed after all other
   // `afterEach` hooks.
-  'packages/browser-core/test/forEach.spec.ts',
+  'packages/browser-core/test/forEach.ts',
   'packages/browser-rum/test/record/toto.css',
 ]
 
