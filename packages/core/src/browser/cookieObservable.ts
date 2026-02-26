76a99ac00236a372caa5196ccb2556ffac1952d1
Thomas Lebeau <thomas.lebeau@datadoghq.com>
2026-02-26 17:33:52 +0100

    ♻️ move cookieObservable from rum-core to core
    The cookieObservable only depends on browser-core types and will be
    needed by the session cookie strategy in core. Move it to core and
    re-export from rum-core to keep existing imports working.

---
+++ packages/core/src/browser/cookieObservable.ts
@@ -0,0 +1,60 @@
+import type { Configuration } from '../domain/configuration'
+import { setInterval, clearInterval } from '../tools/timer'
+import { Observable } from '../tools/observable'
+import { ONE_SECOND } from '../tools/utils/timeUtils'
+import { findCommaSeparatedValue } from '../tools/utils/stringUtils'
+import type { CookieStore } from './browser.types'
+import { addEventListener, DOM_EVENT } from './addEventListener'
+
+export interface CookieStoreWindow {
+  cookieStore?: CookieStore
+}
+
+export type CookieObservable = ReturnType<typeof createCookieObservable>
+
+export function createCookieObservable(configuration: Configuration, cookieName: string) {
+  const detectCookieChangeStrategy = (window as CookieStoreWindow).cookieStore
+    ? listenToCookieStoreChange(configuration)
+    : watchCookieFallback
+
+  return new Observable<string | undefined>((observable) =>
+    detectCookieChangeStrategy(cookieName, (event) => observable.notify(event))
+  )
+}
+
+function listenToCookieStoreChange(configuration: Configuration) {
+  return (cookieName: string, callback: (event: string | undefined) => void) => {
+    const listener = addEventListener(
+      configuration,
+      (window as CookieStoreWindow).cookieStore!,
+      DOM_EVENT.CHANGE,
+      (event) => {
+        // Based on our experimentation, we're assuming that entries for the same cookie cannot be in both the 'changed' and 'deleted' arrays.
+        // However, due to ambiguity in the specification, we asked for clarification: https://github.com/WICG/cookie-store/issues/226
+        const changeEvent =
+          event.changed.find((event) => event.name === cookieName) ||
+          event.deleted.find((event) => event.name === cookieName)
+        if (changeEvent) {
+          callback(changeEvent.value)
+        }
+      }
+    )
+    return listener.stop
+  }
+}
+
+export const WATCH_COOKIE_INTERVAL_DELAY = ONE_SECOND
+
+function watchCookieFallback(cookieName: string, callback: (event: string | undefined) => void) {
+  const previousCookieValue = findCommaSeparatedValue(document.cookie, cookieName)
+  const watchCookieIntervalId = setInterval(() => {
+    const cookieValue = findCommaSeparatedValue(document.cookie, cookieName)
+    if (cookieValue !== previousCookieValue) {
+      callback(cookieValue)
+    }
+  }, WATCH_COOKIE_INTERVAL_DELAY)
+
+  return () => {
+    clearInterval(watchCookieIntervalId)
+  }
+}
