d3ef0369209f4e287ddb5362da0f5abc03eb89e0
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-03-02 10:20:40 +0100

    remove unit test in datadogRumProvider

--- packages/rum-nextjs/src/domain/datadogRumProvider.tsx
+++ packages/rum-nextjs/src/domain/datadogRumProvider.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import React, { useEffect } from 'react'
-import { mockable } from '@datadog/browser-core'
-import { usePathname as nextUsePathname, useParams as nextUseParams } from 'next/navigation'
+import { usePathname, useParams } from 'next/navigation'
 import { computeViewNameFromParams } from './computeViewNameFromParams'
 import { startNextjsView } from './nextjsPlugin'
 
 export function DatadogRumProvider({ children }: { children: React.ReactNode }) {
-  const pathname = mockable(nextUsePathname)()
-  const params = mockable(nextUseParams)()
+  const pathname = usePathname()
+  const params = useParams()
 
   useEffect(() => {
     startNextjsView(computeViewNameFromParams(pathname, params as Record<string, string | string[] | undefined>))
