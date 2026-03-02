535ee16949bab05a288a7bc91a4c0314f0492d32
Benoît <benoit.zugmeyer@datadoghq.com>
2026-03-02 18:11:33 +0100

    🧹 remove unused eslint-disable directives from generated type files (#4260)
    Co-authored-by: benoit.zugmeyer <benoit.zugmeyer@datadoghq.com>

--- scripts/lib/global.d.ts
+++ scripts/lib/global.d.ts
@@ -2,6 +2,8 @@ declare module 'emoji-name-map' {
   export function get(emoji: string): string | undefined
 }
 
+// Declare the `json-schema-to-typescript` types because the package might not be always built.
 declare module 'json-schema-to-typescript' {
-  export function compileFromFile(schemaPath: string, options: any): Promise<string>
+  export type Options = any
+  export function compileFromFile(schemaPath: string, options: Partial<Options>): Promise<string>
 }
