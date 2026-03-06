7da0ba6f49c7d73d0dee3304cc5d4637ddf4c648
sethfowler-datadog <seth.fowler@datadoghq.com>
2026-03-06 10:29:22 +0000

    🐛 [PANA-6258] More robustly prevent serialization of <style> children (#4283)

---
+++ packages/rum/src/domain/record/serialization/serializeNodeAsChange.spec.ts
@@ -0,0 +1,82 @@
+import type { TimeStamp } from '@datadog/browser-core'
+import { noop } from '@datadog/browser-core'
+import { NodePrivacyLevel } from '@datadog/browser-rum-core'
+import { appendElement } from 'packages/rum-core/test'
+import { createRecordingScopeForTesting } from '../test/recordingScope.specHelper'
+import type { RecordingScope } from '../recordingScope'
+import { ChangeType, RecordType, type BrowserChangeRecord, type BrowserRecord } from '../../../types'
+import type { ChangeDecoder } from './conversions'
+import { createChangeDecoder } from './conversions'
+import { serializeNodeAsChange } from './serializeNodeAsChange'
+import type { ChangeSerializationTransaction } from './serializationTransaction'
+import { SerializationKind, serializeChangesInTransaction } from './serializationTransaction'
+import { createRootInsertionCursor } from './insertionCursor'
+
+describe('serializeNodeAsChange', () => {
+  let changeDecoder: ChangeDecoder
+  let scope: RecordingScope
+
+  beforeEach(() => {
+    changeDecoder = createChangeDecoder()
+    scope = createRecordingScopeForTesting()
+  })
+
+  describe('for <style> elements', () => {
+    it('serializes the <style> element', () => {
+      const record = serialize('<style id="foo"></style>')
+      expect(record?.data).toEqual([[ChangeType.AddNode, [null, 'STYLE', ['id', 'foo']]]])
+    })
+
+    it('serializes the contents of the associated stylesheet', () => {
+      const css = 'div { color: green; }'
+      const record = serialize(`<style>${css}</style>`)
+      expect(record?.data).toEqual([
+        [ChangeType.AddNode, [null, 'STYLE']],
+        [ChangeType.AddStyleSheet, [css]],
+        [ChangeType.AttachedStyleSheets, [0, 0]],
+      ])
+    })
+  })
+
+  describe('for <style> element children', () => {
+    it('does not serialize them', () => {
+      const css = 'div { color: green; }'
+      const record = serialize(`<style>${css}</style>`, { target: (node: Node) => node.firstChild! })
+      expect(record).toBeUndefined()
+    })
+  })
+
+  function serialize(
+    html: string,
+    { target }: { target?: (node: Node) => Node } = {}
+  ): BrowserChangeRecord | undefined {
+    const subtreeRoot = appendElement(html)
+    const targetNode = target?.(subtreeRoot) ?? subtreeRoot
+
+    let emittedRecord: BrowserRecord | undefined
+
+    serializeChangesInTransaction(
+      SerializationKind.INITIAL_FULL_SNAPSHOT,
+      (record: BrowserRecord) => {
+        emittedRecord = record
+      },
+      noop,
+      scope,
+      0 as TimeStamp,
+      (transaction: ChangeSerializationTransaction) => {
+        const insertionCursor = createRootInsertionCursor(scope.nodeIds)
+        serializeNodeAsChange(insertionCursor, targetNode, NodePrivacyLevel.ALLOW, transaction)
+      }
+    )
+
+    if (!emittedRecord) {
+      return undefined
+    }
+
+    if (emittedRecord.type !== RecordType.Change) {
+      throw new Error('Expected serialization to yield a BrowserChangeRecord')
+    }
+
+    return changeDecoder.decode(emittedRecord)
+  }
+})
