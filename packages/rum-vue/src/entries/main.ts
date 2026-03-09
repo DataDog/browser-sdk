15a80af275245ff73197fa6ca306d965a65d638d
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-03-09 10:37:26 +0100

    ✨ add addVueError for Vue error tracking

--- packages/rum-vue/src/entries/main.ts
+++ packages/rum-vue/src/entries/main.ts
@@ -1,2 +1,3 @@
 export type { VuePluginConfiguration, VuePlugin } from '../domain/vuePlugin'
 export { vuePlugin } from '../domain/vuePlugin'
+export { addVueError } from '../domain/error/addVueError'
