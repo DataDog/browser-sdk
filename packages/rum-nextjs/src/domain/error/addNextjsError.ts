f9f9d4f288a262b48132e3aae79ddc8a4217fe78
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-03-10 20:36:42 +0100

    Add error tracking for NextJS

---
+++ packages/rum-nextjs/src/domain/error/addNextjsError.ts
@@ -0,0 +1,55 @@
+import {
+  callMonitored,
+  clocksNow,
+  computeRawError,
+  createHandlingStack,
+  ErrorHandling,
+  ErrorSource,
+  generateUUID,
+  NonErrorPrefix,
+} from '@datadog/browser-core'
+import { RumEventType } from '@datadog/browser-rum-core'
+import { onRumStart } from '../nextjsPlugin'
+
+export function addNextjsError(error: Error & { digest?: string }, context?: Record<string, unknown>) {
+  const handlingStack = createHandlingStack('nextjs error')
+  const startClocks = clocksNow()
+  onRumStart((addEvent) => {
+    callMonitored(() => {
+      const nextjsContext = context?.nextjs instanceof Object ? (context.nextjs as Record<string, unknown>) : {}
+      const nextjsExtra = error.digest !== undefined ? { nextjs: { ...nextjsContext, digest: error.digest } } : {}
+      const rawError = computeRawError({
+        originalError: error,
+        handlingStack,
+        startClocks,
+        source: ErrorSource.CUSTOM,
+        handling: ErrorHandling.HANDLED,
+        nonErrorPrefix: NonErrorPrefix.PROVIDED,
+      })
+      addEvent(
+        startClocks.relative,
+        {
+          type: RumEventType.ERROR,
+          date: rawError.startClocks.timeStamp,
+          error: {
+            id: generateUUID(),
+            message: rawError.message,
+            source: rawError.source,
+            stack: rawError.stack,
+            handling_stack: rawError.handlingStack,
+            type: rawError.type,
+            handling: rawError.handling,
+            causes: rawError.causes,
+            source_type: 'browser',
+            csp: rawError.csp,
+          },
+          context: { framework: 'nextjs', ...context, ...nextjsExtra, ...rawError.context },
+        },
+        {
+          error: rawError.originalError,
+          handlingStack: rawError.handlingStack,
+        }
+      )
+    })
+  })
+}
