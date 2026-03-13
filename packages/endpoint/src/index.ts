5d9836034a5bc6e200b8d27d83004707c975a2d4
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-03-13 14:21:13 +0100

    🐛 export SdkVariant and fix SDK version in bundle header

--- packages/endpoint/src/index.ts
+++ packages/endpoint/src/index.ts
@@ -1,2 +1,3 @@
 export { generateBundle, generateCombinedBundle, fetchConfig } from './bundleGenerator.ts'
 export type { GenerateBundleOptions, CombineBundleOptions, FetchConfigOptions } from './bundleGenerator.ts'
+export type { SdkVariant } from './sdkDownloader.ts'
