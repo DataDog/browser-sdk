5f7c4f1deb82b92cc2acd967b47ce7f4afd23f01
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-03-13 14:16:31 +0100

    ✨ add @datadog/browser-sdk-endpoint package with Node code generation

---
+++ packages/endpoint/src/index.ts
@@ -0,0 +1,2 @@
+export { generateBundle, generateCombinedBundle, fetchConfig } from './bundleGenerator.ts'
+export type { GenerateBundleOptions, CombineBundleOptions, FetchConfigOptions } from './bundleGenerator.ts'
