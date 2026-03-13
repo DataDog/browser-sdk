39aad163d8015b7c856b3d6c460692e047da2142
Benoit Zugmeyer <benoit.zugmeyer@datadoghq.com>
2026-03-13 10:14:12 +0100

    👌 add a comment to justify our change

--- packages/core/src/tools/queueMicrotask.ts
+++ packages/core/src/tools/queueMicrotask.ts
@@ -2,8 +2,14 @@ import { monitor } from './monitor'
 import { globalObject } from './globalObject'
 
 export function queueMicrotask(callback: () => void) {
+  // Intentionally avoid .bind(globalObject): in some environments (e.g. Selenium GeckoDriver's
+  // executeScript), globalThis is not a proper global object, so calling the bound function throws
+  // 'queueMicrotask called on an object that does not implement interface Window'. Calling it as an
+  // unbound method is fine, as the proper global object will be used implicitly.
+  // See https://github.com/mozilla/geckodriver/issues/1798
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const nativeImplementation = globalObject.queueMicrotask
+
   if (typeof nativeImplementation === 'function') {
     nativeImplementation(monitor(callback))
   } else {
