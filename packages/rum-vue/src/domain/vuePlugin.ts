05e66f993162708daf6b231b834f3378af69604d
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-03-18 17:03:00 +0100

    ✨ add vuePlugin and addVueError (#4327)

---
+++ packages/rum-vue/src/domain/vuePlugin.ts
@@ -0,0 +1,68 @@
+import type { RumPlugin, RumPublicApi, StartRumResult } from '@datadog/browser-rum-core'
+
+let globalPublicApi: RumPublicApi | undefined
+let globalConfiguration: VuePluginConfiguration | undefined
+let globalAddError: StartRumResult['addError'] | undefined
+
+type InitSubscriber = (configuration: VuePluginConfiguration, rumPublicApi: RumPublicApi) => void
+type StartSubscriber = (addError: StartRumResult['addError']) => void
+
+const onRumInitSubscribers: InitSubscriber[] = []
+const onRumStartSubscribers: StartSubscriber[] = []
+
+export interface VuePluginConfiguration {
+  router?: boolean
+}
+
+export type VuePlugin = Required<RumPlugin>
+
+export function vuePlugin(configuration: VuePluginConfiguration = {}): VuePlugin {
+  return {
+    name: 'vue',
+    onInit({ publicApi, initConfiguration }) {
+      globalPublicApi = publicApi
+      globalConfiguration = configuration
+      for (const subscriber of onRumInitSubscribers) {
+        subscriber(globalConfiguration, globalPublicApi)
+      }
+      if (configuration.router) {
+        initConfiguration.trackViewsManually = true
+      }
+    },
+    onRumStart({ addError }) {
+      globalAddError = addError
+      if (addError) {
+        for (const subscriber of onRumStartSubscribers) {
+          subscriber(addError)
+        }
+      }
+    },
+    getConfigurationTelemetry() {
+      return { router: !!configuration.router }
+    },
+  } satisfies RumPlugin
+}
+
+export function onVueInit(callback: InitSubscriber) {
+  if (globalConfiguration && globalPublicApi) {
+    callback(globalConfiguration, globalPublicApi)
+  } else {
+    onRumInitSubscribers.push(callback)
+  }
+}
+
+export function onVueStart(callback: StartSubscriber) {
+  if (globalAddError) {
+    callback(globalAddError)
+  } else {
+    onRumStartSubscribers.push(callback)
+  }
+}
+
+export function resetVuePlugin() {
+  globalPublicApi = undefined
+  globalConfiguration = undefined
+  globalAddError = undefined
+  onRumInitSubscribers.length = 0
+  onRumStartSubscribers.length = 0
+}
