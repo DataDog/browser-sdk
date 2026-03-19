a05cadac11ce8ead04f4a03e55a9b31dbc976130
roman.gaignault <roman.gaignault@datadoghq.com>
2026-03-19 13:32:54 +0100

    ✨ Add Angular error handling integration

---
+++ packages/rum-angular/src/domain/error/provideDatadogErrorHandler.ts
@@ -0,0 +1,29 @@
+import type { Provider } from '@angular/core'
+// eslint-disable-next-line local-rules/disallow-side-effects
+import { ErrorHandler } from '@angular/core'
+import { addAngularError } from './addAngularError'
+
+// eslint-disable-next-line no-restricted-syntax
+class DatadogErrorHandler extends ErrorHandler {
+  override handleError(error: unknown): void {
+    addAngularError(error)
+    super.handleError(error)
+  }
+}
+
+/**
+ * Provides a Datadog-instrumented Angular ErrorHandler that reports errors to RUM.
+ *
+ * @category Error
+ * @example
+ * ```ts
+ * import { provideDatadogErrorHandler } from '@datadog/browser-rum-angular'
+ *
+ * bootstrapApplication(AppComponent, {
+ *   providers: [provideDatadogErrorHandler()],
+ * })
+ * ```
+ */
+export function provideDatadogErrorHandler(): Provider {
+  return { provide: ErrorHandler, useClass: DatadogErrorHandler }
+}
