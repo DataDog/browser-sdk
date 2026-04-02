cc4db839fab9b60dc3e05f2743dba31334a403c3
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-04-02 11:39:17 +0200

    Remove short session investigation feature flag

--- packages/core/src/domain/session/sessionManager.ts
+++ packages/core/src/domain/session/sessionManager.ts
@@ -2,7 +2,7 @@ import { Observable } from '../../tools/observable'
 import type { Context } from '../../tools/serialisation/context'
 import { createValueHistory } from '../../tools/valueHistory'
 import type { RelativeTime } from '../../tools/utils/timeUtils'
-import { clocksOrigin, dateNow, ONE_MINUTE, relativeNow } from '../../tools/utils/timeUtils'
+import { clocksOrigin, ONE_MINUTE, relativeNow } from '../../tools/utils/timeUtils'
 import { addEventListener, addEventListeners, DOM_EVENT } from '../../browser/addEventListener'
 import { clearInterval, setInterval } from '../../tools/timer'
 import type { Configuration } from '../configuration'
@@ -11,12 +11,9 @@ import { addTelemetryDebug } from '../telemetry'
 import { isSyntheticsTest } from '../synthetics/syntheticsWorkerValues'
 import type { CookieStore } from '../../browser/browser.types'
 import { getCurrentSite } from '../../browser/cookie'
-import { ExperimentalFeature, isExperimentalFeatureEnabled } from '../../tools/experimentalFeatures'
-import { findLast } from '../../tools/utils/polyfills'
 import { SESSION_NOT_TRACKED, SESSION_TIME_OUT_DELAY, SessionPersistence } from './sessionConstants'
 import { startSessionStore } from './sessionStore'
 import type { SessionState } from './sessionState'
-import { toSessionState } from './sessionState'
 import { retrieveSessionCookie } from './storeStrategies/sessionInCookie'
 import { SESSION_STORE_KEY } from './storeStrategies/sessionStoreStrategy'
 import { retrieveSessionFromLocalStorage } from './storeStrategies/sessionInLocalStorage'
@@ -80,12 +77,6 @@ export function startSessionManager<TrackingType extends string>(
   // manager is started.
   sessionStore.expandOrRenewSession()
   sessionContextHistory.add(buildSessionContext(), clocksOrigin().relative)
-  if (isExperimentalFeatureEnabled(ExperimentalFeature.SHORT_SESSION_INVESTIGATION)) {
-    const session = sessionStore.getSession()
-    if (session) {
-      detectSessionIdChange(configuration, session)
-    }
-  }
 
   trackingConsentState.observable.subscribe(() => {
     if (trackingConsentState.isGranted()) {
@@ -200,35 +191,6 @@ async function reportUnexpectedSessionState(configuration: Configuration) {
     expireTimestamp: rawSession?.expire,
     ...cookieContext,
   })
-}
-
-function detectSessionIdChange(configuration: Configuration, initialSessionState: SessionState) {
-  if (!window.cookieStore || !initialSessionState.created) {
-    return
-  }
-
-  const sessionCreatedTime = Number(initialSessionState.created)
-
-  const { stop } = addEventListener(configuration, cookieStore as CookieStore, DOM_EVENT.CHANGE, listener)
-  stopCallbacks.push(stop)
-
-  function listener(event: CookieChangeEvent) {
-    const changed = findLast(event.changed, (change): change is CookieListItem => change.name === SESSION_STORE_KEY)
-    if (!changed) {
-      return
-    }
-
-    const sessionAge = dateNow() - sessionCreatedTime
-    if (sessionAge > 14 * ONE_MINUTE) {
-      // The session might have expired just because it's too old or lack activity
-      stop()
-    } else {
-      const newSessionState = toSessionState(changed.value)
-      if (newSessionState.id && newSessionState.id !== initialSessionState.id) {
-        stop()
-      }
-    }
-  }
 }
 
 async function getSessionCookies(): Promise<{ count: number; domain: string }> {
