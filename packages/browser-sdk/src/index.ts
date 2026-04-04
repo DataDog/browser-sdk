74d8e73fba40034374f19d4105d891b3485aab2d
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-04-04 11:33:50 +0200

    📦 Add barrel exports for browser-sdk

--- packages/browser-sdk/src/index.ts
+++ packages/browser-sdk/src/index.ts
@@ -1,1 +1,4 @@
-export {}
+export { createSdk } from './domain/sdk'
+export type { Sdk, SdkOptions, SdkInitConfiguration } from './domain/sdk'
+export { loadModules, MODULE_MAP } from './domain/moduleLoader'
+export { getTargetGlobal, initCdn } from './boot/cdn'
