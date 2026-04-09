7a39e3c60e03a00c57b9c97e36ce77f42523d85a
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-04-09 22:10:48 +0200

    📦 add browser-views-next event types

---
+++ packages/browser-views-next/src/types.ts
@@ -0,0 +1,43 @@
+export type ViewLoadingType = 'initial_load' | 'route_change' | 'bf_cache'
+
+export interface NavigationResource {
+  url: string
+  startTime: number
+  startDate: number
+  referrer: string
+  loadingType: ViewLoadingType
+  name?: string
+}
+
+export interface StartViewAction {
+  url: string
+  startTime: number
+  startDate: number
+  referrer: string
+  loadingType: 'route_change'
+  name?: string
+}
+
+export interface ViewObservation {
+  id: string
+  url: string
+  referrer: string
+  loadingType: ViewLoadingType
+  startTime: number
+  startDate: number
+  name?: string
+}
+
+export interface ViewChangedSignal {
+  viewId: string
+}
+
+// Extend the shared pipeline event map
+declare module '@datadog/core-next' {
+  interface SdkEventMap {
+    'resource:navigation': NavigationResource
+    'action:start_view': StartViewAction
+    'observation:view': ViewObservation
+    'signal:view_changed': ViewChangedSignal
+  }
+}
