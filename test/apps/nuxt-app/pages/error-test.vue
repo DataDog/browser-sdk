5d73db8d15b1bc0e1e70cb8d45e791d1efd45c40
BeltranBulbarellaDD <beltran.bulbarella@datadoghq.com>
2026-04-20 10:27:07 +0200

    ⚗️ Add Nuxt error tracking (#4477)

---
+++ test/apps/nuxt-app/pages/error-test.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const hasError = ref(false)
+function triggerError() {
+  hasError.value = true
+  throw new Error('Nuxt error from vueApp.config.errorHandler')
+}
+</script>
+<template>
+  <div>
+    <h1>Error Page</h1>
+    <button data-testid="trigger-error" @click="triggerError">Trigger Error</button>
+    <div v-if="hasError" data-testid="error-handled">Error occurred</div>
+  </div>
+</template>
