363201b95fce1fa3b48f03b617075ca4723a3bd9
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-05-29 09:19:37 +0200

    use wire

--- packages/rum-slim/src/entries/salesforce.ts
+++ packages/rum-slim/src/entries/salesforce.ts
@@ -3,7 +3,8 @@ import type { RumPublicApi } from '@datadog/browser-rum-core'
 import { makeRumPublicApi } from '@datadog/browser-rum-core'
 import { makeProfilerApiStub } from '../boot/stubProfilerApi'
 import { makeRecorderApiStub } from '../boot/stubRecorderApi'
-import { startSalesforceViewNameTracking } from '../domain/salesforce/viewNameTracker'
+import { createSalesforceViewsPlugin } from '../domain/salesforce/salesforceViewsPlugin'
+import type { SalesforceViewsPlugin } from '../domain/salesforce/salesforceViewsPlugin'
 
 export type {
   User,
@@ -23,10 +24,13 @@ export type {
 /**
  * @deprecated Use {@link DatadogRum} instead
  */
-export type RumGlobal = RumPublicApi
+export type RumGlobal = DatadogRum
+
+export interface DatadogRum extends RumPublicApi {
+  createSalesforceViewsPlugin: () => SalesforceViewsPlugin
+}
 
 export type {
-  RumPublicApi as DatadogRum,
   RumInitConfiguration,
   RumBeforeSend,
   ViewOptions,
@@ -62,35 +66,20 @@ export type {
 } from '@datadog/browser-rum-core'
 export { DEFAULT_TRACKED_RESOURCE_HEADERS } from '@datadog/browser-rum-core'
 export { DefaultPrivacyLevel } from '@datadog/browser-core'
+export { createSalesforceViewsPlugin }
+export type { SalesforceViewChange, SalesforceViewsPlugin } from '../domain/salesforce/salesforceViewsPlugin'
 
 // eslint-disable-next-line local-rules/disallow-side-effects
-export const datadogRum = createSalesforceDatadogRum(
+export const datadogRum: DatadogRum = Object.assign(
   makeRumPublicApi(makeRecorderApiStub(), makeProfilerApiStub(), {
     sdkName: 'rum-slim',
-  })
+  }),
+  { createSalesforceViewsPlugin }
 )
 
 interface BrowserWindow extends Window {
-  DD_RUM?: RumPublicApi
+  DD_RUM?: DatadogRum
 }
 
 // eslint-disable-next-line local-rules/disallow-side-effects
 defineGlobal(getGlobalObject<BrowserWindow>(), 'DD_RUM', datadogRum)
-
-function createSalesforceDatadogRum(baseRum: RumPublicApi): RumPublicApi {
-  const baseInit = baseRum.init
-  let stopSalesforceViewNameTracking: (() => void) | undefined
-
-  baseRum.init = (initConfiguration) => {
-    baseInit(initConfiguration)
-
-    if (!stopSalesforceViewNameTracking) {
-      const salesforceViewNameTracking = startSalesforceViewNameTracking({
-        getRumPublicApi: () => baseRum,
-      })
-      stopSalesforceViewNameTracking = () => salesforceViewNameTracking.stop()
-    }
-  }
-
-  return baseRum
-}
