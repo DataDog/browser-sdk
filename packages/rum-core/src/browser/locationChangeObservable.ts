783c96bb7166f4c598bdfd41327bb50bd15fec2c
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-05-29 14:23:36 +0200

    Use globalObject in turn of window

--- packages/rum-core/src/browser/locationChangeObservable.ts
+++ packages/rum-core/src/browser/locationChangeObservable.ts
@@ -1,4 +1,4 @@
-import { addEventListener, DOM_EVENT, instrumentMethod, Observable, shallowClone } from '@datadog/browser-core'
+import { addEventListener, DOM_EVENT, globalObject, instrumentMethod, Observable, shallowClone } from '@datadog/browser-core'
 import type { RumConfiguration } from '../domain/configuration'
 
 export interface LocationChange {
@@ -7,7 +7,7 @@ export interface LocationChange {
 }
 
 export function createLocationChangeObservable(configuration: RumConfiguration) {
-  let currentLocation = shallowClone(window.location)
+  let currentLocation = shallowClone(globalObject.location)
 
   return new Observable<LocationChange>((observable) => {
     const { stop: stopHistoryTracking } = trackHistory(configuration, onLocationChange)
