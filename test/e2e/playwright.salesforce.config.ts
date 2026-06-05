8aecb5d0b658af91300b8ebecea350f5e92d71c9
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-06-05 14:05:27 +0200

    Add e2e tests for Salesforce

---
+++ test/e2e/playwright.salesforce.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test'
+import { getTestReportDirectory } from '../envUtils'
+
+const isCi = !!process.env.CI
+
+export default defineConfig({
+  testDir: './scenario/salesforce',
+  testMatch: '**/*.scenario.ts',
+  timeout: 60_000,
+  navigationTimeout: 30_000,
+  workers: 1,
+  retries: isCi ? 1 : 0,
+  reporter: isCi
+    ? [['junit', { outputFile: `${getTestReportDirectory()}/salesforce-results.xml` }], ['line']]
+    : [['line']],
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+})
