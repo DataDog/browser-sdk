47d1b695c5e687f01229431182f8d3dae72fe97f
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2026-06-05 18:19:21 +0200

    🔧 add createHook primitive to browser-core

--- packages/browser-core/src/tools/abstractHooks.ts
+++ packages/browser-core/src/tools/abstractHooks.ts
@@ -1,5 +1,40 @@
 import { combine } from './mergeInto'
 
+export interface Hook<Params, Result> {
+  register(this: void, callback: (params: Params) => Result | DISCARDED | SKIPPED): { unregister: () => void }
+  trigger(this: void, params: Params): Result | DISCARDED | undefined
+}
+
+export function createHook<Params, Result>(): Hook<Params, Result> {
+  type Callback = (params: Params) => Result | DISCARDED | SKIPPED
+  let callbacks: Callback[] = []
+
+  return {
+    register(callback) {
+      callbacks.push(callback)
+      return {
+        unregister() {
+          callbacks = callbacks.filter((cb) => cb !== callback)
+        },
+      }
+    },
+    trigger(params) {
+      const results: Result[] = []
+      for (const callback of callbacks) {
+        const result = callback(params)
+        if (result === DISCARDED) {
+          return DISCARDED
+        }
+        if (result === SKIPPED) {
+          continue
+        }
+        results.push(result)
+      }
+      return combine(...(results as [unknown, unknown])) as Result
+    },
+  }
+}
+
 export const enum HookNames {
   Assemble,
   AssembleTelemetry,
