615986af9ab9c65ba5cfdeeb5b9493736a778f64
Boris Dibon <boris.dibon@datadoghq.com>
2026-06-22 13:32:48 +0200

    👌 Do not emit any event when trackResources is false

--- packages/browser-rum-core/src/boot/startRum.ts
+++ packages/browser-rum-core/src/boot/startRum.ts
@@ -228,7 +228,7 @@ export function startRumEventCollection(
 
   const vitalCollection = startVitalCollection(lifeCycle, pageStateHistory)
 
-  if (isExperimentalFeatureEnabled(ExperimentalFeature.TRACK_WEB_SOCKETS)) {
+  if (isExperimentalFeatureEnabled(ExperimentalFeature.TRACK_WEB_SOCKETS) && configuration.trackResources) {
     const webSocketCollection = startWebSocketCollection(lifeCycle, viewHistory, vitalCollection.addDurationVital)
     cleanupTasks.push(webSocketCollection.stop)
   }
