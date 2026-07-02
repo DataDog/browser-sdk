693e87d861349b64b36034ca64c9f3c7ae2af954
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-07-02 15:59:45 +0200

    Add more tests, simplify test app config

--- test/apps/sf-lwc-app/force-app/main/default/lwc/customActionButtons/customActionButtons.js
+++ test/apps/sf-lwc-app/force-app/main/default/lwc/customActionButtons/customActionButtons.js
@@ -111,7 +111,7 @@ export default class CustomActionButtons extends LightningElement {
   }
 
   getResourceTestUrl(token) {
-    return `https://sample-json-api.com/products/1?${token}`
+    return `${window.location.origin}/services/data/?${token}`
   }
 
   getComposedPathNames(event) {
