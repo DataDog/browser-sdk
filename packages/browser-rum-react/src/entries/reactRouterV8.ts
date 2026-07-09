bab28d6885570b58d38d5af287da5bf1b7cc7009
Adrian de la Rosa <adrian.delarosa@datadoghq.com>
2026-07-09 17:46:20 +0200

    ✨ Add react-router-v8 entry point

---
+++ packages/browser-rum-react/src/entries/reactRouterV8.ts
@@ -0,0 +1,38 @@
+/**
+ * React Router v8 integration.
+ *
+ * @packageDocumentation
+ * @example
+ * ```ts
+ * import { RouterProvider } from 'react-router/dom'
+ * import { datadogRum } from '@datadog/browser-rum'
+ * import { reactPlugin } from '@datadog/browser-rum-react'
+ *
+ * // ⚠️ Use "createBrowserRouter" from `@datadog/browser-rum-react/react-router-v8` instead of `react-router`
+ * import { createBrowserRouter } from '@datadog/browser-rum-react/react-router-v8'
+ *
+ * datadogRum.init({
+ *   applicationId: '<DATADOG_APPLICATION_ID>',
+ *   clientToken: '<DATADOG_CLIENT_TOKEN>',
+ *   plugins: [reactPlugin({ router: true })],
+ *   // ...
+ * })
+ *
+ * const router = createBrowserRouter([
+ *   {
+ *     path: '/',
+ *     element: <Root />,
+ *     // ...
+ *   },
+ * ])
+ *
+ * ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
+ * ```
+ */
+export {
+  createBrowserRouter,
+  createHashRouter,
+  createMemoryRouter,
+  useRoutes,
+  Routes,
+} from './reactRouterV7'
