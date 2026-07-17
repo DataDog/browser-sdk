198fa1c7df922055c714c52192b22364dc0e72b4
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-07-17 17:24:29 +0200

    remove duplicate line

--- eslint-local-rules/disallowSideEffects.ts
+++ eslint-local-rules/disallowSideEffects.ts
@@ -42,7 +42,6 @@ const pathsWithSideEffect = new Set([
   `${packagesRoot}/browser-rum-slim/src/entries/main.ts`,
   `${packagesRoot}/browser-rum-shopify/src/entries/main.ts`,
   `${packagesRoot}/browser-rum-slim/src/entries/salesforce.ts`,
-  `${packagesRoot}/browser-rum-slim/src/entries/salesforce.ts`,
   `${packagesRoot}/browser-debugger/src/entries/main.ts`,
 ])
 
