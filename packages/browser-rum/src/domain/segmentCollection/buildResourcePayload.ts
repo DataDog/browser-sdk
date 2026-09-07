2e92557a5bd4b471efc74ae3cbf121f6eaedc071
beltran.bulbarella <beltran.bulbarella@datadoghq.com>
2026-09-07 18:47:41 +0200

    Create buildResourcePayload

---
+++ packages/browser-rum/src/domain/segmentCollection/buildResourcePayload.ts
@@ -0,0 +1,15 @@
+import type { Payload } from '@datadog/browser-core'
+
+export function buildResourcePayload(hash: string, image: Blob, application: string): Payload {
+  const formData = new FormData()
+
+  formData.append('image', image, hash)
+
+  const event = { application: { id: application }, type: 'resource' }
+  formData.append('event', new Blob([JSON.stringify(event)], { type: 'application/json' }))
+
+  return {
+    data: formData,
+    bytesCount: image.size,
+  }
+}
